dca29d62d5a6254f134b698a41bb2043a8587794
maliagehan <maliadong@gmail.com>
2014-04-09 14:47:14 -0500

    bug fix

--- color_slice_stitch.py
+++ color_slice_stitch.py
@@ -12,17 +12,20 @@ import plantcv as pcv
 ### Parse command-line arguments
 def options():
   parser = argparse.ArgumentParser(description="Sitching and Ordering Image Slices")
-  parser.add_argument("-id", "--directory", help="Input image file directory.", required=True)
+  parser.add_argument("-d", "--directory", help="Input image file directory.", required=True)
   #parser.add_argument("-o", "--outdir", help="Output directory for image files.", required=True)
   #parser.add_argument("-D", "--debug", help="Turn on debug, prints intermediate images.", action="store_true")
   args = parser.parse_args()
   return args
 
-print args.directory
+### Main pipeline
+def main():
+  # Get options
+  args = options()
 
-#for file in os.listdir(args.directory):
-#    if re.match("*rgb_norm_slice*", file):
-#        #print file
+  for file in os.listdir(args.directory):
+      if re.match("*rgb_norm_slice*"):
+          print file
 
 if __name__ == '__main__':
   main()
