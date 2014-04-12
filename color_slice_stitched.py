ac79ff996728f0415e0e5decea5cc9e5a24c4c9a
maliagehan <maliadong@gmail.com>
2014-04-12 01:26:23 -0500

    color slice tool fix

--- color_slice_stitched.py
+++ color_slice_stitched.py
@@ -26,7 +26,7 @@ def main():
   # Get options
   args = options()
 
-  avr.slice_stitch(args.database, args.outdir)
+  time_array, id_unique=avr.slice_stitch(args.database, args.outdir, 'tv', 'off')
   
 if __name__ == '__main__':
   main()
