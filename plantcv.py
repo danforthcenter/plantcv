509fe2589679f301ee9d4590e705f10473c9e063
maliagehan <maliadong@gmail.com>
2014-04-24 14:55:38 -0500

    histograms are now svg files instead of pngs

--- plantcv.py
+++ plantcv.py
@@ -1310,6 +1310,7 @@ def analyze_color(img, imgname, mask,bins,device,debug=False,hist_plot_type='all
   # Create Histogram Plot
   if filename:
     if hist_plot_type=='all':
+      matplotlib.use('SVG')
       hist_plotb=plt.plot(hist_b,color=graph_color[0],label=label[0])
       hist_plotg=plt.plot(hist_g,color=graph_color[1],label=label[1])
       hist_plotr= plt.plot(hist_r,color=graph_color[2],label=label[2])
@@ -1321,48 +1322,52 @@ def analyze_color(img, imgname, mask,bins,device,debug=False,hist_plot_type='all
       hist_plotv=plt.plot(hist_v,color=graph_color[8],label=label[8])
       xaxis=plt.xlim([0,(bins-1)])
       legend=plt.legend()
-      fig_name=(str(filename)+'_' + str(hist_plot_type) + '_hist.png')
-      plt.savefig(fig_name, dpi=600)
+      fig_name=(str(filename)+'_' + str(hist_plot_type) + '_hist.svg')
+      plt.savefig(fig_name)
       print('\t'.join(map(str, ('IMAGE', 'hist', fig_name))))
       if debug:
-        fig_name=(str(device) +'_' + str(hist_plot_type) + '_hist.png')
-        plt.savefig(fig_name, dpi=600)
+        fig_name=(str(device) +'_' + str(hist_plot_type) + '_hist.svg')
+        plt.savefig(fig_name)
       plt.clf()
     elif hist_plot_type=='rgb':
+      matplotlib.use('SVG')
       hist_plotb=plt.plot(hist_b,color=graph_color[0],label=label[0])
       hist_plotg=plt.plot(hist_g,color=graph_color[1],label=label[1])
       hist_plotr= plt.plot(hist_r,color=graph_color[2],label=label[2])
       xaxis=plt.xlim([0,(bins-1)])
       legend=plt.legend()
-      fig_name=(str(filename) +'_' + str(hist_plot_type) + '_hist.png')
-      plt.savefig(fig_name, dpi=600)
+      fig_name=(str(filename) +'_' + str(hist_plot_type) + '_hist.svg')
+      plt.savefig(fig_name)
       plt.clf()
       print('\t'.join(map(str, ('IMAGE', 'hist', fig_name))))
     elif hist_plot_type=='lab':
+      matplotlib.use('SVG')
       hist_plotl=plt.plot(hist_l,color=graph_color[3],label=label[3])
       hist_plotm= plt.plot(hist_m,color=graph_color[4],label=label[4])
       hist_ploty=plt.plot(hist_y,color=graph_color[5],label=label[5])
       xaxis=plt.xlim([0,(bins-1)])
       legend=plt.legend()
-      fig_name=(str(filename) +'_' + str(hist_plot_type) + '_hist.png')
-      plt.savefig(fig_name, dpi=600)
+      fig_name=(str(filename) +'_' + str(hist_plot_type) + '_hist.svg')
+      plt.savefig(fig_name)
       if debug:
-        fig_name=(str(device) +'_' + str(hist_plot_type) + '_hist.png')
-        plt.savefig(fig_name, dpi=600)
+        fig_name=(str(device) +'_' + str(hist_plot_type) + '_hist.svg')
+        plt.savefig(fig_name)
       plt.clf()
       print('\t'.join(map(str, ('IMAGE', 'hist', fig_name))))
     elif hist_plot_type=='hsv':
+      matplotlib.use('SVG')
       hist_ploth=plt.plot(hist_h,color=graph_color[6],label=label[6])
       hist_plots= plt.plot(hist_s,color=graph_color[7],label=label[7])
       hist_plotv=plt.plot(hist_v,color=graph_color[8],label=label[8])
       xaxis=plt.xlim([0,(bins-1)])
       legend=plt.legend()
-      fig_name=(str(filename) +'_' + str(hist_plot_type) + '_hist.png')
-      plt.savefig(fig_name, dpi=600)
+      fig_name=(str(filename) +'_' + str(hist_plot_type) + '_hist.svg')
+      plt.savefig(fig_name)
       print('\t'.join(map(str, ('IMAGE', 'hist', fig_name))))
       if debug:
-        fig_name=(str(device) +'_' + str(hist_plot_type) + '_hist.png')
-        plt.savefig(fig_name, dpi=600)
+        matplotlib.use('SVG')
+        fig_name=(str(device) +'_' + str(hist_plot_type) + '_hist.svg')
+        plt.savefig(fig_name)
       plt.clf()
     elif hist_plot_type==None:
       pass
@@ -1890,6 +1895,7 @@ def fluor_fvfm(fdark,fmin,fmax,mask, device,filename,bins=1000, debug=False):
     )
   
   # Create Histogram Plot, if you change the bin number you might need to change binx so that it prints an appropriate number of labels
+  matplotlib.use('SVG')
   binx=bins/50
   fvfm_plot=plt.plot(tmid,fvfm_hist,color='green', label='FvFm')
   plt.xticks(list(tmid[0::binx]), rotation='vertical',size='xx-small')
@@ -1899,12 +1905,13 @@ def fluor_fvfm(fdark,fmin,fmax,mask, device,filename,bins=1000, debug=False):
   ax.text(0.05,0.95, ('Peak Bin Value: '+ str(max_bin)),transform=ax.transAxes, verticalalignment='top')
   plt.grid()
   plt.title('Fv/Fm of '+ str(filename[0:-4]))
-  fig_name=(str(filename[0:-4]) + '_fvfm_hist.png')
-  plt.savefig(fig_name, dpi=600)
+  fig_name=(str(filename[0:-4]) + '_fvfm_hist.svg')
+  plt.savefig(fig_name)
   plt.clf()
   print('\t'.join(map(str, ('IMAGE', 'hist', fig_name))))
   
   # Histogram Visualization Slice (normalized to 255 for size)
+  matplotlib.use('Agg')
   fvfm_max=np.amax(fvfm_hist)
   fvfm_min=np.amin(fvfm_hist)
   hist_shape=np.shape(fvfm_hist)
