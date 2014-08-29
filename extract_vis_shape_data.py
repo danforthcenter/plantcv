29ca550915c7dbbb0a3fbd510fcbd6f0ab11da21
nfahlgren <noahfahlgren@gmail.com>
2014-08-29 15:51:43 -0500

    Updated with improved trait output and unit conversions

--- extract_vis_shape_data.py
+++ extract_vis_shape_data.py
@@ -33,6 +33,12 @@ def zoom_correction(area, zoom):
   area /= correction_factor
   return area
 
+### Length conversion
+def px_to_cm(px, zoom):
+  pxcm = (0.000002171 * (zoom ** 2)) + (0.002073 * zoom) + 14.27
+  cm = px / pxcm
+  return cm
+
 ### Main pipeline
 def main():
   # Get options
@@ -55,9 +61,9 @@ def main():
     print("IO error")
   
   # Header
-  out.write(','.join(map(str, ('plant_id', 'datetime', 'surface_area', 'solidity', 'perimeter', 'centroid_x', 'centroid_y', 'longest_axis',
+  out.write(','.join(map(str, ('plant_id', 'datetime', 'sv_area', 'tv_area', 'centroid_height', 'solidity', 'perimeter', 'centroid_x', 'centroid_y', 'longest_axis',
                                    'height_above_bound', 'height_below_bound', 'above_bound_area', 'percent_above_bound_area', 'below_bound_area',
-                                   'percent_below_bound_area', 'outlier', 'boundary_line', 'centroid_height'))) + '\n')
+                                   'percent_below_bound_area', 'outlier', 'boundary_line'))) + '\n')
   
   # Replace the row_factory result constructor with a dictionary constructor
   connect.row_factory = dict_factory
@@ -78,7 +84,7 @@ def main():
     outlier = False
     plant_id = ''
     tv_area = 0
-    surface_area = 0
+    sv_area = 0
     solidity = 0
     perimeter = 0
     centroid_x = 0
@@ -97,7 +103,7 @@ def main():
         outlier = True
       if row['camera'] == 'vis_sv':
         sv_image_count += 1
-        surface_area += zoom_correction(row['area'], row['zoom'])
+        sv_area += zoom_correction(row['area'], row['zoom'])
         solidity += row['solidity']
         perimeter += row['perimeter']
         centroid_x += row['centroid_x']
@@ -135,11 +141,18 @@ def main():
         centroid_height = (args.height - boundary_line_y) - centroid_y
         if centroid_height < 0:
           centroid_height = 0
+        
+        # Zoom correct length-based traits
+        centroid_height = px_to_cm(centroid_height, row['zoom'])
+        perimeter = px_to_cm(perimeter, row['zoom'])
+        longest_axis = px_to_cm(longest_axis, row['zoom'])
+        height_above_bound = px_to_cm(height_above_bound, row['zoom'])
+        height_below_bound = px_to_cm(height_below_bound, row['zoom'])
           
-        surface_area += tv_centroid_correction(tv_area, centroid_height)
-        out.write(','.join(map(str, (plant_id, snapshot, surface_area, solidity, perimeter, centroid_x, centroid_y, longest_axis,
+        #surface_area += tv_centroid_correction(tv_area, centroid_height)
+        out.write(','.join(map(str, (plant_id, snapshot, sv_area, tv_area, centroid_height, solidity, perimeter, centroid_x, centroid_y, longest_axis,
                                    height_above_bound, height_below_bound, above_bound_area, percent_above_bound_area, below_bound_area,
-                                   percent_below_bound_area, outlier, boundary_line_y, centroid_height))) + '\n')
+                                   percent_below_bound_area, outlier, boundary_line_y))) + '\n')
 
 if __name__ == '__main__':
   main()
