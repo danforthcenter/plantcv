10e3724d023ee330831e685c4d68fde1aec2cd31
maliagehan <maliadong@gmail.com>
2015-05-16 13:25:31 -0500

    new pipeline scripts

--- lib/plantcv/analyze_object.py
+++ lib/plantcv/analyze_object.py
@@ -183,12 +183,12 @@ def analyze_object(img,imgname,obj, mask, device, debug=False,filename=False):
       
    #Draw properties
   if area and filename:
-    cv2.drawContours(ori_img, obj, -1, (255,0,0), 2)
-    cv2.drawContours(ori_img, [hull], -1, (0,0,255), 3)
-    cv2.line(ori_img, (x,y), (x+width,y), (0,0,255), 3)
-    cv2.line(ori_img, (int(cmx),y), (int(cmx),y+height), (0,0,255), 3)
-    cv2.line(ori_img,(tuple(caliper_transpose[caliper_length-1])),(tuple(caliper_transpose[0])),(0,0,255),3)
-    cv2.circle(ori_img, (int(cmx),int(cmy)), 10, (0,0,255), 3)
+    cv2.drawContours(ori_img, obj, -1, (255,0,0), 1)
+    cv2.drawContours(ori_img, [hull], -1, (0,0,255), 1)
+    cv2.line(ori_img, (x,y), (x+width,y), (0,0,255), 1)
+    cv2.line(ori_img, (int(cmx),y), (int(cmx),y+height), (0,0,255), 1)
+    cv2.line(ori_img,(tuple(caliper_transpose[caliper_length-1])),(tuple(caliper_transpose[0])),(0,0,255),1)
+    cv2.circle(ori_img, (int(cmx),int(cmy)), 10, (0,0,255), 1)
     # Output images with convex hull, extent x and y
     extention = filename.split('.')[-1]
     out_file = str(filename[0:-4]) + '_shapes.' + extention
@@ -198,12 +198,12 @@ def analyze_object(img,imgname,obj, mask, device, debug=False,filename=False):
     pass
   
   if debug:
-    cv2.drawContours(ori_img, obj, -1, (255,0,0), 2)
-    cv2.drawContours(ori_img, [hull], -1, (0,0,255), 3)
-    cv2.line(ori_img, (x,y), (x+width,y), (0,0,255), 3)
-    cv2.line(ori_img, (int(cmx),y), (int(cmx),y+height), (0,0,255), 3)
-    cv2.circle(ori_img, (int(cmx),int(cmy)), 10, (0,0,255), 3)
-    cv2.line(ori_img,(tuple(caliper_transpose[caliper_length-1])),(tuple(caliper_transpose[0])),(0,0,255),3)
+    cv2.drawContours(ori_img, obj, -1, (255,0,0), 1)
+    cv2.drawContours(ori_img, [hull], -1, (0,0,255), 1)
+    cv2.line(ori_img, (x,y), (x+width,y), (0,0,255), 1)
+    cv2.line(ori_img, (int(cmx),y), (int(cmx),y+height), (0,0,255), 1)
+    cv2.circle(ori_img, (int(cmx),int(cmy)), 10, (0,0,255), 1)
+    cv2.line(ori_img,(tuple(caliper_transpose[caliper_length-1])),(tuple(caliper_transpose[0])),(0,0,255),1)
     print_image(ori_img,(str(device)+'_shapes.png'))
  
   return device, shape_header, shape_data, ori_img
---
+++ scripts/utils/kt_rename_files.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import argparse
+import sys, os
+import sqlite3 as sq
+import plantcv as pcv
+from shutil import copy
+import datetime
+import re
+
+### Parse command-line arguments
+def options():
+  parser = argparse.ArgumentParser(description="Extract VIS object shape data from an SQLite database")
+  parser.add_argument("-d", "--dir", help="directory of files to rename.")
+  parser.add_argument("-c", "--camera", help="VIS_SV, VIS_TV.")
+  parser.add_argument("-a", "--angle", help="0,90,180,270.")
+  parser.add_argument("-z", "--zoom", help="zoom.")
+  parser.add_argument("-e", "--date", help="startdate in epoch time", type=int)  
+  parser.add_argument("-i", "--timeinterval", help="timeinterval in seconds.", type=int)
+  parser.add_argument("-p", "--imagetype", help="jpg,png.")  
+  args = parser.parse_args()
+  return args
+
+
+def read_dir(directory):
+    for a,b,c in os.walk(directory):
+        filenames=c
+        path=a
+    return filenames,path
+
+def rename_move(filenames,path,camera,angle, zoom, date, timeinterval, imagetype):
+    new_dir=str(path)+str(datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S"))
+    os.mkdir(new_dir)
+    
+    for i,x in enumerate(filenames):
+        timeinterval_x=int(date)+int(((i+1)*timeinterval))
+        new_time=datetime.datetime.fromtimestamp(int(timeinterval_x)).strftime('%Y-%m-%d %H_%M_%S')
+        new_x=x.replace("_",".",2)
+        new_name=str(new_x)+'-'+str(new_time)+'-'+str(camera)+'_'+str(angle)+'_'+str(zoom)+'.'+str(imagetype)
+        copy(str(path)+ str(x),str(new_dir)+'/'+str(new_name))
+        
+        
+### Main pipeline
+def main():
+  # Get options
+  args = options()
+  
+  filenames,path=read_dir(args.dir)
+  rename_move(filenames,path, args.camera, args.angle, args.zoom, args.date, args.timeinterval, args.imagetype)
+  
+if __name__ == '__main__':
+  main()
+#!/usr/bin/env python
+
+
+
