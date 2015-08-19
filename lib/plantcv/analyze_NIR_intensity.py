cd78ba7b6d32d927f98bed78cdd78d24ee90054a
maliagehan <maliadong@gmail.com>
2015-08-19 14:18:50 -0500

    debug filename in NIR analysis

--- lib/plantcv/analyze_NIR_intensity.py
+++ lib/plantcv/analyze_NIR_intensity.py
@@ -63,21 +63,22 @@ def analyze_NIR_intensity(img, imgname, mask, bins, device, histplot=False,debug
   
   analysis_img=[]
   
-  #make mask to select the background
-  mask_inv=cv2.bitwise_not(mask)
-  img_back=cv2.bitwise_and(img,img, mask=mask_inv)
-  img_back3=np.dstack((img_back,img_back,img_back))
-
-  # mask the background and color the plant with color scheme 'jet'
-  cplant = cv2.applyColorMap(masked, colormap=2)
-  cplant1=cv2.bitwise_and(cplant,cplant, mask=mask)
-  cplant_back=cv2.add(cplant1,img_back3)
+  if filename!=False:
+    #make mask to select the background
+    mask_inv=cv2.bitwise_not(mask)
+    img_back=cv2.bitwise_and(img,img, mask=mask_inv)
+    img_back3=np.dstack((img_back,img_back,img_back))
   
-  fig_name_pseudo =(str(filename[0:-4]) + '_nir_pseudo_col.png')
-  print_image(cplant_back, fig_name_pseudo)
-  analysis_img.append(['IMAGE', 'pseudo', fig_name_pseudo])
+    # mask the background and color the plant with color scheme 'jet'
+    cplant = cv2.applyColorMap(masked, colormap=2)
+    cplant1=cv2.bitwise_and(cplant,cplant, mask=mask)
+    cplant_back=cv2.add(cplant1,img_back3)
+    
+    fig_name_pseudo =(str(filename[0:-4]) + '_nir_pseudo_col.png')
+    print_image(cplant_back, fig_name_pseudo)
+    analysis_img.append(['IMAGE', 'pseudo', fig_name_pseudo])
   
-  if histplot==True or debug:
+  if filename!=False and (histplot==True or debug):
     import matplotlib
     from matplotlib import pyplot as plt
     from matplotlib import cm as cm
@@ -85,34 +86,34 @@ def analyze_NIR_intensity(img, imgname, mask, bins, device, histplot=False,debug
     from matplotlib import colorbar as colorbar
     import pylab as pl
   
-  if histplot==True:
-    # plot hist percent
-    hist_plot_nir=plt.plot(hist_percent, color = 'green', label = 'Signal Intensity')
-    xaxis=plt.xlim([0,(bins-1)])
-    plt.xlabel(('Grayscale pixel intensity (0-'+str(bins)+")"))
-    plt.ylabel('Proportion of pixels (%)')
-    fig_name_hist=(str(filename[0:-4]) + '_nir_hist.png')
-    plt.savefig(fig_name_hist)
-    plt.clf()
-    analysis_img.append(['IMAGE', 'hist', fig_name_hist])
-    print('\t'.join(map(str, ('IMAGE', 'hist', fig_name_hist))))
-
-  if debug:
-    print_image(cplant1,(str(device)+"_nir_pseudo_plant.png"))
-    print_image(img_back3,(str(device)+"_nir_pseudo_background.png"))
-    print_image(cplant_back,(str(device)+"_nir_pseudo_plant_back.png"))
-
-    filename1=str(filename)
-    name_array=filename1.split("/")
-    filename2="/".join(map(str,name_array[:-1]))
-    fig = plt.figure()
-    ax1 = fig.add_axes([0.05, 0.80, 0.9, 0.15])
-    valmin=-0
-    valmax=(bins-1)
-    norm=colors.Normalize(vmin=valmin, vmax=valmax)
-    cb1=colorbar.ColorbarBase(ax1,cmap=cm.jet, norm=norm, orientation='horizontal')
-    fig_name=str(filename2)+'/NIR_pseudocolor_colorbar.svg'
-    fig.savefig(fig_name,bbox_inches='tight')
+    if histplot==True:
+      # plot hist percent
+      hist_plot_nir=plt.plot(hist_percent, color = 'green', label = 'Signal Intensity')
+      xaxis=plt.xlim([0,(bins-1)])
+      plt.xlabel(('Grayscale pixel intensity (0-'+str(bins)+")"))
+      plt.ylabel('Proportion of pixels (%)')
+      fig_name_hist=(str(filename[0:-4]) + '_nir_hist.png')
+      plt.savefig(fig_name_hist)
+      plt.clf()
+      analysis_img.append(['IMAGE', 'hist', fig_name_hist])
+      print('\t'.join(map(str, ('IMAGE', 'hist', fig_name_hist))))
+  
+    if debug:
+      print_image(cplant1,(str(device)+"_nir_pseudo_plant.png"))
+      print_image(img_back3,(str(device)+"_nir_pseudo_background.png"))
+      print_image(cplant_back,(str(device)+"_nir_pseudo_plant_back.png"))
+  
+      filename1=str(filename)
+      name_array=filename1.split("/")
+      filename2="/".join(map(str,name_array[:-1]))
+      fig = plt.figure()
+      ax1 = fig.add_axes([0.05, 0.80, 0.9, 0.15])
+      valmin=-0
+      valmax=(bins-1)
+      norm=colors.Normalize(vmin=valmin, vmax=valmax)
+      cb1=colorbar.ColorbarBase(ax1,cmap=cm.jet, norm=norm, orientation='horizontal')
+      fig_name=str(filename2)+'/NIR_pseudocolor_colorbar.svg'
+      fig.savefig(fig_name,bbox_inches='tight')
     fig.clf()
     
   return device, hist_header, hist_data,analysis_img
