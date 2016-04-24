f1ada52621ae8176b487e07a3bdffa3ec3c97968
nfahlgren <noahfahlgren@gmail.com>
2016-04-24 01:16:14 -0500

    Added plot_image to __all__

--- lib/plantcv/__init__.py
+++ lib/plantcv/__init__.py
@@ -4,7 +4,7 @@ __all__ = ['fatal_error', 'print_image', 'readimage', 'laplace_filter', 'sobel_f
            'median_blur', 'fill', 'invert', 'logical_and', 'logical_or', 'logical_xor', 'apply_mask', 'find_objects',
            'define_roi', 'roi_objects', 'object_composition', 'analyze_object', 'analyze_bound', 'analyze_color',
            '_pseudocolored_image', 'analyze_NIR_intensity', 'fluor_fvfm', 'print_results', 'resize', 'flip',
-           'crop_position_mask', 'get_nir', 'adaptive_threshold', 'otsu_auto_threshold']
+           'crop_position_mask', 'get_nir', 'adaptive_threshold', 'otsu_auto_threshold', 'plot_image']
 
 from fatal_error import fatal_error
 from print_image import print_image
