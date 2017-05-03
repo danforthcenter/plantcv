86ac6632c5c84d1b5a0018d395542d649d27ca6a
nfahlgren <noahfahlgren@gmail.com>
2017-05-03 13:12:07 -0500

    Added a new naive Bayes training method
    The new method can classify pixels for two or more classes given a
    sample of pixel RGB values for each class, using a color picker tool.

--- plantcv-train.py
+++ plantcv-train.py
@@ -21,28 +21,56 @@ def options():
     start_time = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
     print("Starting run " + start_time + '\n', file=sys.stderr)
 
-    methods = ["naive_bayes"]
-
-    parser = argparse.ArgumentParser(description='PlantCV machine learning training script.',
+    # Create an argument parser
+    parser = argparse.ArgumentParser(description="PlantCV machine learning training script.",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("-i", "--imgdir", help='Input directory containing images.', required=True)
-    parser.add_argument("-b", "--maskdir", help="Input directory containing black/white masks.", required=True)
-    parser.add_argument("-m", "--method", help="Learning method. Available methods: " + ", ".join(map(str, methods)),
-                        required=True)
-    parser.add_argument("-o", "--outfile", help="Trained classifier output filename.", required=True)
-    parser.add_argument("-p", "--plots", help="Make output plots.", default=False)
-    args = parser.parse_args()
+    # Create subcommand parsers
+    subparsers = parser.add_subparsers()
 
+    # Create the Naive Bayes subcommand
+    nb_cmd = subparsers.add_parser("naive_bayes", help="Run the naive Bayes two-class training method.")
+    nb_cmd.add_argument("-i", "--imgdir", help="Input directory containing images.", required=True)
+    nb_cmd.add_argument("-b", "--maskdir", help="Input directory containing black/white masks.", required=True)
+    nb_cmd.add_argument("-o", "--outfile", help="Trained classifier output filename.", required=True)
+    nb_cmd.add_argument("-p", "--plots", help="Make output plots.", default=False, action="store_true")
+    nb_cmd.set_defaults(func=run_naive_bayes)
+
+    # Create the Naive Bayes Multiclass subcommand
+    nbm_cmd = subparsers.add_parser("naive_bayes_multiclass",
+                                    help="Run the naive Bayes two or more class training method.")
+    nbm_cmd.add_argument("-f", "--file",
+                         help="Input file containing a table of pixel RGB values sampled for each input class.",
+                         required=True)
+    nbm_cmd.add_argument("-o", "--outfile", help="Trained classifier output filename.", required=True)
+    nbm_cmd.add_argument("-p", "--plots", help="Make output plots.", default=False, action="store_true")
+    nbm_cmd.set_defaults(func=run_naive_bayes_multiclass)
+
+    # Parse command-line options
+    args = parser.parse_args()
+    # Execute the selected training method
+    args.func(args)
+###########################################
+
+
+# Run the naive Bayes method
+###########################################
+def run_naive_bayes(args):
     if not os.path.exists(args.imgdir):
         raise IOError("Directory does not exist: {0}".format(args.imgdir))
     if not os.path.exists(args.maskdir):
         raise IOError("Directory does not exist: {0}".format(args.maskdir))
-    if args.method not in methods:
-        raise KeyError("Method is not supported: {0}".format(args.method))
+    print("Running the naive Bayes two-class training method...")
+    plantcv.learn.naive_bayes(imgdir=args.imgdir, maskdir=args.maskdir, outfile=args.outfile, mkplots=args.plots)
+###########################################
 
-    return args
 
-
+# Run the naive Bayes multiclass method
+###########################################
+def run_naive_bayes_multiclass(args):
+    if not os.path.exists(args.file):
+        raise IOError("File does not exist: {0}".format(args.file))
+    print("Running the naive Bayes multiclass training method...")
+    plantcv.learn.naive_bayes_multiclass(samples_file=args.file, outfile=args.outfile, mkplots=args.plots)
 ###########################################
 
 
@@ -50,16 +78,9 @@ def options():
 ###########################################
 def main():
     """Main program.
-
     """
-
-    # Parse command-line options
-    args = options()
-
-    if args.method == "naive_bayes":
-        plantcv.learn.naive_bayes(imgdir=args.imgdir, maskdir=args.maskdir, outfile=args.outfile, mkplots=args.plots)
-
-
+    # Parse command-line options and run training method
+    options()
 ###########################################
 
 
--- plantcv/naive_bayes_classifier.py
+++ plantcv/naive_bayes_classifier.py
@@ -26,12 +26,12 @@ def naive_bayes_classifier(img, pdf_file, device, debug=None):
     :param device: int
     :param debug: str
     :return device: int
-    :return mask: ndarray
+    :return masks: dict
     """
     device += 1
 
-    # Initialize PDF dictionary. There are two classes: plant and background (non-plant)
-    pdfs = {"plant": {}, "background": {}}
+    # Initialize PDF dictionary
+    pdfs = {}
     # Read the PDF file
     pf = open(pdf_file, "r")
     # Read the first line (header)
@@ -47,7 +47,11 @@ def naive_bayes_classifier(img, pdf_file, device, debug=None):
             fatal_error("Naive Bayes PDF file is not formatted correctly. Error on line:\n" + row)
         # Store the PDFs. Column 0 is the class, Column 1 is the color channel, the rest are p at
         # intensity values 0-255. Cast text p values as float
-        pdfs[cols[0]][cols[1]] = [float(i) for i in cols[2:]]
+        class_name = cols[0]
+        channel = cols[1]
+        if class_name not in pdfs:
+            pdfs[class_name] = {}
+        pdfs[class_name][channel] = [float(i) for i in cols[2:]]
 
     # Split the input BGR image into component channels for BGR, HSV, and LAB colorspaces
     # b, g, r = cv2.split(img)
@@ -58,29 +62,38 @@ def naive_bayes_classifier(img, pdf_file, device, debug=None):
     width, height, depth = np.shape(img)
 
     # Initialize an empty ndarray for plant and background. These will be used to store the joint probabilities
-    plant = np.zeros([width, height])
-    bg = np.zeros([width, height])
+    px_p = {}
+    for class_name in pdfs.keys():
+        px_p[class_name] = np.zeros([width, height])
 
     # Loop over the image coordinates (each i, j pixel)
     for i in range(0, width):
         for j in range(0, height):
-            # Calculate the joint probability that this is a plant pixel
-            plant[i][j] = pdfs["plant"]["hue"][h[i][j]] * pdfs["plant"]["saturation"][s[i][j]] * \
-                          pdfs["plant"]["value"][v[i][j]]
-            # Calculate the joint probability that this is a background pixel
-            bg[i][j] = pdfs["background"]["hue"][h[i][j]] * pdfs["background"]["saturation"][s[i][j]] * \
-                       pdfs["background"]["value"][v[i][j]]
+            for class_name in pdfs.keys():
+                # Calculate the joint probability that this is in the class
+                px_p[class_name][i][j] = pdfs[class_name]["hue"][h[i][j]] * pdfs[class_name]["saturation"][s[i][j]] * \
+                                         pdfs[class_name]["value"][v[i][j]]
 
-    # Initialize an empty mask ndarray
-    mask = np.zeros([width, height],dtype=np.uint8)
-    # Set pixel intensities to 255 (white) where the probability of the pixel being plant is greater than the
-    # probability of the pixel being background
-    mask[np.where(plant > bg)] = 255
+    # Initialize empty masks
+    masks = {}
+    for class_name in pdfs.keys():
+        masks[class_name] = np.zeros([width, height], dtype=np.uint8)
+    # Set pixel intensities to 255 (white) for the mask where the class has the highest probability
+    for class_name in masks:
+        background_classes = []
+        for name in masks:
+            if class_name is not name:
+                background_classes.append(px_p[name])
+        background_class = np.maximum.reduce(background_classes)
+        masks[class_name][np.where(px_p[class_name] > background_class)] = 255
+    # mask[np.where(plant > bg)] = 255
 
     # Print or plot the mask if debug is not None
-    if debug == 'print':
-        print_image(mask, (str(device) + '_naive_bayes_mask.jpg'))
-    elif debug == 'plot':
-        plot_image(mask, cmap='gray')
+    if debug == "print":
+        for class_name, mask in masks.items():
+            print_image(mask, (str(device) + "_naive_bayes_" + class_name + "_mask.jpg"))
+    elif debug == "plot":
+        for class_name, mask in masks.items():
+            plot_image(mask, cmap="gray")
 
-    return device, mask
+    return device, masks
