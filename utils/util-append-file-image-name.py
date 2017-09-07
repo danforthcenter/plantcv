ac290748c56976b8e63ba16c07335c8614b666e9
nfahlgren <noahfahlgren@gmail.com>
2017-09-07 17:42:52 -0500

    Reformatted for PEP8 compliance

--- utils/util-append-file-image-name.py
+++ utils/util-append-file-image-name.py
@@ -1,50 +1,50 @@
 #!/usr/bin/env python
 
 import argparse
-import sys, os
-import re
+import os
 import shutil
 
-### Parse command-line arguments
-def options():
-  parser = argparse.ArgumentParser(description="Get file names to run FASTQC over")
-  parser.add_argument("-d", "--directory", help="directory to run script over.")
-  parser.add_argument("-o", "--outdir", help="out directory to move files to")
-  args = parser.parse_args()
-  return args
 
-def append_img_name(directory,outdir):
-    dirs=os.listdir(directory)
-    
-    dirname=str(directory)
-    dirsplit=dirname.split('/')
-    experiment=(dirsplit[-1])
-    
+# Parse command-line arguments
+def options():
+    parser = argparse.ArgumentParser(description="Get file names to run FASTQC over")
+    parser.add_argument("-d", "--directory", help="directory to run script over.")
+    parser.add_argument("-o", "--outdir", help="out directory to move files to")
+    args = parser.parse_args()
+    return args
+
+
+def append_img_name(directory, outdir):
+    dirs = os.listdir(directory)
+
+    dirname = directory
+    dirsplit = dirname.split('/')
+    experiment = (dirsplit[-1])
+
     if not os.path.exists(outdir):
         os.makedirs(outdir)
-    
+
     for x in dirs:
-        path=str(directory)+"/"+str(x)
-        #print(path)
-        if os.path.isdir(path) == True:
-            files=os.listdir(path)
-            outdirpath=str(outdir)+"/"+str(experiment)+"/"+str(x)
+        path = os.path.join(directory, x)
+        if os.path.isdir(path):
+            files = os.listdir(path)
+            outdirpath = os.path.join(outdir, experiment, x)
             os.makedirs(outdirpath)
             for i in files:
-                filepath=str(path)+"/"+str(i)
-                outfilepath=str(outdirpath)+"/"+str(experiment)+"_"+str(x)+"_"+str(i)
-                shutil.copyfile(filepath,outfilepath)
+                filepath = os.path.join(path, i)
+                outfilepath = os.path.join(outdirpath, experiment + "_" + x + "_" + i)
+                shutil.copyfile(filepath, outfilepath)
                 print(filepath)
                 print(outfilepath)
-            
-    
-### Main pipeline
+
+
+# Main pipeline
 def main():
-  # Get options
-  args = options()
-  
-  append_img_name(args.directory,args.outdir)
-  
+    # Get options
+    args = options()
+
+    append_img_name(args.directory, args.outdir)
+
 
 if __name__ == '__main__':
-  main()
+    main()
