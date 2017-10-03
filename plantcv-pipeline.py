a39387627231c0e150c0611fb59f986ea06ea07c
nfahlgren <noahfahlgren@gmail.com>
2017-10-03 11:06:30 -0500

    Changed SQLite imports to system call without shell

--- plantcv-pipeline.py
+++ plantcv-pipeline.py
@@ -62,7 +62,7 @@ def options():
     parser.add_argument("-a", "--adaptor",
                         help='Image metadata reader adaptor. PhenoFront metadata is stored in a CSV file and the '
                              'image file name. For the filename option, all metadata is stored in the image file '
-                             'name. Current adaptors: phenofront, image', default="phenofront")
+                             'name. Current adaptors: phenofront, filename', default="phenofront")
     parser.add_argument("-p", "--pipeline", help='Pipeline script file.', required=True)
     parser.add_argument("-s", "--db", help='SQLite database file name.', required=True)
     parser.add_argument("-i", "--outdir", help='Output directory for images. Not required by all pipelines.',
@@ -293,11 +293,11 @@ def main():
 
     # Load database
     ###########################################
-    call("sqlite3 " + args.db + " \".import " + runinfo_file.name + " runinfo\"", shell=True)
-    call("sqlite3 " + args.db + " \".import " + args.metadata_file.name + " metadata\"", shell=True)
-    call("sqlite3 " + args.db + " \".import " + args.features_file.name + " features\"", shell=True)
-    call("sqlite3 " + args.db + " \".import " + args.analysis_images_file.name + " analysis_images\"", shell=True)
-    call("sqlite3 " + args.db + " \".import " + args.signal_file.name + " signal\"", shell=True)
+    call(["sqlite3", args.db, '.import ' + runinfo_file.name + ' runinfo'])
+    call(["sqlite3", args.db, '.import ' + args.metadata_file.name + ' metadata'])
+    call(["sqlite3", args.db, '.import ' + args.features_file.name + ' features'])
+    call(["sqlite3", args.db, '.import ' + args.analysis_images_file.name + ' analysis_images'])
+    call(["sqlite3", args.db, '.import ' + args.signal_file.name + ' signal'])
     ###########################################
 
 
