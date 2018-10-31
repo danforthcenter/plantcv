096e9599eb669305725f3f4a52a626cace6aa7fa
cjrobey <calebrobey118@gmail.com>
2018-10-31 16:20:11 -0400

    addding the cvxopt requirement in python

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ setuptools.setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['matplotlib>=1.5', 'numpy>=1.11', 'pandas', 'python-dateutil', 'scipy', 'scikit-image', 'cvxopt'],
+    install_requires=['cvxopt', 'matplotlib>=1.5', 'numpy>=1.11', 'pandas', 'python-dateutil', 'scipy', 'scikit-image'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
