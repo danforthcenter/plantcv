fb3132d05cb17b3d95fb9393c6fc9089fd132774
HaleySchuhl <haleyschuhl@gmail.com>
2018-12-21 16:08:42 -0600

    switch_chip_dims_for_radius

--- tests/tests.py
+++ tests/tests.py
@@ -2560,15 +2560,15 @@ def test_plantcv_transform_find_color_card():
     df, start, space = pcv.transform.find_color_card(img=rgb_img)
     # Test with debug = "print"
     pcv.params.debug = "print"
-    _ = pcv.transform.create_color_card_mask(rgb_img=rgb_img, chip_dims=(10, 10), start_coord=start,
+    _ = pcv.transform.create_color_card_mask(rgb_img=rgb_img, radius=6, start_coord=start,
                                              spacing=space, nrows=6, ncols=4, exclude=[20, 0])
     # Test with debug = "plot"
     pcv.params.debug = "plot"
-    _ = pcv.transform.create_color_card_mask(rgb_img=rgb_img, chip_dims=(10, 10), start_coord=start,
+    _ = pcv.transform.create_color_card_mask(rgb_img=rgb_img, radius=6, start_coord=start,
                                              spacing=space, nrows=6, ncols=4, exclude=[20, 0])
     # Test with debug = None
     pcv.params.debug = None
-    mask = pcv.transform.create_color_card_mask(rgb_img=rgb_img, chip_dims=(10, 10), start_coord=start,
+    mask = pcv.transform.create_color_card_mask(rgb_img=rgb_img, radius=6, start_coord=start,
                                                 spacing=space, nrows=6, ncols=4, exclude=[20, 0])
     assert all([i == j] for i, j in zip(np.unique(mask), np.array([0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110,
                                                                    120, 130, 140, 150, 160, 170, 180, 190, 200, 210,
