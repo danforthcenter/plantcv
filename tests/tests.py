c20eae3c1baea8f4364582a6f84cc83e402ec308
HaleySchuhl <haleyschuhl@gmail.com>
2018-12-26 10:48:32 -0600

    test_more_parameter_options

--- tests/tests.py
+++ tests/tests.py
@@ -2575,22 +2575,22 @@ def test_plantcv_transform_find_color_card():
                                                                    220], dtype=np.uint8)))
 
 
-def test_plantcv_transform_find_color_card_with_sharpening():
+def test_plantcv_transform_find_color_card_optional_parameters():
     # Load rgb image
     rgb_img = cv2.imread(os.path.join(TEST_DATA, TEST_TARGET_IMG))
     # Test cache directory
     cache_dir = os.path.join(TEST_TMPDIR, "test_plantcv_transform_find_color_card")
     os.mkdir(cache_dir)
     pcv.params.debug_outdir = cache_dir
-    df, start, space = pcv.transform.find_color_card(img=rgb_img, threshold='adaptgauss', blurry=True)
-    # Test with debug = "print"
-    pcv.params.debug = "print"
-    _ = pcv.transform.create_color_card_mask(rgb_img=rgb_img, radius=6, start_coord=start,
-                                             spacing=space, nrows=6, ncols=4, exclude=[20, 0])
-    # Test with debug = "plot"
-    pcv.params.debug = "plot"
-    _ = pcv.transform.create_color_card_mask(rgb_img=rgb_img, radius=6, start_coord=start,
-                                             spacing=space, nrows=6, ncols=4, exclude=[20, 0])
+    df1, start1, space1 = pcv.transform.find_color_card(img=rgb_img, threshold='otsu', blurry=True, background='light')
+    # Test with threshold='otsu'
+    _ = pcv.transform.create_color_card_mask(rgb_img=rgb_img, radius=6, start_coord=start1,
+                                             spacing=space1, nrows=6, ncols=4, exclude=[20, 0])
+    # Test with threshold='normal'
+    df2, start2, space2 = pcv.transform.find_color_card(img=rgb_img, threshold='normal', blurry=True,
+                                                        background='light')
+    _ = pcv.transform.create_color_card_mask(rgb_img=rgb_img, radius=6, start_coord=start2,
+                                             spacing=space2, nrows=6, ncols=4, exclude=[20, 0])
     # Test with debug = None
     pcv.params.debug = None
     mask = pcv.transform.create_color_card_mask(rgb_img=rgb_img, radius=6, start_coord=start,
@@ -2598,7 +2598,7 @@ def test_plantcv_transform_find_color_card_with_sharpening():
     assert all([i == j] for i, j in zip(np.unique(mask), np.array([0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110,
                                                                    120, 130, 140, 150, 160, 170, 180, 190, 200, 210,
                                                                    220], dtype=np.uint8)))
-    
+
 
 def test_plantcv_transform_find_color_card_bad_thresh_input():
     # Load rgb image
