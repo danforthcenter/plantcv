7866d8f6755d16614271ff4d71e0920a9e78d4bf
Noah Fahlgren <noahfahlgren@gmail.com>
2018-12-31 10:05:08 -0600

    Update tests/tests.py
    Co-Authored-By: HaleySchuhl <44006936+HaleySchuhl@users.noreply.github.com>

--- tests/tests.py
+++ tests/tests.py
@@ -2614,7 +2614,7 @@ def test_plantcv_transform_find_color_card_bad_background_input():
     rgb_img = cv2.imread(os.path.join(TEST_DATA, TEST_TARGET_IMG))
     with pytest.raises(RuntimeError):
         pcv.params.debug = None
-        _, _, _ = pcv.transform.find_color_card(img=rgb_img, background='lite')
+        _, _, _ = pcv.transform.find_color_card(rgb_img=rgb_img, background='lite')
 
 
 # ##############################
