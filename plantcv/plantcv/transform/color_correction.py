04bad695141144c77f8a7a946b489ef8177a47dd
Noah Fahlgren <noahfahlgren@gmail.com>
2018-12-31 10:05:22 -0600

    Update plantcv/plantcv/transform/color_correction.py
    Co-Authored-By: HaleySchuhl <44006936+HaleySchuhl@users.noreply.github.com>

--- plantcv/plantcv/transform/color_correction.py
+++ plantcv/plantcv/transform/color_correction.py
@@ -492,7 +492,7 @@ def find_color_card(rgb_img, threshold='adaptgauss', threshvalue=125, blurry=Fal
     Inputs:
     rgb_img        = Input RGB image data containing a color card.
     threshold      = Threshold method, either 'normal', 'otsu', or 'adaptgauss', optional (default 'adaptgauss')
-    thresh_value   = Thresholding value, optional (default 125)
+    threshvalue    = Thresholding value, optional (default 125)
     blurry         = Bool (default False) if True then image sharpening applied
     background     = Type of image background either 'dark' or 'light' (default 'dark'); if 'light' then histogram
                         expansion applied to better detect edges, but histogram expansion will be hindered if there
