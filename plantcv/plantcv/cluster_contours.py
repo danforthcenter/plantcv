ddecfd66d003636f24e8878324ae884f25c07af9
HaleySchuhl <haleyschuhl@gmail.com>
2019-01-03 14:24:40 -0600

    cluster_contours
    isinstance will already give you true or false

--- plantcv/plantcv/cluster_contours.py
+++ plantcv/plantcv/cluster_contours.py
@@ -60,7 +60,7 @@ def cluster_contours(img, roi_objects, roi_obj_hierarchy, nrow=1, ncol=1):
     # categorize what bin the center of mass of each contour
 
     def digitize(a, step):
-        if isinstance(step, int) is True:
+        if isinstance(step, int):
             i = step
         else:
             i = len(step)
