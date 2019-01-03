f0922c7e210e04baac60011bb9092d4a5c3930a7
Noah Fahlgren <noahfahlgren@gmail.com>
2019-01-03 17:55:38 -0600

    Added Python 3.7 to metadata

--- setup.py
+++ setup.py
@@ -82,7 +82,8 @@ setuptools.setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
     ],
 
     # What does your project relate to?
