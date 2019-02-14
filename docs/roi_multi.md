5b7c242f51309e0510a0f1a49568cb314ec5f088
HaleySchuhl <haleyschuhl@gmail.com>
2019-02-14 15:52:13 -0600

    roi multi function and docs
    - Added a multi ROI function. Users can use to find a grid of ROIs or can supply a list of centers for ROIs if they are not in a grid arrangement.
    - Added documentation page
    - Still need to add doc page to mkdocs, add tests, images to docs

---
+++ docs/roi_multi.md
@@ -0,0 +1,80 @@
+## Create Multiple Regions of Interest (ROI) 
+
+**plantcv.roi.multi**(*img, coord, radius, spacing=None, nrows=None, ncols=None*)
+
+**returns** roi_contours, roi_hierarchies
+
+- **Parameters:**
+    - img            = Input image data.
+    - coord          = Two-element tuple of the center of the top left object.
+    - radius         = Radius of ROIs.
+    - spacing        = Two-element tuple of the horizontal and vertical spacing between ROIs.
+    - nrows          = Number of chip rows.
+    - ncols          = Number of chip columns.
+- **Context:**
+    - Used to define multiple regions of interest in the same image. Users can either specify a
+      starting coordinate (`coord`), number of row and columns, and spacing to create a grid of ROIs,
+      or a custom list of coordinates that specify the centers of the ROIs. Providing a custom list 
+      of coordinates (list of tuples) is useful for missing plants or any arrangement that isn't 
+      a perfect grid. Returns lists of contours and hierarchies that can be used in downstream steps. 
+
+**Reference Image**
+
+![Screenshot](img/documentation_images/cluster_contour/13_roi_mask.jpg)
+
+```python
+
+from plantcv import plantcv as pcv
+
+# Set global debug behavior to None (default), "print" (to file), or "plot" (Jupyter Notebooks or X11)
+pcv.params.debug = "print"
+
+# Make a grid of ROIs 
+rois1, roi_hierarchy1 = pcv.roi.multi(img, coord=(100,90), radius=40, spacing=(200,200), nrows=2, ncols=3)
+
+# Specify a list of coordinates of desired ROIs 
+rois2, roi_hierarchy2 = pcv.roi.multi(img, coord= [(100,100), (300,300), (300, 100)], radius=30)
+```
+
+**Grid of ROIs**
+![Screenshot](img/documentation_images/multi/image_with_roi_grid.jpg)
+
+**Custom list of ROIs** 
+![Screenshot](img/documentation_images/multi/image_with_roi_grid.jpg)
+
+### Next steps:
+
+Since this function returns lists of objects and hierarchies, the downstream steps require users to loop 
+over each. The [pcv.roi_objects](roi_objects.md) and [pcv.object_composition](object_composition.md) functions usually 
+follow an ROI step.
+
+```python
+import numpy as np 
+
+img_copy = np.copy(img)
+
+for i in range(0, len(rois1)):
+    roi = rois1[i]
+    hierarchy = roi_hierarchy1[i]
+    # Find objects
+    filtered_contours, filtered_hierarchy, filtered_mask, filtered_area = pcv.roi_objects(
+        img=img, roi_type="partial", roi_contour=roi, roi_hierarchy=hierarchy, object_contour=obj, 
+        obj_hierarchy=obj_hierarchy)
+    
+    # Combine objects together in each plant     
+    plant_contour, plant_mask = pcv.object_composition(img=img, contours=filtered_contours, hierarchy=filtered_hierarchy)        
+    
+    # Analyze the shape of each plant 
+    shape_data_headers, shape_data, analysis_images = pcv.analyze_object(img=img_copy, obj=obj, mask=img_mask)
+    
+    # Save the image with shape characteristics 
+    img_copy = analysis_images[0]
+    
+    # Print out a text file with shape data for each plant in the image 
+    pcv.print_results(filename = 'prefix_' + str(i) + '.txt')
+    # Clear the measurements stored globally into the Ouptuts class
+    pcv.outputs.clear()
+    
+# Print the image with shape analysis on each plant in the image 
+pcv.plot_image(img_copy)
+```
--- plantcv/plantcv/roi/roi_methods.py
+++ plantcv/plantcv/roi/roi_methods.py
@@ -207,7 +207,7 @@ def _draw_roi(img, roi_contour):
         plot_image(ref_img)
 
 
-def multiple_roi(img, coord, radius, spacing=None, nrows=None, ncols=None):
+def multi(img, coord, radius, spacing=None, nrows=None, ncols=None):
     """Create a labeled mask for color card chips
     Inputs
     img            = Input image data.
@@ -231,18 +231,28 @@ def multiple_roi(img, coord, radius, spacing=None, nrows=None, ncols=None):
 
     # Autoincrement the device counter
     params.device += 1
+
     # Initialize ROI list
     rois = []
+
     # Store user debug
     debug = params.debug
+
     # Temporarily disable debug
     params.debug = None
 
+    # Get the height and width of the reference image
+    height, width = np.shape(img)[:2]
+
+    # Initialize a binary image of the circle
+    bin_img = np.zeros((height, width), dtype=np.uint8)
+    roi_contour = []
+    roi_hierarchy = []
     # Grid of ROIs
     if (type(coord) == tuple) and ((nrows and ncols) is not None):
-        # Loop over each color card row
+        # Loop over each row
         for i in range(0, nrows):
-            # The upper left corner is the y starting coordinate + the ROI offset * the vertical spacing between chips
+            # The upper left corner is the y starting coordinate + the ROI offset * the vertical spacing
             y = coord[1] + i * spacing[1]
             # Loop over each column
             for j in range(0, ncols):
@@ -251,59 +261,38 @@ def multiple_roi(img, coord, radius, spacing=None, nrows=None, ncols=None):
                 x = coord[0] + j * spacing[0]
                 # Create a chip ROI
                 rois.append(circle(img=img, x=x, y=y, r=radius))
+                # Draw the circle on the binary image
+                cv2.circle(bin_img, (x, y), radius, 255, -1)
+                # Use the binary image to create an ROI contour
+                roi_contour.append(cv2.findContours(np.copy(bin_img), cv2.RETR_EXTERNAL,
+                                                    cv2.CHAIN_APPROX_NONE)[-2:][0])
+                roi_hierarchy.append(cv2.findContours(np.copy(bin_img), cv2.RETR_EXTERNAL,
+                                                      cv2.CHAIN_APPROX_NONE)[-2:][1])
+                roi_contour1, roi_hierarchy1 = cv2.findContours(np.copy(bin_img), cv2.RETR_TREE,
+                                                                cv2.CHAIN_APPROX_NONE)[-2:]
+
+
 
     # User specified ROI centers
     elif (type(coord) == list) and ((nrows and ncols) is None):
-        for i in coord:
+        for i in range(0, len(coord)):
             y = coord[i][1]
             x = coord[i][0]
             rois.append(circle(img=img, x=x, y=y, r=radius))
+            # Draw the circle on the binary image
+            cv2.circle(bin_img, (x, y), radius, 255, -1)
+            # Use the binary image to create an ROI contour
+            roi_contour, roi_hierarchy = cv2.findContours(np.copy(bin_img), cv2.RETR_EXTERNAL,
+                                                          cv2.CHAIN_APPROX_NONE)[-2:]
 
     else:
         fatal_error("Function can either make a grid of ROIs (user must provide nrows, ncols, spacing, and coord) "
                     "or take custom ROI coordinates (user must provide a list of tuples to 'coord' parameter)")
+    # Reset debug
+    params.debug = debug
 
-    # # Create mask
-    # mask = np.zeros(shape=np.shape(img)[:2], dtype=np.uint8())
-    # # Mask label index
-    # i = 1
-    # # Draw labeled chip boxes on the mask
-    # for roi in rois:
-    #     mask = cv2.drawContours(mask, roi[0], -1, (i * 10), -1)
-    #     i += 1
-    # # Reset debug
-    # params.debug = debug
-    #
-    # if params.debug is not None:
-    #     # Create a copy of the input image for plotting
-    #     canvas = np.copy(img)
-    #     # Draw chip ROIs on the canvas image
-    #     for roi in rois:
-    #         cv2.drawContours(canvas, roi[0], -1, (255, 255, 0), 5)
-    #     if params.debug == "print":
-    #         print_image(img=canvas, filename=os.path.join(params.debug_outdir,
-    #                                                       str(params.device) + "_multiple_rois.png"))
-    #         print_image(img=mask, filename=os.path.join(params.debug_outdir,
-    #                                                     str(params.device) + "_multiple_roi_mask.png"))
-    #     elif params.debug == "plot":
-    #         plot_image(canvas)
-    # return mask
-    # Get the height and width of the reference image
-    height, width = np.shape(img)[:2]
-
-    # Loop over all the ROIs that were stored
-    for roi in rois:
-
-        # Initialize a binary image of the circle
-        bin_img = np.zeros((height, width), dtype=np.uint8)
-        # Draw the circle on the binary image
-        cv2.circle(bin_img, (x, y), r, 255, -1)
-
-        # Use the binary image to create an ROI contour
-        roi_contour, roi_hierarchy = cv2.findContours(np.copy(bin_img), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[-2:]
-
-        # Draw the ROI if requested
-        if params.debug is not None:
-            _draw_roi(img=img, roi_contour=roi_contour)
+    # Draw the ROI if requested
+    if params.debug is not None:
+        _draw_roi(img=img, roi_contour=roi_contour1)
 
     return roi_contour, roi_hierarchy
