c370567847677d38d99f22f2c659d91a7620baf5
Dominik Schneider <dominik.schneider@wsu.edu>
2019-03-12 16:45:54 -0700

    indentation woes

--- plantcv/plantcv/roi_objects.py
+++ plantcv/plantcv/roi_objects.py
@@ -88,102 +88,102 @@ def roi_objects(img, roi_type, roi_contour, roi_hierarchy, object_contour, obj_h
         print(
             "Warning: roi_type='largest' will only return the largest contour and its immediate children.")
 
-            # Filter contours outside of the region of interest
-            for c, cnt in enumerate(object_contour):
-                length = (len(cnt) - 1)
-                stack = np.vstack(cnt)
-                keep = False
-                # Test if the contours are within the ROI
-                for i in range(0, length):
-                    pptest = cv2.pointPolygonTest(roi_contour[0], (stack[i][0], stack[i][1]), False)
-                    if int(pptest) != -1:
-                        keep = True
-                if keep:
-                    # Color the "gap contours" white
-                    if obj_hierarchy[0][c][3] > -1:
-                        cv2.drawContours(w_back, object_contour, c, (255, 255, 255), -1, lineType=8,
-                                         hierarchy=obj_hierarchy)
-                    else:
-                        # Color the plant contour parts black
-                        cv2.drawContours(w_back, object_contour, c, (0, 0, 0), -1, lineType=8, hierarchy=obj_hierarchy)
+        # Filter contours outside of the region of interest
+        for c, cnt in enumerate(object_contour):
+            length = (len(cnt) - 1)
+            stack = np.vstack(cnt)
+            keep = False
+            # Test if the contours are within the ROI
+            for i in range(0, length):
+                pptest = cv2.pointPolygonTest(roi_contour[0], (stack[i][0], stack[i][1]), False)
+                if int(pptest) != -1:
+                    keep = True
+            if keep:
+                # Color the "gap contours" white
+                if obj_hierarchy[0][c][3] > -1:
+                    cv2.drawContours(w_back, object_contour, c, (255, 255, 255), -1, lineType=8,
+                                     hierarchy=obj_hierarchy)
                 else:
-                    # If the contour isn't overlapping with the ROI, color it white
-                    cv2.drawContours(w_back, object_contour, c, (255, 255, 255), -1, lineType=8, hierarchy=obj_hierarchy)
+                    # Color the plant contour parts black
+                    cv2.drawContours(w_back, object_contour, c, (0, 0, 0), -1, lineType=8, hierarchy=obj_hierarchy)
+            else:
+                # If the contour isn't overlapping with the ROI, color it white
+                cv2.drawContours(w_back, object_contour, c, (255, 255, 255), -1, lineType=8, hierarchy=obj_hierarchy)
 
-            kept = cv2.cvtColor(w_back, cv2.COLOR_RGB2GRAY)
-            kept_obj = cv2.bitwise_not(kept)
-            kept_cnt, kept_hierarchy = cv2.findContours(kept_obj, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[-2:]
+        kept = cv2.cvtColor(w_back, cv2.COLOR_RGB2GRAY)
+        kept_obj = cv2.bitwise_not(kept)
+        kept_cnt, kept_hierarchy = cv2.findContours(kept_obj, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[-2:]
 
-            # Find the index of the largest contour in the list of contours
-            largest_area = 0
-            for c, cnt in enumerate(kept_cnt):
-                area = cv2.contourArea(cnt)
-                if (area > largest_area):
-                    largest_area = area
-                    index = c
+        # Find the index of the largest contour in the list of contours
+        largest_area = 0
+        for c, cnt in enumerate(kept_cnt):
+            area = cv2.contourArea(cnt)
+            if (area > largest_area):
+                largest_area = area
+                index = c
 
-            # Store the largest contour as a list
-            largest_cnt = [kept_cnt[index]]
-            # Store the hierarchy of the largest contour into a list
-            largest_hierarchy = [kept_hierarchy[0][index]]
+        # Store the largest contour as a list
+        largest_cnt = [kept_cnt[index]]
+        # Store the hierarchy of the largest contour into a list
+        largest_hierarchy = [kept_hierarchy[0][index]]
 
-            # Find the index of the first child of the largest contour
-            child_index = largest_hierarchy[0][2]
-            largest_cnt.append(kept_cnt[child_index])
-            largest_hierarchy.append(kept_hierarchy[0][child_index])
+        # Find the index of the first child of the largest contour
+        child_index = largest_hierarchy[0][2]
+        largest_cnt.append(kept_cnt[child_index])
+        largest_hierarchy.append(kept_hierarchy[0][child_index])
 
-            # Iterate through contours to find children of the largest contour
-            i = 0
-            for i,khi in enumerate(kept_hierarchy[0]):
-                if khi[3] == index: #is the parent equal to the largest contour?
-                    largest_hierarchy.append(khi)
-                    largest_cnt.append(kept_cnt[i])
+        # Iterate through contours to find children of the largest contour
+        i = 0
+        for i,khi in enumerate(kept_hierarchy[0]):
+            if khi[3] == index: #is the parent equal to the largest contour?
+                largest_hierarchy.append(khi)
+                largest_cnt.append(kept_cnt[i])
 
-            # Make the kept hierarchies into an array so that cv2 can use it
-            largest_hierarchy = np.array([largest_hierarchy])
+        # Make the kept hierarchies into an array so that cv2 can use it
+        largest_hierarchy = np.array([largest_hierarchy])
 
-            # w_back2 = background + 255
-            # Overwrite w_back and mask so it only has the largest contour
-            i=2
-            for i, cnt in enumerate(largest_cnt):
-                # print(cnt)
-                if i == 0:
-                    color = (255, 102, 255)
-                else:
-                    color = (255, 255, 255)
-                    # print(i)
-                cv2.drawContours(w_back, largest_cnt, i, color, -1, lineType=8, hierarchy=largest_hierarchy, maxLevel=0)
+        # w_back2 = background + 255
+        # Overwrite w_back and mask so it only has the largest contour
+        i=2
+        for i, cnt in enumerate(largest_cnt):
+            # print(cnt)
+            if i == 0:
+                color = (255, 102, 255)
+            else:
+                color = (255, 255, 255)
+                # print(i)
+            cv2.drawContours(w_back, largest_cnt, i, color, -1, lineType=8, hierarchy=largest_hierarchy, maxLevel=0)
 
-            # assign common name for output
-            kept_hierarchy = largest_hierarchy
+        # assign common name for output
+        kept_hierarchy = largest_hierarchy
 
-            # Overwrite the mask with only pixels that are inside ROI and of the largest (and children) ROI
-            kept = cv2.cvtColor(w_back, cv2.COLOR_BGR2GRAY)
-            kept[kept!=255] = 0 #I hope this is always true. this section was very goofy
-            kept_obj = cv2.bitwise_not(kept)
-            mask = np.copy(kept_obj)
+        # Overwrite the mask with only pixels that are inside ROI and of the largest (and children) ROI
+        kept = cv2.cvtColor(w_back, cv2.COLOR_BGR2GRAY)
+        kept[kept!=255] = 0 #I hope this is always true. this section was very goofy
+        kept_obj = cv2.bitwise_not(kept)
+        mask = np.copy(kept_obj)
 
-            # Create inverted mask to mask contour
-            mask_inv = kept
+        # Create inverted mask to mask contour
+        mask_inv = kept
 
-            # Create green "mask" of plant contour
-            g_back = background.copy()
-            g_back[:,:,1] = g_back[:,:,1] + 255
-            g_back[mask_inv.astype('bool')] = 0
-            # pcv.plot_image(g_mask)
+        # Create green "mask" of plant contour
+        g_back = background.copy()
+        g_back[:,:,1] = g_back[:,:,1] + 255
+        g_back[mask_inv.astype('bool')] = 0
+        # pcv.plot_image(g_mask)
 
-            # Mask image to remove
-            masked_img = cv2.bitwise_and(ori_img, ori_img, mask=mask_inv)
+        # Mask image to remove
+        masked_img = cv2.bitwise_and(ori_img, ori_img, mask=mask_inv)
 
-            # Add green mask to real background with white plant
-            ori_img = cv2.add(masked_img, g_back)
+        # Add green mask to real background with white plant
+        ori_img = cv2.add(masked_img, g_back)
 
-            # Draw ROI onto original image with contours
-            cv2.drawContours(ori_img, roi_contour, -1, (255, 0, 0), 5, lineType=8, hierarchy=roi_hierarchy)
-            # plot_image(ori_img)
+        # Draw ROI onto original image with contours
+        cv2.drawContours(ori_img, roi_contour, -1, (255, 0, 0), 5, lineType=8, hierarchy=roi_hierarchy)
+        # plot_image(ori_img)
 
-            # Compute object area
-            obj_area = cv2.countNonZero(mask)
+        # Compute object area
+        obj_area = cv2.countNonZero(mask)
 
     # Allows user to cut objects to the ROI (all objects completely outside ROI will not be kept)
     elif roi_type.upper() == 'CUTTO':
