8d61546a776da3eca9242ff1d3bc7a42f94f19b9
Noah Fahlgren <noahfahlgren@gmail.com>
2019-06-14 15:35:23 -0500

    Add json2csv conversion utility

--- plantcv-utils.py
+++ plantcv-utils.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-import os
+import sys
 import argparse
-import json
+import plantcv.utils
 
 
 # Parse command-line arguments
@@ -18,39 +18,29 @@ def options():
     subparsers = parser.add_subparsers()
 
     # Create the Naive Bayes subcommand
-    nb_cmd = subparsers.add_parser("json2csv", help="Convert PlantCV output JSON files to CSV.")
-    nb_cmd.add_argument("-j", "--json", help="Input PlantCV JSON filename.", required=True)
-    nb_cmd.add_argument("-c", "--csv", help="Output CSV filename.", required=True)
-    nb_cmd.set_defaults(func=json2csv)
+    json2csv_cmd = subparsers.add_parser("json2csv", help="Convert PlantCV output JSON files to CSV.")
+    json2csv_cmd.add_argument("-j", "--json", help="Input PlantCV JSON filename.", required=True)
+    json2csv_cmd.add_argument("-c", "--csv", help="Output CSV filename.", required=True)
+    json2csv_cmd.set_defaults(func=run_json2csv)
+
+    # If no arguments are given, print the help menu
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
 
     # Parse command-line options
     args = parser.parse_args()
     # Execute the selected method
     args.func(args)
 
-
 ###########################################
 
 
-def json2csv(args):
-    if os.path.exists(args.json):
-        # If the JSON file exists open it for reading
-        json_data = open(args.json, "r")
-        data = json.load(json_data)
-        # If the data is JSON but it does not have the components we expect from PlantCV raise an error
-        if "variables" not in data or "entities" not in data:
-            raise ValueError("Invalide JSON file: {0}".format(args.json))
-
-        # Open the output CSV file for writing
-        csv = open(args.csv, "w")
-
-        
-
-        # Close the CSV file
-        csv.close()
-    else:
-        # If the file does not exist raise an error
-        raise IOError("File does not exist: {0}".format(args.json))
+# run_json2csv
+###########################################
+def run_json2csv(args):
+    plantcv.utils.json2csv(json_file=args.json, csv_file=args.csv)
+###########################################
 
 
 # Main
@@ -60,8 +50,6 @@ def main():
     """
     # Parse command-line options and run the selected method
     options()
-
-
 ###########################################
 
 
