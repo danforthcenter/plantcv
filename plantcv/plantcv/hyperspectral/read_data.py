e831e50f905f0a987d289b141117bbe14699543c
HaleySchuhl <haleyschuhl@gmail.com>
2019-09-11 11:28:36 -0500

    start hyperspectral sub-package

---
+++ plantcv/plantcv/hyperspectral/read_data.py
@@ -0,0 +1,10 @@
+# Read in a hyperspectral data cube as an array and parse metadata from the header file
+
+from plantcv.plantcv import plot_image
+from plantcv.plantcv import print_image
+from plantcv.plantcv import Outputs
+import numpy as np
+import os
+
+
+def read_data():
