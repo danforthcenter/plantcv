f4bd0aba5c63a38771dbe4e6ffda2fd51a47194c
HaleySchuhl <haleyschuhl@gmail.com>
2019-09-12 14:44:03 -0500

    reshape data using metadata from hdr file

--- plantcv/plantcv/hyperspectral/read_data.py
+++ plantcv/plantcv/hyperspectral/read_data.py
@@ -30,14 +30,29 @@ def read_data(filename):
     raw_data = np.fromfile(filename, np.float32, -1)
 
     headername = filename + ".hdr"
-    header = open(headername)
 
-    # Loop through and create a dictonary from the header file
-    for i, string in enumerate(header):
+    with open(headername, "r") as f:
+        # Replace characters for easier parsing
+        hdata = f.read()
+        hdata = hdata.replace("\n,", ",")
+        hdata = hdata.replace("{\n", "{")
+        hdata = hdata.replace("\n}", "}")
+        hdata = hdata.replace(";", "")
+
+    # Loop through and create a dictionary from the header file
+    for i, string in enumerate(hdata):
         if '=' in string:
-            header_data = header[i].split(" = ")
+            header_data = hdata[i].split(" = ")
             header_dict.update({header_data[0]: header_data[1].rstrip()})
+        elif ':' in string:
+            header_data = header_data[i].split(" : ")
+            header_dict.update({header_data[0] : header_data[1].rstrip()})
 
     bands = header_dict["bands"]
+    wavelengths = header_dict["wavelength"]
 
-    return raw_data, bands
+    array_data = raw_data.reshape(header_dict["lines"],
+                                  header_dict["bands"],
+                                  header_dict["samples"]).transpose((0, 2, 1))
+
+    return array_data, header_dict
