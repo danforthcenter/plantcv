26229993e0361d35c3146ddc55acec0b4921f7fc
HaleySchuhl <haleyschuhl@gmail.com>
2019-11-22 13:58:07 -0600

    fix init file

--- plantcv/plantcv/hyperspectral/__init__.py
+++ plantcv/plantcv/hyperspectral/__init__.py
@@ -1,5 +1,5 @@
 from plantcv.plantcv.hyperspectral.read_data import _find_closest
-from plantcv.plantcv.hyperspectral._make_pseudo_rgb import _make_pseudo_rgb
+from plantcv.plantcv.hyperspectral.read_data import _make_pseudo_rgb
 from plantcv.plantcv.hyperspectral.read_data import read_data
 from plantcv.plantcv.hyperspectral.extract_index import extract_index
 from plantcv.plantcv.hyperspectral.analyze_index import analyze_index
