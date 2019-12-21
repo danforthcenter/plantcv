a592de2dbd6c77a433c2642014aec9be0047b7e7
Noah Fahlgren <noahfahlgren@gmail.com>
2019-12-21 17:50:22 -0600

    Save progress

--- plantcv/parallel/__init__.py
+++ plantcv/parallel/__init__.py
@@ -1,5 +1,11 @@
 __all__ = ["metadata_parser", "job_builder", "process_results", "multiprocess", "check_date_range"]
 
+
+class Config:
+    def __init__(self):
+        pass
+
+
 from plantcv.parallel.parsers import metadata_parser
 from plantcv.parallel.parsers import check_date_range
 from plantcv.parallel.job_builder import job_builder
