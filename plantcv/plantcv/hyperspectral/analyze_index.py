3c13ce7a4a9b288e11b04b13e3fad84ac2f3861f
HaleySchuhl <haleyschuhl@gmail.com>
2020-02-03 12:51:14 -0600

    Update analyze_index.py

--- plantcv/plantcv/hyperspectral/analyze_index.py
+++ plantcv/plantcv/hyperspectral/analyze_index.py
@@ -1,15 +1,19 @@
 # Analyze reflectance signal data in an index
 
 import os
+import cv2
 import numpy as np
+import pandas as pd
 from plantcv.plantcv import params
 from plantcv.plantcv import outputs
 from plantcv.plantcv import plot_image
 from plantcv.plantcv import print_image
 from plantcv.plantcv import fatal_error
+from plotnine import ggplot, aes, geom_line, scale_x_continuous
+from plantcv.plantcv.threshold import binary as binary_threshold
 
 
-def analyze_index(index_array, mask):
+def analyze_index(index_array, mask, histplot=False, bins=100):
     """This extracts the hyperspectral index statistics and writes the values  as observations out to
        the Outputs class.
 
@@ -28,10 +32,49 @@ def analyze_index(index_array, mask):
     if len(np.shape(index_array.array_data)) > 2:
         fatal_error("index_array data should be a grayscale image.")
 
+    # Mask data and collect statistics about pixels within the masked image
     masked_array = index_array.array_data[np.where(mask > 0)]
     index_mean = np.average(masked_array)
     index_median = np.median(masked_array)
     index_std = np.std(masked_array)
+
+    mask1 = binary_threshold(mask, 0, 255, 'light')
+    mask1 = (mask1 / 255)
+
+    # Calculate histogram
+    maxval = round(np.amax(index_array.array_data[0]), 4)
+    hist_nir = [float(l[0]) for l in cv2.calcHist([index_array.array_data], [0], mask, [bins], [-2, 2])]
+
+    # Create list of bin labels
+    bin_width = maxval / float(bins)
+    b = 0
+    bin_labels = [float(b)]
+    plotting_labels = [float(b)]
+    for i in range(bins - 1):
+        b += bin_width
+        bin_labels.append(b)
+        plotting_labels.append(round(b, 2))
+
+    # make hist percentage for plotting
+    pixels = cv2.countNonZero(mask1)
+    hist_percent = [(p / float(pixels)) * 100 for p in hist_nir]
+
+    if histplot is True:
+        hist_x = hist_percent
+        dataset = pd.DataFrame({'Index Reflectance': bin_labels,
+                                'Proportion of pixels (%)': hist_x})
+        fig_hist = (ggplot(data=dataset,
+                           mapping=aes(x='Index Reflectance',
+                                       y='Proportion of pixels (%)'))
+                    + geom_line(color='red')
+                    + scale_x_continuous(breaks=plotting_labels, labels=plotting_labels))
+
+        analysis_image = fig_hist
+        if params.debug == "print":
+            fig_hist.save(
+                os.path.join(params.debug_outdir, str(params.device) + index_array.array_type + '_index_hist.png'))
+        elif params.debug == "plot":
+            print(fig_hist)
 
     outputs.add_observation(variable='mean_' + index_array.array_type,
                             trait='Average ' + index_array.array_type + ' reflectance',
@@ -47,6 +90,10 @@ def analyze_index(index_array, mask):
                             trait='Standard deviation ' + index_array.array_type + ' reflectance',
                             method='plantcv.plantcv.hyperspectral.analyze_index', scale='reflectance', datatype=float,
                             value=index_std, label='none')
+
+    outputs.add_observation(variable='index_frequencies_' + index_array.array_type, trait='index frequencies',
+                            method='plantcv.plantcv.analyze_nir_intensity', scale='frequency', datatype=list,
+                            value=hist_nir, label=bin_labels)
 
     if params.debug == "plot":
         plot_image(masked_array)
