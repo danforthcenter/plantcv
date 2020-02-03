0e4252d91cebdb6a3b245ef9b921b6f9e2322c7f
HaleySchuhl <haleyschuhl@gmail.com>
2020-02-03 13:22:54 -0600

    store debug mode

--- plantcv/plantcv/hyperspectral/analyze_index.py
+++ plantcv/plantcv/hyperspectral/analyze_index.py
@@ -2,6 +2,7 @@
 
 import os
 import cv2
+import math
 import numpy as np
 import pandas as pd
 from plantcv.plantcv import params
@@ -9,8 +10,8 @@ from plantcv.plantcv import outputs
 from plantcv.plantcv import plot_image
 from plantcv.plantcv import print_image
 from plantcv.plantcv import fatal_error
-from plotnine import ggplot, aes, geom_line, scale_x_continuous
 from plantcv.plantcv.threshold import binary as binary_threshold
+from plotnine import ggplot, aes, geom_line, scale_x_continuous
 
 
 def analyze_index(index_array, mask, histplot=False, bins=100):
@@ -26,6 +27,9 @@ def analyze_index(index_array, mask, histplot=False, bins=100):
     """
     params.device += 1
 
+    debug = params.debug
+    params.debug = None
+
     if len(np.shape(mask)) > 2 or len(np.unique(mask)) > 2:
         fatal_error("Mask should be a binary image of 0 and nonzero values.")
 
@@ -37,9 +41,6 @@ def analyze_index(index_array, mask, histplot=False, bins=100):
     index_mean = np.average(masked_array)
     index_median = np.median(masked_array)
     index_std = np.std(masked_array)
-
-    mask1 = binary_threshold(mask, 0, 255, 'light')
-    mask1 = (mask1 / 255)
 
     # Calculate histogram
     maxval = round(np.amax(index_array.array_data[0]), 4)
@@ -56,8 +57,10 @@ def analyze_index(index_array, mask, histplot=False, bins=100):
         plotting_labels.append(round(b, 2))
 
     # make hist percentage for plotting
-    pixels = cv2.countNonZero(mask1)
+    pixels = cv2.countNonZero(mask)
     hist_percent = [(p / float(pixels)) * 100 for p in hist_nir]
+
+    params.debug = debug
 
     if histplot is True:
         hist_x = hist_percent
@@ -71,8 +74,7 @@ def analyze_index(index_array, mask, histplot=False, bins=100):
 
         analysis_image = fig_hist
         if params.debug == "print":
-            fig_hist.save(
-                os.path.join(params.debug_outdir, str(params.device) + index_array.array_type + '_index_hist.png'))
+            fig_hist.save(os.path.join(params.debug_outdir, str(params.device) + index_array.array_type + '_hist.png'))
         elif params.debug == "plot":
             print(fig_hist)
 
@@ -99,4 +101,4 @@ def analyze_index(index_array, mask, histplot=False, bins=100):
         plot_image(masked_array)
     elif params.debug == "print":
         print_image(img=masked_array, filename=os.path.join(params.debug_outdir, str(params.device) +
-                                                            index_array.array_type + "_index.png"))
+                                                            index_array.array_type + ".png"))
