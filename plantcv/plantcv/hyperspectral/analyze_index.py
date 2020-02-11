d42be354cba93399d88d3409d65e35a5c9bcf7c0
HaleySchuhl <haleyschuhl@gmail.com>
2020-02-11 11:21:01 -0600

    Update analyze_index.py

--- plantcv/plantcv/hyperspectral/analyze_index.py
+++ plantcv/plantcv/hyperspectral/analyze_index.py
@@ -46,19 +46,18 @@ def analyze_index(index_array, mask, histplot=False, bins=100, bin_max=None, bin
     index_std = np.std(masked_array)
 
     # Calculate histogram
-    maxval = round(np.amax(index_array.array_data[0]), 4) # Auto bins will detect maxval to use for calculating centers
-    b = 0  # Auto bins will always start from 0
-    print(np.shape(mask))
-    print(np.shape([index_array.array_data][0]))
     hist_nir = [float(l[0]) for l in
                 cv2.calcHist([index_array.array_data.astype(np.uint16)], [0], mask, [bins], [-2, 2])]
 
-    #
+    maxval = round(np.amax(index_array.array_data[0]), 4)  # Auto bins will detect maxval to use for calculating centers
+    b = 0  # Auto bins will always start from 0
+    # Overwrite starting bin and maximum value if defined
     if not bin_max == None:
         maxval = bin_max # If bin_max is defined then overwrite maxval variable
     if not bin_min == None:
         b = bin_min # If bin_min is defined then overwrite starting value
 
+    # Calculate bin labels based on centers of bins
     bin_width = (maxval - b) / float(bins)
     bin_labels = [float(b)]
     plotting_labels = [float(b)]
@@ -66,8 +65,8 @@ def analyze_index(index_array, mask, histplot=False, bins=100, bin_max=None, bin
         b += bin_width
         bin_labels.append(b)
         plotting_labels.append(round(b, 2))
-    print(plotting_labels)
-    # make hist percentage for plotting
+
+    # Make hist percentage for plotting
     pixels = cv2.countNonZero(mask)
     hist_percent = [(p / float(pixels)) * 100 for p in hist_nir]
 
