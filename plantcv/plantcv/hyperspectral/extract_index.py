23ccd3a6ba6f3646797ea6017970f09610d97fd3
HaleySchuhl <haleyschuhl@gmail.com>
2020-03-02 14:06:59 -0600

    Update extract_index.py

--- plantcv/plantcv/hyperspectral/extract_index.py
+++ plantcv/plantcv/hyperspectral/extract_index.py
@@ -109,7 +109,7 @@ def extract_index(array, index="NDVI", distance=20):
     obs_min_pixel = float(np.nanmin(index_array_raw))
 
     index_array = Spectral_data(array_data=index_array_raw, max_wavelength=0,
-                                min_wavelength=0, max_value=max_pixel, min_value=min_pixel, d_type=np.uint8,
+                                min_wavelength=0, max_value=obs_max_pixel, min_value=obs_min_pixel, d_type=np.uint8,
                                 wavelength_dict={}, samples=array.samples,
                                 lines=array.lines, interleave=array.interleave,
                                 wavelength_units=array.wavelength_units, array_type="index_" + index.lower(),
