216f186a5fd80bd3a1db150382ef9ef6fc313213
bganglia <bganglia892@gmail.com>
2020-03-03 00:47:23 -0500

    refer to ParseMatchArg class plantcv/parallel/ __init__.py

--- plantcv/parallel/__init__.py
+++ plantcv/parallel/__init__.py
@@ -5,4 +5,4 @@ from plantcv.parallel.parsers import check_date_range
 from plantcv.parallel.job_builder import job_builder
 from plantcv.parallel.process_results import process_results
 from plantcv.parallel.multiprocess import multiprocess
-from plantcv.parallel.parsers import parse_match_arg
+from plantcv.parallel.parsers import ParseMatchArg
