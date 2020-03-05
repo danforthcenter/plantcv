76bb9a3811d4d39069219ed137c837a8535c5d59
bganglia <bganglia892@gmail.com>
2020-03-05 17:37:35 -0500

    create bad input test for no-value case

--- tests/tests.py
+++ tests/tests.py
@@ -314,6 +314,12 @@ def test_plantcv_parallel_parse_match_arg_one_field_close_list():
     assert metadata_filters_are_functionally_equivalent(correct_filter, observed_filter)
 
 
+def test_plantcv_parallel_parse_match_arg_bad_input_no_value():
+    filter_as_string="camera:"
+    with pytest.raises(ValueError):
+        _ = plantcv.parallel.ParseMatchArg(filter_as_string).parse()
+
+
 def test_plantcv_parallel_metadata_parser_snapshots():
     data_dir = os.path.join(PARALLEL_TEST_DATA, TEST_SNAPSHOT_DIR)
     meta_filters = {"imgtype": "VIS"}
