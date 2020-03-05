3122eb5b571676d332c4e50953bde2d5ec795ee1
bganglia <bganglia892@gmail.com>
2020-03-05 17:46:11 -0500

    add no-key test case

--- tests/tests.py
+++ tests/tests.py
@@ -331,6 +331,14 @@ def test_plantcv_parallel_parse_match_arg_bad_input_unexpected_key():
     with pytest.raises(ValueError):
         _ = plantcv.parallel.ParseMatchArg(filter_as_string).parse()
 
+
+
+def test_plantcv_parallel_parse_match_arg_bad_input_no_key():
+    filter_as_string="camera:1,2"
+    with pytest.raises(ValueError):
+        _ = plantcv.parallel.ParseMatchArg(filter_as_string).parse()
+
+
 def test_plantcv_parallel_metadata_parser_snapshots():
     data_dir = os.path.join(PARALLEL_TEST_DATA, TEST_SNAPSHOT_DIR)
     meta_filters = {"imgtype": "VIS"}
