a52d0a12fffba4fb3bb14a14e31b7e381f765cfd
bganglia <bganglia892@gmail.com>
2020-03-05 20:00:12 -0500

    update parallel __init__.py

--- plantcv/parallel/__init__.py
+++ plantcv/parallel/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ["metadata_parser", "job_builder", "process_results", "multiprocess", "check_date_range", "parse_match_arg"]
+__all__ = ["metadata_parser", "job_builder", "process_results", "multiprocess", "check_date_range", "ParseMatchArg", ]
 
 from plantcv.parallel.parsers import metadata_parser
 from plantcv.parallel.parsers import check_date_range
@@ -6,3 +6,10 @@ from plantcv.parallel.job_builder import job_builder
 from plantcv.parallel.process_results import process_results
 from plantcv.parallel.multiprocess import multiprocess
 from plantcv.parallel.parsers import ParseMatchArg
+from plantcv.parallel.parsers import EmptyKeyError
+from plantcv.parallel.parsers import EmptyValueError
+from plantcv.parallel.parsers import UnexpectedSpecialCharacterError
+from plantcv.parallel.parsers import MissingColonError
+from plantcv.parallel.parsers import KeyValuePairInListError
+from plantcv.parallel.parsers import EmptyListError
+from plantcv.parallel.parsers import MissingCommaError
