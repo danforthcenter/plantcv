c7a53c5c8f87dea0a75feb3f8e19dc6fa495e1e7
HaleySchuhl <haleyschuhl@gmail.com>
2020-03-24 08:24:31 -0500

    Update auto_threshold_methods.py

--- plantcv/plantcv/visualize/auto_threshold_methods.py
+++ plantcv/plantcv/visualize/auto_threshold_methods.py
@@ -6,14 +6,15 @@ import numpy as np
 from plantcv.plantcv import params
 from plantcv.plantcv import resize
 from plantcv.plantcv import plot_image
+from plantcv.plantcv import print_image
 from plantcv.plantcv import fatal_error
-from plantcv.plantcv.threshold import gaussian
 from plantcv.plantcv.threshold import mean
 from plantcv.plantcv.threshold import otsu
+from plantcv.plantcv.threshold import gaussian
 from plantcv.plantcv.threshold import triangle
 
 
-def thresholds(gray_img, original_img=True, object_type="light"):
+def thresholds(gray_img, grid_img=True, object_type="light"):
     """ Visualize an RGB image in all potential colorspaces
 
     Inputs:
@@ -28,7 +29,7 @@ def thresholds(gray_img, original_img=True, object_type="light"):
     :return labeled_img: numpy.ndarray
 
     """
-
+    # Check the the image is grayscale
     if not len(np.shape(gray_img)) == 2:
         fatal_error("Input image is not grayscale!")
 
@@ -40,14 +41,16 @@ def thresholds(gray_img, original_img=True, object_type="light"):
     method_names = ["Gaussian", "Mean", "Otsu", "Triangle"]
     all_methods = []
     labeled_imgs = []
-    y = int(np.shape(gray_img)[0] )
-    x = int(np.shape(gray_img)[1] )
+
+    # Set starting location for labeling different masks
+    y = int(np.shape(gray_img)[0] / 4)
+    x = int(np.shape(gray_img)[1] / 4)
 
     # Create mask imgs from each thresholding method
     all_methods.append(gaussian(gray_img=gray_img, max_value=255, object_type=object_type))
     all_methods.append(mean(gray_img=gray_img, max_value=255, object_type=object_type))
     all_methods.append(otsu(gray_img=gray_img, max_value=255, object_type=object_type))
-    all_methods.append(triangle(gray_img=gray_img, max_value=255, object_type=object_type, xstep=10))
+    all_methods.append(triangle(gray_img=gray_img, max_value=255, object_type=object_type, xstep=1))
 
     # Plot labels of each colorspace on the corresponding img
     for i, method in enumerate(all_methods):
@@ -55,25 +58,28 @@ def thresholds(gray_img, original_img=True, object_type="light"):
         labeled = cv2.putText(img=converted_img, text=method_names[i], org=(x, y),
                               fontFace=cv2.FONT_HERSHEY_SIMPLEX,
                               fontScale=params.text_size, color=(255, 0, 255), thickness=params.text_thickness)
+        # Reset debug mode
+        params.debug = debug
+        if params.debug == "print":
+            # If debug is print, save the image to a file
+            print_image(labeled, os.path.join(params.debug_outdir, str(params.device), method + "_vis_colorspaces.png"))
+        elif params.debug == "plot":
+            # If debug is plot, print to the plotting device
+            plot_image(labeled)
         labeled_imgs.append(labeled)
 
-    # Compile images together, including a larger version of the original image
-    plotting_img = np.hstack([labeled_imgs[0], labeled_imgs[1],labeled_imgs[2], labeled_imgs[3]])
+    if grid_img:
+        # Compile images together into one
+        top_row = np.hstack([labeled_imgs[0], labeled_imgs[1]])
+        bot_row = np.hstack([labeled_imgs[2], labeled_imgs[3]])
+        plotting_img = np.vstack([top_row, bot_row])
+        plotting_img = resize(plotting_img, resize_x=.5, resize_y=.5)
 
-    # If original_img is True then also plot the original image with the rest of them
-    if original_img:
-        converted_gray_img = cv2.cvtColor(gray_img, cv2.COLOR_GRAY2RGB)
-        plotting_img = np.hstack([converted_gray_img, plotting_img])
-    plotting_img = resize(plotting_img,  resize_x=.5, resize_y=.5)
+        if params.debug == "print":
+            # If debug is print, save the image to a file
+            print_image(plotting_img, os.path.join(params.debug_outdir, str(params.device) + "_vis_colorspaces.png"))
+        elif params.debug == "plot":
+            # If debug is plot, print to the plotting device
+            plot_image(plotting_img)
 
-    # Reset debug mode
-    params.debug = debug
-
-    if params.debug == "print":
-        # If debug is print, save the image to a file
-        print_image(plotting_img, os.path.join(params.debug_outdir, str(params.device) + "_vis_colorspaces.png"))
-    elif params.debug == "plot":
-        # If debug is plot, print to the plotting device
-        plot_image(plotting_img)
-
-    return plotting_img
+    return labeled_imgs
