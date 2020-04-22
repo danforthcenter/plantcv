ab9a0589323f9c998ee281e0ef733f0656efdaa3
Hudanyun Sheng <hudanyun.sheng@gmail.com>
2020-04-22 11:54:15 -0500

    Update __init__.py

--- plantcv/plantcv/hyperspectral/__init__.py
+++ plantcv/plantcv/hyperspectral/__init__.py
@@ -2,7 +2,6 @@ from plantcv.plantcv.hyperspectral.read_data import _find_closest
 from plantcv.plantcv.hyperspectral.read_data import _make_pseudo_rgb
 from plantcv.plantcv.hyperspectral.read_data import read_data
 from plantcv.plantcv.hyperspectral.extract_wavelength import extract_wavelength
-from plantcv.plantcv.hyperspectral.extract_index import extract_index
 from plantcv.plantcv.hyperspectral.analyze_index import analyze_index
 from plantcv.plantcv.hyperspectral.analyze_spectral import analyze_spectral
 from plantcv.plantcv.hyperspectral.calibrate import calibrate
