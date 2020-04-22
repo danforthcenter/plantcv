19140f1a62e184644134c24b9dfd0f3da5fb8ca9
Hudanyun Sheng <hudanyun.sheng@gmail.com>
2020-04-22 11:54:12 -0500

    Create __init__.py

---
+++ plantcv/plantcv/spectral_index/__init__.py
@@ -0,0 +1,33 @@
+from plantcv.plantcv.spectral_index.spectral_index import ndvi
+from plantcv.plantcv.spectral_index.spectral_index import gdvi
+from plantcv.plantcv.spectral_index.spectral_index import savi
+from plantcv.plantcv.spectral_index.spectral_index import pri
+from plantcv.plantcv.spectral_index.spectral_index import aci
+from plantcv.plantcv.spectral_index.spectral_index import ari
+from plantcv.plantcv.spectral_index.spectral_index import cari
+from plantcv.plantcv.spectral_index.spectral_index import ci_rededge
+from plantcv.plantcv.spectral_index.spectral_index import cri1
+from plantcv.plantcv.spectral_index.spectral_index import cri2
+from plantcv.plantcv.spectral_index.spectral_index import evi
+from plantcv.plantcv.spectral_index.spectral_index import mari
+from plantcv.plantcv.spectral_index.spectral_index import mcari
+from plantcv.plantcv.spectral_index.spectral_index import mtci
+from plantcv.plantcv.spectral_index.spectral_index import ndre
+from plantcv.plantcv.spectral_index.spectral_index import psnd_chla
+from plantcv.plantcv.spectral_index.spectral_index import psnd_chlb
+from plantcv.plantcv.spectral_index.spectral_index import psnd_car
+from plantcv.plantcv.spectral_index.spectral_index import psri
+from plantcv.plantcv.spectral_index.spectral_index import pssr1
+from plantcv.plantcv.spectral_index.spectral_index import pssr2
+from plantcv.plantcv.spectral_index.spectral_index import pssr3
+from plantcv.plantcv.spectral_index.spectral_index import rgri
+from plantcv.plantcv.spectral_index.spectral_index import rvsi
+from plantcv.plantcv.spectral_index.spectral_index import sipi
+from plantcv.plantcv.spectral_index.spectral_index import sr
+from plantcv.plantcv.spectral_index.spectral_index import vari
+from plantcv.plantcv.spectral_index.spectral_index import vi_green
+from plantcv.plantcv.spectral_index.spectral_index import wbi
+
+
+# add new functions to end of lists
+__all__ = ["ndvi", "gdvi", "savi", "pri", "aci", "ari", "cari", "ci_rededge", "cri1", "cri2", "evi", "mari", "mcari", "mtci", "ndre", "psnd_chla", "psnd_chlb", "psri", "pssr1", "pssr2", "pssr3", "rgri", "rvsi", "sipi", "sr", "vi_green", "wbi"]
