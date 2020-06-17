064f72c4c0003e3083faf8ac502da6541b89c899
Hudanyun Sheng <hudanyun.sheng@gmail.com>
2020-06-17 15:44:48 -0500

    Update __init__.py

--- plantcv/plantcv/__init__.py
+++ plantcv/plantcv/__init__.py
@@ -85,6 +85,7 @@ from plantcv.plantcv.crop import crop
 from plantcv.plantcv.stdev_filter import stdev_filter
 from plantcv.plantcv.spatial_clustering import spatial_clustering
 from plantcv.plantcv import photosynthesis
+from plantcv.plantcv import time_series
 # add new functions to end of lists
 
 # Auto versioning
