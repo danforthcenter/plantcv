e384e71cf32022fdfbee7ae6b6192754f45d92f5
HaleySchuhl <haleyschuhl@gmail.com>
2020-06-18 13:02:48 -0500

    add height/width of color card chips

--- plantcv/plantcv/transform/color_correction.py
+++ plantcv/plantcv/transform/color_correction.py
@@ -739,8 +739,12 @@ def find_color_card(rgb_img, threshold_type='adaptgauss', threshvalue=125, blurr
     if record_chip_size is not None:
         if record_chip_size.upper() == "MEDIAN":
             chip_size = df.loc[:, "area"].median()
+            chip_height = df.loc[:, "height"].median()
+            chip_width = df.loc[:, "width"].median()
         elif record_chip_size.upper() == "MEAN":
             chip_size = df.loc[:, "area"].mean()
+            chip_height = df.loc[:, "height"].mean()
+            chip_width = df.loc[:, "width"].mean()
         else:
             print(str(record_chip_size) + " Is not a valid entry for record_chip_size." +
                   " Must be either 'mean', 'median', or None.")
@@ -749,5 +753,11 @@ def find_color_card(rgb_img, threshold_type='adaptgauss', threshvalue=125, blurr
         outputs.add_observation(variable='color_chip_size', trait='size of color card chips identified',
                                 method='plantcv.plantcv.transform.find_color_card', scale='none',
                                 datatype=float, value=chip_size, label=str(record_chip_size))
+        outputs.add_observation(variable='color_chip_height', trait='height of color card chips identified',
+                                method='plantcv.plantcv.transform.find_color_card', scale='none',
+                                datatype=float, value=chip_height, label=str(record_chip_size))
+        outputs.add_observation(variable='color_chip_width', trait='size of color card chips identified',
+                                method='plantcv.plantcv.transform.find_color_card', scale='none',
+                                datatype=float, value=chip_width, label=str(record_chip_size))
 
     return df, start_coord, spacing
