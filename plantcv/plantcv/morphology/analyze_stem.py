40d04baa158d2156116572f6b3231bb10725a1c8
HaleySchuhl <haleyschuhl@gmail.com>
2020-07-09 13:29:44 -0500

    add stem angle

--- plantcv/plantcv/morphology/analyze_stem.py
+++ plantcv/plantcv/morphology/analyze_stem.py
@@ -11,7 +11,7 @@ from plantcv.plantcv import print_image
 from plantcv.plantcv import color_palette
 
 
-def analyze_stem(stem_objects, segmented_img):
+def analyze_stem(stem_objects, segmented_img, mask=mask):
     """ Calculate angle of segments (in degrees) by fitting a linear regression line to segments.
 
         Inputs:
@@ -28,23 +28,36 @@ def analyze_stem(stem_objects, segmented_img):
         """
 
     labeled_img = np.copy(segmented_img)
+    img_x, img_y, _ = np.shape(labeled_img)
     grouped_stem = np.vstack(stem_objects)
     x, y, width, height = cv2.boundingRect(grouped_stem)
-    stem_base = x + (width/2)
 
+    x_max = img_x
+    x_min = 0
 
+    # Find line fit to each segment
+    [vx, vy, x, y] = cv2.fitLine(grouped_stem, cv2.DIST_L2, 0, 0.01, 0.01)
+    slope = -vy / vx
+    left_list = int(((x - x_min) * slope) + y)
+    right_list = int(((x - x_max) * slope) + y)
 
-    outputs.add_observation(variable='culm_height', trait='vertical length of stem segments',
+    outputs.add_observation(variable='stem_height', trait='vertical length of stem segments',
                             method='plantcv.plantcv.morphology.analyze_stem', scale='pixels', datatype=float,
                             value=height, label=None)
-
-
+    outputs.add_observation(variable='stem_angle', trait='angle of combined stem object',
+                            method='plantcv.plantcv.morphology.analyze_stem', scale='degrees', datatype=float,
+                            value=slope, label=None)
 
     if params.debug is not None:
         params.device += 1
-
-        cv2.line(labeled_img, (int(stem_base), y), (int(stem_base), y + height), (0, 255, 0), params.line_thickness)
-
+        # draw culm_height
+        cv2.line(labeled_img, (int(x), y), (int(x), y + height), (0, 255, 0), params.line_thickness)
+        # draw combined stem angle
+        if slope > 1000000 or slope < -1000000:
+            print("Slope  is ", slope, " and cannot be plotted.")
+        else:
+            # Draw slope lines
+            cv2.line(labeled_img, (x_max - 1, right_list), (x_min, left_list), (0, 0, 255), 1)
 
     # Auto-increment device
     params.device += 1
@@ -53,7 +66,5 @@ def analyze_stem(stem_objects, segmented_img):
         print_image(labeled_img, os.path.join(params.debug_outdir, str(params.device) + '_segmented_angles.png'))
     elif params.debug == 'plot':
         plot_image(labeled_img)
-
-
 
     return labeled_img
