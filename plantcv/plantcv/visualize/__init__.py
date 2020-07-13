1757dafc97fdb225142af1b164200118a8edaffe
Hudanyun Sheng <hudanyun.sheng@gmail.com>
2020-07-13 10:28:25 -0500

    Update __init__.py
    for the newly added time_lapse_video into visualize subpackage

--- plantcv/plantcv/visualize/__init__.py
+++ plantcv/plantcv/visualize/__init__.py
@@ -4,5 +4,7 @@ from plantcv.plantcv.visualize.histogram import histogram
 from plantcv.plantcv.visualize.clustered_contours import clustered_contours
 from plantcv.plantcv.visualize.colorspaces import colorspaces
 from plantcv.plantcv.visualize.auto_threshold_methods import auto_threshold_methods
+from plantcv.plantcv.visualize.time_lapse_video import time_lapse_video
+from plantcv.plantcv.visualize.time_lapse_video import _resize_img
 
-__all__ = ["pseudocolor", "colorize_masks", "histogram", "clustered_contours", "colorspaces", "auto_threshold_methods"]
+__all__ = ["pseudocolor", "colorize_masks", "histogram", "clustered_contours", "colorspaces", "auto_threshold_methods", "time_lapse_video", "_resize_img"]
