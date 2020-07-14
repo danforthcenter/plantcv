d2b0077caccfbcadd5adbcd24a8cfd5146093e78
HaleySchuhl <haleyschuhl@gmail.com>
2020-07-14 11:06:11 -0500

    Update __init__.py

--- plantcv/plantcv/morphology/__init__.py
+++ plantcv/plantcv/morphology/__init__.py
@@ -14,7 +14,8 @@ from plantcv.plantcv.morphology.segment_tangent_angle import segment_tangent_ang
 from plantcv.plantcv.morphology.segment_id import segment_id
 from plantcv.plantcv.morphology.segment_insertion_angle import segment_insertion_angle
 from plantcv.plantcv.morphology.segment_combine import segment_combine
+from plantcv.plantcv.morphology.analyze_stem import analyze_stem
 
 __all__ = ["find_branch_pts", "find_tips", "prune", "skeletonize", "check_cycles", "segment_skeleton", "segment_angle",
            "segment_path_length", "segment_euclidean_length", "segment_curvature", "segment_sort", "segment_id",
-           "segment_tangent_angle", "segment_insertion_angle", "segment_combine", "_iterative_prune"]
+           "segment_tangent_angle", "segment_insertion_angle", "segment_combine", "_iterative_prune", "analyze_stem"]
