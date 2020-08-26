4e5f13c05cadb5451531c773c418e01eb7927603
JorgeGtz <jorge.gutierrezor@gmail.com>
2020-08-26 16:59:40 -0500

    update init file with fill segments function

--- plantcv/plantcv/morphology/__init__.py
+++ plantcv/plantcv/morphology/__init__.py
@@ -15,7 +15,8 @@ from plantcv.plantcv.morphology.segment_id import segment_id
 from plantcv.plantcv.morphology.segment_insertion_angle import segment_insertion_angle
 from plantcv.plantcv.morphology.segment_combine import segment_combine
 from plantcv.plantcv.morphology.analyze_stem import analyze_stem
+from plantcv.plantcv.morphology.fill_segments import fill_segments
 
 __all__ = ["find_branch_pts", "find_tips", "prune", "skeletonize", "check_cycles", "segment_skeleton", "segment_angle",
            "segment_path_length", "segment_euclidean_length", "segment_curvature", "segment_sort", "segment_id",
-           "segment_tangent_angle", "segment_insertion_angle", "segment_combine", "_iterative_prune", "analyze_stem"]
+           "segment_tangent_angle", "segment_insertion_angle", "segment_combine", "_iterative_prune", "analyze_stem", "fill_segments"]
