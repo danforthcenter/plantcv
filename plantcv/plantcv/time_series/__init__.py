72f59b73ff78737ff19f6f98a9912b6ee106406b
Hudanyun Sheng <hudanyun.sheng@gmail.com>
2020-08-26 07:24:13 -0500

    Update __init__.py

--- plantcv/plantcv/time_series/__init__.py
+++ plantcv/plantcv/time_series/__init__.py
@@ -3,11 +3,10 @@ from plantcv.plantcv.time_series.link_utilities import _apply_mask
 from plantcv.plantcv.time_series.link_utilities import display_instances
 from plantcv.plantcv.time_series.link_utilities import _compute_overlaps_masks
 from plantcv.plantcv.time_series.link_utilities import get_ax
-from plantcv.plantcv.time_series.link_utilities import _getmatchedIndex
 from plantcv.plantcv.time_series.link_utilities import PlantData
 from plantcv.plantcv.time_series.time_series import time_series_linking
 
 
 # add new functions to end of lists
 __all__ = ["_random_colors", "_apply_mask", "display_instances", "_compute_overlaps_masks", "get_ax",
-           "_getmatchedIndex", "PlantData", "time_series_linking"]
+            "PlantData", "time_series_linking"]
