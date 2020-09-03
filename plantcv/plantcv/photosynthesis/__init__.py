334097ad9ec31ca5c04459fd34f08fdf87a8c3b4
HaleySchuhl <haleyschuhl@gmail.com>
2020-09-03 10:41:20 -0500

    Update __init__.py

--- plantcv/plantcv/photosynthesis/__init__.py
+++ plantcv/plantcv/photosynthesis/__init__.py
@@ -1,4 +1,5 @@
 from plantcv.plantcv.photosynthesis.read_dat import read_dat
+from plantcv.plantcv.photosynthesis.analyze_fvfm import analyze_fvfm
 
 
-__all__ = ["read_dat"]
+__all__ = ["read_dat", "analyze_fvfm"]
