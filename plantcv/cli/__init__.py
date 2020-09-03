a3c01eb52d27d04505628e975166a95b098069e0
Dominik Schneider <dominik.schneider@wsu.edu>
2020-09-03 10:42:38 -0700

    rename subpackage and modules

---
+++ plantcv/cli/__init__.py
@@ -0,0 +1,3 @@
+from plantcv.cli import workflow
+from plantcv.cli import train
+from plantcv.cli import utils
--- plantcv/commandline/__init__.py
+++
@@ -1,3 +0,0 @@
-from plantcv.commandline import plantcvworkflow
-from plantcv.commandline import plantcvtrain
-from plantcv.commandline import plantcvutils
