c8faaf60ac3e339e3410cc05ac79398179f1d0b5
Hudanyun Sheng <hudanyun.sheng@gmail.com>
2020-09-28 15:27:36 -0500

    Update __init__.py

--- plantcv/plantcv/time_series/__init__.py
+++ plantcv/plantcv/time_series/__init__.py
@@ -1,14 +1,12 @@
-# from plantcv.plantcv.time_series.time_series_linking import _random_colors
-# from plantcv.plantcv.time_series.time_series_linking import _apply_mask
-# from plantcv.plantcv.time_series.time_series_linking import _get_ax
-# from plantcv.plantcv.time_series.time_series_linking import _display_instances
-# from plantcv.plantcv.time_series.time_series_linking import _compute_overlaps_masks
+from plantcv.plantcv.time_series.time_series_linking import _random_colors
+from plantcv.plantcv.time_series.time_series_linking import _apply_mask
+from plantcv.plantcv.time_series.time_series_linking import _get_ax
+from plantcv.plantcv.time_series.time_series_linking import _display_instances
+from plantcv.plantcv.time_series.time_series_linking import _compute_overlaps_masks
 from plantcv.plantcv.time_series.time_series_linking import InstanceTimeSeriesLinking
 from plantcv.plantcv.time_series.time_series_linking import InstanceTSLinkingWrapper
 
 
 # add new functions to end of lists
-# __all__ = ["_random_colors", "_apply_mask", "_get_ax", "_display_instances", "_compute_overlaps_masks",
-#             "InstanceTimeSeriesLinking", "InstanceTSLinkingWrapper"]
-
-__all__ = ["InstanceTimeSeriesLinking", "InstanceTSLinkingWrapper"]
+__all__ = ["_random_colors", "_apply_mask", "_get_ax", "_display_instances", "_compute_overlaps_masks",
+            "InstanceTimeSeriesLinking", "InstanceTSLinkingWrapper"]
