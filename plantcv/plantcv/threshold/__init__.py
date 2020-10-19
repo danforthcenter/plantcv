8207c06e6746155c5582153a2a62c09fdc097944
Hudanyun Sheng <hudanyun.sheng@gmail.com>
2020-10-19 12:57:39 -0500

    Update __init__.py

--- plantcv/plantcv/threshold/__init__.py
+++ plantcv/plantcv/threshold/__init__.py
@@ -8,4 +8,4 @@ from plantcv.plantcv.threshold.threshold_methods import custom_range
 from plantcv.plantcv.threshold.threshold_methods import saturation
 from plantcv.plantcv.threshold.threshold_methods import mask_bad
 
-__all__ = ["binary", "gaussian", "mean", "otsu", "triangle", "texture", "custom_range", "saturation"]
+__all__ = ["binary", "gaussian", "mean", "otsu", "triangle", "texture", "custom_range", "saturation","mask_bad"]
