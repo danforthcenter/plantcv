615e755c4cf64f59f6ba23d141b6e828d9b0a584
Hudanyun Sheng <hudanyun.sheng@gmail.com>
2020-10-20 11:16:24 -0500

    Update __init__.py
    Exclude importing legacy _resize_img

--- plantcv/plantcv/visualize/__init__.py
+++ plantcv/plantcv/visualize/__init__.py
@@ -4,7 +4,6 @@ from plantcv.plantcv.visualize.histogram import histogram
 from plantcv.plantcv.visualize.clustered_contours import clustered_contours
 from plantcv.plantcv.visualize.colorspaces import colorspaces
 from plantcv.plantcv.visualize.auto_threshold_methods import auto_threshold_methods
-from plantcv.plantcv.visualize.overlay_two_imgs import _resize_img
 from plantcv.plantcv.visualize.overlay_two_imgs import overlay_two_imgs
 
-__all__ = ["pseudocolor", "colorize_masks", "histogram", "clustered_contours", "colorspaces", "auto_threshold_methods", "_resize_img", "overlay_two_imgs"]
+__all__ = ["pseudocolor", "colorize_masks", "histogram", "clustered_contours", "colorspaces", "auto_threshold_methods", "overlay_two_imgs"]
