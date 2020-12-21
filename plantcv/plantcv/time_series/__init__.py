85cd7617766059a0f5c0c7455f60be114f16a7d2
Hudanyun Sheng <hudanyun.sheng@gmail.com>
2020-12-21 11:07:36 -0600

    Update __init__.py

--- plantcv/plantcv/time_series/__init__.py
+++ plantcv/plantcv/time_series/__init__.py
@@ -3,6 +3,10 @@ from plantcv.plantcv.time_series.time_series_linking import _get_emergence
 from plantcv.plantcv.time_series.time_series_linking import _get_ti
 from plantcv.plantcv.time_series.time_series_linking import _compute_overlaps_masks
 from plantcv.plantcv.time_series.time_series_linking import InstanceTimeSeriesLinking
+from plantcv.plantcv.time_series.evaluation import evaluate_link
+from plantcv.plantcv.time_series.evaluation import mismatch_rate
+from plantcv.plantcv.time_series.evaluation import confusion
+from plantcv.plantcv.time_series.evaluation import get_scores
 
 # add new functions to end of lists
-__all__ = ["_get_link", "_get_emergence", "_get_ti", "_compute_overlaps_masks", "InstanceTimeSeriesLinking"]
+__all__ = ["_get_link", "_get_emergence", "_get_ti", "_compute_overlaps_masks", "InstanceTimeSeriesLinking", "evaluate_link", "mismatch_rate", "confusion", "get_scores"]
