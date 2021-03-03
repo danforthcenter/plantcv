d299e64fcbf9424ef8737457fe7588b33d6e3350
Hudanyun Sheng <hudanyun.sheng@gmail.com>
2021-03-03 16:57:19 -0600

    Update analyze_nir_intensity.py
    based on updated pcv.visualize.histogram

--- plantcv/plantcv/analyze_nir_intensity.py
+++ plantcv/plantcv/analyze_nir_intensity.py
@@ -1,16 +1,14 @@
-# Analyze signal data in NIR image
-
 import os
 import cv2
 import numpy as np
 import pandas as pd
-from plotnine import ggplot, aes, geom_line, scale_x_continuous
-from plantcv.plantcv import print_image
-from plantcv.plantcv import plot_image
-from plantcv.plantcv.threshold import binary as binary_threshold
+from scipy import stats
+from plotnine import ggplot, aes, geom_line, scale_x_continuous, scale_color_manual, labs
+from plantcv.plantcv import fatal_error
 from plantcv.plantcv import params
 from plantcv.plantcv import outputs
 from plantcv.plantcv.visualize import histogram
+
 
 def analyze_nir_intensity(gray_img, mask, bins=256, histplot=False, label="default"):
     """This function calculates the intensity of each pixel associated with the plant and writes the values out to
@@ -33,6 +31,8 @@ def analyze_nir_intensity(gray_img, mask, bins=256, histplot=False, label="defau
     :param label: str
     :return analysis_images: plotnine ggplot
     """
+    params.device += 1
+    debug = params.debug
 
     # calculate histogram
     if gray_img.dtype == 'uint16':
@@ -49,12 +49,14 @@ def analyze_nir_intensity(gray_img, mask, bins=256, histplot=False, label="defau
     rgbimg = cv2.cvtColor(gray_img, cv2.COLOR_GRAY2BGR)
 
     # Calculate histogram
+    params.debug = None
     fig_hist, hist_data = histogram(gray_img, mask=mask, bins=bins, lower_bound=0, upper_bound=maxval, title=None)
 
-    bin_labels, hist_nir, hist_percent = hist_data['pixel intensity'].tolist(), hist_data['hist_count'].tolist(), \
-                                         hist_data['proportion of pixels (%)'].tolist()
+    bin_labels, hist_nir, hist_percent = hist_data["pixel intensity"].tolist(), hist_data['hist_count'].tolist(), \
+                                         hist_data["proportion of pixels (%)"].tolist()
 
     masked1 = cv2.bitwise_and(rgbimg, rgbimg, mask=mask)
+    params.debug = debug
     if params.debug is not None:
         params.device += 1
         if params.debug == "print":
@@ -65,10 +67,12 @@ def analyze_nir_intensity(gray_img, mask, bins=256, histplot=False, label="defau
     analysis_image = None
 
     if histplot:
+        fig_hist = fig_hist + labs(x="Grayscale pixel intensity (0-{})".format(maxval), y="Proportion of pixels (%)")
         if params.debug == "print":
             fig_hist.save(os.path.join(params.debug_outdir, str(params.device) + '_nir_hist.png'), verbose=False)
         elif params.debug == "plot":
             print(fig_hist)
+        analysis_image = fig_hist
 
     outputs.add_observation(sample=label, variable='nir_frequencies', trait='near-infrared frequencies',
                             method='plantcv.plantcv.analyze_nir_intensity', scale='frequency', datatype=list,
@@ -87,3 +91,4 @@ def analyze_nir_intensity(gray_img, mask, bins=256, histplot=False, label="defau
     outputs.images.append(analysis_image)
 
     return analysis_image
+
