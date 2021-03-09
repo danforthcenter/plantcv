cced615aaa97ce7dc544602622d90e1fc2e52791
HaleySchuhl <haleyschuhl@gmail.com>
2021-03-09 15:39:22 -0600

    Update read_cropreporter.py
    switch strategies, use INF file as input and find all image data that corresponds

--- plantcv/plantcv/photosynthesis/read_cropreporter.py
+++ plantcv/plantcv/photosynthesis/read_cropreporter.py
@@ -7,31 +7,22 @@ from plantcv.plantcv.plot_image import plot_image
 from plantcv.plantcv.print_image import print_image
 
 
-def read_cropreporter(filename):
+def read_cropreporter(inf_filename):
     """Read in, reshape, and subset a datacube of fluorescence snapshots
 
         Inputs:
-            filename        = Fluorescence .DAT filename
+            inf_filename     = Fluorescence .INF filename
 
         Returns:
             fdark            = fdark (F0) image
             fmin             = fmin image
             fmax             = fmax image
 
-        :param filename: str
         :param inf_filename: str
         :return fdark: numpy.ndarray
         :return fmin: numpy.ndarray
         :return fmax: numpy.ndarray
         """
-    # Find .INF filename based on the .DAT filename
-    img_filename = filename.split("/")[-1]
-    filename_components = img_filename.split("_")
-    filename_components[1] = "HDR"
-    s = "_"
-    inf_filename = s.join(filename_components)
-    inf_filename = inf_filename.replace(".DAT", ".INF")
-
     # Parse .inf file and create dictionary with metadata stored within
     with open(inf_filename, "r") as f:
         # Replace characters for easier parsing
@@ -56,33 +47,56 @@ def read_cropreporter(filename):
     x = int(inf_dict["ImageCols"])
     y = int(inf_dict["ImageRows"])
 
-    # Dump and reshape raw data so that the z dimension is the number of frames
-    raw_data = np.fromfile(filename, np.uint16, -1)
-    img_cube = raw_data.reshape(int(len(raw_data) / (y * x)), x, y).transpose((2, 1, 0))
+    # Record a dictonary of all measurements done, 0 means not done, 1 means measurement done
+    frames_captured = {key: value for key, value in inf_dict.items() if "Done" in key}
+    # Record a list of measurements
+    frames_expected = [key.upper()[0:3] for key, value in frames_captured.items() if str(value) == "1"]
+    # Define dictionary of measurement name and the corresponding binary image filename
+    corresponding_dict = {"FVF": "PSD", "FQF": "PSL", "CHL": "CHL", "NPQ": "NPQ", "SPC": "SPC",
+                          "CLR": "CLR", "RFD": "RFD", "GFP": "GFP", "RFP": "RFP"}
+    all_imgs = {}
+    # Loop over all expected binary image files
+    for key in frames_expected:
+        inf = os.path.split(inf_filename)[-1]
+        path = os.path.dirname(inf_filename)
+        filename_components = inf.split("_")
+        filename_components[1] = corresponding_dict[key]
 
-    # Extract fdark and fmin from the datacube of stacked frames
-    fdark = img_cube[:, :, [0]]
-    fdark = np.transpose(np.transpose(fdark)[0])  # Reshape frame from (x,y,1) to (x,y)
-    fmin = img_cube[:, :, [1]]
-    fmin = np.transpose(np.transpose(fmin)[0])
+        s = "_"
+        bin_filename = s.join(filename_components)
+        bin_filename = bin_filename.replace(".INF", ".DAT")
+        bin_file = os.path.join(path, bin_filename)
+        # Read in binary image file
+        raw_data = np.fromfile(bin_file, np.uint16, -1)
+        # Reshape into a datacube
+        img_cube = raw_data.reshape(int(len(raw_data) / (y * x)), x, y).transpose((2, 1, 0))
+        # Append the image cube to a dictonary with all images
+        all_imgs[corresponding_dict[key]] = img_cube
 
-    # Identify fmax frame
-    i = 0
-    max_sum = 0
-    max_index = 1
-
-    frame_sums = []
-    for i in range(img_cube.shape[2]):
-        frame_sums.append(np.sum(img_cube[:, :, i]))
-    fmax = img_cube[:, :, np.argmax(frame_sums)]
-
-    if params.debug == "print":
-        print_image(fdark, os.path.join(params.debug_outdir, str(params.device) + "fdark_frame.png"))
-        print_image(fmin, os.path.join(params.debug_outdir, str(params.device) + "fmin_frame.png"))
-        print_image(fmax, os.path.join(params.debug_outdir, str(params.device) + "fmax_frame.png"))
-    elif params.debug == "plot":
-        plot_image(fdark)
-        plot_image(fmin)
-        plot_image(fmax)
-
-    return fdark, fmin, fmax
+# NEEDS UPDATING, plan to use x-array objects to store all frames as a single thing that can be input into analysis fxns 
+    # # Extract fdark and fmin from the datacube of stacked frames
+    # fdark = img_cube[:, :, [0]]
+    # fdark = np.transpose(np.transpose(fdark)[0])  # Reshape frame from (x,y,1) to (x,y)
+    # fmin = img_cube[:, :, [1]]
+    # fmin = np.transpose(np.transpose(fmin)[0])
+    #
+    # # Identify fmax frame
+    # i = 0
+    # max_sum = 0
+    # max_index = 1
+    #
+    # frame_sums = []
+    # for i in range(img_cube.shape[2]):
+    #     frame_sums.append(np.sum(img_cube[:, :, i]))
+    # fmax = img_cube[:, :, np.argmax(frame_sums)]
+    #
+    # if params.debug == "print":
+    #     print_image(fdark, os.path.join(params.debug_outdir, str(params.device) + "fdark_frame.png"))
+    #     print_image(fmin, os.path.join(params.debug_outdir, str(params.device) + "fmin_frame.png"))
+    #     print_image(fmax, os.path.join(params.debug_outdir, str(params.device) + "fmax_frame.png"))
+    # elif params.debug == "plot":
+    #     plot_image(fdark)
+    #     plot_image(fmin)
+    #     plot_image(fmax)
+    #
+    # return fdark, fmin, fmax
