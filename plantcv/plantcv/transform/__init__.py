9a1e37f60ee5bfb9e5488183162ac11f9efec82f
Hudanyun Sheng <hudanyun.sheng@gmail.com>
2021-03-19 14:45:01 -0500

    Update __init__.py

--- plantcv/plantcv/transform/__init__.py
+++ plantcv/plantcv/transform/__init__.py
@@ -13,9 +13,8 @@ from plantcv.plantcv.transform.rotate import rotate
 from plantcv.plantcv.transform.nonuniform_illumination import nonuniform_illumination
 from plantcv.plantcv.transform.resize import resize, resize_factor
 from plantcv.plantcv.transform.warp import warp_align
-from plantcv.plantcv.transform.img_registration import ImageRegistrator
 
 __all__ = ["get_color_matrix", "get_matrix_m", "calc_transformation_matrix", "apply_transformation_matrix",
            "save_matrix", "load_matrix", "correct_color", "create_color_card_mask", "quick_color_check",
            "find_color_card", "rescale", "nonuniform_illumination", "resize", "resize_factor",
-           "warp", "rotate", "warp_align", "ImageRegistrator"]
+           "warp", "rotate", "warp_align"]
