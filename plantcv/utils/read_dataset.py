c29dd10a52f8d89ea5adfe6ffb9da7b35d113f97
JorgeGtz <jorge.gutierrezor@gmail.com>
2021-03-24 17:07:36 -0500

    add pattern matching to read dataset function

--- plantcv/utils/read_dataset.py
+++ plantcv/utils/read_dataset.py
@@ -1,7 +1,8 @@
 import os
+import re
 from plantcv.plantcv import fatal_error
 
-def read_dataset(source_path):
+def read_dataset(source_path, pattern=''):
     if not os.path.exists(source_path):
         raise IOError("Directory does not exist: {0}".format(source_path))
 
@@ -11,9 +12,11 @@ def read_dataset(source_path):
 
     for root, dirs, files in os.walk(source_path):
         for file in files:
-            # Check file type so that only images get selected
-            name, ext = os.path.splitext(file)
-            if ext.lower() in img_extensions:
-                img_path_list.append(os.path.join(root,file))
+            # Look for images that contain [pattern] in the name
+            if re.search(pattern,file):
+                # Check file type so that only images get selected
+                name, ext = os.path.splitext(file)
+                if ext.lower() in img_extensions:
+                    img_path_list.append(os.path.join(root,file))
 
     return img_path_list
