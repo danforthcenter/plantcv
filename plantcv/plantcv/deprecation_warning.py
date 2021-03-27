9ca85c6e232f650bc359689757ac543dc79226d5
nfahlgren <noahfahlgren@gmail.com>
2021-03-27 13:19:03 -0500

    Minor formatting changes

--- plantcv/plantcv/deprecation_warning.py
+++ plantcv/plantcv/deprecation_warning.py
@@ -4,14 +4,18 @@ import warnings
 from plantcv.plantcv import _version
 # from plantcv.plantcv import __version__
 
+
 def deprecation_warning(warning):
     """Print out deprecation warning
 
-    :param warning: warning message text
-    :return:
+    Inputs:
+    warning = warning message text
+
+    :param warning: str
     """
     version = _version.get_versions()
     # f string
-    # warning_msg = "{} \nCurrent PlantCV version: {} released on {}".format(warning, version["version"], version['date'])
+    # warning_msg = "{} \nCurrent PlantCV version: {} released on {}".format(warning, version["version"],
+    # version['date'])
     warning_msg = f"{warning} Current PlantCV version: {version['version']} released on {version['date']}"
     warnings.warn(warning_msg, DeprecationWarning, stacklevel=2)
