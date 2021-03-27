7949f4cb579de11dfe6cf1dea807af91db09c93d
nfahlgren <noahfahlgren@gmail.com>
2021-03-27 10:14:13 -0500

    No need to import private function

--- plantcv/plantcv/visualize/__init__.py
+++ plantcv/plantcv/visualize/__init__.py
@@ -1,11 +1,10 @@
 from plantcv.plantcv.visualize.pseudocolor import pseudocolor
 from plantcv.plantcv.visualize.colorize_masks import colorize_masks
 from plantcv.plantcv.visualize.histogram import histogram
-from plantcv.plantcv.visualize.histogram import _hist_gray
 from plantcv.plantcv.visualize.clustered_contours import clustered_contours
 from plantcv.plantcv.visualize.colorspaces import colorspaces
 from plantcv.plantcv.visualize.auto_threshold_methods import auto_threshold_methods
 from plantcv.plantcv.visualize.overlay_two_imgs import overlay_two_imgs
 
-__all__ = ["pseudocolor", "colorize_masks", "histogram", "_hist_gray", "clustered_contours", "colorspaces", "auto_threshold_methods",
+__all__ = ["pseudocolor", "colorize_masks", "histogram", "clustered_contours", "colorspaces", "auto_threshold_methods",
            "overlay_two_imgs"]
