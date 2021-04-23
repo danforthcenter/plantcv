e6d1bb08461424642287b7252fe03ce717783bfa
Hudanyun Sheng <hudanyun.sheng@gmail.com>
2021-04-23 15:06:43 -0500

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -6341,6 +6341,41 @@ def test_plantcv_visualize_overlay_two_imgs_size_mismatch():
         _ = pcv.visualize.overlay_two_imgs(img1=img1, img2=img2)
 
 
+def test_plantcv_visualize_click_count():
+    pcv.params.debug = None
+    # generate fake testing image
+    temp = pcv.transform.rescale(np.random.rand(5,5))
+    img  = np.stack((temp,)*3, axis=-1)
+    counter = pcv.visualize.ClickCount(img, figsize=(12, 6))
+    assert len(counter.count) == 0
+
+    # import coordinates
+    counter.import_coords([(0,0)], label="total")
+    counter.import_coords([(0,1)], label="c1")
+    counter.import_coords([(0,2)], label="total")
+    assert len(counter.count) == 2
+
+    # view different classes
+    counter.view()
+    counter.view(label="c1", color="r", view_all=True)
+    counter.view(label="c2", color="r", view_all=True)
+    counter.view(label="c2", color="k", view_all=True)
+    assert len(counter.points) == 3
+
+    # create mock events
+    # left click
+    e1 = matplotlib.backend_bases.MouseEvent(name="button_press_event", canvas=counter.fig.canvas, x=0, y=0, button=1)
+    e1.xdata = 0
+    e1.ydata = 0
+
+    # right click
+    e1_ = matplotlib.backend_bases.MouseEvent(name="button_press_event", canvas=counter.fig.canvas, x=0, y=0, button=3)
+    e1_.xdata = 0
+    e1_.ydata = 0
+    counter.onclick(e1)
+    counter.onclick(e1_)
+    assert len(counter.events) == 2
+
 # ##############################
 # Tests for the utils subpackage
 # ##############################
