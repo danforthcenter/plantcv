52776eed3c5d0741d1423ccb44bc6e77f43ecdc4
Hudanyun Sheng <hudanyun.sheng@gmail.com>
2021-04-26 09:24:39 -0500

    Update __init__.py

--- plantcv/plantcv/visualize/__init__.py
+++ plantcv/plantcv/visualize/__init__.py
@@ -5,6 +5,7 @@ from plantcv.plantcv.visualize.clustered_contours import clustered_contours
 from plantcv.plantcv.visualize.colorspaces import colorspaces
 from plantcv.plantcv.visualize.auto_threshold_methods import auto_threshold_methods
 from plantcv.plantcv.visualize.overlay_two_imgs import overlay_two_imgs
+from plantcv.plantcv.visualize.time_lapse_video import time_lapse_video
 
 __all__ = ["pseudocolor", "colorize_masks", "histogram", "clustered_contours", "colorspaces", "auto_threshold_methods",
-           "overlay_two_imgs"]
+           "overlay_two_imgs", "time_lapse_video"]
