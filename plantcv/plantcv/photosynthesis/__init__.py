cd3e7f8d398a5aa2470154613f20716b0d306442
HaleySchuhl <haleyschuhl@gmail.com>
2021-04-29 10:55:04 -0500

    Update __init__.py

--- plantcv/plantcv/photosynthesis/__init__.py
+++ plantcv/plantcv/photosynthesis/__init__.py
@@ -1,6 +1,7 @@
 from plantcv.plantcv.photosynthesis.read_cropreporter import read_cropreporter
 from plantcv.plantcv.photosynthesis.analyze_fvfm import analyze_fvfm
+from plantcv.plantcv.photosynthesis.analyze_npq import _calc_npq
 from plantcv.plantcv.photosynthesis.analyze_npq import analyze_npq
 
 
-__all__ = ["read_cropreporter", "analyze_fvfm", "analyze_npq"]
+__all__ = ["read_cropreporter", "analyze_fvfm", "_calc_npq", "analyze_npq"]
