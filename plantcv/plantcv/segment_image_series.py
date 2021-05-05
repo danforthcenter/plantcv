201c5abeaeac6292afe7ec96ddf8059c18779324
JorgeGtz <jorge.gutierrezor@gmail.com>
2021-05-05 16:24:15 -0500

    clean segment_image_series

--- plantcv/plantcv/segment_image_series.py
+++ plantcv/plantcv/segment_image_series.py
@@ -3,23 +3,25 @@ import numpy as np
 import cv2 as cv
 from scipy import ndimage as ndi
 from skimage.segmentation import watershed
-from skimage.measure import label
+#from skimage.measure import label
 
 from plantcv import plantcv as pcv
-from plantcv.plantcv import fatal_error
+#from plantcv.plantcv import fatal_error
 from plantcv.plantcv import color_palette
 from plantcv.plantcv import params
 from plantcv.plantcv._debug import _debug
 
+
 # to change for colorize_label_img once it's merged
-def _labels2rgb(labels,n_labels,rgb_values):
+def _labels2rgb(labels, n_labels, rgb_values):
 
     h,w = labels.shape
     rgb_img = np.zeros((h,w,3), dtype=np.uint8)
     for l in range(n_labels):
-        rgb_img[labels==l+1] = rgb_values[l]
+        rgb_img[labels == l+1] = rgb_values[l]
 
     return rgb_img
+
 
 def segment_image_series(img_dir, mask_dir, init_frame, init_labels=None, ksize=1):
     """
@@ -47,7 +49,6 @@ def segment_image_series(img_dir, mask_dir, init_frame, init_labels=None, ksize=
 
     image_names = sorted(os.listdir(img_dir))
 
-
     if init_labels is None:
         #get initialization labels
         #init_img, _, _ = pcv.readimage(filename=img_dir+image_names[init_frame])
@@ -68,7 +69,6 @@ def segment_image_series(img_dir, mask_dir, init_frame, init_labels=None, ksize=
     # propagate labels sequentially from init_frame
     # backward
     for n in range(init_frame,-1,-1):
-        print(n)
         # build image and mask stacks
         d = 2*ksize+1
         img_stack = np.zeros((h,w,d))
@@ -83,7 +83,7 @@ def segment_image_series(img_dir, mask_dir, init_frame, init_labels=None, ksize=
             if m == 0:
                 img_n_rgb = img
             mask, _, _ = pcv.readimage(filename=mask_dir+image_names[frame][:-4]+'_mask.png')
-            img_stack[:,:,stack_idx] =  pcv.rgb2gray(rgb_img=img)
+            img_stack[:,:,stack_idx] = pcv.rgb2gray(rgb_img=img)
             mask_stack[:,:,stack_idx] = mask
             markers[:,:,stack_idx] = out_labels[:,:,frame]
             stack_idx += -1
@@ -91,11 +91,11 @@ def segment_image_series(img_dir, mask_dir, init_frame, init_labels=None, ksize=
         # edges using 3D sobel operator as elevation map for watershed
         edges = ndi.generic_gradient_magnitude(img_stack, ndi.sobel)
         # segmentation using the watershed algorithm
-        labels = watershed(edges, markers=markers, mask=mask_stack,compactness=0)
+        labels = watershed(edges, markers=markers, mask=mask_stack, compactness=0)
         out_labels[:,:,n] = labels[:,:,ksize]
 
         # Create images for plotting and printing (debug mode)
-        rgb_seg = _labels2rgb(out_labels[:,:,n],n_labels,rgb_values)
+        rgb_seg = _labels2rgb(out_labels[:,:,n], n_labels, rgb_values)
         vis_seg = cv.addWeighted(img_n_rgb, 0.7, rgb_seg, 0.3, 0.0)
         params.debug = debug
         _debug(visual=vis_seg, filename=os.path.join(params.debug_outdir,
@@ -103,10 +103,8 @@ def segment_image_series(img_dir, mask_dir, init_frame, init_labels=None, ksize=
                                                      image_names[n][:-4] + '_WSeg.png'))
         params.debug = None
 
-
     # forward
     for n in range(init_frame+1,N):
-        print(n)
         # build image and mask stacks
         d = 2*ksize+1
         img_stack = np.zeros((h,w,d))
@@ -123,7 +121,7 @@ def segment_image_series(img_dir, mask_dir, init_frame, init_labels=None, ksize=
             if m == 0:
                 img_n_rgb = img
             mask, _, _ = pcv.readimage(filename=mask_dir+image_names[frame][:-4]+'_mask.png')
-            img_stack[:,:,stack_idx] =  pcv.rgb2gray(rgb_img=img)
+            img_stack[:,:,stack_idx] = pcv.rgb2gray(rgb_img=img)
             mask_stack[:,:,stack_idx] = mask
             markers[:,:,stack_idx] = out_labels[:,:,frame]
             stack_idx += 1
@@ -131,17 +129,16 @@ def segment_image_series(img_dir, mask_dir, init_frame, init_labels=None, ksize=
         # edges using 3D sobel operator as elevation map for watershed
         edges = ndi.generic_gradient_magnitude(img_stack, ndi.sobel)
         # segmentation using the watershed algorithm
-        labels = watershed(edges, markers=markers, mask=mask_stack,compactness=0)
+        labels = watershed(edges, markers=markers, mask=mask_stack, compactness=0)
         out_labels[:,:,n] = labels[:,:,ksize]
 
         # Create images for plotting and printing (debug mode)
-        rgb_seg = _labels2rgb(out_labels[:,:,n],n_labels,rgb_values)
+        rgb_seg = _labels2rgb(out_labels[:,:,n], n_labels, rgb_values)
         vis_seg = cv.addWeighted(img_n_rgb, 0.7, rgb_seg, 0.3, 0.0)
         params.debug = debug
         _debug(visual=vis_seg, filename=os.path.join(params.debug_outdir,
                                                      str(params.device) + '_' +
                                                      image_names[n][:-4] + '_WSeg.png'))
         params.debug = None
-
 
     return out_labels
