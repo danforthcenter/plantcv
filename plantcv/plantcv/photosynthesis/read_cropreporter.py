bd17ff56443ab81d9680bc2a118bfa24c0065c3c
Noah Fahlgren <noahfahlgren@gmail.com>
2021-05-13 21:32:17 -0500

    First PSL frame is white light
    Rename Fdark' to Flight

--- plantcv/plantcv/photosynthesis/read_cropreporter.py
+++ plantcv/plantcv/photosynthesis/read_cropreporter.py
@@ -93,7 +93,7 @@ def read_cropreporter(filename):
             _debug(visual=img_cube[:, :, -1],
                    filename=os.path.join(params.debug_outdir, f"{str(params.device)}_PSD-{frame_labels[-1]}.png"))
         elif corresponding_dict[key] == "PSL":
-            frame_labels = ["Fdark'", "F0'"]
+            frame_labels = ["Flight", "F0'"]
             for i in range(2, np.shape(img_cube)[2]):
                 frame_labels.append(f"F{i - 1}'")
             attributes["F'-frames"] = img_cube.shape[2] - 1
