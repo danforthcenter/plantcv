3106fabb66283a2e3d2c35d5b263b14347786a55
HaleySchuhl <haleyschuhl@gmail.com>
2021-06-09 13:46:53 -0500

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -6361,6 +6361,16 @@ def test_plantcv_visualize_overlay_two_imgs_size_mismatch():
         _ = pcv.visualize.overlay_two_imgs(img1=img1, img2=img2)
 
 
+def test_plantcv_visualize_size():
+    pcv.params.debug = None
+    cache_dir = os.path.join(TEST_TMPDIR, "test_plantcv_visualize_overlay_two_imgs_size_mismatch")
+    os.mkdir(cache_dir)
+    img = cv2.imread(os.path.join(TEST_DATA, TEST_INPUT_BINARY))
+    visualization = pcv.visualize.sizes(img=img, mask=img, num_objects=4)
+    assert np.unique(visualization) == 7
+
+
+
 # ##############################
 # Tests for the utils subpackage
 # ##############################
