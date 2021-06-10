32f39da3e3071687e48feb5d2b0f9150928acb06
Hudanyun Sheng <hudanyun.sheng@gmail.com>
2021-06-10 12:16:58 -0500

    Update plantcv/plantcv/image_fusion.py
    Co-authored-by: Noah Fahlgren <noahfahlgren@gmail.com>

--- plantcv/plantcv/image_fusion.py
+++ plantcv/plantcv/image_fusion.py
@@ -34,7 +34,7 @@ def image_fusion(img1, img2, wvs1, wvs2, array_type=None, filename=None):
     if len(img2.shape) == 2:
         img2 = np.expand_dims(img2, axis=2)
     r2, c2, b2 = img2.shape
-    if (r1,c1) != (r2,c2):
+    if (r1, c1) != (r2, c2):
         fatal_error("Input images should have the same image size")
 
     array_data  = np.concatenate((img1, img2), axis=2)
