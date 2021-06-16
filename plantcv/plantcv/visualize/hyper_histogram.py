63e15f8554c1e88bf92bfde8e5b5e30c9241b8c7
nfahlgren <noahfahlgren@gmail.com>
2021-06-15 23:20:59 -0500

    Use colour-science package for wavelength conversion

--- plantcv/plantcv/visualize/hyper_histogram.py
+++ plantcv/plantcv/visualize/hyper_histogram.py
@@ -5,87 +5,11 @@ import numpy as np
 import pandas as pd
 from plantcv.plantcv._debug import _debug
 from plantcv.plantcv import fatal_error, params, color_palette
-from plotnine import ggplot, aes, geom_line, scale_color_manual
+from plotnine import ggplot, aes, geom_line, scale_color_manual, theme_classic
 from plantcv.plantcv.visualize import histogram
 from plantcv.plantcv.hyperspectral import _find_closest
 from scipy.spatial import distance
-
-'''
-    == A few notes about color ==
-
-    Color   Wavelength(nm) Frequency(THz)
-    Red     620-750        484-400
-    Orange  590-620        508-484
-    Yellow  570-590        526-508
-    Green   495-570        606-526
-    Blue    450-495        668-606
-    Violet  380-450        789-668
-
-    f is frequency (cycles per second)
-    l (lambda) is wavelength (meters per cycle)
-    e is energy (Joules)
-    h (Plank's constant) = 6.6260695729 x 10^-34 Joule*seconds
-                         = 6.6260695729 x 10^-34 m^2*kg/seconds
-    c = 299792458 meters per second
-    f = c/l
-    l = c/f
-    e = h*f
-    e = c*h/l
-
-    List of peak frequency responses for each type of 
-    photoreceptor cell in the human eye:
-        S cone: 437 nm
-        M cone: 533 nm
-        L cone: 564 nm
-        rod:    550 nm in bright daylight, 498 nm when dark adapted. 
-                Rods adapt to low light conditions by becoming more sensitive.
-                Peak frequency response shifts to 498 nm.
-'''
-
-
-def _wavelength_to_rgb(wavelength, gamma=0.8):
-    """
-    reference: "Wavelength to RGB in Python." Noah.org, . 20 Sep 2014, 06:38 UTC. 1 Apr 2021, 19:06
-    <https://www.noah.org/mediawiki-1.34.2/index.php?title=Wavelength_to_RGB_in_Python&oldid=7665>.
-    http://www.noah.org/wiki/Wavelength_to_RGB_in_Python
-
-    This converts a given wavelength of light to an approximate RGB color value. The wavelength must be given in
-    nanometers in the range from 380 nm through 750 nm (789 THz through 400 THz).
-
-    Based on code by Dan Bruton
-    http://www.physics.sfasu.edu/astro/color/spectra.html
-
-    Inputs:
-    wavelength: wavelength of a visible wavelength in nanometers range: (380, 750)
-    gamma: (optional) gamma correction. default value = 0.8
-
-    Returns
-    :param wavelength: float
-    :param gamma: (optional) float
-    :return: tuple
-    """
-
-    wavelength = float(wavelength)
-    if 380 <= wavelength <= 440:
-        attenuation = 0.3 + 0.7 * (wavelength - 380) / (440 - 380)
-        r, g, b = ((-(wavelength - 440) / (440 - 380)) * attenuation) ** gamma, 0.0, (1.0 * attenuation) ** gamma
-    elif 440 <= wavelength <= 490:
-        r, g, b = 0.0, ((wavelength - 440) / (490 - 440)) ** gamma, 1.0
-    elif 490 <= wavelength <= 510:
-        r, g, b = 0.0, 1.0, (-(wavelength - 510) / (510 - 490)) ** gamma
-    elif 510 <= wavelength <= 580:
-        r, g, b = ((wavelength - 510) / (580 - 510)) ** gamma, 1.0, 0.0
-    elif 580 <= wavelength <= 645:
-        r, g, b = 1.0, (-(wavelength - 645) / (645 - 580)) ** gamma, 0.0
-    elif 645 <= wavelength <= 750:
-        attenuation = 0.3 + 0.7 * (750 - wavelength) / (750 - 645)
-        r, g, b = (1.0 * attenuation) ** gamma, 0.0, 0.0
-    # else:
-    #     R, G, B = 0.0, 0.0, 0.0
-    r *= 255
-    g *= 255
-    b *= 255
-    return int(r), int(g), int(b)
+import colour
 
 
 def _rgb_to_webcode(rgb_values):
@@ -146,9 +70,9 @@ def hyper_histogram(array, mask=None, bins=100, lower_bound=None, upper_bound=No
     match_ids = [_find_closest(wls, wv) for wv in wvlengths]
 
     # check if in the visible wavelengths range
-    ids_vis = [idx for (idx, wv) in enumerate(wvlengths) if 380 <= wv <= 750]
+    ids_vis = [idx for (idx, wv) in enumerate(wvlengths) if 390 <= wv <= 830]
     # invisible wavelengths
-    ids_inv = [idx for (idx, wv) in enumerate(wvlengths) if wv < 380 or wv > 750]
+    ids_inv = [idx for (idx, wv) in enumerate(wvlengths) if wv < 390 or wv > 830]
 
     colors = [tuple(x) for x in color_palette(len(wvlengths))]
     if len(ids_vis) < len(wvlengths):
@@ -158,9 +82,26 @@ def hyper_histogram(array, mask=None, bins=100, lower_bound=None, upper_bound=No
     if len(ids_inv) < len(wvlengths):
         colors_vis = []
         colors_inv = colors
+
+        # Color matching function
+        cmfs = colour.MSDS_CMFS["CIE 2012 10 Degree Standard Observer"]
+        matrix = np.array([[3.24062548, -1.53720797, -0.49862860],
+                           [-0.96893071, 1.87575606, 0.04151752],
+                           [0.05571012, -0.20402105, 1.05699594]]
+                          )
         for i in ids_vis:
             # colors_vis.append(_wavelength_to_rgb(wvlengths[i], gamma=0.8))
-            color_vis = _wavelength_to_rgb(wvlengths[i], gamma=0.8)
+            # color_vis = _wavelength_to_rgb(wvlengths[i], gamma=0.8)
+            # Convert wavelength to RGB
+            rgb = colour.XYZ_to_RGB(colour.wavelength_to_XYZ(wvlengths[i], cmfs),
+                                    illuminant_XYZ=np.array([0.9, 0.9]),
+                                    illuminant_RGB=np.array([0.9, 0.9]),
+                                    chromatic_adaptation_transform="Bradford",
+                                    matrix_XYZ_to_RGB=matrix)
+            # Set negative values to zero before scaling
+            rgb[np.where(rgb < 0)] = 0
+            # Convert float RGB to 8-bit unsigned integer
+            color_vis = colour.io.convert_bit_depth(rgb, "uint8")
             colors_vis.append(color_vis)
         # calculate the distances between every pair of (R,G,B) colors
         dists = distance.cdist(colors_vis, colors_inv, 'euclidean')
@@ -205,7 +146,8 @@ def hyper_histogram(array, mask=None, bins=100, lower_bound=None, upper_bound=No
     fig_hist = (ggplot(df_hist, aes(x='reflectance', y='proportion of pixels (%)',
                                     color='Wavelength (' + array.wavelength_units + ')'))
                 + geom_line()
-                + scale_color_manual(color_codes)
+                + scale_color_manual(color_codes, expand=(0, 0))
+                + theme_classic()
                 )
     params.debug = debug
     _debug(fig_hist, filename=os.path.join(params.debug_outdir, str(params.device) + '_histogram.png'))
