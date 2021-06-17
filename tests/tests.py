67a83909f64ba95fafc56a27c28c62fb4410b8fb
Hudanyun Sheng <hudanyun.sheng@gmail.com>
2021-06-17 18:26:14 -0500

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -6384,53 +6384,15 @@ def test_plantcv_visualize_overlay_two_imgs_size_mismatch():
     with pytest.raises(RuntimeError):
         _ = pcv.visualize.overlay_two_imgs(img1=img1, img2=img2)
 
-# ##############################
-# Tests for the ecdf sub-subpackage
-# ##############################
+
 @pytest.mark.parametrize("title", ["Include Title", None])
-def test_plantcv_visualize_ecdf_obj_size(title, tmpdir):
+def test_plantcv_visualize_obj_size_ecdf(title, tmpdir):
     pcv.params.debug = None
     cache_dir = tmpdir.mkdir("sub")
     mask = cv2.imread(os.path.join(TEST_DATA, TEST_INPUT_MASK), -1)
-    fig_ecdf = plantcv.plantcv.visualize.ecdf.obj_size(mask=mask, title=title)
+    fig_ecdf = plantcv.plantcv.visualize.obj_size_ecdf(mask=mask, title=title)
     assert isinstance(fig_ecdf, ggplot)
 
-@pytest.mark.parametrize("title", ["Include Title", None])
-def test_plantcv_visualize_ecdf_pix_intensity(title, tmpdir):
-    pcv.params.debug = None
-    # Create a tmp directory
-    cache_dir = tmpdir.mkdir("sub")
-
-    img = cv2.imread(os.path.join(TEST_DATA, TEST_INPUT_GRAY), -1)
-    # img_rgb  = cv2.imread(os.path.join(TEST_DATA, TEST_INPUT_COLOR), -1)
-    mask     =  np.where(img > 130,255,0).astype(np.uint8)
-    fig_ecdf = plantcv.plantcv.visualize.ecdf.pix_intensity(img, mask=mask, title=title)
-    assert isinstance(fig_ecdf, ggplot)
-    
-def test_plantcv_visualize_ecdf_pix_intensity_no_mask(tmpdir):
-    pcv.params.debug = None
-    # Create a tmp directory
-    cache_dir = tmpdir.mkdir("sub")
-
-    img = cv2.imread(os.path.join(TEST_DATA, TEST_INPUT_GRAY), -1)
-    fig_ecdf = plantcv.plantcv.visualize.ecdf.pix_intensity(img)
-    assert isinstance(fig_ecdf, ggplot)
-
-def test_plantcv_visualize_ecdf_pix_intensity_rgb(tmpdir):
-    pcv.params.debug = None
-    # Create a tmp directory
-    cache_dir = tmpdir.mkdir("sub")
-
-    img_gray = cv2.imread(os.path.join(TEST_DATA, TEST_INPUT_GRAY), -1)
-    img  = cv2.imread(os.path.join(TEST_DATA, TEST_INPUT_COLOR), -1)
-    mask     =  np.where(img_gray > 130,255,0).astype(np.uint8)
-    fig_ecdf = plantcv.plantcv.visualize.ecdf.pix_intensity(img, mask=mask)
-    assert isinstance(fig_ecdf, ggplot)
-
-def test_plantcv_visualize_ecdf_pix_intensity_wrong_input():
-    img = np.arange(0,10)
-    with pytest.raises(RuntimeError):
-        _ = plantcv.plantcv.visualize.ecdf.pix_intensity(img)
 
 # ##############################
 # Tests for the utils subpackage
