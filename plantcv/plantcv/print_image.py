22449c08b831f90135ffca4b2a0cdf4bebc69aba
Noah Fahlgren <noahfahlgren@gmail.com>
2021-07-15 15:01:03 -0500

    Split strings into multiple lines
    Consecutive strings will be concatenated automatically

--- plantcv/plantcv/print_image.py
+++ plantcv/plantcv/print_image.py
@@ -37,12 +37,16 @@ def print_image(img, filename):
     elif isinstance(img, FacetGrid):
         img.fig.savefig(filename, dpi=params.dpi)
 
-    elif isinstance(img, tuple) and len(img)==3:
-        fatal_error('Looks like you are trying to save a histogram. If so, you have 2 options: 1. Use pcv.visualize.histogram() on each numpy.ndarray 2. Create a matplotlib figure by running myfig=plt.gcf() in the same execution as generating the histogram and then use pcv.print_image(myplot, filename=...). ')
+    elif isinstance(img, tuple) and len(img) == 3:
+        fatal_error('Looks like you are trying to save a histogram. If so, you have 2 options: 1. Use '
+                    'pcv.visualize.histogram() on each numpy.ndarray 2. Create a matplotlib figure by running '
+                    'myfig=plt.gcf() in the same execution as generating the histogram and then use pcv.print_image('
+                    'myplot, filename=...). ')
 
     elif isinstance(img, PSII_data):
         fatal_error("You need to plot an underlying DataArray.")
 
     else:
-        fatal_error("Error writing file " + filename + ": input img is " + str(type(img)) + ", not a numpy.ndarray, " +
-                    "matplotlib.figure, plotnine.ggplot, or xarray.plot.facetgrid.FacetGrid and cannot get saved out with print_image.")
+        fatal_error(f"Error writing file {filename}: input img is {str(type(img))}, not a numpy.ndarray, "
+                    "matplotlib.figure, plotnine.ggplot, or xarray.plot.facetgrid.FacetGrid and cannot get "
+                    "saved out with print_image.")
