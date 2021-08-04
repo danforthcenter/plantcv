25e1cf3ed120f39ba18c1562019dec5704e53e3c
JorgeGtz <jorge.gutierrezor@gmail.com>
2021-08-04 16:42:04 -0500

    use random.sample instead of loop

--- plantcv/plantcv/io/random_subset.py
+++ plantcv/plantcv/io/random_subset.py
@@ -10,15 +10,16 @@ def random_subset(dataset, num=100, seed=None):
     if num > N:
         fatal_error("Number of images found less than 'num'.")
 
-    samples = []
-    indices = []
+    # samples = []
+    # indices = []
 
     # Get random images
-    for i in range(0, num):
-        idx = random.randint(0, N - 1)
-        while idx in indices:
-            idx = random.randint(0, N - 1)
-        samples.append(dataset[idx])
-        indices.append(idx)
+    samples = random.sample(dataset, k=num)
+    # for i in range(0, num):
+    #     idx = random.randint(0, N - 1)
+    #     while idx in indices:
+    #         idx = random.randint(0, N - 1)
+    #     samples.append(dataset[idx])
+    #     indices.append(idx)
 
     return samples
