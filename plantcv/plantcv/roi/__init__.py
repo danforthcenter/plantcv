7fa45a46ede7a5c2596ba0e72e4b4b718a843d11
HaleySchuhl <haleyschuhl@gmail.com>
2021-08-16 09:35:27 -0500

    add interactive custom roi tool and helper function

--- plantcv/plantcv/roi/__init__.py
+++ plantcv/plantcv/roi/__init__.py
@@ -5,5 +5,8 @@ from plantcv.plantcv.roi.roi_methods import rectangle
 from plantcv.plantcv.roi.roi_methods import multi
 from plantcv.plantcv.roi.roi_methods import custom
 from plantcv.plantcv.roi.roi2mask import roi2mask
+from plantcv.plantcv.roi.roi_methods import _find_closest_pt
+from plantcv.plantcv.roi.roi_methods import custom_interactive
 
-__all__ = ["circle", "ellipse", "from_binary_image", "rectangle", "multi", "custom", "roi2mask"]
+__all__ = ["circle", "ellipse", "from_binary_image", "rectangle", "multi", "custom", "roi2mask", "_find_closest_pt",
+           "custom_interactive"]
