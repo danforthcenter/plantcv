22156e59927b70906985f6b6492a52351eec5e2d
JorgeGtz <jorge.gutierrezor@gmail.com>
2021-08-17 09:53:22 -0500

    copied tests.py from up to date master

--- tests/tests.py
+++ tests/tests.py
@@ -580,7 +580,7 @@ def test_plantcv_parallel_workflowconfig_subdaily_timestampformat():
         }
     }
 
-
+    
 def test_plantcv_parallel_check_date_range_wrongdateformat():
     start_date = 10
     end_date = 10
@@ -3780,54 +3780,6 @@ def test_plantcv_spatial_clustering_badinput():
     pcv.params.debug = None
     with pytest.raises(NameError):
         _ = pcv.spatial_clustering(img, algorithm="Hydra", min_cluster_size=5, max_distance=100)
-
-def test_plantcv_segment_image_series():
-    # parameters for the synthetic image series to generate
-    H, W, C = 32, 32, 3
-    FRAMES = 10
-    RGB_VAL = [50,150,50]
-    N_OBJ = 10
-    OBJ1_COORDS = [10,10]
-    OBJ2_COORDS = [20,20]
-    OBJ_SIZE = 4
-
-    rng = np.random.default_rng(0)
-    # test cache directory
-    cache_img_dir = os.path.join(TEST_TMPDIR, 'segment_image_series_images')
-    os.mkdir(cache_img_dir)
-    cache_mask_dir = os.path.join(TEST_TMPDIR, 'segment_image_series_masks')
-    os.mkdir(cache_mask_dir)
-    cache_out_dir = os.path.join(TEST_TMPDIR, 'segment_image_series_out')
-    os.mkdir(cache_out_dir)
-
-    # generate image series
-    obj1_init = np.array(OBJ1_COORDS)
-    obj2_init = np.array(OBJ2_COORDS)
-    for i in range(FRAMES):
-        img = np.zeros((H,W,C), dtype=np.uint8)
-        obj1  = obj1_init + rng.integers(low=-1, high=1, size=2)
-        img[obj1[0]:obj1[0]+OBJ_SIZE,obj1[1]:obj1[1]+OBJ_SIZE,:] = RGB_VAL
-        obj2  = obj2_init  + rng.integers(low=-1, high=1, size=2)
-        img[obj2[0]:obj2[0]+OBJ_SIZE,obj2[1]:obj2[1]+OBJ_SIZE,:] = RGB_VAL
-
-        cv2.imwrite(os.path.join(cache_img_dir, f"{i}.png"), img)
-        cv2.imwrite(os.path.join(cache_mask_dir, f"{i}_mask.png"),
-                    255*(img!=0).astype(np.uint8))
-
-    pcv.params.debug_outdir = cache_out_dir
-    pcv.params.color_sequence = 'random'
-    pcv.params.debug = None
-
-    # test that the function detects the two objects and propagates the labels
-    # to the last frame
-    markers = pcv.segment_image_series(cache_img_dir, cache_mask_dir,
-                                        init_frame=5, save_labels=True,
-                                        init_labels=None, ksize=3)
-
-    nb_obj = np.unique(markers[:,:,FRAMES-1]).size - 1
-
-    assert nb_obj == 2
-
 
 
 # ##############################
