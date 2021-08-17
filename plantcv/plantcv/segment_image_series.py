a3c62b0fe24ac73e6d263880bee42d6c976ee817
JorgeGtz <jorge.gutierrezor@gmail.com>
2021-08-16 19:43:42 -0500

    update segment image series to use rois as base labels

--- plantcv/plantcv/segment_image_series.py
+++ plantcv/plantcv/segment_image_series.py
@@ -24,29 +24,8 @@ def _labels2rgb(labels, n_labels, rgb_values):
     return rgb_img
 
 
-#def segment_image_series(img_dir, mask_dir, init_frame, save_labels=True, init_labels=None, ksize=3)
-def segment_image_series(imgs_paths, masks_paths, init_img_name, save_labels=True, init_labels=None, ksize=3):
-    """
+def segment_image_series(imgs_paths, masks_paths, rois, save_labels=True, ksize=3):
 
-    Inputs:
-    img_dir     = Path to the image directory
-    mask_dir    = Path to the mask directory
-    init_frame  = Number of frame in the image series from where the labels propagate
-    save_labels = Save the labels for each frame as a numpy array
-    init_labels = Array containing the initialization labels (same shape as the images)
-    ksize       = Size for the block of images considered at each frame
-
-    Returns:
-    out_labels       = 3D stack of segmented images
-
-    :param img_dir: str
-    :param mask_dir: str
-    :param init_frame: int
-    :param save_labels: bool
-    :param init_labels: numpy.ndarray
-    :param ksize: int
-    :return out_labels: numpy.ndarray
-    """
     debug = params.debug
     params.debug = None
     params.color_sequence = 'random'
@@ -54,110 +33,83 @@ def segment_image_series(imgs_paths, masks_paths, init_img_name, save_labels=Tru
     # for symmetry, using blocks (kernels) of size 2*floor(ksize/2) + 1
     half_k = math.floor(ksize/2)
 
-    #image_names = sorted(os.listdir(img_dir))
-
     image_names = [os.path.basename(img_path) for img_path in imgs_paths]
-    init_frame = image_names.index(init_img_name)
 
-    if init_labels is None:
-        #get initialization labels
-        init_mask, _, _ = pcv.readimage(filename=masks_paths[init_frame])
-        #init_mask, _, _ = pcv.readimage(filename=os.path.join(mask_dir,
-        #                                f"{image_names[init_frame][:-4]}_mask.png"),
-        #                                mode='gray')
-        init_labels, _ = ndi.label(init_mask)
+    #get the size of the images
+    tmp, _, _ = pcv.readimage(filename=masks_paths[0])
+    h, w = tmp.shape[0], tmp.shape[1]
+
+    # create an image where all the pixels inside each roi have the roi label
+    roi_labels = np.zeros((h,w), dtype=np.uint8)
+    n_labels = len(rois)
+    for i in range(n_labels):
+        img_roi = np.zeros((h,w), dtype=np.uint8)
+        img_roi = cv.drawContours(img_roi, rois[i], -1, 255, 3)
+        img_roi = pcv.fill_holes(img_roi)
+        roi_labels = roi_labels + (img_roi==255)*(i+1)
 
     # output initialization
     N = len(image_names)
-    h, w = init_labels.shape
     out_labels = np.zeros((h,w,N),dtype=np.uint8)
-    out_labels[:,:,init_frame] = init_labels
+    out_labels[:,:,0] = roi_labels
 
-    n_labels = np.unique(init_labels).size - 1
+    # values for visualization output image
     rgb_values = color_palette(n_labels)
 
-    # propagate labels sequentially from init_frame
-    # backward
-    for n in range(init_frame,-1,-1):
-        print(n)
-        # build image and mask stacks
+    # Propagate labels sequentially n is the index in the output of the frame currently
+    # in process. At each iteration only one frame is labeled.
+    for n in range(0,N):
+        # size of the stack used at each iteration
         d = 2*half_k+1
+
+        # stacks init
         img_stack = np.zeros((h,w,d))
         mask_stack = np.zeros((h,w,d))
         markers = np.zeros((h,w,d))
-        # with this loop, the number of frames used is always the same,
-        # if stack_idx is always initialized to 2*half_k the borders are 'constant'
-        stack_idx = 2*half_k
-        for m in range(half_k, -half_k-1, -1):
-            frame = min(N-1, max(n+m,0))
-            #img, _, _ = pcv.readimage(filename=os.path.join(img_dir,image_names[frame]))
-            img, _, _ = pcv.readimage(filename=imgs_paths[frame])
-            if m == 0:
-                img_n_rgb = img
-            mask, _, _ = pcv.readimage(filename=masks_paths[frame], mode='gray')
-            #mask, _, _ = pcv.readimage(filename=os.path.join(mask_dir,
-            #                            f"{image_names[frame][:-4]}_mask.png"),
-            #                            mode='gray')
-            img_stack[:,:,stack_idx] = pcv.rgb2gray(rgb_img=img)
-            mask_stack[:,:,stack_idx] = mask
-            markers[:,:,stack_idx] = out_labels[:,:,frame]
-            stack_idx += -1
 
-        # edges using 3D sobel operator as elevation map for watershed
-        edges = ndi.generic_gradient_magnitude(img_stack, ndi.sobel)
-        # segmentation using the watershed algorithm
-        labels = watershed(edges, markers=markers, mask=mask_stack, compactness=0)
-        out_labels[:,:,n] = labels[:,:,half_k]
+        # The number of frames used is always the same but the borders are
+        # treated as 'constant' or 'zero padding'
+        stack_idx = 0 # borders are 'constant'
+        #stack_idx = -min(0,n-half_k) # left border is 'zero padded' not necessary
 
-        # Create images for plotting and printing (debug mode)
-        rgb_seg = _labels2rgb(out_labels[:,:,n], n_labels, rgb_values)
-        vis_seg = cv.addWeighted(img_n_rgb, 0.7, rgb_seg, 0.3, 0.0)
-        params.debug = debug
-        _debug(visual=vis_seg, filename=os.path.join(params.debug_outdir,
-                                f"{str(params.device)}_{image_names[n][:-4]}_WSeg.png"))
-        params.debug = None
-
-    # forward
-    for n in range(init_frame+1,N):
-        print(n)
-        # build image and mask stacks
-        d = 2*half_k+1
-        img_stack = np.zeros((h,w,d))
-        mask_stack = np.zeros((h,w,d))
-        markers = np.zeros((h,w,d))
-        # with this loop, the number of frames used is always the same,
-        # if stack_idx is always initialized to 0 the borders are 'constant'
-        stack_idx = 0
-        # if stack_idx is initialized this way, the laft border is 'zero padded'
-        #stack_idx = -min(0,n-half_k)
+        # loop to build the stacks. half_k gives the index of the frame in process
         for m in range(-half_k,half_k+1):
-            frame = min(N-1, max(n+m,0))
+            frame = min(N-1, max(n+m,0)) # borders handling
+
             img, _, _ = pcv.readimage(filename=imgs_paths[frame])
-            #img, _, _ = pcv.readimage(filename=os.path.join(img_dir,image_names[frame]))
-            if m == 0:
-                img_n_rgb = img
-            mask, _, _ = pcv.readimage(filename=masks_paths[frame], mode='gray')
-            #mask, _, _ = pcv.readimage(filename=os.path.join(mask_dir,
-            #                            f"{image_names[frame][:-4]}_mask.png"),
-            #                            mode='gray')
             img_stack[:,:,stack_idx] = pcv.rgb2gray(rgb_img=img)
+            mask, _, _ = pcv.readimage(filename=masks_paths[frame], mode='gray')
             mask_stack[:,:,stack_idx] = mask
-            markers[:,:,stack_idx] = out_labels[:,:,frame]
+
+            if m == 0:
+                # required to create the output image
+                img_n_rgb = img
+                # enforcing the label inside the regions of interest
+                markers[:,:,stack_idx] = (mask!=0)*roi_labels
+            else:
+                markers[:,:,stack_idx] = out_labels[:,:,frame]
+
             stack_idx += 1
 
         # edges using 3D sobel operator as elevation map for watershed
         edges = ndi.generic_gradient_magnitude(img_stack, ndi.sobel)
+
         # segmentation using the watershed algorithm
         labels = watershed(edges, markers=markers, mask=mask_stack, compactness=0)
+
+        # add the resulting labels to the outputs block
         out_labels[:,:,n] = labels[:,:,half_k]
+
 
         # Create images for plotting and printing (debug mode)
         rgb_seg = _labels2rgb(out_labels[:,:,n], n_labels, rgb_values)
-        vis_seg = cv.addWeighted(img_n_rgb, 0.7, rgb_seg, 0.3, 0.0)
+        #vis_seg = cv.addWeighted(img_n_rgb, 0.7, rgb_seg, 0.3, 0.0)
+        vis_seg = pcv.visualize.overlay_two_imgs(img1=img_n_rgb, img2=rgb_seg, alpha=0.3)
         params.debug = debug
         _debug(visual=vis_seg, filename=os.path.join(params.debug_outdir,
                                 f"{str(params.device)}_{image_names[n][:-4]}_WSeg.png"))
         params.debug = None
+
 
     if save_labels == True:
         [np.save(os.path.join(params.debug_outdir, f"{image_names[i][:-4]}_labels"),
