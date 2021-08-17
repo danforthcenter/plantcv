34f473858891e5c805455312088269000bffa859
Dominik Schneider <dschneiderch@users.noreply.github.com>
2021-08-17 15:16:03 -0700

    erode

--- tests/tests.py
+++ tests/tests.py
@@ -1987,12 +1987,6 @@ def test_plantcv_erode():
     pcv.params.debug_outdir = cache_dir
     # Read in test data
     img = cv2.imread(os.path.join(TEST_DATA, TEST_INPUT_BINARY), -1)
-    # Test with debug = "print"
-    pcv.params.debug = "print"
-    _ = pcv.erode(gray_img=img, ksize=5, i=1)
-    # Test with debug = "plot"
-    pcv.params.debug = "plot"
-    _ = pcv.erode(gray_img=img, ksize=5, i=1)
     # Test with debug = None
     pcv.params.debug = None
     erode_img = pcv.erode(gray_img=img, ksize=5, i=1)
