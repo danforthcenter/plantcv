c89b0b4759636efcf1009fc9951ba3bc495693aa
HaleySchuhl <haleyschuhl@gmail.com>
2021-08-31 11:23:27 -0500

    refactor class name within init

--- plantcv/plantcv/roi/__init__.py
+++ plantcv/plantcv/roi/__init__.py
@@ -6,7 +6,7 @@ from plantcv.plantcv.roi.roi_methods import multi
 from plantcv.plantcv.roi.roi_methods import custom
 from plantcv.plantcv.roi.roi2mask import roi2mask
 from plantcv.plantcv.roi.roi_methods import _find_closest_pt
-from plantcv.plantcv.roi.roi_methods import custom_interactive
+from plantcv.plantcv.roi.roi_methods import CustomROI
 
 __all__ = ["circle", "ellipse", "from_binary_image", "rectangle", "multi", "custom", "roi2mask", "_find_closest_pt",
-           "custom_interactive"]
+           "CustomROI"]
