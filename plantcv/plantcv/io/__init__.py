57b652d14cb092f0ca5d87faab9d9566f6a55133
JorgeGtz <jorge.gutierrezor@gmail.com>
2021-09-15 12:11:37 -0500

    remove io package. It is now in a different branch

--- plantcv/plantcv/io/__init__.py
+++
@@ -1,5 +0,0 @@
-from plantcv.plantcv.io.read_dataset import read_dataset
-from plantcv.plantcv.io.random_subset import random_subset
-
-# add new functions to end of lists
-__all__ = ["read_dataset", "random_subset"]
