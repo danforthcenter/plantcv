69abdb26af7e87c3de245c484e007643b3632b2e
Noah Fahlgren <noahfahlgren@gmail.com>
2021-09-18 11:53:41 -0500

    Refactor to avoid modifying input df

--- plantcv/plantcv/homology/constella.py
+++ plantcv/plantcv/homology/constella.py
@@ -10,10 +10,10 @@ def constella(cur_plms, pc_starscape, group_iter, outfile_prefix):
     Group pseudo-landmarks into homology groupings
 
     Inputs:
-    cur_plms      = A pandas array of plm multivariate space representing capturing two adjacent frames in a
-                    time series or otherwise analogous dataset in order to enable homology assignments
-    pc_starscape  = PCA results from starscape
-    group_iter    =
+    cur_plms       = A pandas array of plm multivariate space representing capturing two adjacent frames in a
+                     time series or otherwise analogous dataset in order to enable homology assignments
+    pc_starscape   = PCA results from starscape
+    group_iter     = Group ID counter
     outfile_prefix = User defined file path and prefix name for PCA output graphics
 
     :param cur_plms: pandas.core.frame.DataFrame
@@ -21,6 +21,9 @@ def constella(cur_plms, pc_starscape, group_iter, outfile_prefix):
     :param group_iter: int
     :param outfile_prefix: str
     """
+    # Copy dataframe to avoid modifying the input dataframe
+    cur_plms_copy = cur_plms.copy(deep=True)
+
     sanity_check_pos = 2  # Needs to point at days in image identifier!
 
     singleton_no = pc_starscape.shape[0]
@@ -42,23 +45,23 @@ def constella(cur_plms, pc_starscape, group_iter, outfile_prefix):
             # Create list of current clusters row indices in pandas dataframe
             cur_index = [i for i, x in enumerate(cutree == g) if x]
             # Create list of current clusters present group identity assignments
-            cur_index_id = np.array(cur_plms.iloc[cur_index, 0])
+            cur_index_id = np.array(cur_plms_copy.iloc[cur_index, 0])
             # Are any of the plms in the current cluster unnamed, how many?
             empty_count = np.count_nonzero(cur_index_id is None)
-            empty_index = [i for (i, v) in zip(cur_index, cur_plms.iloc[cur_index, 0].values is None) if v]
+            empty_index = [i for (i, v) in zip(cur_index, cur_plms_copy.iloc[cur_index, 0].values is None) if v]
             # Are any of the plms in the current cluster already assigned an identity, what are those identities?
             unique_ids = np.unique(cur_index_id[np.array(cur_index_id) is not None])
 
             # If cluster is two unnamed plms exactly, assign this group their own identity as a pair
             if empty_count == 2:
-                pair_names = cur_plms.iloc[empty_index, 1].values
+                pair_names = cur_plms_copy.iloc[empty_index, 1].values
                 # Sanity check! Pairs must be on different days
                 if pair_names[0].split('_')[sanity_check_pos] != pair_names[1].split('_')[sanity_check_pos]:
-                    cur_plms.iloc[empty_index, 0] = group_iter
+                    cur_plms_copy.iloc[empty_index, 0] = group_iter
                     group_iter = group_iter + 1
                 else:
-                    cur_plms.iloc[empty_index[0], 0] = group_iter
-                    cur_plms.iloc[empty_index[1], 0] = group_iter + 1
+                    cur_plms_copy.iloc[empty_index[0], 0] = group_iter
+                    cur_plms_copy.iloc[empty_index[1], 0] = group_iter + 1
                     group_iter = group_iter + 2
 
             # For the identities that already exist...
@@ -67,26 +70,26 @@ def constella(cur_plms, pc_starscape, group_iter, outfile_prefix):
                 # transfer ID over to create a pair
                 if np.count_nonzero(np.array(cur_index_id) == ID) < 2 and empty_count == 1:
                     # Store boolean positions for plms with IDs matching current ID out of current cluster
-                    match_ids = [i for i, x in enumerate(cur_plms.iloc[cur_index, 0].values == ID) if x]
+                    match_ids = [i for i, x in enumerate(cur_plms_copy.iloc[cur_index, 0].values == ID) if x]
                     # Store boolean positions for plms which are unnamed out of current cluster
-                    null_ids = [i for i, x in enumerate(cur_plms.iloc[cur_index, 0].values is None) if x]
+                    null_ids = [i for i, x in enumerate(cur_plms_copy.iloc[cur_index, 0].values is None) if x]
                     # If exactly 1 matching ID and 1 null ID (i.e. 2 plms total)
                     # continue to pass ID name to the unnamed plm
                     if len(match_ids) + len(null_ids) == 2:
                         # Sanity check! Pairs must be on different days
-                        pair_names = cur_plms.iloc[[cur_index[i] for i in match_ids + null_ids], 1].values
+                        pair_names = cur_plms_copy.iloc[[cur_index[i] for i in match_ids + null_ids], 1].values
                         if pair_names[0].split('_')[sanity_check_pos] != pair_names[1].split('_')[sanity_check_pos]:
                             # Transfer identities to the unnamed plm
-                            cur_plms.iloc[[cur_index[i] for i in null_ids], 0] = ID
+                            cur_plms_copy.iloc[[cur_index[i] for i in null_ids], 0] = ID
 
     # Now that all groups that can be linked are formed, name rogues...
-    rogues = [i for i, x in enumerate(cur_plms.loc[:, 'group'].values is None) if x]
+    rogues = [i for i, x in enumerate(cur_plms_copy.loc[:, 'group'].values is None) if x]
     for rogue in rogues:
-        cur_plms.iloc[[rogue], 0] = group_iter
+        cur_plms_copy.iloc[[rogue], 0] = group_iter
         group_iter = group_iter + 1
 
-    grpnames = cur_plms.loc[:, ['group']].values
-    plmnames = cur_plms.loc[:, ['plmname']].values
+    grpnames = cur_plms_copy.loc[:, ['group']].values
+    plmnames = cur_plms_copy.loc[:, ['plmname']].values
 
     labelnames = []
 
@@ -107,4 +110,4 @@ def constella(cur_plms, pc_starscape, group_iter, outfile_prefix):
         elif params.debug == "plot":
             plt.show()
 
-    return cur_plms, group_iter
+    return cur_plms_copy, group_iter
