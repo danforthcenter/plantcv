85e55a9a5e4c02100036b2b2f82f7f46d22db741
HaleySchuhl <haleyschuhl@gmail.com>
2021-11-12 14:22:37 -0600

    Update __init__.py

--- plantcv/plantcv/roi/__init__.py
+++ plantcv/plantcv/roi/__init__.py
@@ -5,8 +5,6 @@ from plantcv.plantcv.roi.roi_methods import rectangle
 from plantcv.plantcv.roi.roi_methods import multi
 from plantcv.plantcv.roi.roi_methods import custom
 from plantcv.plantcv.roi.roi2mask import roi2mask
-from plantcv.plantcv.roi.roi_methods import _find_closest_pt
-from plantcv.plantcv.roi.roi_methods import CustomROI
 
-__all__ = ["circle", "ellipse", "from_binary_image", "rectangle", "multi", "custom", "roi2mask", "_find_closest_pt",
-           "CustomROI"]
+
+__all__ = ["circle", "ellipse", "from_binary_image", "rectangle", "multi", "custom", "roi2mask"]
