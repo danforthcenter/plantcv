cdd04f84f5b57cd85a7fd821d5939e36092aad8c
HaleySchuhl <haleyschuhl@gmail.com>
2021-11-16 13:21:59 -0600

    Update __init__.py

--- plantcv/plantcv/annotate/__init__.py
+++ plantcv/plantcv/annotate/__init__.py
@@ -1,4 +1,3 @@
-from plantcv.plantcv.annotate.points import Points
 from plantcv.plantcv.annotate.points import _find_closest_pt
 
-__all__ = ["Points", "_find_closest_pt"]
+__all__ = ["_find_closest_pt"]
