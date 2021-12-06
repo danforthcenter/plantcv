4c6f93771ac3229733f29eaa6ff45b66a62468ce
JorgeGtz <jorge.gutierrezor@gmail.com>
2021-12-06 16:28:09 -0600

    add back remove exact point in tests for Points class to cover related lines

--- tests/tests.py
+++ tests/tests.py
@@ -5221,11 +5221,23 @@ def test_plantcv_annotate_Points_interactive():
     e2.xdata, e2.ydata = (300, 200)
     drawer_rgb.onclick(e2)
 
-    # event 3, right click to remove point
+    # event 3, left click to add point
     e3 = matplotlib.backend_bases.MouseEvent(name="button_press_event", canvas=drawer_rgb.fig.canvas,
-                                             x=0, y=0, button=3)
-    e3.xdata, e3.ydata = (301, 200)
+                                             x=0, y=0, button=1)
+    e3.xdata, e3.ydata = (50, 50)
     drawer_rgb.onclick(e3)
+
+    # event 4, right click to remove point with exact coordinates
+    e4 = matplotlib.backend_bases.MouseEvent(name="button_press_event", canvas=drawer_rgb.fig.canvas,
+                                             x=0, y=0, button=3)
+    e4.xdata, e4.ydata = (50, 50)
+    drawer_rgb.onclick(e4)
+
+    # event 5, right click to remove point with coordinates close but not equal
+    e5 = matplotlib.backend_bases.MouseEvent(name="button_press_event", canvas=drawer_rgb.fig.canvas,
+                                             x=0, y=0, button=3)
+    e5.xdata, e5.ydata = (301, 200)
+    drawer_rgb.onclick(e5)
 
     assert drawer_rgb.points[0] == point1
 
