cb51eda826aedad54a127fc57cc706071647e6dd
Noah Fahlgren <noahfahlgren@gmail.com>
2021-12-14 15:24:33 -0600

    Simplify code

--- plantcv/plantcv/warn.py
+++ plantcv/plantcv/warn.py
@@ -2,13 +2,13 @@
 
 import sys
 
+
 def warn(warning):
     """Print out warning message
 
     Inputs:
-    warning_msg = warning message text
+    warning = warning message text
 
     :param warning: str
     """
-    warning_msg = f"Warning: {warning}"
-    print(warning_msg, file=sys.stderr)
+    print(f"Warning: {warning}", file=sys.stderr)
