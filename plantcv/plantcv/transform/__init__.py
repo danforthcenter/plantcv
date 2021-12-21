1002c8707147aefa0420f5e237efc1db3f83a8c8
HaleySchuhl <haleyschuhl@gmail.com>
2021-12-21 10:06:58 -0600

    remove registrator from transform init

--- plantcv/plantcv/transform/__init__.py
+++ plantcv/plantcv/transform/__init__.py
@@ -14,9 +14,8 @@ from plantcv.plantcv.transform.nonuniform_illumination import nonuniform_illumin
 from plantcv.plantcv.transform.resize import resize, resize_factor
 from plantcv.plantcv.transform.warp import warp, warp_align
 from plantcv.plantcv.transform.gamma_correct import gamma_correct
-from plantcv.plantcv.transform.img_registration import ImageRegistrator
 
 __all__ = ["get_color_matrix", "get_matrix_m", "calc_transformation_matrix", "apply_transformation_matrix",
            "save_matrix", "load_matrix", "correct_color", "create_color_card_mask", "quick_color_check",
            "find_color_card", "rescale", "nonuniform_illumination", "resize", "resize_factor",
-           "warp", "rotate", "warp", "warp_align", "gamma_correct", "ImageRegistrator"]
+           "warp", "rotate", "warp", "warp_align", "gamma_correct"]
