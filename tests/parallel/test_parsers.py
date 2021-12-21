29ee81654494d326515b0885acf24869f5aadcc5
Noah Fahlgren <noahfahlgren@gmail.com>
2021-12-21 17:12:33 -0600

    Reorganize tests into modules

--- tests/parallel/test_parsers.py
+++ tests/parallel/test_parsers.py
@@ -2,13 +2,13 @@ import pytest
 from plantcv.parallel import check_date_range, convert_datetime_to_unixtime, metadata_parser, WorkflowConfig
 
 
-def test_plantcv_parallel_metadata_parser_snapshots(test_data):
+def test_metadata_parser_snapshots(parallel_test_data):
     # Create config instance
     config = WorkflowConfig()
-    config.input_dir = test_data.snapshot_imgdir
+    config.input_dir = parallel_test_data.snapshot_imgdir
     config.json = "output.json"
     config.filename_metadata = ["imgtype", "camera", "frame", "zoom", "lifter", "gain", "exposure", "id"]
-    config.workflow = test_data.workflow_script
+    config.workflow = parallel_test_data.workflow_script
     config.metadata_filters = {"imgtype": "VIS", "camera": "SV"}
     config.start_date = "2014-10-21 00:00:00.0"
     config.end_date = "2014-10-23 00:00:00.0"
@@ -16,16 +16,16 @@ def test_plantcv_parallel_metadata_parser_snapshots(test_data):
     config.imgformat = "jpg"
 
     meta = metadata_parser(config=config)
-    assert meta == test_data.metadata_snapshot_vis
+    assert meta == parallel_test_data.metadata_snapshot_vis
 
 
-def test_plantcv_parallel_metadata_parser_snapshots_coimg(test_data):
+def test_metadata_parser_snapshots_coimg(parallel_test_data):
     # Create config instance
     config = WorkflowConfig()
-    config.input_dir = test_data.snapshot_imgdir
+    config.input_dir = parallel_test_data.snapshot_imgdir
     config.json = "output.json"
     config.filename_metadata = ["imgtype", "camera", "frame", "zoom", "lifter", "gain", "exposure", "id"]
-    config.workflow = config.workflow = test_data.workflow_script
+    config.workflow = config.workflow = parallel_test_data.workflow_script
     config.metadata_filters = {"imgtype": "VIS"}
     config.start_date = "2014-10-21 00:00:00.0"
     config.end_date = "2014-10-23 00:00:00.0"
@@ -34,17 +34,17 @@ def test_plantcv_parallel_metadata_parser_snapshots_coimg(test_data):
     config.coprocess = "FAKE"
 
     meta = metadata_parser(config=config)
-    assert meta == test_data.metadata_snapshot_vis
+    assert meta == parallel_test_data.metadata_snapshot_vis
 
 
 @pytest.mark.parametrize("subdirs", [True, False])
-def test_plantcv_parallel_metadata_parser_images(test_data, subdirs):
+def test_metadata_parser_images(parallel_test_data, subdirs):
     # Create config instance
     config = WorkflowConfig()
-    config.input_dir = test_data.flat_imgdir
+    config.input_dir = parallel_test_data.flat_imgdir
     config.json = "output.json"
     config.filename_metadata = ["imgtype", "camera", "frame", "zoom", "lifter", "gain", "exposure", "id"]
-    config.workflow = test_data.workflow_script
+    config.workflow = parallel_test_data.workflow_script
     config.metadata_filters = {"imgtype": "VIS"}
     config.start_date = "2014"
     config.end_date = "2014"
@@ -53,44 +53,44 @@ def test_plantcv_parallel_metadata_parser_images(test_data, subdirs):
     config.include_all_subdirs = subdirs
 
     meta = metadata_parser(config=config)
-    assert meta == test_data.metadata_flat_vis
+    assert meta == parallel_test_data.metadata_flat_vis
 
 
-def test_plantcv_parallel_metadata_parser_multivalue_filter(test_data):
+def test_metadata_parser_multivalue_filter(parallel_test_data):
     # Create config instance
     config = WorkflowConfig()
-    config.input_dir = test_data.flat_imgdir
+    config.input_dir = parallel_test_data.flat_imgdir
     config.json = "output.json"
     config.filename_metadata = ["imgtype", "camera", "frame", "zoom", "lifter", "gain", "exposure", "id"]
-    config.workflow = test_data.workflow_script
+    config.workflow = parallel_test_data.workflow_script
     config.metadata_filters = {"imgtype": ["VIS", "NIR"]}
     config.imgformat = "jpg"
 
     meta = metadata_parser(config=config)
-    assert meta == test_data.metadata_flat_coprocess
+    assert meta == parallel_test_data.metadata_flat_coprocess
 
 
-def test_plantcv_parallel_metadata_parser_multivalue_filter_nomatch(test_data):
+def test_metadata_parser_multivalue_filter_nomatch(parallel_test_data):
     # Create config instance
     config = WorkflowConfig()
-    config.input_dir = test_data.flat_imgdir
+    config.input_dir = parallel_test_data.flat_imgdir
     config.json = "output.json"
     config.filename_metadata = ["imgtype", "camera", "frame", "zoom", "lifter", "gain", "exposure", "id"]
-    config.workflow = test_data.workflow_script
+    config.workflow = parallel_test_data.workflow_script
     config.metadata_filters = {"imgtype": ["VIS", "PSII"]}
     config.imgformat = "jpg"
 
     meta = metadata_parser(config=config)
-    assert meta == test_data.metadata_flat_vis
+    assert meta == parallel_test_data.metadata_flat_vis
 
 
-def test_plantcv_parallel_metadata_parser_regex(test_data):
+def test_metadata_parser_regex(parallel_test_data):
     # Create config instance
     config = WorkflowConfig()
-    config.input_dir = test_data.flat_imgdir
+    config.input_dir = parallel_test_data.flat_imgdir
     config.json = "output.json"
     config.filename_metadata = ["imgtype", "camera", "frame", "zoom", "lifter", "gain", "exposure", "id"]
-    config.workflow = test_data.workflow_script
+    config.workflow = parallel_test_data.workflow_script
     config.metadata_filters = {"imgtype": "VIS"}
     config.start_date = "2014-10-21 00:00:00.0"
     config.end_date = "2014-10-23 00:00:00.0"
@@ -99,16 +99,16 @@ def test_plantcv_parallel_metadata_parser_regex(test_data):
     config.delimiter = r'(VIS)_(SV)_(\d+)_(z1)_(h1)_(g0)_(e82)_(\d+)'
 
     meta = metadata_parser(config=config)
-    assert meta == test_data.metadata_flat_vis
+    assert meta == parallel_test_data.metadata_flat_vis
 
 
-def test_plantcv_parallel_metadata_parser_images_outside_daterange(test_data):
+def test_metadata_parser_images_outside_daterange(parallel_test_data):
     # Create config instance
     config = WorkflowConfig()
-    config.input_dir = test_data.flat_imgdir_dates
+    config.input_dir = parallel_test_data.flat_imgdir_dates
     config.json = "output.json"
     config.filename_metadata = ["imgtype", "camera", "frame", "zoom", "lifter", "gain", "exposure", "timestamp"]
-    config.workflow = test_data.workflow_script
+    config.workflow = parallel_test_data.workflow_script
     config.metadata_filters = {"imgtype": "NIR"}
     config.start_date = "1970-01-01 00_00_00"
     config.end_date = "1970-01-01 00_00_00"
@@ -120,13 +120,13 @@ def test_plantcv_parallel_metadata_parser_images_outside_daterange(test_data):
     assert meta == {}
 
 
-def test_plantcv_parallel_metadata_parser_no_default_dates(test_data):
+def test_metadata_parser_no_default_dates(parallel_test_data):
     # Create config instance
     config = WorkflowConfig()
-    config.input_dir = test_data.snapshot_imgdir
+    config.input_dir = parallel_test_data.snapshot_imgdir
     config.json = "output.json"
     config.filename_metadata = ["imgtype", "camera", "frame", "zoom", "lifter", "gain", "exposure", "id"]
-    config.workflow = test_data.workflow_script
+    config.workflow = parallel_test_data.workflow_script
     config.metadata_filters = {"imgtype": "VIS", "camera": "SV", "id": "117770"}
     config.start_date = None
     config.end_date = None
@@ -134,26 +134,16 @@ def test_plantcv_parallel_metadata_parser_no_default_dates(test_data):
     config.imgformat = "jpg"
 
     meta = metadata_parser(config=config)
-    assert meta == test_data.metadata_snapshot_vis
+    assert meta == parallel_test_data.metadata_snapshot_vis
 
 
-def test_plantcv_parallel_check_date_range_wrongdateformat():
-    start_date = 10
-    end_date = 10
-    img_time = '2010-10-10'
-
-    with pytest.raises(SystemExit, match=r'does not match format'):
-        date_format = '%Y%m%d'
-        _ = check_date_range(start_date, end_date, img_time, date_format)
-
-
-def test_plantcv_parallel_metadata_parser_snapshot_outside_daterange(test_data):
+def test_metadata_parser_snapshot_outside_daterange(parallel_test_data):
     # Create config instance
     config = WorkflowConfig()
-    config.input_dir = test_data.snapshot_imgdir
+    config.input_dir = parallel_test_data.snapshot_imgdir
     config.json = "output.json"
     config.filename_metadata = ["imgtype", "camera", "frame", "zoom", "lifter", "gain", "exposure", "id"]
-    config.workflow = test_data.workflow_script
+    config.workflow = parallel_test_data.workflow_script
     config.metadata_filters = {"imgtype": "VIS"}
     config.start_date = "1970-01-01 00:00:00.0"
     config.end_date = "1970-01-01 00:00:00.0"
@@ -165,13 +155,13 @@ def test_plantcv_parallel_metadata_parser_snapshot_outside_daterange(test_data):
     assert meta == {}
 
 
-def test_plantcv_parallel_metadata_parser_fail_images(test_data):
+def test_metadata_parser_fail_images(parallel_test_data):
     # Create config instance
     config = WorkflowConfig()
-    config.input_dir = test_data.snapshot_imgdir
+    config.input_dir = parallel_test_data.snapshot_imgdir
     config.json = "output.json"
     config.filename_metadata = ["imgtype", "camera", "frame", "zoom", "lifter", "gain", "exposure", "id"]
-    config.workflow = test_data.workflow_script
+    config.workflow = parallel_test_data.workflow_script
     config.metadata_filters = {"cartag": "VIS"}
     config.start_date = "1970-01-01 00:00:00.0"
     config.end_date = "1970-01-01 00:00:00.0"
@@ -180,16 +170,16 @@ def test_plantcv_parallel_metadata_parser_fail_images(test_data):
     config.coprocess = "NIR"
 
     meta = metadata_parser(config=config)
-    assert meta == test_data.metadata_snapshot_nir
+    assert meta == parallel_test_data.metadata_snapshot_nir
 
 
-def test_plantcv_parallel_metadata_parser_images_with_frame(test_data):
+def test_metadata_parser_images_with_frame(parallel_test_data):
     # Create config instance
     config = WorkflowConfig()
-    config.input_dir = test_data.snapshot_imgdir
+    config.input_dir = parallel_test_data.snapshot_imgdir
     config.json = "output.json"
     config.filename_metadata = ["imgtype", "camera", "frame", "zoom", "lifter", "gain", "exposure", "id"]
-    config.workflow = test_data.workflow_script
+    config.workflow = parallel_test_data.workflow_script
     config.metadata_filters = {"imgtype": "VIS"}
     config.start_date = "2014-10-21 00:00:00.0"
     config.end_date = "2014-10-23 00:00:00.0"
@@ -199,16 +189,16 @@ def test_plantcv_parallel_metadata_parser_images_with_frame(test_data):
 
     meta = metadata_parser(config=config)
 
-    assert meta == test_data.metadata_snapshot_coprocess
+    assert meta == parallel_test_data.metadata_snapshot_coprocess
 
 
-def test_plantcv_parallel_metadata_parser_images_no_frame(test_data):
+def test_metadata_parser_images_no_frame(parallel_test_data):
     # Create config instance
     config = WorkflowConfig()
-    config.input_dir = test_data.snapshot_imgdir
+    config.input_dir = parallel_test_data.snapshot_imgdir
     config.json = "output.json"
     config.filename_metadata = ["imgtype", "camera", "X", "zoom", "lifter", "gain", "exposure", "id"]
-    config.workflow = test_data.workflow_script
+    config.workflow = parallel_test_data.workflow_script
     config.metadata_filters = {"imgtype": "VIS"}
     config.start_date = "2014-10-21 00:00:00.0"
     config.end_date = "2014-10-23 00:00:00.0"
@@ -218,16 +208,16 @@ def test_plantcv_parallel_metadata_parser_images_no_frame(test_data):
 
     meta = metadata_parser(config=config)
 
-    assert meta == test_data.metadata_snapshot_noframe
+    assert meta == parallel_test_data.metadata_snapshot_noframe
 
 
-def test_plantcv_parallel_metadata_parser_images_no_camera(test_data):
+def test_metadata_parser_images_no_camera(parallel_test_data):
     # Create config instance
     config = WorkflowConfig()
-    config.input_dir = test_data.snapshot_imgdir
+    config.input_dir = parallel_test_data.snapshot_imgdir
     config.json = "output.json"
     config.filename_metadata = ["imgtype", "X", "frame", "zoom", "lifter", "gain", "exposure", "id"]
-    config.workflow = test_data.workflow_script
+    config.workflow = parallel_test_data.workflow_script
     config.metadata_filters = {"imgtype": "VIS"}
     config.start_date = "2014-10-21 00:00:00.0"
     config.end_date = "2014-10-23 00:00:00.0"
@@ -237,14 +227,46 @@ def test_plantcv_parallel_metadata_parser_images_no_camera(test_data):
 
     meta = metadata_parser(config=config)
 
-    assert meta == test_data.metadata_snapshot_nocamera
+    assert meta == parallel_test_data.metadata_snapshot_nocamera
 
 
-def test_plantcv_parallel_convert_datetime_to_unixtime():
+def test_metadata_parser_subdaily_timestampformat(parallel_test_data):
+    '''
+    timestampformats with only hours and smaller units of time were failing if the script was run earlier in the day than the
+    images were taken. this was fixed by setting end_date to 23-59-59 if we don't detect the year-month-day
+    '''
+    # Create config instance
+    config = WorkflowConfig()
+    config.input_dir = parallel_test_data.flat_imgdir_dates
+    config.json = "output.json"
+    config.filename_metadata = ["imgtype", "camera", "frame", "zoom", "lifter", "gain", "exposure", "timestamp"]
+    config.workflow = parallel_test_data.workflow_script
+    config.metadata_filters = {"imgtype": "NIR", "camera": "SV"}
+    config.start_date = None
+    config.end_date = None
+    config.timestampformat = "%H_%M_%S"
+    config.imgformat = "jpg"
+    config.delimiter = r"(NIR)_(SV)_(\d)_(z1)_(h1)_(g0)_(e65)_(\d{2}_\d{2}_\d{2})"
+
+    meta = metadata_parser(config=config)
+    assert meta == parallel_test_data.metadata_flat_subdaily
+
+
+def test_convert_datetime_to_unixtime():
     unix_time = convert_datetime_to_unixtime(timestamp_str="1970-01-01", date_format="%Y-%m-%d")
     assert unix_time == 0
 
 
-def test_plantcv_parallel_convert_datetime_to_unixtime_bad_strptime():
+def test_convert_datetime_to_unixtime_bad_strptime():
     with pytest.raises(SystemExit):
         _ = convert_datetime_to_unixtime(timestamp_str="1970-01-01", date_format="%Y-%m")
+
+
+def test_check_date_range_wrongdateformat():
+    start_date = 10
+    end_date = 10
+    img_time = '2010-10-10'
+
+    with pytest.raises(SystemExit, match=r'does not match format'):
+        date_format = '%Y%m%d'
+        _ = check_date_range(start_date, end_date, img_time, date_format)
--- tests/parallel/test_workflowconfig.py
+++ tests/parallel/test_workflowconfig.py
@@ -1,68 +1,68 @@
 from plantcv.parallel import WorkflowConfig
 
 
-def test_plantcv_parallel_workflowconfig_save_config_file(test_data, tmpdir):
+def test_save_config_file(parallel_test_data, tmpdir):
     # Create a tmp JSON file
     template_file = tmpdir.mkdir("cache").join("config.json")
     # Create config instance
     config = WorkflowConfig()
     # Save template file
     config.save_config(config_file=template_file)
-    content = test_data.load_json(json_file=template_file)
+    content = parallel_test_data.load_json(json_file=template_file)
 
-    assert content == test_data.workflowconfig_template()
+    assert content == parallel_test_data.workflowconfig_template()
 
 
-def test_plantcv_parallel_workflowconfig_import_config_file(test_data):
+def test_import_config_file(parallel_test_data):
     # Create config instance
     config = WorkflowConfig()
     # import config file
-    config.import_config(config_file=test_data.workflowconfig_template_file)
+    config.import_config(config_file=parallel_test_data.workflowconfig_template_file)
 
-    assert vars(config) == test_data.workflowconfig_template()
+    assert vars(config) == parallel_test_data.workflowconfig_template()
 
 
-def test_plantcv_parallel_workflowconfig_validate_config(test_data, tmpdir):
+def test_validate_config(parallel_test_data, tmpdir):
     # Create a test tmp directory
     img_outdir = tmpdir.mkdir("cache")
     # Create config instance
     config = WorkflowConfig()
     # Set valid values in config
-    config.input_dir = test_data.flat_imgdir
+    config.input_dir = parallel_test_data.flat_imgdir
     config.json = "valid_config.json"
     config.filename_metadata = ["imgtype", "camera", "frame", "zoom", "lifter", "gain", "exposure", "id"]
-    config.workflow = test_data.workflow_script
+    config.workflow = parallel_test_data.workflow_script
     config.img_outdir = img_outdir
     # Validate config
     assert config.validate_config()
 
 
-def test_plantcv_parallel_workflowconfig_invalid_startdate(test_data, tmpdir):
+def test_invalid_startdate(parallel_test_data, tmpdir):
     # Create a test tmp directory
     img_outdir = tmpdir.mkdir("cache")
     # Create config instance
     config = WorkflowConfig()
     # Set valid values in config
-    config.input_dir = test_data.flat_imgdir
+    config.input_dir = parallel_test_data.flat_imgdir
     config.json = "valid_config.json"
     config.filename_metadata = ["imgtype", "camera", "frame", "zoom", "lifter", "gain", "exposure", "id"]
-    config.workflow = test_data.workflow_script
+    config.workflow = parallel_test_data.workflow_script
     config.img_outdir = img_outdir
     config.start_date = "2020-05-10"
     # Validate config
     assert not config.validate_config()
 
 
-def test_plantcv_parallel_workflowconfig_invalid_enddate(test_data, tmpdir):
+def test_invalid_enddate(parallel_test_data, tmpdir):
     # Create a test tmp directory
     img_outdir = tmpdir.mkdir("cache")
     # Create config instance
     config = WorkflowConfig()
     # Set valid values in config
-    config.input_dir = config.input_dir = test_data.flat_imgdir
+    config.input_dir = config.input_dir = parallel_test_data.flat_imgdir
     config.json = "valid_config.json"
     config.filename_metadata = ["imgtype", "camera", "frame", "zoom", "lifter", "gain", "exposure", "id"]
-    config.workflow = config.workflow = test_data.workflow_script
+    config.workflow = config.workflow = parallel_test_data.workflow_script
     config.img_outdir = img_outdir
     config.end_date = "2020-05-10"
     config.timestampformat = "%Y%m%d"
@@ -70,7 +70,7 @@ def test_plantcv_parallel_workflowconfig_invalid_enddate(test_data, tmpdir):
     assert not config.validate_config()
 
 
-def test_plantcv_parallel_workflowconfig_invalid_metadata_terms():
+def test_invalid_metadata_terms():
     # Create config instance
     config = WorkflowConfig()
     # Set invalid values in config
@@ -81,7 +81,7 @@ def test_plantcv_parallel_workflowconfig_invalid_metadata_terms():
     assert not config.validate_config()
 
 
-def test_plantcv_parallel_workflowconfig_invalid_filename_metadata():
+def test_invalid_filename_metadata():
     # Create config instance
     config = WorkflowConfig()
     # Set invalid values in config
@@ -91,7 +91,7 @@ def test_plantcv_parallel_workflowconfig_invalid_filename_metadata():
     assert not config.validate_config()
 
 
-def test_plantcv_parallel_workflowconfig_invalid_cluster():
+def test_invalid_cluster():
     # Create config instance
     config = WorkflowConfig()
     # Set invalid values in config
