726dda47465b06d45aaa5f44e858cc073f55ec10
HaleySchuhl <haleyschuhl@gmail.com>
2021-12-21 10:13:58 -0600

    re-arrange tests

--- tests/tests.py
+++ tests/tests.py
@@ -5202,6 +5202,10 @@ def test_plantcv_roi_custom_bad_input():
         _ = pcv.roi.custom(img=img, vertices=[[226, -1], [3130, 1848], [2404, 2029], [2205, 2298], [1617, 1761]])
 
 
+# ##############################
+# Tests for the annotate subpackage
+# ##############################
+
 def test_plantcv_annotate_Points_interactive():
     # Read in a test grayscale image
     img = cv2.imread(os.path.join(TEST_DATA, TEST_INPUT_COLOR), -1)
@@ -5242,6 +5246,61 @@ def test_plantcv_annotate_Points_interactive():
     drawer_rgb.onclick(e5)
 
     assert drawer_rgb.points[0] == point1
+
+
+def test_plantcv_annotate_img_registration(tmpdir):
+    # Create a test tmp directory
+    cache_dir = tmpdir.mkdir("sub")
+    # generate fake testing images
+    img_ref = create_test_img((12, 10, 3))
+    img_tar = create_test_img((12, 10, 3))
+    img_registrator = pcv.annotate.ImageRegistrator(img_ref, img_tar)
+
+    # create mock events: left click
+    for pt in [(0, 0), (1, 0), (0, 3), (4, 4), (3, 2)]:
+        # left click, left axis
+        e1 = matplotlib.backend_bases.MouseEvent(name="button_press_event", canvas=img_registrator.fig.canvas,
+                                                 x=0, y=0, button=1)
+        e1.inaxes = img_registrator.axes[0]
+        e1.inaxes._subplotspec = matplotlib.gridspec.SubplotSpec(
+            matplotlib.gridspec.GridSpec(1, 2, img_registrator.fig), 0)
+        e1.xdata, e1.ydata = pt
+        img_registrator.onclick(e1)
+
+        # left click, right axis
+        e2 = matplotlib.backend_bases.MouseEvent(name="button_press_event", canvas=img_registrator.fig.canvas,
+                                                 x=0, y=0, button=1)
+        e2.inaxes = img_registrator.axes[0]
+        e2.inaxes._subplotspec = matplotlib.gridspec.SubplotSpec(
+            matplotlib.gridspec.GridSpec(1, 2, img_registrator.fig), 1)
+        e2.xdata, e2.ydata = pt
+        img_registrator.onclick(e2)
+    # right click, left axis
+    e1_ = matplotlib.backend_bases.MouseEvent(name="button_press_event", canvas=img_registrator.fig.canvas,
+                                              x=0, y=0, button=3)
+    e1_.inaxes = img_registrator.axes[0]
+    e1_.inaxes._subplotspec = matplotlib.gridspec.SubplotSpec(matplotlib.gridspec.GridSpec(1, 2, img_registrator.fig),
+                                                              0)
+    e1_.xdata, e1_.ydata = 3, 2
+    img_registrator.onclick(e1_)
+
+    # right click, right axis
+    e2_ = matplotlib.backend_bases.MouseEvent(name="button_press_event", canvas=img_registrator.fig.canvas,
+                                              x=0, y=0, button=3)
+    e2_.inaxes = img_registrator.axes[0]
+    e2_.inaxes._subplotspec = matplotlib.gridspec.SubplotSpec(matplotlib.gridspec.GridSpec(1, 2, img_registrator.fig),
+                                                              1)
+    e2_.xdata, e2_.ydata = 3, 2
+    img_registrator.onclick(e2_)
+
+    img_registrator.regist()
+    img_registrator.display_coords()
+    img_registrator.save_model(model_file=os.path.join(cache_dir, "model"))
+
+    assert img_registrator.model is not None \
+           and len(img_registrator.points[0]) == 4 \
+           and len(img_registrator.points[1]) == 4 \
+           and os.path.isfile(os.path.join(cache_dir, "model.pkl"))
 
 
 # ##############################
@@ -5792,59 +5851,7 @@ def test_plantcv_transform_nonuniform_illumination_gray():
     corrected = pcv.transform.nonuniform_illumination(img=gray_img, ksize=11)
     assert np.shape(corrected) == np.shape(gray_img)
     
-    
-def test_plantcv_transform_img_registration(tmpdir):
-    # Create a test tmp directory
-    cache_dir = tmpdir.mkdir("sub")
-    # generate fake testing images
-    img_ref = create_test_img((12, 10, 3))
-    img_tar = create_test_img((12, 10, 3))
-    img_registrator = pcv.transform.ImageRegistrator(img_ref, img_tar)
 
-    # create mock events: left click
-    for pt in [(0, 0), (1, 0), (0, 3), (4, 4), (3,2)]:
-        # left click, left axis
-        e1 = matplotlib.backend_bases.MouseEvent(name="button_press_event", canvas=img_registrator.fig.canvas,
-                                                 x=0, y=0, button=1)
-        e1.inaxes = img_registrator.axes[0]
-        e1.inaxes._subplotspec = matplotlib.gridspec.SubplotSpec(matplotlib.gridspec.GridSpec(1,2,img_registrator.fig), 0)
-        e1.xdata, e1.ydata = pt
-        img_registrator.onclick(e1)
-
-        # left click, right axis
-        e2 = matplotlib.backend_bases.MouseEvent(name="button_press_event", canvas=img_registrator.fig.canvas,
-                                                 x=0, y=0, button=1)
-        e2.inaxes = img_registrator.axes[0]
-        e2.inaxes._subplotspec = matplotlib.gridspec.SubplotSpec(matplotlib.gridspec.GridSpec(1,2,img_registrator.fig), 1)
-        e2.xdata, e2.ydata = pt
-        img_registrator.onclick(e2)
-    # right click, left axis
-    e1_ = matplotlib.backend_bases.MouseEvent(name="button_press_event", canvas=img_registrator.fig.canvas,
-                                             x=0, y=0, button=3)
-    e1_.inaxes = img_registrator.axes[0]
-    e1_.inaxes._subplotspec = matplotlib.gridspec.SubplotSpec(matplotlib.gridspec.GridSpec(1, 2, img_registrator.fig), 0)
-    e1_.xdata, e1_.ydata = 3, 2
-    img_registrator.onclick(e1_)
-
-    # right click, right axis
-    e2_ = matplotlib.backend_bases.MouseEvent(name="button_press_event", canvas=img_registrator.fig.canvas,
-                                             x=0, y=0, button=3)
-    e2_.inaxes = img_registrator.axes[0]
-    e2_.inaxes._subplotspec = matplotlib.gridspec.SubplotSpec(matplotlib.gridspec.GridSpec(1, 2, img_registrator.fig), 1)
-    e2_.xdata, e2_.ydata = 3, 2
-    img_registrator.onclick(e2_)
-
-
-    img_registrator.regist()
-    img_registrator.display_coords()
-    img_registrator.save_model(model_file=os.path.join(cache_dir, "model"))
-
-    assert img_registrator.model is not None \
-           and len(img_registrator.points[0]) == 4 \
-           and len(img_registrator.points[1]) == 4 \
-           and os.path.isfile(os.path.join(cache_dir, "model.pkl"))
-    
-    
 def test_plantcv_transform_img_registration_gray():
     # generate fake testing images
     img_ref = create_test_img((12, 10))
