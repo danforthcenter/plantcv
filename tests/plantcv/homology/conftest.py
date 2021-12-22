60649b00f57681de2749f09029b3c519c929b236
Noah Fahlgren <noahfahlgren@gmail.com>
2021-12-22 11:01:40 -0600

    Initialize tests subpackages

---
+++ tests/plantcv/homology/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+import os
+import matplotlib
+
+# Disable plotting
+matplotlib.use("Template")
+
+
+class HomologyTestData:
+    def __init__(self):
+        """Initialize simple variables."""
+        # Test data directory
+        self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "testdata")
+
+
+@pytest.fixture(scope="session")
+def homology_test_data():
+    return HomologyTestData()
---
+++ tests/plantcv/morphology/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+import os
+import matplotlib
+
+# Disable plotting
+matplotlib.use("Template")
+
+
+class MorphologyTestData:
+    def __init__(self):
+        """Initialize simple variables."""
+        # Test data directory
+        self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "testdata")
+
+
+@pytest.fixture(scope="session")
+def morphology_test_data():
+    return MorphologyTestData()
