32a2dc821d4c82b580ca0d33e763293e6a8d4adf
Noah Fahlgren <noahfahlgren@gmail.com>
2021-12-22 10:50:07 -0600

    Add naive_bayes tests module

---
+++ tests/learn/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+import os
+import matplotlib
+
+# Disable plotting
+matplotlib.use("Template")
+
+
+class LearnTestData:
+    def __init__(self):
+        """Initialize simple variables."""
+        # Test data directory
+        self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "testdata")
+        # RGB image
+        self.setaria_small_rgb_img = os.path.join(self.datadir, "setaria_small_rgb_img.png")
+        # Binary mask
+        self.setaria_small_bin_img = os.path.join(self.datadir, "setaria_small_bin_img.png")
+        # RGB values table
+        self.rgb_values_table = os.path.join(self.datadir, "rgb_values_table.txt")
+
+
+@pytest.fixture(scope="session")
+def learn_test_data():
+    return LearnTestData()
---
+++ tests/learn/test_naive_bayes.py
@@ -0,0 +1,29 @@
+import os
+from shutil import copyfile
+from plantcv.learn import naive_bayes, naive_bayes_multiclass
+
+
+def test_naive_bayes(learn_test_data, tmpdir):
+    # Create tmp directory
+    tmp_dir = tmpdir.mkdir("cache")
+    imgdir = os.path.join(str(tmp_dir), "images")
+    maskdir = os.path.join(str(tmp_dir), "masks")
+    # Make image and mask directories in the cache directory
+    os.makedirs(imgdir, exist_ok=True)
+    os.makedirs(maskdir, exist_ok=True)
+    # Copy and image and mask to the image/mask directories
+    copyfile(learn_test_data.setaria_small_rgb_img, os.path.join(imgdir, "image.png"))
+    copyfile(learn_test_data.setaria_small_bin_img, os.path.join(maskdir, "image.png"))
+    # Run the naive Bayes training module
+    outfile = os.path.join(str(tmp_dir), "naive_bayes_pdfs.txt")
+    naive_bayes(imgdir=imgdir, maskdir=maskdir, outfile=outfile, mkplots=True)
+    assert os.path.exists(outfile)
+
+
+def test_naive_bayes_multiclass(learn_test_data, tmpdir):
+    # Create tmp directory
+    tmp_dir = tmpdir.mkdir("cache")
+    # Run the naive Bayes multiclass training module
+    outfile = os.path.join(str(tmp_dir), "naive_bayes_multiclass_pdfs.txt")
+    naive_bayes_multiclass(samples_file=learn_test_data.rgb_values_table, outfile=outfile, mkplots=True)
+    assert os.path.exists(outfile)
