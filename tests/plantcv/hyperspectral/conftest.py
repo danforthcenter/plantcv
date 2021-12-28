c4b36dfe22e1d79e496800d4d94a2d59c9a26b95
Noah Fahlgren <noahfahlgren@gmail.com>
2021-12-27 19:46:27 -0600

    Create hyperspectral read_data tests module

--- tests/plantcv/hyperspectral/conftest.py
+++ tests/plantcv/hyperspectral/conftest.py
@@ -10,7 +10,11 @@ class HyperspectralTestData:
     def __init__(self):
         """Initialize simple variables."""
         # Test data directory
-        self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "testdata")
+        self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "testdata")
+        self.envi_bil_file = os.path.join(self.datadir, "darkReference")
+        self.envi_no_default = os.path.join(self.datadir, "darkReference2")
+        self.envi_appox_pseudo = os.path.join(self.datadir, "darkReference3")
+        self.envi_bad_interleave = os.path.join(self.datadir, "darkReference4")
 
 
 @pytest.fixture(scope="session")
