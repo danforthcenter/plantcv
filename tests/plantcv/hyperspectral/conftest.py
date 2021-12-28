d162f977b52e9d349a78f9952a2e6929b2c802e6
Noah Fahlgren <noahfahlgren@gmail.com>
2021-12-28 12:23:38 -0600

    Create analyze_spectral tests module

--- tests/plantcv/hyperspectral/conftest.py
+++ tests/plantcv/hyperspectral/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 import os
 import matplotlib
+import pickle as pkl
 
 # Disable plotting
 matplotlib.use("Template")
@@ -15,6 +16,13 @@ class HyperspectralTestData:
         self.envi_no_default = os.path.join(self.datadir, "darkReference2")
         self.envi_appox_pseudo = os.path.join(self.datadir, "darkReference3")
         self.envi_bad_interleave = os.path.join(self.datadir, "darkReference4")
+        self.hsi_file = os.path.join(self.datadir, "hsi.pkl")
+        self.hsi_mask_file = os.path.join(self.datadir, "hsi_mask.png")
+
+    def load_hsi(self):
+        """Load PlantCV Spectral_data pickled object."""
+        with open(self.hsi_file, "rb") as fp:
+            return pkl.load(fp)
 
 
 @pytest.fixture(scope="session")
