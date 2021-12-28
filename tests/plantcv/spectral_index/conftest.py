aebbf971c1fb8f3f649bcefb547003882659836b
Noah Fahlgren <noahfahlgren@gmail.com>
2021-12-28 12:07:59 -0600

    Create spectral_index tests module

--- tests/plantcv/spectral_index/conftest.py
+++ tests/plantcv/spectral_index/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 import os
 import matplotlib
+import pickle as pkl
 
 # Disable plotting
 matplotlib.use("Template")
@@ -10,7 +11,14 @@ class SpectralIndexTestData:
     def __init__(self):
         """Initialize simple variables."""
         # Test data directory
-        self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "testdata")
+        self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "testdata")
+        self.hsi_file = os.path.join(self.datadir, "hsi.pkl")
+        self.camelina_rgb_img = os.path.join(self.datadir, "camelina_rgb_img.jpg")
+
+    def load_hsi(self):
+        """Load PlantCV Spectral_data pickled object."""
+        with open(self.hsi_file, "rb") as fp:
+            return pkl.load(fp)
 
 
 @pytest.fixture(scope="session")
