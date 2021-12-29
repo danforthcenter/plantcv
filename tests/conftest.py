02e46b24c168abab87e1e41240b82f565a13523f
Noah Fahlgren <noahfahlgren@gmail.com>
2021-12-29 11:06:09 -0600

    Create outputs tests module

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,10 +12,18 @@ class TestData:
         """Initialize simple variables."""
         # Test data directory
         self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testdata")
+        # RGB image
         self.camelina_rgb_img = os.path.join(self.datadir, "camelina_rgb_img.jpg")
+        # Binary mask for RGB image
         self.camelina_bin_img = os.path.join(self.datadir, "camelina_bin_img.png")
+        # PlantCV Spectral_data object
         self.hsi_file = os.path.join(self.datadir, "hsi.pkl")
+        # Binary mask for HSI
         self.hsi_mask_file = os.path.join(self.datadir, "hsi_mask.png")
+        # Outputs results file - JSON
+        self.outputs_results_json = os.path.join(self.datadir, "outputs_results.json")
+        # Outputs results file - CSV
+        self.outputs_results_csv = os.path.join(self.datadir, "outputs_results.csv")
 
     def load_hsi(self, pkl_file):
         """Load PlantCV Spectral_data pickled object."""
