91e57cce7a861ab96edc5fa7ce73aa7ed18450d3
Noah Fahlgren <noahfahlgren@gmail.com>
2021-12-29 10:30:26 -0600

    Create apply_mask tests module

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 import os
+import pickle as pkl
 import matplotlib
 
 # Disable plotting
@@ -12,6 +13,14 @@ class TestData:
         # Test data directory
         self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testdata")
         self.camelina_rgb_img = os.path.join(self.datadir, "camelina_rgb_img.jpg")
+        self.camelina_bin_img = os.path.join(self.datadir, "camelina_bin_img.png")
+        self.hsi_file = os.path.join(self.datadir, "hsi.pkl")
+        self.hsi_mask_file = os.path.join(self.datadir, "hsi_mask.png")
+
+    def load_hsi(self, pkl_file):
+        """Load PlantCV Spectral_data pickled object."""
+        with open(pkl_file, "rb") as fp:
+            return pkl.load(fp)
 
 
 @pytest.fixture(scope="session")
