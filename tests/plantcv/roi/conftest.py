8435e59e7868bdb0e708cf353ebafe5b400aacc1
Noah Fahlgren <noahfahlgren@gmail.com>
2021-12-30 17:43:00 -0600

    Create roi_methods tests module

--- tests/plantcv/roi/conftest.py
+++ tests/plantcv/roi/conftest.py
@@ -10,7 +10,11 @@ class RoiTestData:
     def __init__(self):
         """Initialize simple variables."""
         # Test data directory
-        self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "testdata")
+        self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "testdata")
+        # RGB image
+        self.small_rgb_img = os.path.join(self.datadir, "setaria_small_rgb_img.png")
+        # Gray image
+        self.small_gray_img = os.path.join(self.datadir, "setaria_small_gray_img.png")
 
 
 @pytest.fixture(scope="session")
