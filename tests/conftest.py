44decfe6fbd87135153a17b78f0bf3c493bb2329
Noah Fahlgren <noahfahlgren@gmail.com>
2021-12-30 11:26:21 -0600

    Use smaller test data

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,11 +13,11 @@ class TestData:
         # Test data directory
         self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testdata")
         # RGB image
-        self.large_rgb_img = os.path.join(self.datadir, "camelina_rgb_img.jpg")
+        self.small_rgb_img = os.path.join(self.datadir, "setaria_small_rgb_img.png")
         # Binary mask for RGB image
-        self.large_bin_img = os.path.join(self.datadir, "camelina_bin_img.png")
+        self.small_bin_img = os.path.join(self.datadir, "setaria_small_bin_img.png")
         # Gray image
-        self.large_gray_img = os.path.join(self.datadir, "camelina_gray_img.jpg")
+        self.small_gray_img = os.path.join(self.datadir, "setaria_small_gray_img.png")
         # PlantCV Spectral_data object
         self.hsi_file = os.path.join(self.datadir, "hsi.pkl")
         # Binary mask for HSI
--- tests/plantcv/test_readimage.py
+++ tests/plantcv/test_readimage.py
@@ -4,8 +4,8 @@ from plantcv.plantcv import readimage
 
 
 def test_plantcv_readimage_native(test_data):
-    img, path, img_name = readimage(filename=test_data.large_rgb_img, mode='native')
-    expected = [3] + list(os.path.split(test_data.large_rgb_img))
+    img, path, img_name = readimage(filename=test_data.small_rgb_img, mode='native')
+    expected = [3] + list(os.path.split(test_data.small_rgb_img))
     # Assert that the image name returned equals the name of the input image
     # Assert that the path of the image returned equals the path of the input image
     # Assert that the dimensions of the returned image equals the expected dimensions
@@ -14,12 +14,12 @@ def test_plantcv_readimage_native(test_data):
 
 @pytest.mark.parametrize("mode", ["gray", "grey"])
 def test_readimage_grayscale(mode, test_data):
-    img, _, _ = readimage(filename=test_data.large_gray_img, mode=mode)
+    img, _, _ = readimage(filename=test_data.small_gray_img, mode=mode)
     assert len(img.shape) == 2
 
 
 def test_readimage_rgb(test_data):
-    img, _, _ = readimage(filename=test_data.large_gray_img, mode="rgb")
+    img, _, _ = readimage(filename=test_data.small_gray_img, mode="rgb")
     assert len(img.shape) == 3
 
 
