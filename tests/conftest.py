bb01d4462cd2d1c6e261f86f7bf90249391bc516
Noah Fahlgren <noahfahlgren@gmail.com>
2021-12-30 11:38:08 -0600

    Create readbayer tests module

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,11 +27,15 @@ class TestData:
         # Outputs results file - CSV
         self.outputs_results_csv = os.path.join(self.datadir, "outputs_results.csv")
         # RGBA image
+        # Image from http://www.libpng.org/pub/png/png-OwlAlpha.html
+        # This image may be used, edited and reproduced freely.
         self.rgba_img = os.path.join(self.datadir, "owl_rgba_img.png")
         # ENVI hyperspectral data
         self.envi_bil_file = os.path.join(self.datadir, "darkReference")
         # Thermal image
         self.thermal_img = os.path.join(self.datadir, "FLIR2600.csv")
+        # Bayer image
+        self.bayer_img = os.path.join(self.datadir, "bayer_img.png")
 
     def load_hsi(self, pkl_file):
         """Load PlantCV Spectral_data pickled object."""
---
+++ tests/plantcv/test_readbayer.py
@@ -0,0 +1,24 @@
+import pytest
+from plantcv.plantcv import readbayer
+
+
+@pytest.mark.parametrize("alg, pattern", [["default", 'BG'],
+                                          ["default", 'GB'],
+                                          ["default", 'RG'],
+                                          ["default", 'GR'],
+                                          ["edgeaware", 'BG'],
+                                          ["edgeaware", 'GB'],
+                                          ["edgeaware", 'RG'],
+                                          ["edgeaware", 'GR'],
+                                          ["variablenumbergradients", 'BG'],
+                                          ["variablenumbergradients", 'GB'],
+                                          ["variablenumbergradients", 'RG'],
+                                          ["variablenumbergradients", 'GR']])
+def test_readbayer(alg, pattern, test_data):
+    img, _, _ = readbayer(filename=test_data.bayer_img, bayerpattern=pattern, alg=alg)
+    assert img.shape == (335, 400, 3)
+
+
+def test_readbayer_default_bad_input():
+    with pytest.raises(RuntimeError):
+        _, _, _ = readbayer(filename="no-image.png", bayerpattern="GR", alg="default")
