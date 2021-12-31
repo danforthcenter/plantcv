893a3583075ba340ce4c9b0833fc5ecbb8904518
Noah Fahlgren <noahfahlgren@gmail.com>
2021-12-31 11:55:15 -0600

    Add color_correct tests module

--- tests/plantcv/transform/conftest.py
+++ tests/plantcv/transform/conftest.py
@@ -16,6 +16,34 @@ class TransformTestData:
         self.small_rgb_img = os.path.join(self.datadir, "setaria_small_plant_rgb.png")
         # Gray image
         self.small_gray_img = os.path.join(self.datadir, "setaria_small_plant_gray.png")
+        # Target RGB image
+        self.target_img = os.path.join(self.datadir, "target_img.png")
+        # Target matrix file
+        self.target_matrix_file = os.path.join(self.datadir, "target_matrix.npz")
+        # Color card mask
+        self.colorcard_mask = os.path.join(self.datadir, "colorcard_mask.png")
+        # Source 1 RGB image
+        self.source1_img = os.path.join(self.datadir, "source1_img.png")
+        # Source 1 matrix file
+        self.source1_matrix_file = os.path.join(self.datadir, "source1_matrix.npz")
+        # Source 2 RGB image
+        self.source2_img = os.path.join(self.datadir, "source2_img.png")
+        # Source 1 matrix file
+        self.source2_matrix_file = os.path.join(self.datadir, "source2_matrix.npz")
+        # Matrix M (source 1)
+        self.matrix_m1_file = os.path.join(self.datadir, "matrix_m1.npz")
+        # Matrix B (source 1)
+        self.matrix_b1_file = os.path.join(self.datadir, "matrix_b1.npz")
+        # Matrix M (source 2)
+        self.matrix_m2_file = os.path.join(self.datadir, "matrix_m2.npz")
+        # Matrix B (source 2)
+        self.matrix_b2_file = os.path.join(self.datadir, "matrix_b2.npz")
+        # Transformation matrix
+        self.transformation_matrix_file = os.path.join(self.datadir, "transformation_matrix.npz")
+        # Corrected source image
+        self.source_corrected = os.path.join(self.datadir, "source_corrected.png")
+        # Color card image
+        self.colorcard_img = os.path.join(self.datadir, "colorcard_img.png")
 
     def create_test_img(self, sz_img):
         img = np.random.randint(np.prod(sz_img), size=sz_img) * 255
@@ -26,6 +54,11 @@ class TransformTestData:
         img = np.zeros(sz_img)
         img[3:7, 2:8] = 1
         return img
+
+    def load_npz(self, npz_file):
+        """Load data saved in a NumPy .npz file."""
+        data = np.load(npz_file, encoding="latin1")
+        return data['arr_0']
 
 
 @pytest.fixture(scope="session")
