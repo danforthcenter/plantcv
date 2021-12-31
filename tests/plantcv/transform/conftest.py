9bd0dec5e313e8b4eaae5d9e349889dfe7526aea
Noah Fahlgren <noahfahlgren@gmail.com>
2021-12-31 09:54:45 -0600

    Create warp tests module

--- tests/plantcv/transform/conftest.py
+++ tests/plantcv/transform/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 import os
+import numpy as np
 import matplotlib
 
 # Disable plotting
@@ -10,7 +11,17 @@ class TransformTestData:
     def __init__(self):
         """Initialize simple variables."""
         # Test data directory
-        self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "testdata")
+        self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "testdata")
+
+    def create_test_img(self, sz_img):
+        img = np.random.randint(np.prod(sz_img), size=sz_img) * 255
+        img = img.astype(np.uint8)
+        return img
+
+    def create_test_img_bin(self, sz_img):
+        img = np.zeros(sz_img)
+        img[3:7, 2:8] = 1
+        return img
 
 
 @pytest.fixture(scope="session")
