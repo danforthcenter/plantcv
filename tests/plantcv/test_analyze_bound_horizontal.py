f20c48b09c099f7fdd9ea070120eaeb423cd5a0a
Noah Fahlgren <noahfahlgren@gmail.com>
2022-01-05 13:49:59 -0600

    Add analyze_bound_vertical tests module

--- tests/plantcv/test_analyze_bound_horizontal.py
+++ tests/plantcv/test_analyze_bound_horizontal.py
@@ -9,13 +9,8 @@ def test_analyze_bound_horizontal(pos, exp, test_data):
     outputs.clear()
     # Read in test data
     img = cv2.imread(test_data.small_rgb_img)
-    # img_above_bound_only = cv2.imread(os.path.join(TEST_DATA, TEST_MASK_SMALL_PLANT))
     mask = cv2.imread(test_data.small_bin_img, -1)
     object_contours = test_data.load_composed_contours(test_data.small_composed_contours_file)
-    # _ = pcv.analyze_bound_horizontal(img=img, obj=object_contours, mask=mask, line_position=300)
-    # _ = pcv.analyze_bound_horizontal(img=img, obj=object_contours, mask=mask, line_position=100)
-    # _ = pcv.analyze_bound_horizontal(img=img_above_bound_only, obj=object_contours, mask=mask, line_position=1756)
-    # _ = pcv.analyze_bound_horizontal(img=img, obj=object_contours, mask=mask, line_position=1756)
     _ = analyze_bound_horizontal(img=img, obj=object_contours, mask=mask, line_position=pos)
     assert outputs.observations["default"]["height_above_reference"]["value"] == exp
 
