c788ff97a48d54d2b2c2cb181a1a0a95d100b850
Noah Fahlgren <noahfahlgren@gmail.com>
2022-01-08 16:34:08 -0600

    Add flip tests module

---
+++ tests/plantcv/test_flip.py
@@ -0,0 +1,23 @@
+import pytest
+import cv2
+from plantcv.plantcv import flip
+
+
+def test_flip(test_data):
+    # Read in test data
+    img = cv2.imread(test_data.small_rgb_img)
+    flipped_img = flip(img=img, direction="horizontal")
+    assert img.shape == flipped_img.shape
+
+
+def test_flip_grayscale(test_data):
+    # Read in test data
+    gray_img = cv2.imread(test_data.small_gray_img, -1)
+    flipped_img = flip(img=gray_img, direction="vertical")
+    assert gray_img.shape == flipped_img.shape
+
+
+def test_flip_bad_input(test_data):
+    img = cv2.imread(test_data.small_rgb_img)
+    with pytest.raises(RuntimeError):
+        _ = flip(img=img, direction="vert")
