16e7b56e047aa8cf0742f90951a0f9492c48f7de
Noah Fahlgren <noahfahlgren@gmail.com>
2022-01-15 15:52:25 -0600

    Add analyze_fvfm tests module

--- tests/plantcv/photosynthesis/conftest.py
+++ tests/plantcv/photosynthesis/conftest.py
@@ -13,6 +13,14 @@ class PhotosynthesisTestData:
         self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "testdata")
         # CropReporter data file
         self.cropreporter = os.path.join(self.datadir, "PSII_PSD_supopt_temp_btx623_22_rep1.DAT")
+        # Fdark image
+        self.fdark = os.path.join(self.datadir, "FLUO_TV_dark.png")
+        # Fmin image
+        self.fmin = os.path.join(self.datadir, "FLUO_TV_min.png")
+        # Fmax image
+        self.fmax = os.path.join(self.datadir, "FLUO_TV_max.png")
+        # Mask image
+        self.ps_mask = os.path.join(self.datadir, "FLUO_TV_MASK.png")
 
 
 @pytest.fixture(scope="session")
