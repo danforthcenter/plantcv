17bc5c11ba5d9afea1d5da0041df693b5057f809
Noah Fahlgren <noahfahlgren@gmail.com>
2022-01-15 15:33:44 -0600

    Add read_cropreporter tests module

--- tests/plantcv/photosynthesis/conftest.py
+++ tests/plantcv/photosynthesis/conftest.py
@@ -10,7 +10,9 @@ class PhotosynthesisTestData:
     def __init__(self):
         """Initialize simple variables."""
         # Test data directory
-        self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "testdata")
+        self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "testdata")
+        # CropReporter data file
+        self.cropreporter = os.path.join(self.datadir, "PSII_PSD_supopt_temp_btx623_22_rep1.DAT")
 
 
 @pytest.fixture(scope="session")
