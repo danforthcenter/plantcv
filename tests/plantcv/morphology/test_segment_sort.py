9bc7ce0363df5402dcafa54e57db7c26b00176eb
Noah Fahlgren <noahfahlgren@gmail.com>
2022-01-21 11:26:50 -0600

    Add segment_sort tests module

---
+++ tests/plantcv/morphology/test_segment_sort.py
@@ -0,0 +1,17 @@
+import cv2
+from plantcv.plantcv.morphology import segment_sort
+
+
+def test_segment_sort(morphology_test_data):
+    skeleton = cv2.imread(morphology_test_data.skel_img, -1)
+    leaf_obj, _ = segment_sort(skel_img=skeleton,
+                               objects=morphology_test_data.load_segments(morphology_test_data.segments_file, "edges"),
+                               mask=skeleton)
+    assert len(leaf_obj) == 4
+
+
+def test_segment_sort_no_mask(morphology_test_data):
+    skeleton = cv2.imread(morphology_test_data.skel_img, -1)
+    leaf_obj, _ = segment_sort(skel_img=skeleton,
+                               objects=morphology_test_data.load_segments(morphology_test_data.segments_file, "edges"))
+    assert len(leaf_obj) == 4
