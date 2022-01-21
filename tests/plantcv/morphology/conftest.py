96c919a38ee8d5bd5735a5fb6c47c02161479695
Noah Fahlgren <noahfahlgren@gmail.com>
2022-01-21 11:23:08 -0600

    Add morphology test data

--- tests/plantcv/morphology/conftest.py
+++ tests/plantcv/morphology/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 import os
+import numpy as np
 import matplotlib
 
 # Disable plotting
@@ -10,7 +11,20 @@ class MorphologyTestData:
     def __init__(self):
         """Initialize simple variables."""
         # Test data directory
-        self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "testdata")
+        self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "testdata")
+        # Binary mask
+        self.bin_img = os.path.join(self.datadir, "morphology_bin_img.png")
+        # Skeleton image
+        self.skel_img = os.path.join(self.datadir, "morphology_skel_img.png")
+        # Skeleton segments
+        self.segments_file = os.path.join(self.datadir, "morphology_segments.npz")
+        # Mask image
+        self.ps_mask = os.path.join(self.datadir, "FLUO_TV_MASK.png")
+
+    def load_segments(self, npz_file, segment):
+        """Load data saved in a NumPy .npz file."""
+        data = np.load(npz_file, encoding="latin1", allow_pickle=True)
+        return data[segment].tolist()
 
 
 @pytest.fixture(scope="session")
