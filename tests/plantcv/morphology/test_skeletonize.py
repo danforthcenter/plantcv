22ec68969fdea709a6865cd9758d4f9742c7b682
Noah Fahlgren <noahfahlgren@gmail.com>
2022-01-21 11:27:03 -0600

    Add skeletonize tests module

---
+++ tests/plantcv/morphology/test_skeletonize.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+from plantcv.plantcv.morphology import skeletonize
+
+
+def test_skeletonize(morphology_test_data):
+    mask = cv2.imread(morphology_test_data.bin_img, -1)
+    input_skeleton = cv2.imread(morphology_test_data.skel_img, -1)
+    skeleton = skeletonize(mask=mask)
+    arr = np.array(skeleton == input_skeleton)
+    assert arr.all()
