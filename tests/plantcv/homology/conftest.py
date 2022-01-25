7064d98e5931aa81675edc691fbc3a110c41ec92
Noah Fahlgren <noahfahlgren@gmail.com>
2022-01-25 16:51:35 -0600

    Add starscape tests module

--- tests/plantcv/homology/conftest.py
+++ tests/plantcv/homology/conftest.py
@@ -15,12 +15,16 @@ class HomologyTestData:
         self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "testdata")
         # RGB image
         self.small_rgb_img = os.path.join(self.datadir, "setaria_small_plant_rgb.png")
+        # Gray image
+        self.small_gray_img = os.path.join(self.datadir, "setaria_small_plant_gray.png")
         # Binary mask for RGB image
         self.small_bin_img = os.path.join(self.datadir, "setaria_small_plant_mask.png")
         # Composed contours file
         self.small_composed_contours_file = os.path.join(self.datadir, "setaria_small_plant_composed_contours.npz")
         # Pseudolandmarks CSV file
         self.plms = os.path.join(self.datadir, "plms_df.csv")
+        # Pseudolandmarks Space CSV file
+        self.plms_space = os.path.join(self.datadir, "plms_space_df.csv")
 
     @staticmethod
     def load_composed_contours(npz_file):
