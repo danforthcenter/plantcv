e38a8173086596e281db68cdd0d5e62c7f5ab831
Noah Fahlgren <noahfahlgren@gmail.com>
2022-01-25 16:28:24 -0600

    Fix acute tests from 4x updates

--- tests/plantcv/homology/conftest.py
+++ tests/plantcv/homology/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 import os
+import numpy as np
 import matplotlib
 
 # Disable plotting
@@ -11,6 +12,18 @@ class HomologyTestData:
         """Initialize simple variables."""
         # Test data directory
         self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "testdata")
+        # RGB image
+        self.small_rgb_img = os.path.join(self.datadir, "setaria_small_plant_rgb.png")
+        # Binary mask for RGB image
+        self.small_bin_img = os.path.join(self.datadir, "setaria_small_plant_mask.png")
+        # Composed contours file
+        self.small_composed_contours_file = os.path.join(self.datadir, "setaria_small_plant_composed_contours.npz")
+
+    @staticmethod
+    def load_composed_contours(npz_file):
+        """Load data saved in a NumPy .npz file."""
+        data = np.load(npz_file, encoding="latin1")
+        return data['contour']
 
 
 @pytest.fixture(scope="session")
