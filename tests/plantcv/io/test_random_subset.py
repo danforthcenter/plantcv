a980e6ae3f02306161a8c7b8fe13603d8afcffd0
Noah Fahlgren <noahfahlgren@gmail.com>
2022-01-25 21:56:44 -0600

    Add io subpackage tests modules

---
+++ tests/plantcv/io/test_random_subset.py
@@ -0,0 +1,17 @@
+import pytest
+from plantcv.plantcv.io import random_subset
+
+
+def test_random_subset():
+    full_list = ['a', 'b', 'c', 'd', 'e']
+    n_samples = 3
+    samples_list = random_subset(dataset=full_list, num=n_samples, seed=None)
+    assert set(samples_list).issubset(set(full_list))
+
+
+def test_random_subset_greater_than_len():
+    full_list = ['a', 'b', 'c', 'd', 'e']
+    # test error when asking for one more sample than the existent number in the list
+    n_samples = len(full_list) + 1
+    with pytest.raises(RuntimeError):
+        _ = random_subset(dataset=full_list, num=n_samples, seed=None)
