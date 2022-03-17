d7a8e7776be68ccb8c39ab10815db4539b25f3df
JorgeGtz <jorge.gutierrezor@gmail.com>
2022-03-17 13:38:32 -0500

    remove handling of filenames of images, now a list of paths is required

--- plantcv/plantcv/visualize/time_lapse_video.py
+++ plantcv/plantcv/visualize/time_lapse_video.py
@@ -3,7 +3,6 @@
 import os
 import cv2
 import numpy as np
-import mimetypes
 from plantcv.plantcv import params
 from plantcv.plantcv import fatal_error
 from plantcv.plantcv.transform import resize
@@ -11,8 +10,7 @@ from plantcv.plantcv.transform import resize
 from plantcv.plantcv import warn
 
 
-def time_lapse_video(img_directory, list_img=None, auto_sort=True, suffix_img=None, size_frame=None, fps=29.97,
-                     name_video='time_lapse_video', path_video=None, display='on'):
+def time_lapse_video(list_img, size_frame=None, fps=29.97, out_filename='./time_lapse_video.mp4', display='on'):
     """ Generate time-lapse video given a folder of images
     Inputs:
     img_directory  = the directory of folder of images to make the time-lapse video.
@@ -55,57 +53,16 @@ def time_lapse_video(img_directory, list_img=None, auto_sort=True, suffix_img=No
     debug = params.debug
     params.debug = None
 
-    # Get the list of image files in the given directory and sort them alphabetically by their names
-    temp_list = []
-    for f in os.listdir(img_directory):
-        type_f = mimetypes.guess_type(f)[0]
-        if type_f:
-            if type_f.startswith('image'):
-                temp_list.append(f)
-    if len(temp_list) <= 0:
-        fatal_error("There is no file in the provided folder that is an image, please check the provided directory!")
-
-    # get an "extension free" list of images
-    temp_list_no_ext = [os.path.splitext(f)[0] if os.path.splitext(f)[1] else f for f in temp_list]
-
-    # if the list of images is provided, stick to it, but automatically handle the extension issue
-    if list_img is not None:
-        #  make the list of images extension free
-        list_img_no_ext = [os.path.splitext(f)[0] if os.path.splitext(f)[1] else f for f in list_img]
-        temp_set = set(temp_list_no_ext)
-        list_img_ = []
-        # check if the images in the given list exist in the directory by comparing both lists
-        # only include the images in the provided list who is also availabe in the given directory
-        for (f, f_) in zip(list_img_no_ext, list_img):
-            if f in temp_set:
-                list_img_.append(f_)
-
-        list_img = list_img_
-        if len(list_img) == 0:
-            fatal_error("The provided list of files is not contained in the given directory")
-        elif len(list_img) < len(list_img_no_ext):
-            # warnings.warn("Warning: Some files in the provided list not found, the video will be created based on "
-            #               "available files in the provided directory!")
-            warn("Some files in the provided list not found, the video will be created based on "
-                 "available files in the provided directory!")
-
-    # if the list of images is not provided, check if suffix information is available
-    elif suffix_img is not None:
-        list_img = [f for f in temp_list if f.endswith(suffix_img)]
-        if len(list_img) == 0:
-            fatal_error("There is no file that matches the provided suffix in the provided directory, please check again!")
-
-    # neither the list of image nor the suffix is provided
-    else:
-        list_img = temp_list
-    if auto_sort:
-        list_img.sort()
+    if len(list_img) <= 0:
+        fatal_error("Image list is empty")
 
     imgs = []
     list_r = []
     list_c = []
     for file in list_img:
-        img = cv2.imread(os.path.join(img_directory, file))
+        img = cv2.imread(file)
+        if img is None:
+            fatal_error(f"Unable to read {file}")
         list_r.append(img.shape[0])
         list_c.append(img.shape[1])
         imgs.append(img)
@@ -116,24 +73,20 @@ def time_lapse_video(img_directory, list_img=None, auto_sort=True, suffix_img=No
 
     if not (len(np.unique(list_r)) == 1 and len(np.unique(list_c)) == 1):
         warn("The sizes of images are not the same, an image resizing (cropping or zero-padding) will be done "
-             f"to make all images the same size ({size_frame[0]}x{size_frame[1]}) before creating the video! "
-             "If you assume the images should have the same size, please check the images used to generate this video!")
+             f"to make all images the same size ({size_frame[0]}x{size_frame[1]}) before creating the video! ")
 
-    # If the video saving directory is not provided, save it in the same directory of the images
-    if path_video is None:
-        path_video = img_directory
-
-    # Full video save name
-    save_name = os.path.join(path_video, name_video + '.mp4')
+    out_path, out_ext = os.path.splitext(out_filename)
+    if out_ext !=  '.mp4':
+        out_filename =  out_path + '.mp4'
 
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Be sure to use lower case
-    out = cv2.VideoWriter(save_name, fourcc, fps, size_frame)
+    out = cv2.VideoWriter(out_filename, fourcc, fps, size_frame)
 
     for img in imgs:
         out.write(resize(img, size_frame, interpolation=None))
     out.release()
     cv2.destroyAllWindows()
     if display == 'on':
-        print(f'The generated video: \n{save_name},\nAnd is saved here:\n{path_video}.')
+        print(f'Path to generated video: \n{out_filename}')
 
     return list_img, size_frame
