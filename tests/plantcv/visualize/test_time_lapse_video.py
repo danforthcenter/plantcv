a1bd790fa6b0b2ba5edce1861a7ee968c1a9d89d
JorgeGtz <jorge.gutierrezor@gmail.com>
2022-03-18 15:17:07 -0500

    tests for time lapse video errors working

--- tests/plantcv/visualize/test_time_lapse_video.py
+++ tests/plantcv/visualize/test_time_lapse_video.py
@@ -22,33 +22,20 @@ def test_plantcv_visualize_time_lapse_video_passes(display, tmpdir):
 
     vid_name = os.path.join(tmpdir, 'test_time_lapse_video.mp4')
     _, _ = time_lapse_video(img_list=list_im, out_filename=vid_name, fps=29.97, display=display)
-
     assert os.path.exists(vid_name)
 
-@pytest.mark.parametrize("img_dir_f, suffix_img, list_im_f",
-                         [["bad", None, None],    # bad image directory
-                         ["good", '.jpg', None],  # bad suffix
-                         ["good", None, "bad"]])  # bad image list
-def test_plantcv_visualize_time_lapse_video_errors(img_dir_f, suffix_img, list_im_f, tmpdir):
+@pytest.mark.parametrize("list_im_f",
+                         [([]),    # empty list
+                         (['./this_img_does_not_exist.png'])])  # non existent image
+def test_plantcv_visualize_time_lapse_video_errors(list_im_f, tmpdir):
     # Generate 3 test images and saved in cache_dir
-    for i in range(3):
-        temp_img = np.random.rand(3, 3)
-        min_, max_ = np.nanmin(temp_img), np.nanmax(temp_img)
-        temp_img = np.interp(temp_img, (min_, max_), (0, 255)).astype('uint8')
-        cv2.imwrite(os.path.join(tmpdir, f"img{i}.png"), temp_img)
-    if img_dir_f == "good":
-        img_dir = tmpdir
-    else:
-        img_dir = os.path.join(TEST_TMPDIR, 'visualize_time_lapse_video_bad')
-        os.mkdir(img_dir)
-    if list_im_f == "bad":
-        list_img_ = [img for img in os.listdir(tmpdir) if img.endswith('.png')]
-        list_img = [img.replace('.png', '_.png') for img in list_img_]
-    else:
-        list_img = None
-
+    # for i in range(3):
+    #     temp_img = np.random.rand(3, 3)
+    #     min_, max_ = np.nanmin(temp_img), np.nanmax(temp_img)
+    #     temp_img = np.interp(temp_img, (min_, max_), (0, 255)).astype('uint8')
+    #     cv2.imwrite(os.path.join(tmpdir, f"img{i}.png"), temp_img)
     with pytest.raises(RuntimeError):
-        pcv.visualize.time_lapse_video(img_directory=img_dir, suffix_img=suffix_img, img_list=list_img, fps=29.97)
+        _, _ = time_lapse_video(img_list=list_im_f, fps=29.97)
 
 # the correct directory of images as well as a list of files are provided, however the
 # list is incorrect (contains correct part, but also contains incorrect part)
