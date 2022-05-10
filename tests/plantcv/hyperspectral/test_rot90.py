76a98106aa85678ebdf5bff04b2527d3580e8485
HaleySchuhl <haleyschuhl@gmail.com>
2022-05-10 10:13:01 -0500

    Create test_rot90.py

---
+++ tests/plantcv/hyperspectral/test_rot90.py
@@ -0,0 +1,8 @@
+import numpy as np
+from plantcv.plantcv.hyperspectral import rot90
+
+
+def test_rot90(hyperspectral_test_data):
+    """Test for PlantCV."""
+    new = rot90(spectral_data=hyperspectral_test_data.load_hsi(hyperspectral_test_data.hsi_file), k=1)
+    assert np.shape(new.array_data) == (1600, 1, 978)
