2018f85c20b3dba00534bb59f2441b96934e5570
HaleySchuhl <haleyschuhl@gmail.com>
2022-05-10 10:22:17 -0500

    Update test_rot90.py
    change the way hsi data is called? error said cannor call module

--- tests/plantcv/hyperspectral/test_rot90.py
+++ tests/plantcv/hyperspectral/test_rot90.py
@@ -4,5 +4,7 @@ from plantcv.plantcv.hyperspectral import rot90
 
 def test_rot90(hyperspectral_test_data):
     """Test for PlantCV."""
-    new = rot90(spectral_data=hyperspectral_test_data.load_hsi(hyperspectral_test_data.hsi_file), k=1)
+    spectral_data = hyperspectral_test_data.load_hsi(hyperspectral_test_data.hsi_file)
+    new = rot90(spectral_data=spectral_data, k=1)
+
     assert np.shape(new.array_data) == (1600, 1, 978)
