d26a26faf21a0158bdb7755f02588ec7068c3e45
David Peery <63679152+jdavidpeery@users.noreply.github.com>
2022-07-05 16:40:33 -0500

    Actually added the import, not just the string to __init__

--- plantcv/plantcv/roi/__init__.py
+++ plantcv/plantcv/roi/__init__.py
@@ -2,6 +2,7 @@ from plantcv.plantcv.roi.roi_methods import circle
 from plantcv.plantcv.roi.roi_methods import ellipse
 from plantcv.plantcv.roi.roi_methods import from_binary_image
 from plantcv.plantcv.roi.roi_methods import rectangle
+from plantcv.plantcv.roi.roi_methods import auto_grid
 from plantcv.plantcv.roi.roi_methods import multi
 from plantcv.plantcv.roi.roi_methods import custom
 from plantcv.plantcv.roi.roi2mask import roi2mask
