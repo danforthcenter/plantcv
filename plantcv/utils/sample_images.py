03557918e8142190e22b44be9ee2d18a00d74a9c
Noah Fahlgren <noahfahlgren@gmail.com>
2022-07-25 09:56:17 -0500

    Modularize sampling methods

--- plantcv/utils/sample_images.py
+++ plantcv/utils/sample_images.py
@@ -11,28 +11,36 @@ def sample_images(source_path, dest_path, num=100):
     if not os.path.exists(dest_path):
         os.makedirs(dest_path)  # exist_ok argument does not exist in python 2
 
-    img_element_array = []
-    img_extensions = ['.png', '.jpg', '.jpeg', '.tif', '.tiff', '.gif']
-
     # If SnapshotInfo exists then need to make a new csv for the random image sample
     if os.path.exists(os.path.join(source_path, 'SnapshotInfo.csv')):
+        _sample_phenofront(source_path, dest_path, num)
+    else:
+        _sample_filenames(source_path, dest_path, num)
 
-        line_array = []
-        input_csv = open(os.path.join(source_path, 'SnapshotInfo.csv'))
-        header = input_csv.readline()
-        for line in input_csv:
+
+def _sample_phenofront(source_path, dest_path, num=100):
+    """
+    Sample images from a phenofront dataset.
+    :param source_path: Path to phenofront images.
+    :param dest_path: Path to save sampled images.
+    :param num: Number of images to sample.
+    :return: None
+    """
+    line_array = []
+    with open(os.path.join(source_path, 'SnapshotInfo.csv')) as fp:
+        header = fp.readline()
+        for line in fp:
             line = line.rstrip("\n")
             element_arr = line.split(',')
             if element_arr[-1]:
                 line_array.append(element_arr)
-        input_csv.close()
 
-        # Check to make sure number of imgs to select is less than number of images found
-        if num > len(line_array):
-            fatal_error(f"Number of images found ({len(line_array)}) less than 'num'.")
+    # Check to make sure number of imgs to select is less than number of images found
+    if num > len(line_array):
+        fatal_error(f"Number of images found ({len(line_array)}) less than 'num'.")
 
-        # Create SnapshotInfo file
-        out_file = open(os.path.join(dest_path, 'SnapshotInfo.csv'), 'w')
+    # Create SnapshotInfo file
+    with open(os.path.join(dest_path, 'SnapshotInfo.csv'), 'w') as out_file:
         out_file.write(header)
 
         # Get random snapshots
@@ -44,23 +52,34 @@ def sample_images(source_path, dest_path, num=100):
             folder_path = os.path.join(dest_path, "snapshot" + row[1])
             if not os.path.exists(folder_path):
                 os.mkdir(folder_path)  # the beginning of folder_path (dest_path) already exists from above
-            for root, dirs, files in os.walk(snap_path):
+            for root, _, files in os.walk(snap_path):
                 for file in files:
                     shutil.copy(os.path.join(root, file), folder_path)
-    else:
-        for root, dirs, files in os.walk(source_path):
-            for file in files:
-                # Check file type so that only images get copied over
-                name, ext = os.path.splitext(file)
-                if ext.lower() in img_extensions:
-                    img_element_array.append(os.path.join(root, file))
 
-        # Check to make sure number of imgs to select is less than number of images found
-        if num > len(img_element_array):
-            fatal_error(f"Number of images found ({len(img_element_array)}) less than 'num'.")
 
-        # Get random images
-        random_index = random.sample(range(0, len(img_element_array) - 1), num)
-        # Copy images over to destination
-        for i in random_index:
-            shutil.copy(img_element_array[int(i)], dest_path)
+def _sample_filenames(source_path, dest_path, num=100):
+    """
+    Sample images from a filenames dataset.
+    :param source_path: Path to images.
+    :param dest_path: Path to save sampled images.
+    :param num: Number of images to sample.
+    :return: None
+    """
+    img_element_array = []
+    img_extensions = ['.png', '.jpg', '.jpeg', '.tif', '.tiff', '.gif']
+    for root, _, files in os.walk(source_path):
+        for file in files:
+            # Check file type so that only images get copied over
+            ext = os.path.splitext(file)[1]
+            if ext.lower() in img_extensions:
+                img_element_array.append(os.path.join(root, file))
+
+    # Check to make sure number of imgs to select is less than number of images found
+    if num > len(img_element_array):
+        fatal_error(f"Number of images found ({len(img_element_array)}) less than 'num'.")
+
+    # Get random images
+    random_index = random.sample(range(0, len(img_element_array) - 1), num)
+    # Copy images over to destination
+    for i in random_index:
+        shutil.copy(img_element_array[int(i)], dest_path)
