e0b9d23d9a2c37b89ae137648a22a22371afeea1
JorgeGtz <jorge.gutierrezor@gmail.com>
2022-08-04 15:56:34 -0500

    created std color matrix function

--- plantcv/plantcv/transform/color_correction.py
+++ plantcv/plantcv/transform/color_correction.py
@@ -10,6 +10,65 @@ from plantcv.plantcv import fatal_error
 from plantcv.plantcv._debug import _debug
 
 
+def std_color_matrix(pos=0):
+    """Standard color values for the x-rite ColorCheker Classic, ColorChecker Mini,
+    and ColorChecker Passport targets.
+
+    Inputs:
+    pos     = reference value indicating orientation of the color card
+
+    Outputs:
+    color_matrix    = matrix containing the standard red, green, and blue
+                        values for each color chip
+
+    :param pos: int
+    """
+    # list of rgb values as indicated in the color card specs. They need to be
+    # aranged depending on the orientation of the color card of reference in the
+    # image to be corrected.
+    values_list = np.array([[115,82,68], # dark skin
+            [194,150,130], # light skin
+            [98,122,157], # blue sky
+            [87,108,67], # foliage
+            [133,128,177], # blue flower
+            [103,189,170], # bluish green
+            [214,126,44], # orange
+            [80,91,166], # purplish blue
+            [193,90,99], # moderate red
+            [94,60,108], # purple
+            [157,188,64], # yellow green
+            [224,163,46], # orange yellow
+            [56,61,150], # blue
+            [70,148,73], # green
+            [175,54,60], # red
+            [231,199,31], # yellow
+            [187,86,149], # magenta
+            [8,133,161], # cyan
+            [243,243,242], # white (.05*)
+            [200,200,200], # neutral 8 (.23*)
+            [160,160,160], # neutral 6.5 (.44*)
+            [122,122,121], # neutral 5 (.7*)
+            [85,85,85], # neutral 3.5 (1.05*)
+            [52,52,52]], dtype=np.float64) # black (1.50*)
+
+    N_chips = values_list.shape[0]
+
+    # array of indices from 1 to N chips in order to match the chip numbering
+    # in the color card specs. Later when used for indexing, we substract the 1.
+    idx = np.arange(N_chips)+1
+    # indices in the shape of the color card
+    cc_indices = idx.reshape((4,6), order='C')
+    # rotate the indices depending on the specified orientation
+    cc_indices_rot = np.rot90(cc_indices, k=pos, axes=(0,1))
+    # arange color values based on the indices
+    color_matrix_wo_chip_nb = values_list[(cc_indices_rot-1).reshape(-1),:]/255.
+
+    # add chip number compatible with other PlantCV functions
+    chip_nb = np.arange(10,10*N_chips+1,10)
+    color_matrix = np.concatenate((chip_nb.reshape(N_chips,1), color_matrix_wo_chip_nb), axis=1)
+
+    return color_matrix
+
 def get_color_matrix(rgb_img, mask):
     """Calculate the average value of pixels in each color chip for each color channel.
 
