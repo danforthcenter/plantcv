6764cd9f16165c56e18466598ede80cc3b6159dc
HaleySchuhl <haleyschuhl@gmail.com>
2022-08-24 12:48:46 -0500

    update roi_objects tests to use Objects class

--- tests/plantcv/test_roi_objects.py
+++ tests/plantcv/test_roi_objects.py
@@ -1,7 +1,7 @@
 import pytest
 import cv2
 import numpy as np
-from plantcv.plantcv import roi_objects
+from plantcv.plantcv import roi_objects, Objects
 
 
 @pytest.mark.parametrize("mode,exp", [["largest", 221], ["cutto", 152], ["partial", 221]])
@@ -12,8 +12,9 @@ def test_roi_objects(mode, exp, test_data):
     cnt, cnt_str = test_data.load_contours(test_data.small_contours_file)
     roi = [np.array([[[150, 150]], [[150, 174]], [[249, 174]], [[249, 150]]], dtype=np.int32)]
     roi_str = np.array([[[-1, -1, -1, -1]]], dtype=np.int32)
-    _, _, _, area = roi_objects(img=img, roi_contour=roi, roi_hierarchy=roi_str, object_contour=cnt,
-                                obj_hierarchy=cnt_str, roi_type=mode)
+    objs = Objects(cnt, cnt_str)
+    rois = Objects(roi, roi_str)
+    _, _, _, area = roi_objects(img=img, roi_objects=rois, objects=objs, roi_type=mode)
     # Assert that the contours were filtered as expected
     assert area == exp
 
@@ -25,9 +26,10 @@ def test_roi_objects_bad_input(test_data):
     cnt, cnt_str = test_data.load_contours(test_data.small_contours_file)
     roi = [np.array([[[150, 150]], [[150, 174]], [[249, 174]], [[249, 150]]], dtype=np.int32)]
     roi_str = np.array([[[-1, -1, -1, -1]]], dtype=np.int32)
+    objs = Objects(cnt, cnt_str)
+    rois = Objects(roi, roi_str)
     with pytest.raises(RuntimeError):
-        _ = roi_objects(img=img, roi_type="cut", roi_contour=roi, roi_hierarchy=roi_str,
-                        object_contour=cnt, obj_hierarchy=cnt_str)
+        _ = roi_objects(img=img, roi_type="cut", roi_objects=rois, objects=objs)
 
 
 def test_roi_objects_grayscale_input(test_data):
@@ -37,8 +39,9 @@ def test_roi_objects_grayscale_input(test_data):
     cnt, cnt_str = test_data.load_contours(test_data.small_contours_file)
     roi = [np.array([[[150, 150]], [[150, 174]], [[249, 174]], [[249, 150]]], dtype=np.int32)]
     roi_str = np.array([[[-1, -1, -1, -1]]], dtype=np.int32)
-    _, _, _, area = roi_objects(img=img, roi_type="partial", roi_contour=roi, roi_hierarchy=roi_str,
-                                object_contour=cnt, obj_hierarchy=cnt_str)
+    objs = Objects(cnt, cnt_str)
+    rois = Objects(roi, roi_str)
+    _, _, _, area = roi_objects(img=img, roi_type="partial", roi_objects=rois, objects=objs)
     # Assert that the contours were filtered as expected
     assert area == 221
 
@@ -50,8 +53,9 @@ def test_roi_objects_no_overlap(test_data):
     cnt, cnt_str = test_data.load_contours(test_data.small_contours_file)
     roi = [np.array([[[0, 0]], [[0, 24]], [[24, 24]], [[24, 0]]], dtype=np.int32)]
     roi_str = np.array([[[-1, -1, -1, -1]]], dtype=np.int32)
-    _, _, _, area = roi_objects(img=img, roi_contour=roi, roi_hierarchy=roi_str, object_contour=cnt,
-                                obj_hierarchy=cnt_str, roi_type="partial")
+    objs = Objects(cnt, cnt_str)
+    rois = Objects(roi, roi_str)
+    _, _, _, area = roi_objects(img=img, roi_objects=rois, objects=objs, roi_type="partial")
     # Assert that the contours were filtered as expected
     assert area == 0
 
@@ -66,6 +70,7 @@ def test_roi_objects_nested():
     cnt_str = np.array([[[-1, -1,  1, -1], [-1, -1, -1,  0]]], dtype=np.int32)
     roi = [np.array([[[0, 0]], [[0, 99]], [[99, 99]], [[99, 0]]], dtype=np.int32)]
     roi_str = np.array([[[-1, -1, -1, -1]]], dtype=np.int32)
-    _, _, _, area = roi_objects(img=img, roi_contour=roi, roi_hierarchy=roi_str, object_contour=cnt,
-                                obj_hierarchy=cnt_str, roi_type="largest")
+    objs = Objects(cnt, cnt_str)
+    rois = Objects(roi, roi_str)
+    _, _, _, area = roi_objects(img=img, roi_objects=rois, objects=objs, roi_type="largest")
     assert area == 580
