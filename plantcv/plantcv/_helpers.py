1d95448e6b927ac44caeb6251fb53d89060e0c0f
nfahlgren <noahfahlgren@gmail.com>
2022-08-30 10:57:35 -0500

    Add findContours helper function

---
+++ plantcv/plantcv/_helpers.py
@@ -0,0 +1,19 @@
+import cv2
+
+
+def _cv2_findcontours(bin_img):
+    """
+    Helper function for OpenCV findContours.
+
+    Reduces duplication of calls to findContours in the event the OpenCV function changes.
+
+    Keyword inputs:
+    bin_img = Binary image (np.ndarray)
+
+    :param bin_img: np.ndarray
+    :return contours: list
+    :return hierarchy: np.array
+    """
+    contours, hierarchy = cv2.findContours(bin_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[-2:]
+
+    return contours, hierarchy
