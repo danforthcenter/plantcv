75234e77b452fe4beb7b507357292ff8f2e84bee
nfahlgren <noahfahlgren@gmail.com>
2022-09-02 15:41:35 -0500

    Copy input bin image

--- plantcv/plantcv/_helpers.py
+++ plantcv/plantcv/_helpers.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 
 def _cv2_findcontours(bin_img):
@@ -14,6 +15,6 @@ def _cv2_findcontours(bin_img):
     :return contours: list
     :return hierarchy: np.array
     """
-    contours, hierarchy = cv2.findContours(bin_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[-2:]
+    contours, hierarchy = cv2.findContours(np.copy(bin_img), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[-2:]
 
     return contours, hierarchy
