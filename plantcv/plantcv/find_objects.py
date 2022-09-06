e081d788d1831229004f338b34ffd92069d6281a
HaleySchuhl <haleyschuhl@gmail.com>
2022-09-06 13:06:25 -0500

    update find_objects.py to utilize objects class and change the way we cast to list

--- plantcv/plantcv/find_objects.py
+++ plantcv/plantcv/find_objects.py
@@ -5,7 +5,7 @@ import numpy as np
 import os
 from plantcv.plantcv._debug import _debug
 from plantcv.plantcv._helpers import _cv2_findcontours
-from plantcv.plantcv import params
+from plantcv.plantcv import params, Objects
 
 
 def find_objects(img, mask):
@@ -18,25 +18,25 @@ def find_objects(img, mask):
 
 
     Returns:
-    objects   = list of contours
-    hierarchy = contour hierarchy list
+    objects   = a dataclass with roi objects and hierarchies
 
     :param img: numpy.ndarray
     :param mask: numpy.ndarray
-    :return objects: list
-    :return hierarchy: numpy.ndarray
+    :return objects: plantcv.plantcv.classes.Objects
     """
     ori_img = np.copy(img)
     # If the reference image is grayscale convert it to color
     if len(np.shape(ori_img)) == 2:
         ori_img = cv2.cvtColor(ori_img, cv2.COLOR_GRAY2BGR)
-    objects, hierarchy = _cv2_findcontours(bin_img=mask)
+    cnts, hierarchy = _cv2_findcontours(bin_img=mask)
     # Cast tuple objects as a list
-    objects = list(objects)
+    objects = [cnts]
     for i, cnt in enumerate(objects):
         cv2.drawContours(ori_img, objects, i, (255, 102, 255), -1, lineType=8, hierarchy=hierarchy)
+
+    objects = Objects(contours=objects, hierarchy=hierarchy) # contours already in list format
 
     _debug(visual=ori_img,
            filename=os.path.join(params.debug_outdir, str(params.device) + '_id_objects.png'))
 
-    return objects, hierarchy
+    return objects
