6029cb8c79117dd7e49c5763940aaa3ad0a83df7
HaleySchuhl <haleyschuhl@gmail.com>
2022-09-12 14:38:49 -0500

    start updating roi.custom

--- plantcv/plantcv/roi/roi_methods.py
+++ plantcv/plantcv/roi/roi_methods.py
@@ -397,19 +397,18 @@ def custom(img, vertices):
         vertices      = List of vertices of the desired polygon ROI
 
         Outputs:
-        roi_contour   = An ROI set of points (contour).
-        roi_hierarchy = The hierarchy of ROI contour(s).
+        roi           = a dataclass with the roi object and hierarchy
 
         :param img: numpy.ndarray
         :param vertices: list
-        :return roi_contour: list
-        :return roi_hierarchy: numpy.ndarray
+        :return roi: plantcv.plantcv.classes.Objects
     """
     # Get the height and width of the reference image
     height, width = np.shape(img)[:2]
 
     roi_contour = [np.array(vertices, dtype=np.int32)]
     roi_hierarchy = np.array([[[-1, -1, -1, -1]]], dtype=np.int32)
+    roi = Objects(contours=[roi_contour], hierarchy=[roi_hierarchy])
 
     # Draw the ROIs if requested
     _draw_roi(img=img, roi_contour=roi_contour)
@@ -419,4 +418,9 @@ def custom(img, vertices):
         (x, y) = i
         if x < 0 or x > width or y < 0 or y > height:
             fatal_error("An ROI extends outside of the image!")
+<<<<<<< HEAD
     return roi_contour, roi_hierarchy
+=======
+
+    return roi
+>>>>>>> 47996c5f (start updating roi.custom)
