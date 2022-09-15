2a96b93dd1c43d7c4c077c733bcbf06ebb02518c
nfahlgren <noahfahlgren@gmail.com>
2022-09-15 14:16:06 -0500

    Linting reformatting

--- plantcv/plantcv/transform/color_correction.py
+++ plantcv/plantcv/transform/color_correction.py
@@ -35,29 +35,29 @@ def std_color_matrix(pos=0):
     # aranged depending on the orientation of the color card of reference in the
     # image to be corrected.
     values_list = np.array([[115, 82, 68],  # dark skin
-            [194, 150, 130],  # light skin
-            [98, 122, 157],  # blue sky
-            [87, 108, 67],  # foliage
-            [133, 128, 177],  # blue flower
-            [103, 189, 170],  # bluish green
-            [214, 126, 44],  # orange
-            [80, 91, 166],  # purplish blue
-            [193, 90, 99],  # moderate red
-            [94, 60, 108],  # purple
-            [157, 188, 64],  # yellow green
-            [224, 163, 46],  # orange yellow
-            [56, 61, 150],  # blue
-            [70, 148, 73],  # green
-            [175, 54, 60],  # red
-            [231, 199, 31],  # yellow
-            [187, 86, 149],  # magenta
-            [8, 133, 161],  # cyan
-            [243, 243, 242],  # white (.05*)
-            [200, 200, 200],  # neutral 8 (.23*)
-            [160, 160, 160],  # neutral 6.5 (.44*)
-            [122, 122, 121],  # neutral 5 (.7*)
-            [85, 85, 85],  # neutral 3.5 (1.05*)
-            [52, 52, 52]], dtype=np.float64)  # black (1.50*)
+                            [194, 150, 130],  # light skin
+                            [98, 122, 157],  # blue sky
+                            [87, 108, 67],  # foliage
+                            [133, 128, 177],  # blue flower
+                            [103, 189, 170],  # bluish green
+                            [214, 126, 44],  # orange
+                            [80, 91, 166],  # purplish blue
+                            [193, 90, 99],  # moderate red
+                            [94, 60, 108],  # purple
+                            [157, 188, 64],  # yellow green
+                            [224, 163, 46],  # orange yellow
+                            [56, 61, 150],  # blue
+                            [70, 148, 73],  # green
+                            [175, 54, 60],  # red
+                            [231, 199, 31],  # yellow
+                            [187, 86, 149],  # magenta
+                            [8, 133, 161],  # cyan
+                            [243, 243, 242],  # white (.05*)
+                            [200, 200, 200],  # neutral 8 (.23*)
+                            [160, 160, 160],  # neutral 6.5 (.44*)
+                            [122, 122, 121],  # neutral 5 (.7*)
+                            [85, 85, 85],  # neutral 3.5 (1.05*)
+                            [52, 52, 52]], dtype=np.float64)  # black (1.50*)
 
     pos = math.floor(pos)
     if (pos < 0) or (pos > 3):
@@ -69,15 +69,15 @@ def std_color_matrix(pos=0):
     # in the color card specs. Later when used for indexing, we substract the 1.
     idx = np.arange(N_chips)+1
     # indices in the shape of the color card
-    cc_indices = idx.reshape((4,6), order='C')
+    cc_indices = idx.reshape((4, 6), order='C')
     # rotate the indices depending on the specified orientation
-    cc_indices_rot = np.rot90(cc_indices, k=pos, axes=(0,1))
+    cc_indices_rot = np.rot90(cc_indices, k=pos, axes=(0, 1))
     # arange color values based on the indices
-    color_matrix_wo_chip_nb = values_list[(cc_indices_rot-1).reshape(-1),:]/255.
+    color_matrix_wo_chip_nb = values_list[(cc_indices_rot-1).reshape(-1), :]/255.
 
     # add chip number compatible with other PlantCV functions
-    chip_nb = np.arange(10,10*N_chips+1,10)
-    color_matrix = np.concatenate((chip_nb.reshape(N_chips,1), color_matrix_wo_chip_nb), axis=1)
+    chip_nb = np.arange(10, 10*N_chips+1, 10)
+    color_matrix = np.concatenate((chip_nb.reshape(N_chips, 1), color_matrix_wo_chip_nb), axis=1)
 
     return color_matrix
 
