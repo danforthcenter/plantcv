37f018b909d9b071cfe6f27ce01a10f3e5c2b5e1
tj-schultz <73593696+tj-schultz@users.noreply.github.com>
2022-09-29 16:04:19 -0400

    Update test_object_composition.py

--- tests/plantcv/test_object_composition.py
+++ tests/plantcv/test_object_composition.py
@@ -1,14 +1,14 @@
 import cv2
 import numpy as np
-from plantcv.plantcv import object_composition
-
+from plantcv.plantcv import object_composition, Objects
 
 def test_object_composition(test_data):
     """Test for PlantCV."""
     # Read in test data
     img = cv2.imread(test_data.small_rgb_img)
     cnt, cnt_str = test_data.load_contours(test_data.small_contours_file)
-    contours, _ = object_composition(img=img, contours=cnt, hierarchy=cnt_str)
+    objs = Objects(contours=cnt, hierarchy=cnt_str)
+    contours, _ = object_composition(img=img, objs=objs)
     expected = test_data.load_composed_contours(test_data.small_composed_contours_file)
     assert np.all(expected) == np.all(contours)
 
@@ -18,7 +18,8 @@ def test_object_composition_grayscale(test_data):
     # Read in test data
     img = cv2.imread(test_data.small_gray_img, -1)
     cnt, cnt_str = test_data.load_contours(test_data.small_contours_file)
-    contours, _ = object_composition(img=img, contours=cnt, hierarchy=cnt_str)
+    objs = Objects(contours=cnt, hierarchy=cnt_str)
+    contours, _ = object_composition(img=img, objs=objs)
     expected = test_data.load_composed_contours(test_data.small_composed_contours_file)
     assert np.all(expected) == np.all(contours)
 
@@ -27,7 +28,8 @@ def test_object_composition_no_contours(test_data):
     """Test for PlantCV."""
     # Read in test data
     img = cv2.imread(test_data.small_rgb_img)
-    contours, _ = object_composition(img=img, contours=[], hierarchy=np.array([]))
+    objs = Objects(contours=[], hierarchy=np.array([]))
+    contours, _ = object_composition(img=img, objs=objs)
     assert contours is None
 
 
@@ -39,5 +41,6 @@ def test_object_composition_nested():
            np.array([[[34, 35]], [[35, 34]], [[39, 34]], [[40, 35]], [[40, 39]], [[39, 40]], [[35, 40]], [[34, 39]]],
                     dtype=np.int32)]
     cnt_str = np.array([[[-1, -1,  1, -1], [-1, -1, -1,  0]]], dtype=np.int32)
-    _, mask = object_composition(img=img, contours=cnt, hierarchy=cnt_str)
+    objs = Objects(contours=cnt, hierarchy=cnt_str)
+    _, mask = object_composition(img=img, objs=objs)
     assert np.count_nonzero(mask) == 600
