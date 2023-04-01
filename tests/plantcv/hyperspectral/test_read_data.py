e2bc53433fcaf507e6842bad84502fdd6e58f85f
Noah Fahlgren <noahfahlgren@gmail.com>
2023-04-01 13:18:02 -0500

    Remove excess line returns

--- tests/plantcv/hyperspectral/test_read_data.py
+++ tests/plantcv/hyperspectral/test_read_data.py
@@ -37,5 +37,3 @@ def test_read_data_parse_arcgis(hyperspectral_test_data):
     """Test for PlantCV."""
     array_data = read_data(filename=hyperspectral_test_data.arcgis, mode="arcgis")
     assert np.shape(array_data.array_data) == (1, 1600, 978)
-
-
