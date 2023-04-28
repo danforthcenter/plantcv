cd3c503a730aa27d7b3dba4de6dc611e603753e3
nfahlgren <noahfahlgren@gmail.com>
2023-04-28 15:17:35 -0500

    Add iteration helper function

--- plantcv/plantcv/_helpers.py
+++ plantcv/plantcv/_helpers.py
@@ -18,3 +18,22 @@ def _cv2_findcontours(bin_img):
     contours, hierarchy = cv2.findContours(np.copy(bin_img), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[-2:]
 
     return contours, hierarchy
+
+
+def _iterate_analysis(labeled_mask, n_labels, function, **kwargs):
+    """Iterate over labels and apply an analysis function.
+    Inputs:
+    mask = labeled mask
+    n_labels = number of expected labels
+    function = analysis function to apply to each submask
+
+    :param mask: np.ndarray
+    :param n_labels: int
+    :param function: function
+    """
+    mask_copy = np.copy(labeled_mask)
+    if len(np.unique(mask_copy)) == 2 and np.max(mask_copy) == 255:
+        mask_copy = np.where(mask_copy == 255, 1, 0).astype(np.uint8)
+    for i in range(1, n_labels + 1):
+        submask = np.where(mask_copy == i, 255, 0).astype(np.uint8)
+        function(mask=submask, **kwargs)
