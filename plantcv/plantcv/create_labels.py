a9c1de73f07d096ae275eebadd4f94544805c2bc
HaleySchuhl <haleyschuhl@gmail.com>
2023-04-28 14:44:55 -0500

    update to include rois=None option

--- plantcv/plantcv/create_labels.py
+++ plantcv/plantcv/create_labels.py
@@ -1,7 +1,8 @@
-
 from plantcv.plantcv import params, roi_objects, Objects
 from plantcv.plantcv._helpers import _cv2_findcontours
 from skimage.color import label2rgb
+import cv2
+import numpy as np 
 
 
 def create_labels(mask, rois, roi_type="partial"):
@@ -21,10 +22,17 @@ def create_labels(mask, rois, roi_type="partial"):
     # Initialize chip list
     masks = []
     bin_img = np.zeros((np.shape(mask)[0], np.shape(mask)[1]), dtype=np.uint8)
-    num_rois = len(rois.contours)
     mask_copy = np.copy(mask)
-    
     contours, hierarchy = _cv2_findcontours(mask) 
+    
+    if rois is not None:
+        num_rois = len(rois.contours)
+    else:
+        # Dealing with a single entity/plant, or the edge case of seed scatter (have to assume 1:1 ratio of contours and entities)
+        for i, cnt in enumerate(contours):
+            labeled_masks = cv2.drawContours(mask_copy, cnt, -1, (i+1), -1)
+    
+    # Intermediate. Will change when implementing _roi_filter
     plant_obj = Objects(contours=[contours], hierarchy=[hierarchy])
 
     # Store debug mode
@@ -33,7 +41,8 @@ def create_labels(mask, rois, roi_type="partial"):
 
     # Loop over each color card row
     for i, roi in enumerate(rois):
-        objects, intermediate_mask, obj_area = pcv.roi_objects(img=mask_copy, roi=roi, 
+        # Intermediate
+        objects, intermediate_mask, obj_area = roi_objects(img=mask_copy, roi=roi, 
                                                                obj=plant_obj, roi_type=roi_type)
         # Pixel intensity of (i+1) such that the first object has value 
         labeled_masks = cv2.drawContours(mask_copy, objects.contours[0], -1, (i+1), -1)
@@ -42,5 +51,5 @@ def create_labels(mask, rois, roi_type="partial"):
     # Restore debug parameter
     params.debug = debug
     
-    return labeled_masks
+    return labeled_masks, num_rois
 
