54ad0c82a217ef47ba659a5f42981f073c06c137
HaleySchuhl <haleyschuhl@gmail.com>
2023-05-04 12:49:54 -0500

    update annotates init file

---
+++ plantcv/plantcv/annotate/__init__.py
@@ -0,0 +1,5 @@
+from plantcv.plantcv.annotate.detect_discs import detect_discs
+from plantcv.plantcv.annotate.get_centroids import get_centroids
+
+# add new functions to end of lists
+__all__ = ["detect_discs", "get_centroids"]
