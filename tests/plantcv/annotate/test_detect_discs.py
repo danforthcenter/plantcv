ddc969299d7f735dd749017b800985029e6c74bf
HaleySchuhl <haleyschuhl@gmail.com>
2023-05-04 18:28:07 -0500

    Update test_detect_discs.py

--- tests/plantcv/annotate/test_detect_discs.py
+++ tests/plantcv/annotate/test_detect_discs.py
@@ -2,9 +2,9 @@ import cv2
 from plantcv.plantcv.annotate import detect_discs
 
 
-def test_detect_discs(test_data):
+def test_detect_discs(annotate_test_data):
     # Read in test data
-    mask = cv2.imread(test_data.discs_mask, -1)
+    mask = cv2.imread(annotate_test_data.discs_mask, -1)
 
     _, coor = detect_discs(bin_img=mask, ecc_thresh=0.3)
 
