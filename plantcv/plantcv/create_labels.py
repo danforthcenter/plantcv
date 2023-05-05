d16c11b480d905d113419c495d754acbb93e477a
JorgeGtz <jorge.gutierrezor@gmail.com>
2023-05-05 11:43:50 -0500

    create labels working using _roi_filter

--- plantcv/plantcv/create_labels.py
+++ plantcv/plantcv/create_labels.py
@@ -5,7 +5,8 @@ from skimage.measure import label
 from skimage.color import label2rgb
 from plantcv.plantcv import params, roi_objects, Objects, warn
 from plantcv.plantcv._debug import _debug
-# from plantcv.plantcv._helpers import _cv2_findcontours
+from plantcv.plantcv._helpers import _roi_filter
+from plantcv.plantcv._helpers import _cv2_findcontours
 
 
 
@@ -38,41 +39,25 @@ def create_labels(mask, rois, roi_type="partial"):
     debug = params.debug
     params.debug = None
 
-    # Initialize chip list
-    bin_img = np.zeros((np.shape(mask)[0], np.shape(mask)[1]), dtype=np.uint8)
-    mask_copy = np.copy(mask)
-    # contours, hierarchy = _cv2_findcontours(mask)
-
+    # Auto logic for when the order of labels is not important
     if roi_type.upper() == "AUTO":
-
         if rois is not None:
             warn("When using roi_type='auto' the rois parameter is ignored")
 
+        # label will work with any number of objects in the mask
         labeled_mask, num_labels = label(mask, background=0, return_num=True, connectivity=2)
 
+    # use the rois for labeling
+    else:
+        contours, hierarchy = _cv2_findcontours(mask)
+        labeled_mask = np.zeros(mask.shape[:2], dtype=np.int32)
+        for i, roi in enumerate(rois):
+            kept_cnt, kept_hierarchy, mask = _roi_filter(img=mask, roi=roi, obj=contours,
+                                                hierarchy=hierarchy, roi_type=roi_type)
 
-
-    # if rois is not None:
-    #     num_labels = len(rois.contours)
-    # else:
-    #     if roi_type.upper() == "AUTO":
-    #         for i, cnt in enumerate(contours): # assume 1:1 ratio of contours and entities (seed scatter edge case)
-    #             labeled_masks = cv2.drawContours(mask_copy, cnt, -1, (i+1), -1)
-    #
-    #     else: # assume single entity even if there are multiple contours (single plant)
-    #         labeled_masks = cv2.drawContours(mask_copy, contours, -1, (255), -1)
-    #         num_labels = len(contours)
-    # # Intermediate. Will change when implementing _roi_filter
-    # plant_obj = Objects(contours=[contours], hierarchy=[hierarchy])
-    #
-    #
-    # # Loop over each color card row
-    # for i, roi in enumerate(rois):
-    #     # Intermediate
-    #     objects, intermediate_mask, obj_area = pcv.roi_objects(img=mask_copy, roi=roi,
-    #                                                            obj=plant_obj, roi_type=roi_type)
-    #     # Pixel intensity of (i+1) such that the first object has value
-    #     labeled_masks = cv2.drawContours(mask_copy, objects.contours[0], -1, (i+1), -1)
+            # Pixel intensity of (i+1) such that the first object has value
+            cv2.drawContours(labeled_mask, kept_cnt, -1, (i+1), -1)
+            num_labels = np.unique(labeled_mask).size -1
 
     # Restore debug parameter
     params.debug = debug
