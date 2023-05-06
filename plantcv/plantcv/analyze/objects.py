b1dc670a4d4b3541292acef86db71139e18c253b
Noah Fahlgren <noahfahlgren@gmail.com>
2023-05-05 21:26:35 -0500

    Add img inputs

--- plantcv/plantcv/analyze/objects.py
+++ plantcv/plantcv/analyze/objects.py
@@ -2,10 +2,11 @@
 from plantcv.plantcv._helpers import _iterate_analysis
 
 
-def objects(labeled_mask, n_labels, label="default"):
+def objects(img, labeled_mask, n_labels, label="default"):
     """A function that analyzes the shape and size of objects and outputs data.
 
     Inputs:
+    img          = RGB or grayscale image data for plotting
     labeled_mask = Labeled mask of objects (32-bit).
     n_labels     = Total number of labels in the image.
     label        = Optional label parameter, modifies the variable name of observations recorded.
@@ -13,18 +14,20 @@ def objects(labeled_mask, n_labels, label="default"):
     Returns:
     analysis_image = Diagnostic image showing measurements.
 
+    :param img: numpy.ndarray
     :param labeled_mask: numpy.ndarray
     :param n_labels: int
     :param label: str
     :return analysis_image: numpy.ndarray
     """
-    _iterate_analysis(labeled_mask=labeled_mask, n_labels=n_labels, function=_analyze_object, label=label)
+    _iterate_analysis(img=img, labeled_mask=labeled_mask, n_labels=n_labels, function=_analyze_object, label=label)
 
 
-def _analyze_object(mask, label):
+def _analyze_object(img, mask, label):
     """Analyze individual objects.
 
     Inputs:
+    img   = RGB or grayscale image data for plotting
     mask  = Binary image data
     label = Label of object
 
