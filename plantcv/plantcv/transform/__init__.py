3e0937166455529a495d7fd895b0b2f4f7f0e215
JorgeGtz <jorge.gutierrezor@gmail.com>
2023-05-11 14:11:23 -0500

    add init for affine color correction

--- plantcv/plantcv/transform/__init__.py
+++ plantcv/plantcv/transform/__init__.py
@@ -9,6 +9,7 @@ from plantcv.plantcv.transform.color_correction import create_color_card_mask
 from plantcv.plantcv.transform.color_correction import quick_color_check
 from plantcv.plantcv.transform.color_correction import find_color_card
 from plantcv.plantcv.transform.color_correction import std_color_matrix
+from plantcv.plantcv.transform.color_correction import affine_color_correction
 from plantcv.plantcv.transform.rescale import rescale
 from plantcv.plantcv.transform.rotate import rotate
 from plantcv.plantcv.transform.nonuniform_illumination import nonuniform_illumination
@@ -18,5 +19,5 @@ from plantcv.plantcv.transform.gamma_correct import gamma_correct
 
 __all__ = ["get_color_matrix", "get_matrix_m", "calc_transformation_matrix", "apply_transformation_matrix",
            "save_matrix", "load_matrix", "correct_color", "create_color_card_mask", "quick_color_check",
-           "find_color_card", "std_color_matrix", "rescale", "nonuniform_illumination", "resize", "resize_factor",
+           "find_color_card", "std_color_matrix", "affine_color_correction", "rescale", "nonuniform_illumination", "resize", "resize_factor",
            "warp", "rotate", "warp", "warp_align", "gamma_correct"]
