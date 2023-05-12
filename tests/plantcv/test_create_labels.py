3f584c8392dcedb8a3c658f4e186838992d9b234
Haley Schuhl <44006936+HaleySchuhl@users.noreply.github.com>
2023-05-12 10:39:50 -0500

    Update test to just use one mode

--- tests/plantcv/test_create_labels.py
+++ tests/plantcv/test_create_labels.py
@@ -4,13 +4,12 @@ import cv2
 from plantcv.plantcv import create_labels, Objects
 
 
-@pytest.mark.parametrize("mode", ["largest", "cutto", "partial"])
-def test_create_labels(mode, test_data):
+def test_create_labels(test_data):
     """Test for PlantCV."""
     cnt, cnt_str = test_data.load_contours(test_data.small_contours_file)
     mask = cv2.imread(test_data.small_bin_img, -1)
     cnt_Obj = Objects(contours=[cnt], hierarchy=[cnt_str])
-    masks, num = create_labels(mask=mask, rois=cnt_Obj, roi_type=mode)
+    masks, num = create_labels(mask=mask, rois=cnt_Obj, roi_type="partial)
     assert np.unique(masks).size == (num + 1)
 
 
