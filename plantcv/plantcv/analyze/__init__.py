ccce5ac313c58acb82a9fbb8cd409c0b6e9cb9fe
JorgeGtz <jorge.gutierrezor@gmail.com>
2023-05-19 11:37:40 -0500

    fix bound horizontal init

--- plantcv/plantcv/analyze/__init__.py
+++ plantcv/plantcv/analyze/__init__.py
@@ -1,4 +1,4 @@
 from plantcv.plantcv.analyze.size import size
-from plantcv.plantcv.analize.bound_horizontal import bound_horizontal
+from plantcv.plantcv.analyze.bound_horizontal import bound_horizontal
 
 __all__ = ["size", "bound_horizontal"]
