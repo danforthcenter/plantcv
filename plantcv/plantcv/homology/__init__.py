61dd61ae2a46d9d4a5cb8447de7b743a0b103f95
HaleySchuhl <haleyschuhl@gmail.com>
2023-05-19 12:34:38 -0500

    add back in to init

--- plantcv/plantcv/homology/__init__.py
+++ plantcv/plantcv/homology/__init__.py
@@ -3,6 +3,7 @@ from plantcv.plantcv.homology.space import space
 from plantcv.plantcv.homology.starscape import starscape
 from plantcv.plantcv.homology.constella import constella
 from plantcv.plantcv.homology.constellaqc import constellaqc
-#from plantcv.plantcv.homology.x_axis_pseudolandmarks import x_axis_pseudolandmarks
+from plantcv.plantcv.homology.x_axis_pseudolandmarks import x_axis_pseudolandmarks
 
-__all__ = ["acute", "space", "starscape", "constella", "constellaqc"]
+__all__ = ["acute", "space", "starscape", "constella", "constellaqc",
+           "x_axis_pseudolandmarks"]
