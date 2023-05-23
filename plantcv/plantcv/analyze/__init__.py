dfc0bed5abd18105e5775aa11cc090c02e684778
Noah Fahlgren <noahfahlgren@gmail.com>
2023-05-23 16:53:49 -0500

    Add bound_vertical function

--- plantcv/plantcv/analyze/__init__.py
+++ plantcv/plantcv/analyze/__init__.py
@@ -1,4 +1,5 @@
 from plantcv.plantcv.analyze.size import size
 from plantcv.plantcv.analyze.bound_horizontal import bound_horizontal
+from plantcv.plantcv.analyze.bound_vertical import bound_vertical
 
-__all__ = ["size", "bound_horizontal"]
+__all__ = ["size", "bound_horizontal", "bound_vertical"]
