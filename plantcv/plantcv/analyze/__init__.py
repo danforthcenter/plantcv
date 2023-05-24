5d51c0ffe80b9c9afbb0ebd1fef1fa81c3b41897
Noah Fahlgren <noahfahlgren@gmail.com>
2023-05-23 22:14:33 -0500

    Add grayscale to subpackage import

--- plantcv/plantcv/analyze/__init__.py
+++ plantcv/plantcv/analyze/__init__.py
@@ -2,5 +2,6 @@ from plantcv.plantcv.analyze.color import color
 from plantcv.plantcv.analyze.size import size
 from plantcv.plantcv.analyze.bound_horizontal import bound_horizontal
 from plantcv.plantcv.analyze.bound_vertical import bound_vertical
+from plantcv.plantcv.analyze.grayscale import grayscale
 
-__all__ = ["color", "bound_horizontal", "bound_vertical", "size"]
+__all__ = ["color", "bound_horizontal", "bound_vertical", "grayscale", "size"]
