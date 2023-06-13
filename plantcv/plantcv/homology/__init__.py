cb332af64568a0b6510d14dea1ab3f36f0b6732f
HaleySchuhl <haleyschuhl@gmail.com>
2023-06-13 10:48:16 -0500

    update homology init file

--- plantcv/plantcv/homology/__init__.py
+++ plantcv/plantcv/homology/__init__.py
@@ -3,5 +3,6 @@ from plantcv.plantcv.homology.space import space
 from plantcv.plantcv.homology.starscape import starscape
 from plantcv.plantcv.homology.constella import constella
 from plantcv.plantcv.homology.constellaqc import constellaqc
+from plantcv.plantcv.homology.y_axis_pseudolandmarks import y_axis_pseudolandmarks
 
-__all__ = ["acute", "space", "starscape", "constella", "constellaqc"]
+__all__ = ["acute", "space", "starscape", "constella", "constellaqc", "y_axis_pseudolandmarks"]
