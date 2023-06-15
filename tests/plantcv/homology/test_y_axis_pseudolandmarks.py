7d89e6e7ae7bfde580ba2970f7ccc0d4ef3389df
Haley Schuhl <44006936+HaleySchuhl@users.noreply.github.com>
2023-06-15 09:43:51 -0500

    Update test_y_axis_pseudolandmarks.py format simplify

--- tests/plantcv/homology/test_y_axis_pseudolandmarks.py
+++ tests/plantcv/homology/test_y_axis_pseudolandmarks.py
@@ -11,16 +11,17 @@ def test_y_axis_pseudolandmarks(test_data):
     left, right, center_h = y_axis_pseudolandmarks(mask=mask, img=img)
     assert all([left.shape == (20, 1, 2), right.shape == (20, 1, 2), center_h.shape == (20, 1, 2)])
 
-
-@pytest.mark.parametrize("mask,shape", [
-    [np.array(([[0, 0]], [[10, 0]], [[10, 10]], [[0, 10]]), dtype=np.uint32),
-     (20, 1, 2)],
-    [np.array(([[42, 161]], [[2, 47]], [[211, 222]]), dtype=np.uint32), (20, 1, 2)],
-    [np.array(([[38, 54]], [[144, 169]], [[81, 137]]), dtype=np.uint32), (20, 1, 2)]
+@pytest.mark.parametrize("obj,shape", [
+    [np.array(([[0, 0]], [[10, 0]], [[10, 10]], [[0, 10]])), (20, 1, 2)],
+    [np.array(([[42, 161]], [[2, 47]], [[211, 222]])), (20, 1, 2)],
+    [np.array(([[38, 54]], [[144, 169]], [[81, 137]])), (20, 1, 2)]
 ])
-def test_y_axis_pseudolandmarks_small_obj(mask, shape, test_data):
+
+def test_y_axis_pseudolandmarks_small_obj(obj, shape, test_data):
     """Test for PlantCV."""
     img = cv2.imread(test_data.small_rgb_img)
+    mask = np.zeros((250, 250), dtype=np.uint8)
+    cv2.drawContours(mask, obj, -1, (255), thickness=-1)
     left, right, center_h = y_axis_pseudolandmarks(mask=mask, img=img)
     assert all([np.shape(left) == shape, np.shape(right) == shape, np.shape(center_h) == shape])
 
