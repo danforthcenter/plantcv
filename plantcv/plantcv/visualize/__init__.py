0829b8c63ef74f9e3febcb065420779c896bd5ce
HaleySchuhl <haleyschuhl@gmail.com>
2023-07-13 13:11:41 -0500

    update visualize subpackage init file to remove cluster_contours

--- plantcv/plantcv/visualize/__init__.py
+++ plantcv/plantcv/visualize/__init__.py
@@ -1,7 +1,6 @@
 from plantcv.plantcv.visualize.pseudocolor import pseudocolor
 from plantcv.plantcv.visualize.colorize_masks import colorize_masks
 from plantcv.plantcv.visualize.histogram import histogram
-from plantcv.plantcv.visualize.clustered_contours import clustered_contours
 from plantcv.plantcv.visualize.colorspaces import colorspaces
 from plantcv.plantcv.visualize.auto_threshold_methods import auto_threshold_methods
 from plantcv.plantcv.visualize.overlay_two_imgs import overlay_two_imgs
@@ -10,5 +9,5 @@ from plantcv.plantcv.visualize.obj_sizes import obj_sizes
 from plantcv.plantcv.visualize.obj_size_ecdf import obj_size_ecdf
 from plantcv.plantcv.visualize.hyper_histogram import hyper_histogram
 
-__all__ = ["pseudocolor", "colorize_masks", "histogram", "clustered_contours", "colorspaces", "auto_threshold_methods",
+__all__ = ["pseudocolor", "colorize_masks", "histogram", "colorspaces", "auto_threshold_methods",
            "overlay_two_imgs", "colorize_label_img", "obj_size_ecdf", "obj_sizes", "hyper_histogram"]
