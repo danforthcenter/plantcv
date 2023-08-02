7095e377781a93ccd27deb8031dfbce91c5f73ee
nfahlgren <noahfahlgren@gmail.com>
2023-08-02 07:37:54 -0700

    Revert "Replace plotnine figure with altair chart"
    This reverts commit d18c4dd832fc1d1487389f886a2d0d485d7b9809.

--- plantcv/plantcv/visualize/obj_size_ecdf.py
+++ plantcv/plantcv/visualize/obj_size_ecdf.py
@@ -7,21 +7,23 @@ from plantcv.plantcv import params
 from plantcv.plantcv._debug import _debug
 from plantcv.plantcv._helpers import _cv2_findcontours
 from statsmodels.distributions.empirical_distribution import ECDF
-import altair as alt
+from plotnine import ggplot, aes, geom_point, labels, scale_x_log10
 
 
-def obj_size_ecdf(mask):
+def obj_size_ecdf(mask, title=None):
     """
     Plot empirical cumulative distribution for object size based on binary mask.
 
     Inputs:
     mask  = binary mask
+    title = a custom title for the plot (default=None)
 
     Returns:
-    chart = empirical cumulative distribution function plot
+    fig_ecdf = empirical cumulative distribution function plot
 
     :param mask: numpy.ndarray
-    :return chart: plotnine.ggplot.ggplot
+    :param title: str
+    :return fig_ecdf: plotnine.ggplot.ggplot
     """
     objects, _ = _cv2_findcontours(bin_img=mask)
     areas = [cv2.contourArea(cnt) for cnt in objects]
@@ -32,13 +34,13 @@ def obj_size_ecdf(mask):
 
     ecdf_df = pd.DataFrame({'object area': ecdf.x[1:], 'cumulative probability': ecdf.y[1:]})
     # create ecdf plot and apply log-scale for x-axis (areas)
-    chart = alt.Chart(ecdf_df).mark_circle(size=10).encode(
-        x=alt.X("object area:Q").scale(type='log'),
-        y="cumulative probability:Q",
-        tooltip=['object area', 'cumulative probability']
-    ).interactive()
+    fig_ecdf = (ggplot(data=ecdf_df, mapping=aes(x='object area', y='cumulative probability'))
+                + geom_point(size=.1)
+                + scale_x_log10())
+    if title is not None:
+        fig_ecdf = fig_ecdf + labels.ggtitle(title)
 
     # Plot or print the ecdf
-    _debug(visual=chart,
+    _debug(visual=fig_ecdf,
            filename=os.path.join(params.debug_outdir, str(params.device) + '_area_ecdf.png'))
-    return chart
+    return fig_ecdf
