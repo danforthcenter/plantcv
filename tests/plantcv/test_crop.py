e63c24e4bdc3dbd471fdf3b37ade77e897c0da53
nfahlgren <noahfahlgren@gmail.com>
2023-08-09 20:32:55 -0500

    Update spacing/formatting

--- tests/plantcv/test_crop.py
+++ tests/plantcv/test_crop.py
@@ -1,3 +1,4 @@
+"""Tests for pcv.crop."""""
 import cv2
 import numpy as np
 from plantcv.plantcv import crop
@@ -20,10 +21,10 @@ def test_plantcv_crop_multiarray():
     cropped = crop(img=img_stacked, x=10, y=10, h=50, w=50)
     assert np.shape(cropped) == (50, 50, 4)
 
+
 def test_plantcv_crop_hyperspectral(test_data):
     """Test for PlantCV."""
     # Read in test data
-    hyperimg = readimage(test_data.envi_sample_data,mode='envi')
-    cropped = crop(img=hyperimg,x=0,y=0,h=20,w=20)
+    hyperimg = readimage(test_data.envi_sample_data, mode='envi')
+    cropped = crop(img=hyperimg, x=0, y=0, h=20, w=20)
     assert np.shape(cropped.array_data) == (20, 20, 580)
-
