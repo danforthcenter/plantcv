86483e98100efc480dbe2b500c32b4b8a03c6f28
Noah Fahlgren <noahfahlgren@gmail.com>
2023-09-20 15:51:47 -0500

    Reorganize annotate methods

--- plantcv/plantcv/annotate/__init__.py
+++ plantcv/plantcv/annotate/__init__.py
@@ -1,5 +1,6 @@
 from plantcv.plantcv.annotate.detect_discs import detect_discs
 from plantcv.plantcv.annotate.get_centroids import get_centroids
+from plantcv.plantcv.annotate.classes import Points, ClickCount
 
 # add new functions to end of lists
-__all__ = ["detect_discs", "get_centroids"]
+__all__ = ["detect_discs", "get_centroids", "Points", "ClickCount"]
