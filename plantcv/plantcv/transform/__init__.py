767ecab814332d7e1f841b3ee71668820a69d9db
HaleySchuhl <haleyschuhl@gmail.com>
2023-10-02 14:57:09 -0500

    Update __init__.py

--- plantcv/plantcv/transform/__init__.py
+++ plantcv/plantcv/transform/__init__.py
@@ -16,8 +16,9 @@ from plantcv.plantcv.transform.nonuniform_illumination import nonuniform_illumin
 from plantcv.plantcv.transform.resize import resize, resize_factor
 from plantcv.plantcv.transform.warp import warp, warp_align
 from plantcv.plantcv.transform.gamma_correct import gamma_correct
+from plantcv.plantcv.transform.color_correction import detect_color_card
 
 __all__ = ["get_color_matrix", "get_matrix_m", "calc_transformation_matrix", "apply_transformation_matrix",
            "save_matrix", "load_matrix", "correct_color", "create_color_card_mask", "quick_color_check",
            "find_color_card", "std_color_matrix", "affine_color_correction", "rescale", "nonuniform_illumination", "resize",
-           "resize_factor", "warp", "rotate", "warp", "warp_align", "gamma_correct"]
+           "resize_factor", "warp", "rotate", "warp", "warp_align", "gamma_correct", "detect_color_card"]
