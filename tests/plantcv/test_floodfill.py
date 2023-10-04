ef1450daeaa243fda6923b65a0ae83a0ffef7f35
Malia Gehan <mgehan@danforthcenter.org>
2023-10-04 11:37:41 -0500

    floodfill documentation
    floodfill docs and tests

--- tests/plantcv/test_floodfill.py
+++ tests/plantcv/test_floodfill.py
@@ -1,3 +1,4 @@
+from ctypes import pointer
 import pytest
 import cv2
 import numpy as np
@@ -7,10 +8,10 @@ from plantcv.plantcv import floodfill
 def test_floodfill(test_data):
     """Test for PlantCV."""
     # Read in test data
-    img = cv2.imread(test_data.floodfill, -1)
-    fill_img = floodfill(bin_img=img,(137,31),0)
-    fill_img = floodfill(bin_img=fill_img,(189,214),0)
-    fill_img = floodfill(bin_img=fill_img,(312,361),0)
+    img = cv2.imread(test_data.small_bin_fill, -1)
+    fill_img = floodfill(bin_img=img,point=(137,31),value=0)
+    fill_img = floodfill(bin_img=fill_img,point=(189,214),value=0)
+    fill_img = floodfill(bin_img=fill_img,point=(312,361),value=0)
     # Assert that the image has been filled in
     assert np.sum(fill_img)==0
 
@@ -20,4 +21,4 @@ def test_floodfill_bad_input(test_data):
     # Read in test data
     img = cv2.imread(test_data.small_gray_img, -1)
     with pytest.raises(RuntimeError):
-        _ = floodfill(bin_img=img)
+        _ = floodfill(bin_img=img, point = (1,1),value=0)
