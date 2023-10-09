156ccbb488ed1922e50fcbf19d83a47c7fabf28f
Malia Gehan <mgehan@danforthcenter.org>
2023-10-09 12:24:03 -0500

    added tests for clickcount_labels and clickcount_correct
    tests added

---
+++ tests/plantcv/annotate/test_clickcount_correct.py
@@ -0,0 +1,30 @@
+#import os
+import cv2
+import numpy as np
+import matplotlib
+from plantcv.plantcv.annotate import ClickCount
+from plantcv.plantcv.annotate import clickcount_correct
+
+
+def test_plantcv_click_count_correct(annotate_test_data):
+    """Test for PlantCV."""
+    # Create a test tmp directory
+    # generate fake testing image
+    img= cv2.imread(annotate_test_data.pollen, -1)
+    allmask = cv2.imread(annotate_test_data.pollen_all, -1)
+    discs = cv2.imread(annotate_test_data.pollen_discs, -1)
+    
+    coor=[(158, 531), (265, 427), (361, 112), (500, 418)]
+    totalpoints1=[(158, 531), (361, 112), (500, 418), (269.25303806488864, 385.69839981447126), 
+                  (231.21964288863632, 445.995245825603), (293.37177646934134, 448.778177179963), (240.49608073650273, 277.1640769944342), 
+                  (279.4571196975417, 240.05832560296852), (77.23077461405376, 165.84682282003712), (423.24190633947126, 364.3625927643785), 
+                  (509.5127783246289, 353.2308673469388), (527.1380102355752, 275.3087894248609), (445.50535717435065, 138.94515306122452)]
+    germinated1= [(283.3633696975417, 92.56296382189242), (429.93108769383116, 369.0008116883117)]
+    counter = ClickCount(img, figsize=(8, 6))
+    counter.import_coords(totalpoints1, label="total")
+    counter.import_coords(germinated1, label="germinated")
+
+    corrected_mask = clickcount_correct(discs,allmask, counter, coor)
+
+    assert np.count_nonzero(discs)< np.count_nonzero(corrected_mask) 
+    assert np.count_nonzero(corrected_mask) < np.count_nonzero(allmask)
---
+++ tests/plantcv/annotate/test_clickcount_label.py
@@ -0,0 +1,31 @@
+#import os
+import cv2
+import numpy as np
+import matplotlib
+from plantcv.plantcv.annotate import ClickCount
+from plantcv.plantcv.annotate import clickcount_label
+
+
+def test_clickcount_label(annotate_test_data):
+    """Test for PlantCV."""
+    # Read in test data
+    img= cv2.imread(annotate_test_data.pollen, -1)
+    recovered = cv2.imread(annotate_test_data.pollen_recovered, -1)
+    watershed = cv2.imread(annotate_test_data.pollen_watershed, -1)
+    
+    totalpoints1=[(158, 531), (361, 112), (500, 418), (269.25303806488864, 385.69839981447126), 
+                  (231.21964288863632, 445.995245825603), (293.37177646934134, 448.778177179963), (240.49608073650273, 277.1640769944342), 
+                  (279.4571196975417, 240.05832560296852), (77.23077461405376, 165.84682282003712), (423.24190633947126, 364.3625927643785), 
+                  (509.5127783246289, 353.2308673469388), (527.1380102355752, 275.3087894248609), (445.50535717435065, 138.94515306122452)]
+    germinated1= [(283.3633696975417, 92.56296382189242), (429.93108769383116, 369.0008116883117)]
+    counter = ClickCount(img, figsize=(8, 6))
+    counter.import_coords(totalpoints1, label="total")
+    counter.import_coords(germinated1, label="germinated")
+
+    imagesname = "test"
+
+    class_label,class_list,num = clickcount_label(recovered, watershed,counter, imagesname)
+
+    assert num == 15
+
+  
