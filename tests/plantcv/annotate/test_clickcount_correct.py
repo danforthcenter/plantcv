0621e7a2dec5d2422e93279bf23f126a3675a87f
Malia Gehan <mgehan@danforthcenter.org>
2023-10-10 14:43:46 -0500

    fixed tests to add coverage

--- tests/plantcv/annotate/test_clickcount_correct.py
+++ tests/plantcv/annotate/test_clickcount_correct.py
@@ -18,13 +18,13 @@ def test_plantcv_click_count_correct(annotate_test_data):
     totalpoints1=[(158, 531), (361, 112), (500, 418), (269.25303806488864, 385.69839981447126), 
                   (231.21964288863632, 445.995245825603), (293.37177646934134, 448.778177179963), (240.49608073650273, 277.1640769944342), 
                   (279.4571196975417, 240.05832560296852), (77.23077461405376, 165.84682282003712), (423.24190633947126, 364.3625927643785), 
-                  (509.5127783246289, 353.2308673469388), (527.1380102355752, 275.3087894248609), (445.50535717435065, 138.94515306122452)]
+                  (509.5127783246289, 353.2308673469388), (527, 275), (445.50535717435065, 138.94515306122452)]
     germinated1= [(283.3633696975417, 92.56296382189242), (429.93108769383116, 369.0008116883117)]
     counter = ClickCount(img, figsize=(8, 6))
     counter.import_coords(totalpoints1, label="total")
     counter.import_coords(germinated1, label="germinated")
 
-    corrected_mask = clickcount_correct(discs,allmask, counter, coor)
+    corrected_mask, counter1 = clickcount_correct(discs,allmask, counter, coor)
 
     assert np.count_nonzero(discs)< np.count_nonzero(corrected_mask) 
     assert np.count_nonzero(corrected_mask) < np.count_nonzero(allmask)
