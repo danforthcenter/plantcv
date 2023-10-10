8c7ab9baecab1771fc148aa8ee062a88756c3f42
Malia Gehan <mgehan@danforthcenter.org>
2023-10-10 16:35:53 -0500

    deepsource corrections
    deepsource corrections

--- plantcv/plantcv/annotate/clickcount_label.py
+++ plantcv/plantcv/annotate/clickcount_label.py
@@ -9,7 +9,6 @@ from plantcv.plantcv import outputs
 
 def _clickcount_labels(counter):
     """Get the label names"""
-
     labels = list(counter.count)
     
     return labels
@@ -20,25 +19,25 @@ def clickcount_label(gray_img, counter, imgname='default'):
     # gray_img - gray image with objects labeled (e.g.watershed output)
     # counter - ClickCount object
     # imagename - imagename or sample identification to add to output information
-    
+
     debug = params.debug
     params.debug = None
 
     labelnames = _clickcount_labels(counter)
-    
+
     dict_class_labels = {}
-    
+
     for i, x in enumerate(labelnames):
         dict_class_labels[x] = i+1
-    
+
     shape = np.shape(gray_img)
     class_label = np.zeros((shape[0], shape[1]), dtype=np.uint8)
 
     class_number = []
     class_name = []
-    
+
     # Only keep watersed results that overlap with a clickpoint and do not ==0
-    
+
     for cl in list(dict_class_labels.keys()):
         for (y, x) in counter.points[cl]:
             x = int(x)
@@ -48,12 +47,12 @@ def clickcount_label(gray_img, counter, imgname='default'):
                 class_number.append(seg_label)
                 class_name.append(cl)
                 class_label[gray_img == seg_label] = seg_label
-    
+
     # Get corrected name
-    
+
     corrected_number = []
     corrected_name = []
-    
+
     for i, x in enumerate(class_number):
         if x in corrected_number:
             ind = (corrected_number.index(x))
@@ -63,25 +62,24 @@ def clickcount_label(gray_img, counter, imgname='default'):
             corrected_number.append(x)
             y = str(class_name[i])
             corrected_name.append(y)
-            
+   
     classes = np.unique(corrected_name)
     class_dict = {}
     count_class_dict = {}
-    
+
     for i, x in enumerate(classes):
         class_dict[x] = i+1
         count_class_dict[x] = corrected_name.count(x)
-    
+
     corrected_label = np.zeros((shape[0], shape[1]), dtype=np.uint8)
     corrected_class = np.zeros((shape[0], shape[1]), dtype=np.uint8)
-    
 
     for i, value in enumerate(corrected_number):
         if value != 0:
             corrected_label[gray_img == value] = i+1
             corrected_class[gray_img == value] = class_dict[corrected_name[i]]
             corrected_name[i] = str(i+1)+"_"+corrected_name[i]
-    
+
     num = len(corrected_name)
 
     vis_labeled = colorize_label_img(corrected_label)
@@ -92,14 +90,13 @@ def clickcount_label(gray_img, counter, imgname='default'):
            filename=os.path.join(params.debug_outdir, str(params.device) + '_corrected__labels_img.png'))
     _debug(visual=vis_class,
            filename=os.path.join(params.debug_outdir, str(params.device) + '_corrected_class.png'))
-    
+
     for i, x in enumerate(count_class_dict.keys()):
         variable = x
         value = count_class_dict[x]
-        outputs.add_observation(sample=imgname, variable=variable, 
+        outputs.add_observation(sample=imgname, variable=variable,
                                 trait='count of category',
                                 method='count', scale='count', datatype=int,
                                 value=value, label=variable)
-    
 
     return corrected_label, corrected_name, num
