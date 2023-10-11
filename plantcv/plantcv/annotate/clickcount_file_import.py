654ad4c2325eccb67e80380bd935c99bd54ff979
Malia Gehan <mgehan@danforthcenter.org>
2023-10-11 14:45:00 -0500

    deepsource fix

--- plantcv/plantcv/annotate/clickcount_file_import.py
+++ plantcv/plantcv/annotate/clickcount_file_import.py
@@ -18,5 +18,5 @@ def clickcount_file_import(img, coord_file):
         keycoor = coords[key]
         keycoor = list(map(lambda sub: (sub[1], sub[0]), keycoor))
         counter.import_coords(keycoor, label=key)
- 
+
     return counter
--- tests/plantcv/annotate/test_clickcount_file_import.py
+++ tests/plantcv/annotate/test_clickcount_file_import.py
@@ -2,9 +2,8 @@ import cv2
 from plantcv.plantcv.annotate import clickcount_file_import
 
 
-def test_plantcv_visualize_click_count(annotate_test_data):
+def test_plantcv_clickcount_file_import(annotate_test_data):
     """Test for PlantCV."""
-
     img = cv2.imread(annotate_test_data.discs_mask, -1)
     file  = annotate_test_data.pollen_coords
     counter = clickcount_file_import(img, file)
