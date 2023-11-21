3c4d3c7b8577722973e27b7a3e765572b1c22b96
HaleySchuhl <haleyschuhl@gmail.com>
2023-11-21 12:00:43 -0600

    remove extra param from file_import method call in test

--- tests/plantcv/annotate/test_clickcount_file_import.py
+++ tests/plantcv/annotate/test_clickcount_file_import.py
@@ -7,6 +7,6 @@ def test_plantcv_clickcount_file_import(annotate_test_data):
     img = cv2.imread(annotate_test_data.discs_mask, -1)
     counter = ClickCount(img, figsize=(8, 6))
     file  = annotate_test_data.pollen_coords
-    counter.file_import(img, file)
+    counter.file_import(file)
 
     assert counter.count['total'] == 70
