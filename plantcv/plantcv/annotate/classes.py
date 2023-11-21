8d5252a287dbc6cf99e49e01acda3343dd0133fa
HaleySchuhl <haleyschuhl@gmail.com>
2023-11-21 11:40:17 -0600

    move clickcount_correct to .correct method in annotate/classes.py

--- plantcv/plantcv/annotate/classes.py
+++ plantcv/plantcv/annotate/classes.py
@@ -5,7 +5,8 @@ import numpy as np
 from math import floor
 import matplotlib.pyplot as plt
 from scipy.spatial import distance
-from plantcv.plantcv import warn
+from plantcv.plantcv import warn, floodfill, _debug
+
 
 
 # Class helpers
@@ -23,6 +24,93 @@ def _find_closest_pt(pt, pts):
     dists = distance.cdist([pt], pts, 'euclidean')
     idx = np.argmin(dists)
     return idx, pts[idx]
+
+def _recover_circ(bin_img, c):
+    """function to recover circular objects"""
+    # Generates a binary image of a disc based on the coordinates c
+    # and the surrounding white pixels in bin_img
+    # make bin_img 1-0
+    bin_img = 1*(bin_img != 0)
+    h, w = bin_img.shape
+    # coordinates of pixels in the shape of the bin_image
+    X, Y = np.meshgrid(np.linspace(0, w-1, w), np.linspace(0, h-1, h))
+
+    # Alternate two steps:
+    # 1 - growing the disc centered in c until it overlaps with black pixels in bin_img.
+    # 2 - move c a step of at least one pixel towards the center of mass (mean of coordinate values)
+    # of the white pixels of bin_img that overlap with the disc.
+    # It terminates when the direction of the step in both axis has changed once.
+    # radius
+    r = 0
+    # sum of previous directions
+    dir_x_hist = 0
+    dir_y_hist = 0
+    # flags indicate if there has been a change in direction in each axis
+    chg_dir_x = False
+    chg_dir_y = False
+
+    while (chg_dir_x and chg_dir_y) is False:
+        # image of concentric circles centered in c
+        circ = np.sqrt((X-c[1])**2+(Y-c[0])**2)
+        # growing radius until it reaches black pixels
+        inside = True
+        while inside is True:
+            circ_mask = 1*(circ < r)
+            circ_mask_area = np.sum(circ_mask)
+            masked_circ = bin_img*circ_mask
+            # if masked_circ has a smaller count it means the circle is
+            # overlapping black pixels -> no longer inside the white part
+            if np.abs(np.sum(masked_circ) - circ_mask_area) > 100:
+                inside = False
+            else:
+                r += 1
+        # moving center towards the center of mass
+        Cx = np.mean(X[masked_circ == 1])
+        Cy = np.mean(Y[masked_circ == 1])
+
+        dir_x = np.sign(Cx - c[1])
+        dir_y = np.sign(Cy - c[0])
+
+        stepx = dir_x*np.ceil(np.abs(Cx - c[1]))
+        stepy = dir_y*np.ceil(np.abs(Cy - c[0]))
+
+        dir_x_hist += dir_x
+        dir_y_hist += dir_y
+        # register that a change in direction has occurred to terminate when
+        # is has happened in both directions
+        if np.sign(dir_x_hist) != dir_x or dir_x == 0:
+            chg_dir_x = True
+
+        if np.sign(dir_y_hist) != dir_y or dir_y == 0:
+            chg_dir_y = True
+
+        c[1] += stepx.astype(np.int32)
+        c[0] += stepy.astype(np.int32)
+
+    circ_mask = 1*(circ < r)
+    masked_circ = (bin_img*circ_mask).astype(np.uint8)
+    masked_circ[c[0], c[1]] = 1  # center of mass always part of the mask
+
+    return masked_circ, c
+
+
+def _clickcount_labels(self):
+    """Function to get label names"""
+    labels = list(self.count)
+
+    return labels
+
+
+def _remove_points(autolist, confirmedlist):
+    """Function to remove points if interactively removed by user"""
+    # internal function to remove to remove points specified by a user
+    removecoor = []
+
+    for element in autolist:
+        if element not in confirmedlist:
+            removecoor.append(element)
+
+    return removecoor
 
 
 class Points:
@@ -167,3 +255,62 @@ class ClickCount:
             self.ax.lines[idx_remove].remove()
             self.count[self.label] -= 1
         self.fig.canvas.draw()
+
+    def correct(self, bin_img, bin_img_recover, coords):
+    """
+    Method to correct ClickCount object instance by removing or recovering points
+    
+    Inputs:
+    bin_img = binary image, image with selected objects
+    bin_img_recover = binary image, image with all potential objects
+    coords = coordinates of 'auto' detected points (coordinate output of detect_discs)
+
+    :param bin_img: ndarray
+    :param bin_img_recover = ndarray
+    :param coords = list
+    :return completed_mask: ndarray
+    :return self: plantcv.plantcv.classes.ClickCount
+    """
+
+    debug = params.debug
+    params.debug = None
+
+    labelnames = _clickcount_labels(self)
+
+    completed_mask = np.copy(bin_img)
+
+    totalcoor = []
+
+    for names in labelnames:
+        for i, (x, y) in enumerate(self.points[names]):
+            x = int(x)
+            y = int(y)
+            totalcoor.append((y, x))
+
+    removecoor = _remove_points(coords, totalcoor)
+    removecoor = list(map(lambda sub: (sub[1], sub[0]), removecoor))
+    completed_mask = floodfill(completed_mask, removecoor, 0)
+
+    # points in class used for recovering and labeling
+    for names in labelnames:
+        for i, (x, y) in enumerate(self.points[names]):
+            x = int(x)
+            y = int(y)
+            # corrected coordinates
+            self.points[names][i] = (x, y)
+            # if the coordinates point to 0 in the binary image, recover the grain and coordinates of center
+            if completed_mask[y, x] == 1 or completed_mask[y, x] == 0:
+                print(f"Recovering grain at coordinates: x = {x}, y = {y}")
+                masked_circ, [a, b] = _recover_circ(bin_img_recover, [y, x])
+                completed_mask = completed_mask + masked_circ
+                self.points[names][i] = (b, a)
+
+    completed_mask1 = 1*((completed_mask + 1*(completed_mask == 255)) != 0).astype(np.uint8)
+
+    params.debug = debug
+
+    _debug(visual=completed_mask1, filename=os.path.join(params.debug_outdir, f"{params.device}_clickcount-corrected.png"))
+
+    return completed_mask, self
+
+
