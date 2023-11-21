2fb3b4bfcf9cac806896c64afb3a4ca2a9ffa31d
HaleySchuhl <haleyschuhl@gmail.com>
2023-11-21 11:59:14 -0600

    Update test_clickcount_file_import.py

--- tests/plantcv/annotate/test_clickcount_file_import.py
+++ tests/plantcv/annotate/test_clickcount_file_import.py
@@ -1,11 +1,12 @@
 import cv2
-from plantcv.plantcv.annotate import clickcount_file_import
+from plantcv.plantcv.annotate import ClickCount
 
 
 def test_plantcv_clickcount_file_import(annotate_test_data):
     """Test for PlantCV."""
     img = cv2.imread(annotate_test_data.discs_mask, -1)
+    counter = ClickCount(img, figsize=(8, 6))
     file  = annotate_test_data.pollen_coords
-    counter = clickcount_file_import(img, file)
+    counter.file_import(img, file)
 
     assert counter.count['total'] == 70
