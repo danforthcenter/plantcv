c3871ec1eac1c182170acb65355fb446e26c086a
HaleySchuhl <haleyschuhl@gmail.com>
2024-01-18 13:43:20 -0600

    Create test_detect_discs.py

---
+++ tests/plantcv/test_detect_discs.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np 
+from plantcv.plantcv.detect_discs import detect_discs
+
+
+def test_detect_discs(test_data):
+    """Test for PlantCV."""
+    # Read in test data
+    mask = cv2.imread(test_data.small_bin_img, -1)
+
+    filtered_mask = detect_discs(bin_img=mask, ecc_thresh=0.8)
+
+    assert  np.sum(mask) > np.sum(filtered_mask) 
