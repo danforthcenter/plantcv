3a67030fc823f531be0c3040dc17757552c7bceb
HaleySchuhl <haleyschuhl@gmail.com>
2024-01-18 13:38:51 -0600

    Update test_detect_discs.py
    whitespace corrections

--- plantcv/plantcv/test_detect_discs.py
+++ plantcv/plantcv/test_detect_discs.py
@@ -10,4 +10,4 @@ def test_detect_discs(test_data):
 
     filtered_mask = detect_discs(bin_img=mask, ecc_thresh=0.8)
 
-    assert  np.sum(mask) > np.sum(filtered_mask) 
+    assert np.sum(mask) > np.sum(filtered_mask)
