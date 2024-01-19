7e811447b9351cede389ba595577dab0879224df
HaleySchuhl <haleyschuhl@gmail.com>
2024-01-19 11:45:33 -0600

    Update test_detect_discs.py
    different test data?

--- tests/plantcv/test_detect_discs.py
+++ tests/plantcv/test_detect_discs.py
@@ -6,8 +6,8 @@ from plantcv.plantcv.detect_discs import detect_discs
 def test_detect_discs(test_data):
     """Test for PlantCV."""
     # Read in test data
-    mask = cv2.imread(test_data.small_bin_img, -1)
+    mask = cv2.imread(test_data.small_bin_fill, -1)
 
-    filtered_mask = detect_discs(bin_img=mask, ecc_thresh=0.3)
+    filtered_mask = detect_discs(bin_img=mask, ecc_thresh=0.9)
 
     assert np.sum(mask) > np.sum(filtered_mask)
