336e254c145889dfc01af578fe9a04c79b5e48a9
KeelyBrown <156026125+k034b363@users.noreply.github.com>
2024-01-23 16:05:41 -0600

    fix returns in spectral_index.py

--- tests/plantcv/spectral_index/test_spectral_index.py
+++ tests/plantcv/spectral_index/test_spectral_index.py
@@ -13,8 +13,7 @@ def test_ndvi(spectral_index_test_data):
 def test_ndvi_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.ndvi(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.ndvi(hsi=index_array, distance=20)
+    assert spectral_index.ndvi(hsi=index_array, distance=20) is None
 
 
 def test_gdvi(spectral_index_test_data):
@@ -26,8 +25,7 @@ def test_gdvi(spectral_index_test_data):
 def test_gdvi_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.gdvi(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.gdvi(hsi=index_array, distance=20)
+    assert spectral_index.gdvi(hsi=index_array, distance=20) is None
 
 
 def test_savi(spectral_index_test_data):
@@ -39,8 +37,7 @@ def test_savi(spectral_index_test_data):
 def test_savi_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.savi(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.savi(hsi=index_array, distance=20)
+    assert spectral_index.savi(hsi=index_array, distance=20) is None
 
 
 def test_pri(spectral_index_test_data):
@@ -52,8 +49,7 @@ def test_pri(spectral_index_test_data):
 def test_pri_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.pri(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.pri(hsi=index_array, distance=20)
+    assert spectral_index.pri(hsi=index_array, distance=20) is None
 
 
 def test_ari(spectral_index_test_data):
@@ -65,8 +61,7 @@ def test_ari(spectral_index_test_data):
 def test_ari_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.ari(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.ari(hsi=index_array, distance=20)
+    assert spectral_index.ari(hsi=index_array, distance=20) is None
 
 
 def test_ci_rededge(spectral_index_test_data):
@@ -78,8 +73,7 @@ def test_ci_rededge(spectral_index_test_data):
 def test_ci_rededge_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.ci_rededge(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.ci_rededge(hsi=index_array, distance=20)
+    assert spectral_index.ci_rededge(hsi=index_array, distance=20) is None
 
 
 def test_cri550(spectral_index_test_data):
@@ -91,8 +85,7 @@ def test_cri550(spectral_index_test_data):
 def test_cri550_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.cri550(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.cri550(hsi=index_array, distance=20)
+    assert spectral_index.cri550(hsi=index_array, distance=20) is None
 
 
 def test_cri700(spectral_index_test_data):
@@ -104,8 +97,7 @@ def test_cri700(spectral_index_test_data):
 def test_cri700_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.cri700(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.cri700(hsi=index_array, distance=20)
+    assert spectral_index.cri700(hsi=index_array, distance=20) is None
 
 
 def test_egi(spectral_index_test_data):
@@ -124,8 +116,7 @@ def test_evi(spectral_index_test_data):
 def test_evi_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.evi(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.evi(hsi=index_array, distance=20)
+    assert spectral_index.evi(hsi=index_array, distance=20) is None
 
 
 def test_mari(spectral_index_test_data):
@@ -137,8 +128,7 @@ def test_mari(spectral_index_test_data):
 def test_mari_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.mari(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.mari(hsi=index_array, distance=20)
+    assert spectral_index.mari(hsi=index_array, distance=20) is None
 
 
 def test_mcari(spectral_index_test_data):
@@ -150,8 +140,7 @@ def test_mcari(spectral_index_test_data):
 def test_mcari_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.mcari(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.mcari(hsi=index_array, distance=20)
+    assert spectral_index.mcari(hsi=index_array, distance=20) is None
 
 
 def test_mtci(spectral_index_test_data):
@@ -163,8 +152,7 @@ def test_mtci(spectral_index_test_data):
 def test_mtci_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.mtci(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.mtci(hsi=index_array, distance=20)
+    assert spectral_index.mtci(hsi=index_array, distance=20) is None
 
 
 def test_ndre(spectral_index_test_data):
@@ -176,8 +164,7 @@ def test_ndre(spectral_index_test_data):
 def test_ndre_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.ndre(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.ndre(hsi=index_array, distance=20)
+    assert spectral_index.ndre(hsi=index_array, distance=20) is None
 
 
 def test_psnd_chla(spectral_index_test_data):
@@ -189,8 +176,7 @@ def test_psnd_chla(spectral_index_test_data):
 def test_psnd_chla_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.psnd_chla(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.psnd_chla(hsi=index_array, distance=20)
+    assert spectral_index.psnd_chla(hsi=index_array, distance=20) is None
 
 
 def test_psnd_chlb(spectral_index_test_data):
@@ -202,8 +188,7 @@ def test_psnd_chlb(spectral_index_test_data):
 def test_psnd_chlb_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.psnd_chlb(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.psnd_chlb(hsi=index_array, distance=20)
+    assert spectral_index.psnd_chlb(hsi=index_array, distance=20) is None
 
 
 def test_psnd_car(spectral_index_test_data):
@@ -215,8 +200,7 @@ def test_psnd_car(spectral_index_test_data):
 def test_psnd_car_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.psnd_car(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.psnd_car(hsi=index_array, distance=20)
+    assert spectral_index.psnd_car(hsi=index_array, distance=20) is None
 
 
 def test_psri(spectral_index_test_data):
@@ -228,8 +212,7 @@ def test_psri(spectral_index_test_data):
 def test_psri_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.psri(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.psri(hsi=index_array, distance=20)
+    assert spectral_index.psri(hsi=index_array, distance=20) is None
 
 
 def test_pssr_chla(spectral_index_test_data):
@@ -241,8 +224,7 @@ def test_pssr_chla(spectral_index_test_data):
 def test_pssr_chla_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.pssr_chla(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.pssr_chla(hsi=index_array, distance=20)
+    assert spectral_index.pssr_chla(hsi=index_array, distance=20) is None
 
 
 def test_pssr_chlb(spectral_index_test_data):
@@ -254,8 +236,7 @@ def test_pssr_chlb(spectral_index_test_data):
 def test_pssr_chlb_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.pssr_chlb(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.pssr_chlb(hsi=index_array, distance=20)
+    assert spectral_index.pssr_chlb(hsi=index_array, distance=20) is None
 
 
 def test_pssr_car(spectral_index_test_data):
@@ -267,8 +248,7 @@ def test_pssr_car(spectral_index_test_data):
 def test_pssr_car_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.pssr_car(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.pssr_car(hsi=index_array, distance=20)
+    assert spectral_index.pssr_car(hsi=index_array, distance=20) is None
 
 
 def test_rgri(spectral_index_test_data):
@@ -280,8 +260,7 @@ def test_rgri(spectral_index_test_data):
 def test_rgri_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.rgri(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.rgri(hsi=index_array, distance=20)
+    assert spectral_index.rgri(hsi=index_array, distance=20) is None
 
 
 def test_rvsi(spectral_index_test_data):
@@ -293,8 +272,7 @@ def test_rvsi(spectral_index_test_data):
 def test_rvsi_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.rvsi(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.rvsi(hsi=index_array, distance=20)
+    assert spectral_index.rvsi(hsi=index_array, distance=20) is None
 
 
 def test_sipi(spectral_index_test_data):
@@ -306,8 +284,7 @@ def test_sipi(spectral_index_test_data):
 def test_sipi_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.sipi(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.sipi(hsi=index_array, distance=20)
+    assert spectral_index.sipi(hsi=index_array, distance=20) is None
 
 
 def test_sr(spectral_index_test_data):
@@ -319,8 +296,7 @@ def test_sr(spectral_index_test_data):
 def test_sr_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.sr(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.sr(hsi=index_array, distance=20)
+    assert spectral_index.sr(hsi=index_array, distance=20) is None
 
 
 def test_vari(spectral_index_test_data):
@@ -332,8 +308,7 @@ def test_vari(spectral_index_test_data):
 def test_vari_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.vari(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.vari(hsi=index_array, distance=20)
+    assert spectral_index.vari(hsi=index_array, distance=20) is None
 
 
 def test_vi_green(spectral_index_test_data):
@@ -345,8 +320,7 @@ def test_vi_green(spectral_index_test_data):
 def test_vi_green_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.vi_green(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.vi_green(hsi=index_array, distance=20)
+    assert spectral_index.vi_green(hsi=index_array, distance=20) is None
 
 
 def test_wi(spectral_index_test_data):
@@ -358,5 +332,4 @@ def test_wi(spectral_index_test_data):
 def test_wi_bad_input(spectral_index_test_data):
     """Test for PlantCV."""
     index_array = spectral_index.wi(spectral_index_test_data.load_hsi(), distance=20)
-    with pytest.raises(RuntimeError):
-        _ = spectral_index.wi(hsi=index_array, distance=20)
+    assert spectral_index.wi(hsi=index_array, distance=20) is None
