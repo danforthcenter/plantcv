ae26245d869dcd1cbd3ea523a71f4d9717d77f0c
HaleySchuhl <haleyschuhl@gmail.com>
2024-02-02 13:19:28 -0600

    Update eccentricity.py
    update to call sample data from the filter_test_data now

--- tests/plantcv/filter/eccentricity.py
+++ tests/plantcv/filter/eccentricity.py
@@ -1,11 +1,12 @@
 import cv2
+import pytest
 import numpy as np
 from plantcv.plantcv.filter import eccentricity
 
-def test_detect_discs(test_data):
+def test_detect_discs(filter_test_data):
     """Test for PlantCV."""
     # Read in test data
-    mask = cv2.imread(test_data.small_bin_fill, -1)
+    mask = cv2.imread(filter_test_data.small_bin_fill, -1)
 
     filtered_mask = eccentricity(bin_img=mask, ecc_thresh=0.5)
 
