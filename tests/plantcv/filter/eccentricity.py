8ebd57cfd24ca67eb0ec8fb5b230547d01ea7c45
HaleySchuhl <haleyschuhl@gmail.com>
2024-02-02 12:00:36 -0600

    move and rename test file, update function name too

--- tests/plantcv/filter/eccentricity.py
+++ tests/plantcv/filter/eccentricity.py
@@ -1,12 +1,12 @@
 import cv2
 import numpy as np
-from plantcv.plantcv import detect_discs
+from plantcv.plantcv.filter import eccentricity
 
 def test_detect_discs(test_data):
     """Test for PlantCV."""
     # Read in test data
     mask = cv2.imread(test_data.small_bin_fill, -1)
 
-    filtered_mask = detect_discs(bin_img=mask, ecc_thresh=0.5)
+    filtered_mask = eccentricity(bin_img=mask, ecc_thresh=0.5)
 
     assert np.sum(mask) > np.sum(filtered_mask)
