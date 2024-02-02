b2070f86f1022401dd59a6423fe5102e7bcfc9fc
HaleySchuhl <haleyschuhl@gmail.com>
2024-02-02 13:30:41 -0600

    Update eccentricity.py
    rename test function name

--- tests/plantcv/filter/eccentricity.py
+++ tests/plantcv/filter/eccentricity.py
@@ -3,7 +3,7 @@ import pytest
 import numpy as np
 from plantcv.plantcv.filter import eccentricity
 
-def test_detect_discs(filter_test_data):
+def test_filter_eccentricity(filter_test_data):
     """Test for PlantCV."""
     # Read in test data
     mask = cv2.imread(filter_test_data.small_bin_fill, -1)
