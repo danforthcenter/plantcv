708e32ce5f0146c9d2af267c23e5dcc2530a21f5
KeelyBrown <156026125+k034b363@users.noreply.github.com>
2024-02-05 16:05:23 -0600

    Add test config

--- tests/plantcv/transform/conftest.py
+++ tests/plantcv/transform/conftest.py
@@ -44,6 +44,22 @@ class TransformTestData:
         self.source_corrected = os.path.join(self.datadir, "source_corrected.png")
         # Color card image
         self.colorcard_img = os.path.join(self.datadir, "colorcard_img.png")
+        # Merged image HS
+        self.merged_HS = os.path.join(self.datadir, "merged_HS.jpg")
+        # Merged image HA
+        self.merged_HA = os.path.join(self.datadir, "merged_HA.jpg")
+        # Merged image HG
+        self.merged_HG = os.path.join(self.datadir, "merged_HG.jpg")
+        # Merged image VS
+        self.merged_VS = os.path.join(self.datadir, "merged_VS.jpg")
+        # Merged image VA
+        self.merged_VA = os.path.join(self.datadir, "merged_VA.jpg")
+        # Merged image VG
+        self.merged_VG = os.path.join(self.datadir, "merged_VG.jpg")
+        # Horizontal images to merge
+        self.mergehoriz = os.path.join(self.datadir, "mergehoriz")
+        # Horizontal images to merge
+        self.mergevert = os.path.join(self.datadir, "mergevert")
 
     @staticmethod
     def create_test_img(sz_img):
--- tests/plantcv/transform/test_merge_images.py
+++ tests/plantcv/transform/test_merge_images.py
@@ -1,59 +1,59 @@
 import cv2
 from plantcv.plantcv.transform import merge_images
 
-def test_merge_images_HS(test_data):
+def test_merge_images_HS(transform_test_data):
     """Test for PlantCV."""
-    corrected_img = merge_images(test_data.mergehoriz, overlap_percentage = 30,
+    corrected_img = merge_images(transform_test_data.mergehoriz, overlap_percentage = 30,
                                  direction = "horizontal", method = "stacked")
-    query_img = cv2.imread(test_data.merged_HS)
+    query_img = cv2.imread(transform_test_data.merged_HS)
     assert corrected_img == query_img
     
-def test_merge_images_HR(test_data):
+def test_merge_images_HR(transform_test_data):
     """Test for PlantCV."""
-    corrected_img = merge_images(test_data.mergehoriz, overlap_percentage = 30,
+    corrected_img = merge_images(transform_test_data.mergehoriz, overlap_percentage = 30,
                                  direction = "horizontal", method = "random")
-    query_img = cv2.imread(test_data.merged_HS)
+    query_img = cv2.imread(transform_test_data.merged_HS)
     assert corrected_img.shape == query_img.shape
     
-def test_merge_images_HA(test_data):
+def test_merge_images_HA(transform_test_data):
     """Test for PlantCV."""
-    corrected_img = merge_images(test_data.mergehoriz, overlap_percentage = 30,
+    corrected_img = merge_images(transform_test_data.mergehoriz, overlap_percentage = 30,
                                  direction = "horizontal", method = "average")
-    query_img = cv2.imread(test_data.merged_HA)
+    query_img = cv2.imread(transform_test_data.merged_HA)
     assert corrected_img == query_img
     
-def test_merge_images_HG(test_data):
+def test_merge_images_HG(transform_test_data):
     """Test for PlantCV."""
-    corrected_img = merge_images(test_data.mergehoriz, overlap_percentage = 30,
+    corrected_img = merge_images(transform_test_data.mergehoriz, overlap_percentage = 30,
                                  direction = "horizontal", method = "gradual")
-    query_img = cv2.imread(test_data.merged_HG)
+    query_img = cv2.imread(transform_test_data.merged_HG)
     assert corrected_img == query_img
     
-def test_merge_images_VS(test_data):
+def test_merge_images_VS(transform_test_data):
     """Test for PlantCV."""
-    corrected_img = merge_images(test_data.mergevert, overlap_percentage = 30,
+    corrected_img = merge_images(transform_test_data.mergevert, overlap_percentage = 30,
                                  direction = "vertical", method = "stacked")
-    query_img = cv2.imread(test_data.merged_VS)
+    query_img = cv2.imread(transform_test_data.merged_VS)
     assert corrected_img == query_img
     
-def test_merge_images_VR(test_data):
+def test_merge_images_VR(transform_test_data):
     """Test for PlantCV."""
-    corrected_img = merge_images(test_data.mergevert, overlap_percentage = 30,
+    corrected_img = merge_images(transform_test_data.mergevert, overlap_percentage = 30,
                                  direction = "vertical", method = "random")
-    query_img = cv2.imread(test_data.merged_VS)
+    query_img = cv2.imread(transform_test_data.merged_VS)
     assert corrected_img.shape == query_img.shape
     
-def test_merge_images_VA(test_data):
+def test_merge_images_VA(transform_test_data):
     """Test for PlantCV."""
-    corrected_img = merge_images(test_data.mergevert, overlap_percentage = 30,
+    corrected_img = merge_images(transform_test_data.mergevert, overlap_percentage = 30,
                                  direction = "vertical", method = "average")
-    query_img = cv2.imread(test_data.merged_VA)
+    query_img = cv2.imread(transform_test_data.merged_VA)
     assert corrected_img == query_img
     
-def test_merge_images_VG(test_data):
+def test_merge_images_VG(transform_test_data):
     """Test for PlantCV."""
-    corrected_img = merge_images(test_data.mergevert, overlap_percentage = 30,
+    corrected_img = merge_images(transform_test_data.mergevert, overlap_percentage = 30,
                                  direction = "vertical", method = "gradual")
-    query_img = cv2.imread(test_data.merged_VG)
+    query_img = cv2.imread(transform_test_data.merged_VG)
     assert corrected_img == query_img
     
