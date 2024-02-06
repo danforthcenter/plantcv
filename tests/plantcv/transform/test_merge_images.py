8e9b4636fff1db91b50f5a819ba8d66cd760ecd7
KeelyBrown <156026125+k034b363@users.noreply.github.com>
2024-02-06 10:04:38 -0600

    fix ambiguous tests

--- tests/plantcv/transform/test_merge_images.py
+++ tests/plantcv/transform/test_merge_images.py
@@ -6,7 +6,7 @@ def test_merge_images_HS(transform_test_data):
     corrected_img = merge_images(transform_test_data.mergehoriz, overlap_percentage = 30,
                                  direction = "horizontal", method = "stacked")
     query_img = cv2.imread(transform_test_data.merged_HS)
-    assert corrected_img == query_img
+    assert corrected_img.all() == query_img.all()
     
 def test_merge_images_HR(transform_test_data):
     """Test for PlantCV."""
@@ -20,21 +20,21 @@ def test_merge_images_HA(transform_test_data):
     corrected_img = merge_images(transform_test_data.mergehoriz, overlap_percentage = 30,
                                  direction = "horizontal", method = "average")
     query_img = cv2.imread(transform_test_data.merged_HA)
-    assert corrected_img == query_img
+    assert corrected_img.all() == query_img.all()
     
 def test_merge_images_HG(transform_test_data):
     """Test for PlantCV."""
     corrected_img = merge_images(transform_test_data.mergehoriz, overlap_percentage = 30,
                                  direction = "horizontal", method = "gradual")
     query_img = cv2.imread(transform_test_data.merged_HG)
-    assert corrected_img == query_img
+    assert corrected_img.all() == query_img.all()
     
 def test_merge_images_VS(transform_test_data):
     """Test for PlantCV."""
     corrected_img = merge_images(transform_test_data.mergevert, overlap_percentage = 30,
                                  direction = "vertical", method = "stacked")
     query_img = cv2.imread(transform_test_data.merged_VS)
-    assert corrected_img == query_img
+    assert corrected_img.all() == query_img.all()
     
 def test_merge_images_VR(transform_test_data):
     """Test for PlantCV."""
@@ -48,12 +48,12 @@ def test_merge_images_VA(transform_test_data):
     corrected_img = merge_images(transform_test_data.mergevert, overlap_percentage = 30,
                                  direction = "vertical", method = "average")
     query_img = cv2.imread(transform_test_data.merged_VA)
-    assert corrected_img == query_img
+    assert corrected_img.all() == query_img.all()
     
 def test_merge_images_VG(transform_test_data):
     """Test for PlantCV."""
     corrected_img = merge_images(transform_test_data.mergevert, overlap_percentage = 30,
                                  direction = "vertical", method = "gradual")
     query_img = cv2.imread(transform_test_data.merged_VG)
-    assert corrected_img == query_img
+    assert corrected_img.all() == query_img.all()
     
