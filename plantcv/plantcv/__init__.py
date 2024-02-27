352a77116c14ba68216b4c3213af8d47f99b48ba
HaleySchuhl <haleyschuhl@gmail.com>
2024-02-27 11:16:59 -0600

    Update __init__.py
    filter to filters

--- plantcv/plantcv/__init__.py
+++ plantcv/plantcv/__init__.py
@@ -74,7 +74,7 @@ from plantcv.plantcv.segment_image_series import segment_image_series
 from plantcv.plantcv.create_labels import create_labels
 from plantcv.plantcv.floodfill import floodfill
 from plantcv.plantcv import analyze
-from plantcv.plantcv import filter
+from plantcv.plantcv import filters
 # add new functions to end of lists
 
 # Auto versioning
@@ -152,5 +152,5 @@ __all__ = [
     "create_labels",
     "analyze",
     "floodfill",
-    "filter"
+    "filters"
 ]
