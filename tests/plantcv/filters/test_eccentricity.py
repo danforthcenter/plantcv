6c58ab4044abc9090bd88fee5b9ded550e12c47e
Noah Fahlgren <noahfahlgren@gmail.com>
2024-02-27 12:31:54 -0600

    Fix assertion direction and lower threshold for coverage

--- tests/plantcv/filters/test_eccentricity.py
+++ tests/plantcv/filters/test_eccentricity.py
@@ -8,6 +8,6 @@ def test_filter_eccentricity(filter_test_data):
     # Read in test data
     mask = cv2.imread(filter_test_data.small_bin_fill, -1)
 
-    filtered_mask = eccentricity(bin_img=mask, ecc_thresh=0.8)
+    filtered_mask = eccentricity(bin_img=mask, ecc_thresh=0.5)
 
-    assert np.sum(mask) < np.sum(filtered_mask)
+    assert np.sum(mask) > np.sum(filtered_mask)
