fde5e19e9b9c779238ebae65226ebb7b781dac40
HaleySchuhl <haleyschuhl@gmail.com>
2024-02-28 07:45:03 -0600

    update test data variable name

--- tests/plantcv/filter/eccentricity.py
+++ tests/plantcv/filter/eccentricity.py
@@ -3,10 +3,10 @@ import pytest
 import numpy as np
 from plantcv.plantcv.filters import eccentricity
 
-def test_filter_eccentricity(filter_test_data):
+def test_filters_eccentricity(filters_test_data):
     """Test for PlantCV."""
     # Read in test data
-    mask = cv2.imread(filter_test_data.small_bin_fill, -1)
+    mask = cv2.imread(filters_test_data.small_bin_fill, -1)
 
     filtered_mask = eccentricity(bin_img=mask, ecc_thresh=0.5)
 
