0ef68ce251dd4d2abfe334432d48ff2a036b1c47
nfahlgren <noahfahlgren@gmail.com>
2024-03-10 18:16:21 -0500

    Add test for analyze distribution

---
+++ tests/plantcv/analyze/test_distribution.py
@@ -0,0 +1,16 @@
+# Tests for pcv.analyze.distribution
+import cv2
+import numpy as np
+from plantcv.plantcv import outputs
+from plantcv.plantcv.analyze import distribution as analyze_distribution
+
+
+def test_distribution(test_data):
+    """Test for PlantCV."""
+    # Clear previous outputs
+    outputs.clear()
+    # Read in test data
+    mask = cv2.imread(test_data.small_bin_img, -1)
+
+    _ = analyze_distribution(labeled_mask=mask, n_labels=1)
+    assert int(outputs.observations['default_1']['X_distribution_mean']['value']) == 200
