d882dbe5e90b16431d10b02687d0d3218b437771
nfahlgren <noahfahlgren@gmail.com>
2024-03-10 18:28:48 -0500

    Do valid math

--- plantcv/plantcv/analyze/distribution.py
+++ plantcv/plantcv/analyze/distribution.py
@@ -66,7 +66,7 @@ def _analyze_distribution(img, mask, bin_size_x=100, bin_size_y=100, label=None)
     :return distribution_images: list
     """
     # Image not needed
-    img /= 1
+    img -= 0
     # Initialize output data
     # find the height and width, in pixels, for this image
     height, width = mask.shape[:2]
