e965e3404e84646bcecf1b5a7a46f87cff0b7f20
KeelyBrown <156026125+k034b363@users.noreply.github.com>
2024-03-15 15:34:55 -0500

    added '_' for unused readimage vars in test

--- tests/plantcv/test_kmeans_classifier.py
+++ tests/plantcv/test_kmeans_classifier.py
@@ -10,16 +10,16 @@ def test_kmeans_classifier(test_data):
     """Test for PlantCV."""
     input_dir = test_data.kmeans_classifier_dir
     labeled_img = predict_kmeans(img=input_dir+"/test_image.png", model_path=input_dir+"/kmeans_out.fit")
-    test_labeled = pcv.readimage(input_dir+"/labeled_image.png")
+    test_labeled, _, _ = pcv.readimage(input_dir+"/labeled_image.png")
     assert labeled_img == test_labeled
     
     mask_dict = mask_kmeans(labeled_img=labeled_img, K=5)
     test_dict = {}
     for i in range(5):
-        test_dict[str(i)] = pcv.readimage(input_dir+"/label_example_"+str(i)+".png")
+        test_dict[str(i)], _, _ = pcv.readimage(input_dir+"/label_example_"+str(i)+".png")
     assert mask_dict == test_dict
 
     combo_mask = mask_kmeans(labeled_img=labeled_img, K=5, cat_list=[1,2,3])
-    combo_example = pcv.readimage(input_dir+"/combo_mask_example.png")
+    combo_example, _, _ = pcv.readimage(input_dir+"/combo_mask_example.png")
     assert combo_mask == combo_example
     
