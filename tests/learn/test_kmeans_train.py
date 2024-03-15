857655c571e22effb6d534981dd9acf1d6a1d7f5
KeelyBrown <156026125+k034b363@users.noreply.github.com>
2024-03-15 15:26:28 -0500

    changed test to assert file exists

--- tests/learn/test_kmeans_train.py
+++ tests/learn/test_kmeans_train.py
@@ -10,18 +10,13 @@ def test_train_kmeans(learn_test_data, tmpdir):
     # Create a test tmp directory
     cache_dir = tmpdir.mkdir("cache")
     training_dir = learn_test_data.kmeans_train_dir
+    outfile_subset = os.path.join(str(cache_dir), "kmeansout_subset.fit")
+    outfile_full = os.path.join(str(cache_dir), "kmeansout_full.fit")
     # Train full model and partial model 
     fit_subset = train_kmeans(img_dir=training_dir, prefix="image", 
-                              out_path=cache_dir+"/kmeansout_subset.fit", K=5, num_imgs=5)
+                              out_path=outfile_subset, K=5, num_imgs=5)
     fit_full = train_kmeans(img_dir=training_dir, prefix="image", 
-                            out_path=cache_dir+"/kmeansout_full.fit", K=5)
-    # Load example ouputs and test equality
-    f = open(training_dir+"/kmeansout_subset.fit", "rb")
-    test_subset = load(f)
-    assert fit_subset == test_subset
-    f.close()
+                            out_path=outfile_full, K=5)    
+    assert os.path.exists(outfile_subset)
+    assert os.path.exists(outfile_full)
     
-    f = open(training_dir+"/kmeansout_full.fit", "rb")
-    test_full = load(f)
-    assert fit_full == test_full
-    f.close()
