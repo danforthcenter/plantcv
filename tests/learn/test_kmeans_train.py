b676259ca561b26e0adf96fddd1351543973f96b
KeelyBrown <156026125+k034b363@users.noreply.github.com>
2024-03-18 13:15:08 -0500

    Added leaf example to test data

--- tests/learn/test_kmeans_train.py
+++ tests/learn/test_kmeans_train.py
@@ -13,9 +13,9 @@ def test_train_kmeans(learn_test_data, tmpdir):
     outfile_subset = os.path.join(str(cache_dir), "kmeansout_subset.fit")
     outfile_full = os.path.join(str(cache_dir), "kmeansout_full.fit")
     # Train full model and partial model 
-    fit_subset = train_kmeans(img_dir=training_dir, prefix="image", 
-                              out_path=outfile_subset, K=5, num_imgs=5)
-    fit_full = train_kmeans(img_dir=training_dir, prefix="image", 
+    fit_subset = train_kmeans(img_dir=training_dir, prefix="kmeans_train", 
+                              out_path=outfile_subset, K=5, num_imgs=3)
+    fit_full = train_kmeans(img_dir=training_dir, prefix="kmeans_train", 
                             out_path=outfile_full, K=5)    
     assert os.path.exists(outfile_subset)
     assert os.path.exists(outfile_full)
