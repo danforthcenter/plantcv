f5866bad7dc23b6b1cbef6b663fa639a7f301f7b
KeelyBrown <156026125+k034b363@users.noreply.github.com>
2024-03-18 13:57:58 -0500

    Try to fix cyclical import

--- tests/plantcv/test_kmeans_classifier.py
+++ tests/plantcv/test_kmeans_classifier.py
@@ -1,20 +1,20 @@
-from plantcv import plantcv as pcv
 from plantcv.plantcv.kmeans_classifier import mask_kmeans
 from plantcv.plantcv.kmeans_classifier import predict_kmeans
+from plantcv.plantcv import readimage
 
 
 def test_kmeans_classifier(test_data):
     """Test for PlantCV."""
     input_dir = test_data.kmeans_classifier_dir
     labeled_img = predict_kmeans(img=input_dir+"/test_image.jpg", model_path=input_dir+"/kmeans_out.fit", patch_size=5)
-    test_labeled, _, _ = pcv.readimage(input_dir+"/labeled_image.png")
+    test_labeled, _, _ = readimage(input_dir+"/labeled_image.png")
     assert (labeled_img == test_labeled).all()
     
     mask_dict = mask_kmeans(labeled_img=labeled_img, K=4, patch_size=5)
     for i in range(4):
-        assert (pcv.readimage(input_dir+"/label_example_"+str(i)+".png")[0] == mask_dict[str(i)]).all()
+        assert (readimage(input_dir+"/label_example_"+str(i)+".png")[0] == mask_dict[str(i)]).all()
 
     combo_mask = mask_kmeans(labeled_img=labeled_img, K=4, patch_size=5, cat_list=[1,2])
-    combo_example, _, _ = pcv.readimage(input_dir+"/combo_mask_example.png")
+    combo_example, _, _ = readimage(input_dir+"/combo_mask_example.png")
     assert (combo_mask == combo_example).all()
     
