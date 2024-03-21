ab787740d784e29240bbf9b91ba3665d8d41ca82
KeelyBrown <156026125+k034b363@users.noreply.github.com>
2024-03-21 12:27:48 -0500

    Fixed all tests using tmpdir

--- tests/plantcv/transform/test_merge_images.py
+++ tests/plantcv/transform/test_merge_images.py
@@ -23,28 +23,37 @@ def test_merge_images_HR(transform_test_data):
     assert corrected_img.shape == query_img.shape
 
 
-def test_merge_images_HA(transform_test_data):
+def test_merge_images_HA(transform_test_data, tmpdir):
     """Test for PlantCV."""
+    cache_dir = tmpdir.mkdir("cache")
     corrected_img = merge_images(transform_test_data.mergehoriz, overlap_percentage=30,
                                  direction="horizontal", method="average")
+    pcv.print_image(corrected_img, os.path.join(cache_dir, "merged_img.jpg"))
+    merged_img = cv2.imread(os.path.join(cache_dir, "merged_img.jpg"))
     query_img = cv2.imread(transform_test_data.merged_HA)
-    assert (corrected_img == query_img).all()
+    assert (merged_img == query_img).all()
 
 
-def test_merge_images_HG(transform_test_data):
+def test_merge_images_HG(transform_test_data, tmpdir):
     """Test for PlantCV."""
+    cache_dir = tmpdir.mkdir("cache")
     corrected_img = merge_images(transform_test_data.mergehoriz, overlap_percentage=30,
                                  direction="horizontal", method="gradual")
+    pcv.print_image(corrected_img, os.path.join(cache_dir, "merged_img.jpg"))
+    merged_img = cv2.imread(os.path.join(cache_dir, "merged_img.jpg"))
     query_img = cv2.imread(transform_test_data.merged_HG)
-    assert (corrected_img == query_img).all()
+    assert (merged_img == query_img).all()
 
 
-def test_merge_images_VS(transform_test_data):
+def test_merge_images_VS(transform_test_data, tmpdir):
     """Test for PlantCV."""
-    corrected_img = merge_images(transform_test_data.mergevert, overlap_percentage=30,
+    cache_dir = tmpdir.mkdir("cache")
+    corrected_img = merge_images(transform_test_data.mergehoriz, overlap_percentage=30,
                                  direction="vertical", method="stacked")
+    pcv.print_image(corrected_img, os.path.join(cache_dir, "merged_img.jpg"))
+    merged_img = cv2.imread(os.path.join(cache_dir, "merged_img.jpg"))
     query_img = cv2.imread(transform_test_data.merged_VS)
-    assert (corrected_img == query_img).all()
+    assert (merged_img == query_img).all()
 
 
 def test_merge_images_VR(transform_test_data):
@@ -55,17 +64,23 @@ def test_merge_images_VR(transform_test_data):
     assert corrected_img.shape == query_img.shape
 
 
-def test_merge_images_VA(transform_test_data):
+def test_merge_images_VA(transform_test_data, tmpdir):
     """Test for PlantCV."""
-    corrected_img = merge_images(transform_test_data.mergevert, overlap_percentage=30,
+    cache_dir = tmpdir.mkdir("cache")
+    corrected_img = merge_images(transform_test_data.mergehoriz, overlap_percentage=30,
                                  direction="vertical", method="average")
+    pcv.print_image(corrected_img, os.path.join(cache_dir, "merged_img.jpg"))
+    merged_img = cv2.imread(os.path.join(cache_dir, "merged_img.jpg"))
     query_img = cv2.imread(transform_test_data.merged_VA)
-    assert (corrected_img == query_img).all()
+    assert (merged_img == query_img).all()
 
 
-def test_merge_images_VG(transform_test_data):
+def test_merge_images_VG(transform_test_data, tmpdir):
     """Test for PlantCV."""
-    corrected_img = merge_images(transform_test_data.mergevert, overlap_percentage=30,
+    cache_dir = tmpdir.mkdir("cache")
+    corrected_img = merge_images(transform_test_data.mergehoriz, overlap_percentage=30,
                                  direction="vertical", method="gradual")
+    pcv.print_image(corrected_img, os.path.join(cache_dir, "merged_img.jpg"))
+    merged_img = cv2.imread(os.path.join(cache_dir, "merged_img.jpg"))
     query_img = cv2.imread(transform_test_data.merged_VG)
-    assert (corrected_img == query_img).all()
+    assert (merged_img == query_img).all()
