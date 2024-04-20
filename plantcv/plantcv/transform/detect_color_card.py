628c9dd5dbde876c6d0161a77efed81d069ebdde
Noah Fahlgren <noahfahlgren@gmail.com>
2024-04-20 15:39:50 -0500

    Refactor to inline check

--- plantcv/plantcv/transform/detect_color_card.py
+++ plantcv/plantcv/transform/detect_color_card.py
@@ -52,8 +52,7 @@ def detect_color_card(rgb_img, label=None, **kwargs):
         Labeled mask of chips.
     """
     # Set lable to params.sample_label if None
-    if label is None:
-        label = params.sample_label
+    label = params.sample_label if label is None else label
 
     # Get keyword arguments and set defaults if not set
     min_size = kwargs.get("min_size", 1000)  # Minimum size for _is_square chip filtering
