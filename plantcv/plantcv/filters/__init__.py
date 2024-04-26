a8f20f64cf7f29ce6022dd9219036532832b65f5
HaleySchuhl <haleyschuhl@gmail.com>
2024-04-26 09:43:50 -0500

    add solidity to init for filters subpackage

--- plantcv/plantcv/filters/__init__.py
+++ plantcv/plantcv/filters/__init__.py
@@ -1,3 +1,5 @@
 from plantcv.plantcv.filters.eccentricity import eccentricity
+from plantcv.plantcv.filters.solidity import solidity
 
-__all__ = ["eccentricity"]
+__all__ = ["eccentricity",
+           "solidity"]
