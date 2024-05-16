9bea876fe57781e4632e71ad41d6bef7ec64d497
natejly <natejly@gmail.com>
2024-05-16 13:57:56 -0500

    changed filter to mask_filter

--- plantcv/plantcv/roi/__init__.py
+++ plantcv/plantcv/roi/__init__.py
@@ -5,9 +5,9 @@ from plantcv.plantcv.roi.roi_methods import rectangle
 from plantcv.plantcv.roi.roi_methods import auto_grid
 from plantcv.plantcv.roi.roi_methods import multi
 from plantcv.plantcv.roi.roi_methods import custom
-from plantcv.plantcv.roi.roi_methods import filter
+from plantcv.plantcv.roi.roi_methods import mask_filter
 from plantcv.plantcv.roi.roi2mask import roi2mask
 from plantcv.plantcv.roi.quick_filter import quick_filter
 
 __all__ = ["circle", "ellipse", "from_binary_image", "rectangle", "auto_grid", "multi", "custom",
-           "filter", "roi2mask", "quick_filter"]
+           "mask_filter", "roi2mask", "quick_filter"]
