b6a76a94a9ef6df7c2ae959b566b66ff833b9e06
Noah Fahlgren <noahfahlgren@gmail.com>
2024-05-16 16:18:21 -0500

    Add open_url function

--- plantcv/plantcv/io/__init__.py
+++ plantcv/plantcv/io/__init__.py
@@ -1,5 +1,6 @@
 from plantcv.plantcv.io.read_dataset import read_dataset
 from plantcv.plantcv.io.random_subset import random_subset
+from plantcv.plantcv.io.open_url import open_url
 
 # add new functions to end of lists
-__all__ = ["read_dataset", "random_subset"]
+__all__ = ["read_dataset", "random_subset", "open_url"]
