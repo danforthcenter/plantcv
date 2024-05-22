dc9890d995354c4c1e860a3796116a6f6a63c454
HaleySchuhl <haleyschuhl@gmail.com>
2024-05-22 09:16:59 -0500

    Update __init__.py

--- plantcv/plantcv/filters/__init__.py
+++ plantcv/plantcv/filters/__init__.py
@@ -1,4 +1,4 @@
 from plantcv.plantcv.filters.eccentricity import eccentricity
-from plantcv.plantcv.filters.area import obj_area
+from plantcv.plantcv.filters.obj_area import obj_area
 
 __all__ = ["eccentricity", "obj_area"]
