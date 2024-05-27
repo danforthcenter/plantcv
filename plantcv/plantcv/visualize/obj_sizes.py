622fc82ee9ebf2575e00d843c4f7290dead06aa5
natejly <natejly@gmail.com>
2024-05-27 15:35:28 -0500

    removed lambda and refactored

--- plantcv/plantcv/visualize/obj_sizes.py
+++ plantcv/plantcv/visualize/obj_sizes.py
@@ -37,12 +37,11 @@ def obj_sizes(img, mask, num_objects=100):
 
     # ID contours and sort them from largest to smallest
     id_objects, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[-2:]
-    sorted_objects = sorted(id_objects, key=lambda x: cv2.contourArea(x))
+    sorted_objects = sorted(id_objects, key=cv2.contourArea, reverse=True)
+    sorted_objects = sorted_objects[:num_objects]
     # Function sorts smallest to largest so keep the last X objects listed
     # sorted_objects = sorted_objects[len(sorted_objects) - num_objects: len(sorted_objects)]
     # Reverse the sorted list to order contours from largest to smallest
-    sorted_objects.reverse()
-
     rand_color = color_palette(num=num_objects, saved=False)
     random.shuffle(rand_color)
 
