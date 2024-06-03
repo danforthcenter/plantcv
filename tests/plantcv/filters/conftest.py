a0b2f6da37690186abc3d815851a021a2e4eb114
HaleySchuhl <haleyschuhl@gmail.com>
2024-06-03 16:23:38 -0500

    additional img to filters conftest

--- tests/plantcv/filters/conftest.py
+++ tests/plantcv/filters/conftest.py
@@ -13,6 +13,8 @@ class FiltersTestData:
         self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "testdata")
         # Binary mask for eccentricity filter
         self.small_bin_fill = os.path.join(self.datadir, "floodfill.png")
+        # Binary mask for solidity filter
+        self.small_bin = os.path.join(self.datadir, "setaria_small_plant_mask.png")
 
 
 @pytest.fixture(scope="session")
