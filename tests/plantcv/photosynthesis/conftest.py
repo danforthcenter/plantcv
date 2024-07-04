e073ab477f06eab2b8d5a3f6fb0d3a21662e7c48
Noah Fahlgren <noahfahlgren@gmail.com>
2024-07-04 12:41:02 -0500

    Remove unused code

--- tests/plantcv/photosynthesis/conftest.py
+++ tests/plantcv/photosynthesis/conftest.py
@@ -2,7 +2,6 @@ import pytest
 import os
 import numpy as np
 import xarray as xr
-import pandas as pd
 import matplotlib
 
 # Disable plotting
@@ -22,55 +21,6 @@ class PhotosynthesisTestData:
         self.cropreporter_v653 = os.path.join(self.datadir_v653, "HDR_dark_light.INF")
         # Mask image
         self.ps_mask = os.path.join(self.datadir_v441, "PSII_HDR_020321_WT_TOP_1_mask.png")
-
-    @staticmethod
-    def psii_walz(var):
-        """Create and return synthetic psii dataarrays from walz"""
-        # create ojip_dark
-        if var == 'ojip_dark':
-            i = 0
-            fmin = np.ones((10, 10), dtype='uint8') * ((i+15)*2)
-            fmax = np.ones((10, 10), dtype='uint8') * (200-i*15)
-            data = np.stack([fmin, fmax], axis=2)
-
-            frame_nums = range(0, 2)
-            indf = ['F0', 'Fm']
-            ps_da = xr.DataArray(
-                data=data[..., None],
-                dims=('x', 'y', 'frame_label', 'measurement'),
-                coords={'frame_label': indf,
-                        'frame_num': ('frame_label', frame_nums),
-                        'measurement': ['t0']},
-                name='ojip_dark'
-            )
-
-        # create lightadapted
-        elif var == 'ojip_light':
-            da_list = []
-            measurement = []
-
-            for i in np.arange(1, 3):
-                indf = ['Fp', 'Fmp']
-                fmin = np.ones((10, 10), dtype='uint8') * ((i+15)*2)
-                fmax = np.ones((10, 10), dtype='uint8') * (200-i*15)
-                data = np.stack([fmin, fmax], axis=2)
-
-                lightadapted = xr.DataArray(
-                    data=data[..., None],
-                    dims=('x', 'y', 'frame_label', 'measurement'),
-                    coords={'frame_label': indf,
-                            'frame_num': ('frame_label', range(0, 2))}
-                )
-
-                measurement.append((f't{i*40}'))
-                da_list.append(lightadapted)
-
-            prop_idx = pd.Index(measurement)
-            ps_da = xr.concat(da_list, 'measurement')
-            ps_da.name = 'ojip_light'
-            ps_da.coords['measurement'] = prop_idx
-
-        return ps_da
 
     @staticmethod
     def create_ps_mask():
