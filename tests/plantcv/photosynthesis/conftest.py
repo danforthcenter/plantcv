f89508f92dc3ecd95225e4398d836e9369df7e4d
Noah Fahlgren <noahfahlgren@gmail.com>
2024-07-04 12:36:58 -0500

    Fix formatting issues

--- tests/plantcv/photosynthesis/conftest.py
+++ tests/plantcv/photosynthesis/conftest.py
@@ -13,8 +13,10 @@ class PhotosynthesisTestData:
     def __init__(self):
         """Initialize simple variables."""
         # Test data directories
-        self.datadir_v441 = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "testdata","cropreporter_v441")
-        self.datadir_v653 = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "testdata","cropreporter_v653")
+        self.datadir_v441 = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                         "..", "..", "testdata", "cropreporter_v441")
+        self.datadir_v653 = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                         "..", "..", "testdata", "cropreporter_v653")
         # CropReporter data file
         self.cropreporter = os.path.join(self.datadir_v441, "PSII_HDR_020321_WT_TOP_1.INF")
         self.cropreporter_v653 = os.path.join(self.datadir_v653, "HDR_dark_light.INF")
@@ -68,14 +70,14 @@ class PhotosynthesisTestData:
             ps_da.name = 'ojip_light'
             ps_da.coords['measurement'] = prop_idx
 
-        return(ps_da)
+        return ps_da
 
     @staticmethod
     def create_ps_mask():
         """Create simple mask for PSII"""
         mask = np.zeros((10, 10), dtype=np.uint8)
         mask[5, 5] = 255
-        return(mask)
+        return mask
 
     def psii_cropreporter(self, var):
         """Create simple data for PSII"""
@@ -102,7 +104,7 @@ class PhotosynthesisTestData:
                           dims=('x', 'y', 'frame_label', 'measurement'),
                           coords={'frame_label': frame_labels, 'frame_num': ('frame_label', [0, 1, 2, 3]),
                                   'measurement': measurements}, name=var)
-        return(da)
+        return da
 
 
 @pytest.fixture(scope="session")
