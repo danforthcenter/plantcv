3c7dfc1f98f32f575c9123df1bc1bc5dd615895a
Noah Fahlgren <noahfahlgren@gmail.com>
2024-07-04 21:28:40 -0500

    Simplify code for readability

--- plantcv/plantcv/visualize/obj_sizes.py
+++ plantcv/plantcv/visualize/obj_sizes.py
@@ -36,13 +36,13 @@ def obj_sizes(img, mask, num_objects=100):
     params.debug = None
 
     # ID contours and sort them from largest to smallest
-    contours = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    id_objects, _ = contours[-2:]
+    id_objects = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[-2]
     # Function sorts smallest to largest so keep the last X objects listed
     # sorted_objects = sorted_objects[len(sorted_objects) - num_objects: len(sorted_objects)]
     sorted_objects = sorted(id_objects, key=cv2.contourArea)
     # Reverse the sorted list to order contours from largest to smallest
-    sorted_objects = sorted_objects[::-1]
+    sorted_objects.reverse()
+
     rand_color = color_palette(num=num_objects, saved=False)
     random.shuffle(rand_color)
 
