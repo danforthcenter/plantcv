b4f415da7e9a81cab0dcdaca84ca4bf1d0bfa639
HaleySchuhl <haleyschuhl@gmail.com>
2024-08-12 14:46:07 -0500

    Create test_quality_control.py

---
+++ tests/plantcv/test_quality_control.py
@@ -0,0 +1,12 @@
+import pytest
+import os
+from plantcv.plantcv import outputs
+from plantcv.plantcv import readimage
+from plantcv.plantcv import quality_control
+
+
+def test_plantcv_quality_control(test_data):
+    """Test for PlantCV."""
+    img, path, img_name = readimage(filename=test_data.small_rgb_img, mode='native')
+    quality_control(img)
+    assert outputs.metadata["red_percent_bad_exposure_qc"]["value"] == 0.0007238805970149253
