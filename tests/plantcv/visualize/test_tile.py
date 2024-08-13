d062a649d74ac796c317dd0da1105007ddb9d713
KeelyBrown <156026125+k034b363@users.noreply.github.com>
2024-08-13 14:20:00 -0500

    hard code order of paths in tile test

--- tests/plantcv/visualize/test_tile.py
+++ tests/plantcv/visualize/test_tile.py
@@ -7,7 +7,9 @@ def test_tile(visualize_test_data):
     """Test for PlantCV."""
     # Read in image list
     images = []
-    paths = [visualize_test_data.tile_dir+i for i in os.listdir(visualize_test_data.tile_dir)]
+    paths = [visualize_test_data.tile_dir+"Tile1.png", visualize_test_data.tile_dir+"Tile2.png",
+             visualize_test_data.tile_dir+"Tile3.png", visualize_test_data.tile_dir+"Tile4.png"]
+    #paths = [visualize_test_data.tile_dir+i for i in os.listdir(visualize_test_data.tile_dir)]
     paths.sort
     for i in paths:
         images.append(pcv.readimage(i)[0])
