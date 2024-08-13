afe48b40d2d3b8c266d455acdafa2f68c7a12200
HaleySchuhl <haleyschuhl@gmail.com>
2024-08-13 08:33:12 -0500

    Update test_quality_control.py
    add print, parameterize it, and update read to cv2 function so only one pcv function file is getting tested

--- tests/plantcv/test_quality_control.py
+++ tests/plantcv/test_quality_control.py
@@ -1,12 +1,17 @@
+import cv2
+import pytest
 from plantcv.plantcv import params
 from plantcv.plantcv import outputs
 from plantcv.plantcv import readimage
 from plantcv.plantcv import quality_control
 
 
-def test_plantcv_quality_control(test_data):
+@pytest.mark.parametrize("mode", ["print", "plot"])
+def test_plantcv_quality_control(mode, test_data, tmpdir):
     """Test for PlantCV."""
-    img, _, _ = readimage(filename=test_data.small_rgb_img, mode='native')
-    params.debug = "plot"
+    cache_dir = tmpdir.mkdir("cache")
+    params.debug_outdir = cache_dir
+    img = cv2.imread(test_data.small_rgb_img, -1)
+    params.debug = mode
     quality_control(img)
     assert outputs.metadata["red_percent_bad_exposure_qc"]["value"] == 0.0007238805970149253
