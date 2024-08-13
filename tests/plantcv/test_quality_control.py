2546ce7cf5c142d4481c1f5b532681f6e40567a1
HaleySchuhl <haleyschuhl@gmail.com>
2024-08-13 08:11:59 -0500

    turn debug to plot so hist code is tested

--- tests/plantcv/test_quality_control.py
+++ tests/plantcv/test_quality_control.py
@@ -1,3 +1,4 @@
+from plantcv.plantcv import params
 from plantcv.plantcv import outputs
 from plantcv.plantcv import readimage
 from plantcv.plantcv import quality_control
@@ -6,5 +7,6 @@ from plantcv.plantcv import quality_control
 def test_plantcv_quality_control(test_data):
     """Test for PlantCV."""
     img, _, _ = readimage(filename=test_data.small_rgb_img, mode='native')
+    params.debug = "plot"
     quality_control(img)
     assert outputs.metadata["red_percent_bad_exposure_qc"]["value"] == 0.0007238805970149253
