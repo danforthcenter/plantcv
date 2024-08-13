fc4e3d40027a6f40ae1d6a44f38476b11b27a0ba
KeelyBrown <156026125+k034b363@users.noreply.github.com>
2024-08-13 14:30:35 -0500

    clean up tile tests

--- tests/plantcv/visualize/test_tile.py
+++ tests/plantcv/visualize/test_tile.py
@@ -1,4 +1,3 @@
-import os
 import plantcv.plantcv as pcv
 from plantcv.plantcv.visualize import tile
 
@@ -9,8 +8,6 @@ def test_tile(visualize_test_data):
     images = []
     paths = [visualize_test_data.tile_dir+"Tile1.png", visualize_test_data.tile_dir+"Tile2.png",
              visualize_test_data.tile_dir+"Tile3.png", visualize_test_data.tile_dir+"Tile4.png"]
-    #paths = [visualize_test_data.tile_dir+i for i in os.listdir(visualize_test_data.tile_dir)]
-    paths.sort
     for i in paths:
         images.append(pcv.readimage(i)[0])
     composite = tile(images=images, nrow=2, ncol=3)
