7f3fd05ca835047f6d6d5d2242db541982a6c443
KeelyBrown <156026125+k034b363@users.noreply.github.com>
2024-08-13 13:47:53 -0500

    added sort for tile test paths

--- tests/plantcv/visualize/test_tile.py
+++ tests/plantcv/visualize/test_tile.py
@@ -5,12 +5,12 @@ from plantcv.plantcv.visualize import tile
 
 def test_tile(visualize_test_data):
     """Test for PlantCV."""
-    pcv.params.debug = "plot"
     # Read in image list
     images = []
-    for i in os.listdir(visualize_test_data.tile_dir):
-        images.append(pcv.readimage(visualize_test_data.tile_dir+i)[0])
-    images.sort
+    paths = [visualize_test_data.tile_dir+i for i in visualize_test_data.tile_dir]
+    paths.sort
+    for i in paths:
+        images.append(pcv.readimage(i)[0])
     composite = tile(images=images, nrow=2, ncol=3)
     query_img, _, _ = pcv.readimage(visualize_test_data.tile_out)
     assert (composite == query_img).all()
