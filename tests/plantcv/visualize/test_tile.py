44d88ec9bd44792501303db393f7d634b4702a32
KeelyBrown <156026125+k034b363@users.noreply.github.com>
2024-08-13 15:48:17 -0500

    trying cv2 read for tile test

--- tests/plantcv/visualize/test_tile.py
+++ tests/plantcv/visualize/test_tile.py
@@ -1,4 +1,5 @@
 import plantcv.plantcv as pcv
+import cv2
 from plantcv.plantcv.visualize import tile
 
 
@@ -11,5 +12,5 @@ def test_tile(visualize_test_data):
     for i in paths:
         images.append(pcv.readimage(i)[0])
     composite = tile(images=images, nrow=2, ncol=3)
-    query_img, _, _ = pcv.readimage(visualize_test_data.tile_out)
+    query_img = cv2.imread(visualize_test_data.tile_out)
     assert (composite == query_img).all()
