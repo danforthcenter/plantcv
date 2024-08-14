a50c52d36457e57f9525704c87f10f936f42f410
KeelyBrown <156026125+k034b363@users.noreply.github.com>
2024-08-14 09:46:16 -0500

    switch tile test to shape

--- tests/plantcv/visualize/test_tile.py
+++ tests/plantcv/visualize/test_tile.py
@@ -1,5 +1,4 @@
 import plantcv.plantcv as pcv
-import cv2
 from plantcv.plantcv.visualize import tile
 
 
@@ -12,5 +11,4 @@ def test_tile(visualize_test_data):
     for i in paths:
         images.append(pcv.readimage(i)[0])
     composite = tile(images=images, nrow=2, ncol=3)
-    query_img, _, _ = pcv.readimage(visualize_test_data.tile_out)
-    assert (composite == query_img).all()
+    assert composite.shape == (663, 855, 3)
