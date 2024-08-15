f33ee4fc16143e658196b1540eb687cf7183ebe2
Noah Fahlgren <noahfahlgren@gmail.com>
2024-08-15 10:59:39 -0500

    Update test to assert approximate equality
    No need to test different debug modes since the debug logic now uses _debug
    
    Float equality is not a stable test, use np.isclose instead

--- tests/plantcv/qc/test_exposure.py
+++ tests/plantcv/qc/test_exposure.py
@@ -1,15 +1,11 @@
 import cv2
-import pytest
-from plantcv.plantcv import params, outputs
+import numpy as np
+from plantcv.plantcv import outputs
 from plantcv.plantcv.qc import exposure
 
 
-@pytest.mark.parametrize("mode", ["print", "plot"])
-def test_plantcv_quality_control(mode, test_data, tmpdir):
+def test_plantcv_quality_control(test_data):
     """Test for PlantCV."""
-    cache_dir = tmpdir.mkdir("cache")
-    params.debug_outdir = cache_dir
     img = cv2.imread(test_data.small_rgb_img, -1)
-    params.debug = mode
     _ = exposure(img)
-    assert outputs.metadata["red_percent_bad_exposure_qc"]["value"] == 0.0007238805970149253
+    assert np.isclose(outputs.metadata["red_percent_bad_exposure_qc"]["value"], 0.0007238805970149253)
