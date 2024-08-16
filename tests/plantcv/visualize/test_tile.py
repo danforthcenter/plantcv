41946e84345c0fbfb0ca24f0c73c25125d9e4c0b
HaleySchuhl <haleyschuhl@gmail.com>
2024-08-16 15:15:12 -0500

    line return

--- tests/plantcv/visualize/test_tile.py
+++ tests/plantcv/visualize/test_tile.py
@@ -10,3 +10,4 @@ def test_tile(visualize_test_data):
         images.append(cv2.imread(visualize_test_data.small_rgb_img))
     composite = tile(images=images, ncol=3)
     assert composite.shape == (670, 1200, 3)
+    
