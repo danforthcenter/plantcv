96f9fe0ecc21f36d377a32c08d9b4a3c4bbed127
HaleySchuhl <haleyschuhl@gmail.com>
2024-08-16 15:14:04 -0500

    update test to use preexisting rgb img

--- tests/plantcv/visualize/test_tile.py
+++ tests/plantcv/visualize/test_tile.py
@@ -1,4 +1,4 @@
-import plantcv.plantcv as pcv
+import cv2
 from plantcv.plantcv.visualize import tile
 
 
@@ -6,9 +6,7 @@ def test_tile(visualize_test_data):
     """Test for PlantCV."""
     # Read in image list
     images = []
-    paths = [visualize_test_data.tile_dir+"Tile_1.jpg", visualize_test_data.tile_dir+"Tile_2.jpg",
-             visualize_test_data.tile_dir+"Tile_3.jpg", visualize_test_data.tile_dir+"Tile_4.jpg"]
-    for i in paths:
-        images.append(pcv.readimage(i)[0])
+    for _ in range(4):
+        images.append(cv2.imread(visualize_test_data.small_rgb_img))
     composite = tile(images=images, ncol=3)
-    assert composite.shape == (663, 855, 3)
+    assert composite.shape == (670, 1200, 3)
