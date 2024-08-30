668216efd9645dd5363db93f6cb01f8e1bb97504
HaleySchuhl <haleyschuhl@gmail.com>
2024-08-30 11:35:44 -0500

    removed extra tests

--- tests/plantcv/test_create_labels.py
+++ tests/plantcv/test_create_labels.py
@@ -17,12 +17,3 @@ def test_create_labels_no_roi(test_data):
     mask = cv2.imread(test_data.small_bin_img, -1)
     _, num = create_labels(mask=mask)
     assert num == 1
-
-
-def test_create_labels_largest(test_data):
-    """Test for PlantCV."""
-    cnt, cnt_str = test_data.load_contours(test_data.small_contours_file)
-    mask = cv2.imread(test_data.small_bin_img, -1)
-    cnt_Obj = Objects(contours=[cnt], hierarchy=[cnt_str])
-    masks, num = create_labels(mask=mask, rois=cnt_Obj, roi_type="largest")
-    assert np.unique(masks).size == (num + 1)
