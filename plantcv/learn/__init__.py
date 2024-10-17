175abf7d1a6d7857dba1f50736367a1e218ce8ee
KeelyBrown <156026125+k034b363@users.noreply.github.com>
2024-10-17 16:45:55 -0500

    Removed fatal error in patch_extract due to circular import

--- plantcv/learn/__init__.py
+++ plantcv/learn/__init__.py
@@ -1,5 +1,6 @@
 from plantcv.learn.naive_bayes import naive_bayes
 from plantcv.learn.naive_bayes import naive_bayes_multiclass
-from plantcv.learn.train_kmeans import train_kmeans, patch_extract
+from plantcv.learn.train_kmeans import train_kmeans
+from plantcv.learn.train_kmeans import patch_extract
 
 __all__ = ["naive_bayes", "naive_bayes_multiclass", "train_kmeans", "patch_extract"]
