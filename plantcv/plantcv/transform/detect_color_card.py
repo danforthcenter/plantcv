4fee79e56db87c16bc83f758f1503f9a670c2ced
HaleySchuhl <haleyschuhl@gmail.com>
2024-10-18 08:32:08 -0500

    rename var and update indentation

--- plantcv/plantcv/transform/detect_color_card.py
+++ plantcv/plantcv/transform/detect_color_card.py
@@ -223,10 +223,12 @@ def auto_detect_correct(rgb_img, label=None, **kwargs):
         label = params.sample_label
 
     # Get keyword arguments and set defaults if not set
-    labeled_mask = detect_color_card(rgb_img=rgb_img, min_size=kwargs.get("min_size", 1000), radius=kwargs.get("radius", 20),
-                                     adaptive_method=kwargs.get("adaptive_method", 1), block_size=kwargs.get("block_size", 51))
+    labeled_mask = detect_color_card(rgb_img=rgb_img, min_size=kwargs.get("min_size", 1000),
+                                     radius=kwargs.get("radius", 20),
+                                     adaptive_method=kwargs.get("adaptive_method", 1),
+                                     block_size=kwargs.get("block_size", 51))
     _, card_matrix = get_color_matrix(rgb_img=rgb_img, mask=labeled_mask)
-    std_color_matrix = std_color_matrix(pos=3)
+    std_matrix = std_color_matrix(pos=3)
     return affine_color_correction(rgb_img=rgb_img, source_matrix=card_matrix,
-                                   target_matrix=std_color_matrix)
+                                   target_matrix=std_matrix)
     
