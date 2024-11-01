85720dc2cb4c70cfe8551ecd871e468d8abf5f12
HaleySchuhl <haleyschuhl@gmail.com>
2024-11-01 09:32:11 -0500

    Update test_auto_correct_color.py

--- tests/plantcv/transform/test_auto_correct_color.py
+++ tests/plantcv/transform/test_auto_correct_color.py
@@ -8,4 +8,4 @@ def test_auto_correct_color(transform_test_data):
     # Load rgb image
     rgb_img = cv2.imread(transform_test_data.colorcard_img)
     corrected_img = auto_correct_color(rgb_img=rgb_img)
-    assert np.shape(corrected_img) == np.shape(rgb_img) and np.sum(corrected_img) != rgb_img
+    assert np.shape(corrected_img) == np.shape(rgb_img) and np.sum(corrected_img) != np.sum(rgb_img)
