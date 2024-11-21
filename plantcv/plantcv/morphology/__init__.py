9a523de1ceae4fa2f16856d19e4919e80fb78793
HaleySchuhl <haleyschuhl@gmail.com>
2024-11-21 08:39:11 -0600

    morphology init add prune by height partial

--- plantcv/plantcv/morphology/__init__.py
+++ plantcv/plantcv/morphology/__init__.py
@@ -3,7 +3,7 @@ from plantcv.plantcv.morphology.find_tips import find_tips
 from plantcv.plantcv.morphology._iterative_prune import _iterative_prune
 from plantcv.plantcv.morphology.segment_skeleton import segment_skeleton
 from plantcv.plantcv.morphology.segment_sort import segment_sort
-from plantcv.plantcv.morphology.prune import prune
+from plantcv.plantcv.morphology.prune import prune, prune_by_height_partial
 from plantcv.plantcv.morphology.skeletonize import skeletonize
 from plantcv.plantcv.morphology.check_cycles import check_cycles
 from plantcv.plantcv.morphology.segment_angle import segment_angle
@@ -17,7 +17,8 @@ from plantcv.plantcv.morphology.segment_combine import segment_combine
 from plantcv.plantcv.morphology.analyze_stem import analyze_stem
 from plantcv.plantcv.morphology.fill_segments import fill_segments
 
-__all__ = ["find_branch_pts", "find_tips", "prune", "skeletonize", "check_cycles", "segment_skeleton", "segment_angle",
+__all__ = ["find_branch_pts", "find_tips", "prune", "prune_by_height_partial",
+           "skeletonize", "check_cycles", "segment_skeleton", "segment_angle",
            "segment_path_length", "segment_euclidean_length", "segment_curvature", "segment_sort", "segment_id",
            "segment_tangent_angle", "segment_insertion_angle", "segment_combine", "_iterative_prune", "analyze_stem",
            "fill_segments"]
