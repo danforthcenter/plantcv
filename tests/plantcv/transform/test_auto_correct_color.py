38085f219ce53a263e88cc833efd04914e174140
HaleySchuhl <haleyschuhl@gmail.com>
2024-12-04 11:54:07 -0600

    test_auto_correct_color.py suggested update
    add whitespace

--- tests/plantcv/transform/test_auto_correct_color.py
+++ tests/plantcv/transform/test_auto_correct_color.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 from plantcv.plantcv.transform import auto_correct_color
 
+
 def test_auto_correct_color(transform_test_data):
     """Test for PlantCV."""
     # Load rgb image
