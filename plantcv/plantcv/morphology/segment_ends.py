d8fd5c017d8cc3812cb6fe5055b8a00ef1515a7b
HaleySchuhl <haleyschuhl@gmail.com>
2025-01-02 09:36:37 -0600

    add logic for returning the optimal segment assignment

--- plantcv/plantcv/morphology/segment_ends.py
+++ plantcv/plantcv/morphology/segment_ends.py
@@ -1,5 +1,6 @@
 # Find both segment end coordinates
 import os
+import numpy as np
 from plantcv.plantcv import params, outputs
 from plantcv.plantcv._debug import _debug
 from plantcv.plantcv._helpers import _find_segment_ends
@@ -14,10 +15,15 @@ def segment_ends(skel_img, leaf_objects, mask=None, label=None):
     mask             = (Optional) binary mask for debugging. If provided, debug image will be overlaid on the mask.
     label            = Optional label parameter, modifies the variable name of
                        observations recorded (default = pcv.params.sample_label).
+                       
+    Returns:
+    sorted_ids       = Optimal assignment of leaf objects based on inner-segment y-coordinates
 
     :param segmented_img: numpy.ndarray
-    :param objects: list
+    :param leaf_objects: list
+    :param mask: numpy.ndarray
     :param label: str
+    :return sorted_ids: list
     """
     # Store debug
     debug = params.debug
@@ -27,6 +33,7 @@ def segment_ends(skel_img, leaf_objects, mask=None, label=None):
         labeled_img = skel_img.copy()
     else:
         labeled_img = mask.copy()
+    # Find and sort segment ends, and create debug image
     labeled_img, tip_list, inner_list, labels = _find_segment_ends(
         skel_img=skel_img, leaf_objects=leaf_objects, plotting_img=labeled_img, size=1)
     # Set lable to params.sample_label if None
@@ -44,3 +51,14 @@ def segment_ends(skel_img, leaf_objects, mask=None, label=None):
     # Reset debug mode
     params.debug = debug
     _debug(visual=labeled_img, filename=os.path.join(params.debug_outdir, f"{params.device}_segment_ends.png"))
+    
+    # Determine optimal segment order by y-coordinate order
+    d = {} 
+    for i, coord in enumerate(inner_list):
+        d[coord[1]] = i 
+
+    keys = list(d.keys())
+    values = list(d.values())
+    sorted_key_index = np.argsort(keys)
+    sorted_ids = [values[i] for i in sorted_key_index]
+    return sorted_ids[::-1]
