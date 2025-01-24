294f2b6622777ba7a321af4225f9503d2e467812
HaleySchuhl <haleyschuhl@gmail.com>
2025-01-24 13:30:46 -0600

    add deprecation warning to auto_correct_color and detect_color_card

--- plantcv/plantcv/transform/auto_correct_color.py
+++ plantcv/plantcv/transform/auto_correct_color.py
@@ -1,16 +1,18 @@
 # Automatically detect a color card and color correct to standard chip values
 
-from plantcv.plantcv import params
+from plantcv.plantcv import params, deprecation_warning
 from plantcv.plantcv.transform.detect_color_card import detect_color_card
 from plantcv.plantcv.transform.color_correction import get_color_matrix, std_color_matrix, affine_color_correction
 
 
-def auto_correct_color(rgb_img, **kwargs):
+def auto_correct_color(rgb_img, label=None, **kwargs):
     """Automatically detect a color card.
     Parameters
     ----------
     rgb_img : numpy.ndarray
         Input RGB image data containing a color card.
+    label : str, optional
+        modifies the variable name of observations recorded (default = pcv.params.sample_label).
     **kwargs
         Other keyword arguments passed to cv2.adaptiveThreshold and cv2.circle.
         Valid keyword arguments:
@@ -23,6 +25,13 @@ def auto_correct_color(rgb_img, **kwargs):
     numpy.ndarray
         Color corrected image
     """
+    # Set lable to params.sample_label if None
+    if label is None:
+        label = params.sample_label
+    deprecation_warning(
+        "The 'label' parameter is no longer utilized, since color chip size is now metadata."
+        "It will be removed in PlantCV v5.0."
+        )
     # Get keyword arguments and set defaults if not set
     labeled_mask = detect_color_card(rgb_img=rgb_img, min_size=kwargs.get("min_size", 1000),
                                      radius=kwargs.get("radius", 20),
