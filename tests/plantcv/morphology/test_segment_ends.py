8b42d0e434f519376ca1cfcde3443dca425fbfe0
HaleySchuhl <haleyschuhl@gmail.com>
2025-03-17 15:12:58 -0500

    update segment_ends unit test
    no longer checking outputs since the tips not stored there, instead check the returned list of tips for correct length

--- tests/plantcv/morphology/test_segment_ends.py
+++ tests/plantcv/morphology/test_segment_ends.py
@@ -9,8 +9,8 @@ def test_segment_ends(morphology_test_data):
     outputs.clear()
     leaf_obj = morphology_test_data.load_segments(morphology_test_data.segments_file, "leaves")
     skeleton = cv2.imread(morphology_test_data.skel_img, -1)
-    segment_ends(skel_img=skeleton, leaf_objects=leaf_obj, mask=skeleton)
-    assert len(outputs.observations['default']['segment_branch_points']['value']) == 4
+    _, _, _, tips = segment_ends(skel_img=skeleton, leaf_objects=leaf_obj, mask=skeleton)
+    assert len(tips) == 4
 
 
 def test_segment_ends_no_mask(morphology_test_data):
@@ -19,5 +19,5 @@ def test_segment_ends_no_mask(morphology_test_data):
     outputs.clear()
     leaf_obj = morphology_test_data.load_segments(morphology_test_data.segments_file, "leaves")
     skeleton = cv2.imread(morphology_test_data.skel_img, -1)
-    segment_ends(skel_img=skeleton, leaf_objects=leaf_obj, mask=None)
-    assert len(outputs.observations['default']['segment_branch_points']['value']) == 4
+    _, _, _, tips = segment_ends(skel_img=skeleton, leaf_objects=leaf_obj, mask=None)
+    assert len(tips) == 4
