10402734e4ee66d10360d0ef2576050a4bfec063
HaleySchuhl <haleyschuhl@gmail.com>
2025-04-25 15:40:47 -0500

    Update test_segment_id.py

--- tests/plantcv/morphology/test_segment_id.py
+++ tests/plantcv/morphology/test_segment_id.py
@@ -6,7 +6,7 @@ def test_segment_id(morphology_test_data):
     """Test for PlantCV."""
     skel = cv2.imread(morphology_test_data.skel_img, -1)
     leaf_obj = morphology_test_data.load_segments(morphology_test_data.segments_file, "leaves")
-    _, labeled_img = segment_id(skel_img=skel, objects=leaf_obj, mask=skel, optimal_assignment=[0,1,2,3])
+    _, labeled_img = segment_id(skel_img=skel, objects=leaf_obj, mask=skel)
     assert skel.shape == labeled_img.shape[:2]
 
 
