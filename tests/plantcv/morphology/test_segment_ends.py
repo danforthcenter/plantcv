593edcb7a0da063e92806856f3fca79020858d58
HaleySchuhl <haleyschuhl@gmail.com>
2025-05-01 12:40:40 -0500

    Update test_segment_ends.py

--- tests/plantcv/morphology/test_segment_ends.py
+++ tests/plantcv/morphology/test_segment_ends.py
@@ -1,5 +1,5 @@
 import cv2
-import pytest
+import numpy as np
 from plantcv.plantcv import outputs
 from plantcv.plantcv.morphology import segment_ends
 
@@ -28,7 +28,8 @@ def test_segment_ends_unsortable(morphology_test_data):
     """Test for PlantCV."""
     # Clear previous outputs
     outputs.clear()
-    leaf_objects=morphology_test_data.load_segments(morphology_test_data.segments_file, "edges")
-    skeleton = cv2.imread(morphology_test_data.skel_img, -1)
-    with pytest.raises(IndexError):
-        _ = segment_ends(skel_img=skeleton, leaf_objects=leaf_objects, mask=None)
+    obj = np.load(morphology_test_data.disconnected_segment_file)
+    leaf_object = obj["data1"]
+    skeleton = cv2.imread(morphology_test_data.disconnected_skel_img, -1)
+    sorted_objs, _, _, tip_list = segment_ends(skel_img=skeleton, leaf_objects=[leaf_object], mask=None)
+    assert len(tip_list) == 2 and len(sorted_objs) == 0
