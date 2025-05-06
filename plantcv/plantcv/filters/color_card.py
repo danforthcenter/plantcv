0c17d7703a14bdc78d46fcdca576f0cfd57ddf74
HaleySchuhl <haleyschuhl@gmail.com>
2025-05-06 16:23:02 -0500

    update debug image to show outline of cc detection

--- plantcv/plantcv/filters/color_card.py
+++ plantcv/plantcv/filters/color_card.py
@@ -1,8 +1,11 @@
 # Filter the results of the color card detection algorithm
 
+from plantcv.plantcv._helpers import _cv2_findcontours, _object_composition
 from plantcv.plantcv.transform.detect_color_card import _color_card_detection 
 from plantcv.plantcv._debug import _debug
 from plantcv.plantcv import params
+import numpy as np
+import cv2
 import os
 
 def color_card(rgb_img, **kwargs):
@@ -28,8 +31,15 @@ def color_card(rgb_img, **kwargs):
         Binary bounding box mask of the detected color card chips
     """
     _, _, bounding_mask, _, _, _ = _color_card_detection(rgb_img, **kwargs)
+    
+    # Find contours
+    cnt, cnt_str = _cv2_findcontours(bin_img=bounding_mask)
+
+    # Consolidate contours
+    obj = _object_composition(contours=cnt, hierarchy=cnt_str)
+    bb_debug = cv2.drawContours(np.copy(rgb_img), [obj], -1, (255, 0, 255), params.line_thickness)
 
     # Debugging
-    _debug(visual=bounding_mask, filename=os.path.join(params.debug_outdir, f'{params.device}_color_card.png'))
+    _debug(visual=bb_debug, filename=os.path.join(params.debug_outdir, f'{params.device}_color_card.png'))
 
     return bounding_mask
