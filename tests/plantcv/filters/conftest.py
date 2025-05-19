83b4d4e06cdd79b7c17b06b118e8e2ccdb034fb3
HaleySchuhl <haleyschuhl@gmail.com>
2025-05-19 11:12:55 -0500

    write unit test for filters.color_card

--- tests/plantcv/filters/conftest.py
+++ tests/plantcv/filters/conftest.py
@@ -15,6 +15,8 @@ class FiltersTestData:
         self.small_bin_fill = os.path.join(self.datadir, "floodfill.png")
         # Binary mask for object filter
         self.barley_example = os.path.join(self.datadir, "example_barley_mask.png")
+        # Color card image
+        self.colorcard_img = os.path.join(self.datadir, "colorcard_img.png")
 
 
 @pytest.fixture(scope="session")
