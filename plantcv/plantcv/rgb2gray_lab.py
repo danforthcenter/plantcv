0657ec4dc6f0fe91044bce2bd0e9bcb6ab05c3e5
Noah Fahlgren <noahfahlgren@gmail.com>
2025-05-19 14:19:09 -0500

    Convert to wrapper function for RGB to LAB helper

--- plantcv/plantcv/rgb2gray_lab.py
+++ plantcv/plantcv/rgb2gray_lab.py
@@ -1,43 +1,35 @@
 # RGB -> LAB -> Gray
 
-import cv2
 import os
 from plantcv.plantcv._debug import _debug
-from plantcv.plantcv import fatal_error
 from plantcv.plantcv import params
+from plantcv.plantcv._helpers import _rgb2lab
 
 
 def rgb2gray_lab(rgb_img, channel):
+    """Convert image from RGB colorspace to LAB colorspace. Returns the specified subchannel as a gray image.
+
+    Parameters
+    ----------
+    rgb_img : numpy.ndarray
+        RGB image data
+    channel : str
+        color subchannel (l = lightness, a = green-magenta, b = blue-yellow)
+
+    Returns
+    -------
+    numpy.ndarray
+        grayscale image from one LAB color channel
     """
-    Convert image from RGB colorspace to LAB colorspace. Returns the specified subchannel as a gray image.
+    # Convert RGB to LAB and return the specified subchannel as a gray image
+    gray_img = _rgb2lab(rgb_img=rgb_img, channel=channel)
 
-    Inputs:
-    rgb_img   = RGB image data
-    channel   = color subchannel (l = lightness, a = green-magenta, b = blue-yellow)
-
-    Returns:
-    l | a | b = grayscale image from one LAB color channel
-
-    :param rgb_img: numpy.ndarray
-    :param channel: str
-    :return channel: numpy.ndarray
-    """
     # The allowable channel inputs are l, a or b
     names = {"l": "lightness", "a": "green-magenta", "b": "blue-yellow"}
-    channel = channel.lower()
-    if channel not in names:
-        fatal_error("Channel " + str(channel) + " is not l, a or b!")
 
-    # Convert the input BGR image to LAB colorspace
-    lab = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2LAB)
-    # Split LAB channels
-    l, a, b = cv2.split(lab)
-    # Create a channel dictionaries for lookups by a channel name index
-    channels = {"l": l, "a": a, "b": b}
-
-    _debug(visual=channels[channel],
+    # Display debug image
+    _debug(visual=gray_img,
            filename=os.path.join(params.debug_outdir,
-                                 str(params.device) + "_lab_" + names[channel] + ".png"),
-           cmap="gray")
+                                 str(params.device) + "_lab_" + names[channel.lower()] + ".png"), cmap="gray")
 
-    return channels[channel]
+    return gray_img
