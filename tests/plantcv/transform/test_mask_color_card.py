29f5d9ddb98084051fdd351d749b74d5f0d07c7f
HaleySchuhl <haleyschuhl@gmail.com>
2025-05-23 12:02:52 -0500

    move unit test

---
+++ tests/plantcv/transform/test_mask_color_card.py
@@ -0,0 +1,12 @@
+"""Tests for filters.color_card."""
+import cv2
+import numpy as np
+from plantcv.plantcv.transform import mask_color_card
+
+
+def test_mask_color_card(transform_test_data):
+    """Test for PlantCV."""
+    # Load rgb image
+    rgb_img = cv2.imread(transform_test_data.colorcard_img)
+    cc_mask = mask_color_card(rgb_img=rgb_img)
+    assert np.sum(cc_mask) == 124040160
