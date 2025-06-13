f302c069418bf30d1050994c3d36482d18d5523d
NMiklave <50243212+NMiklave@users.noreply.github.com>
2025-06-13 13:25:45 -0500

    Update test imports of read_image

--- tests/plantcv/test_crop.py
+++ tests/plantcv/test_crop.py
@@ -2,7 +2,7 @@
 import cv2
 import numpy as np
 from plantcv.plantcv import crop
-from plantcv.plantcv import readimage
+from plantcv.plantcv import read_image
 
 
 def test_plantcv_crop(test_data):
@@ -25,6 +25,6 @@ def test_plantcv_crop_multiarray():
 def test_plantcv_crop_hyperspectral(test_data):
     """Test for PlantCV."""
     # Read in test data
-    hyperimg = readimage(test_data.envi_sample_data, mode='envi')
+    hyperimg = read_image(test_data.envi_sample_data, mode='envi')
     cropped = crop(img=hyperimg, x=0, y=0, h=20, w=20)
     assert np.shape(cropped.array_data) == (20, 20, 580)
