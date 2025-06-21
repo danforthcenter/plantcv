35500cce89b7c3539a191395d91405063ee2d761
Noah Fahlgren <noahfahlgren@gmail.com>
2025-06-21 10:19:12 -0500

    Reduce logical hierarchy

--- plantcv/plantcv/readbayer.py
+++ plantcv/plantcv/readbayer.py
@@ -39,33 +39,36 @@ def readbayer(filename, bayerpattern='BG', alg='default'):
     if image_raw is None:
         fatal_error("Failed to open " + filename)
 
-    if alg.upper() == 'DEFAULT':
-        if bayerpattern.upper() == 'BG':
-            img = cv2.cvtColor(image_raw, cv2.COLOR_BayerBG2BGR)
-        elif bayerpattern.upper() == 'GB':
-            img = cv2.cvtColor(image_raw, cv2.COLOR_BayerGB2BGR)
-        elif bayerpattern.upper() == 'RG':
-            img = cv2.cvtColor(image_raw, cv2.COLOR_BayerRG2BGR)
-        elif bayerpattern.upper() == 'GR':
-            img = cv2.cvtColor(image_raw, cv2.COLOR_BayerGR2BGR)
-    elif alg.upper() == 'EDGEAWARE':
-        if bayerpattern.upper() == 'BG':
-            img = cv2.cvtColor(image_raw, cv2.COLOR_BayerBG2BGR_EA)
-        elif bayerpattern.upper() == 'GB':
-            img = cv2.cvtColor(image_raw, cv2.COLOR_BayerGB2BGR_EA)
-        elif bayerpattern.upper() == 'RG':
-            img = cv2.cvtColor(image_raw, cv2.COLOR_BayerRG2BGR_EA)
-        elif bayerpattern.upper() == 'GR':
-            img = cv2.cvtColor(image_raw, cv2.COLOR_BayerGR2BGR_EA)
-    elif alg.upper() == 'VARIABLENUMBERGRADIENTS':
-        if bayerpattern.upper() == 'BG':
-            img = cv2.cvtColor(image_raw, cv2.COLOR_BayerBG2BGR_VNG)
-        elif bayerpattern.upper() == 'GB':
-            img = cv2.cvtColor(image_raw, cv2.COLOR_BayerGB2BGR_VNG)
-        elif bayerpattern.upper() == 'RG':
-            img = cv2.cvtColor(image_raw, cv2.COLOR_BayerRG2BGR_VNG)
-        elif bayerpattern.upper() == 'GR':
-            img = cv2.cvtColor(image_raw, cv2.COLOR_BayerGR2BGR_VNG)
+    # Itemize the Bayer pattern and algorithm
+    converters = {
+        "DEFAULT": {
+            "BG": cv2.COLOR_BayerBG2BGR,
+            "GB": cv2.COLOR_BayerGB2BGR,
+            "RG": cv2.COLOR_BayerRG2BGR,
+            "GR": cv2.COLOR_BayerGR2BGR
+        },
+        "EDGEAWARE": {
+            "BG": cv2.COLOR_BayerBG2BGR_EA,
+            "GB": cv2.COLOR_BayerGB2BGR_EA,
+            "RG": cv2.COLOR_BayerRG2BGR_EA,
+            "GR": cv2.COLOR_BayerGR2BGR_EA
+        },
+        "VARIABLENUMBERGRADIENTS": {
+            "BG": cv2.COLOR_BayerBG2BGR_VNG,
+            "GB": cv2.COLOR_BayerGB2BGR_VNG,
+            "RG": cv2.COLOR_BayerRG2BGR_VNG,
+            "GR": cv2.COLOR_BayerGR2BGR_VNG
+        }
+    }
+
+    # Check if the algorithm is valid
+    if alg.upper() not in converters:
+        fatal_error(f"Invalid algorithm '{alg}'. Choose from {list(converters.keys())}.")
+    # Check if the bayer pattern is valid
+    if bayerpattern.upper() not in converters[alg.upper()]:
+        fatal_error(f"Invalid Bayer pattern '{bayerpattern}'. Choose from {list(converters[alg.upper()].keys())}.")
+    # Convert the image using the specified algorithm and Bayer pattern
+    img = cv2.cvtColor(image_raw, converters[alg.upper()][bayerpattern.upper()])
 
     # Split path from filename
     path, img_name = os.path.split(filename)
