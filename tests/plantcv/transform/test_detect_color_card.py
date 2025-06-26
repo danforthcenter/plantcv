c6849b54ceff6e955ee1eb4e6cb4c02f25f53efe
HaleySchuhl <haleyschuhl@gmail.com>
2025-06-26 11:25:38 -0500

    add bad card_type input unit test

--- tests/plantcv/transform/test_detect_color_card.py
+++ tests/plantcv/transform/test_detect_color_card.py
@@ -27,3 +27,11 @@ def test_detect_color_card_incorrect_block_size(transform_test_data):
     rgb_img = cv2.imread(transform_test_data.colorcard_img)
     with pytest.raises(RuntimeError):
         _ = detect_color_card(rgb_img=rgb_img, block_size=2)
+
+
+def test_detect_color_card_incorrect_card_type(transform_test_data):
+    """Test for PlantCV."""
+    # Load rgb image
+    rgb_img = cv2.imread(transform_test_data.colorcard_img)
+    with pytest.raises(RuntimeError):
+        _ = detect_color_card(rgb_img=rgb_img, card_type="pantone")
