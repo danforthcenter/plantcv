33d27a6539192fa718220a7919666db1aa421a4d
HaleySchuhl <haleyschuhl@gmail.com>
2025-06-26 11:10:32 -0500

    Update test_detect_color_card.py
    add test that include auto size scaling factor definition

--- tests/plantcv/transform/test_detect_color_card.py
+++ tests/plantcv/transform/test_detect_color_card.py
@@ -9,7 +9,7 @@ def test_detect_color_card(transform_test_data):
     """Test for PlantCV."""
     # Load rgb image
     rgb_img = cv2.imread(transform_test_data.colorcard_img)
-    labeled_mask = detect_color_card(rgb_img=rgb_img)
+    labeled_mask = detect_color_card(rgb_img=rgb_img, card_type="classic")
     assert len(np.unique(labeled_mask)) == 25
 
 
