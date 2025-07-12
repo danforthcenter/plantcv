8602c7eba0ff74aa3cad3bfa6afbb65e90c50258
NMiklave <50243212+NMiklave@users.noreply.github.com>
2025-07-12 16:41:13 -0500

    Add card_type arg to auto_correct_color

--- plantcv/plantcv/transform/auto_correct_color.py
+++ plantcv/plantcv/transform/auto_correct_color.py
@@ -5,21 +5,27 @@ from plantcv.plantcv.transform.detect_color_card import detect_color_card
 from plantcv.plantcv.transform.color_correction import get_color_matrix, std_color_matrix, affine_color_correction
 
 
-def auto_correct_color(rgb_img, label=None, **kwargs):
+def auto_correct_color(rgb_img, label=None, card_type=0, **kwargs):
     """Automatically detect a color card.
     Parameters
     ----------
     rgb_img : numpy.ndarray
         Input RGB image data containing a color card.
     label : str, optional
-        modifies the variable name of observations recorded (default = pcv.params.sample_label).
+        Modifies the variable name of observations recorded (default = pcv.params.sample_label).
+    card_type : int
+        reference value indicating the type of card being used for correction:
+                card_type = 0: calibrite color card (default)
+                card_type = 1: astrobotany.com AIRI calibration sticker
     **kwargs
         Other keyword arguments passed to cv2.adaptiveThreshold and cv2.circle.
+
         Valid keyword arguments:
         adaptive_method: 0 (mean) or 1 (Gaussian) (default = 1)
         block_size: int (default = 51)
         radius: int (default = 20)
         min_size: int (default = 1000)
+
     Returns
     -------
     numpy.ndarray
@@ -33,11 +39,13 @@ def auto_correct_color(rgb_img, label=None, **kwargs):
         "It will be removed in PlantCV v5.0."
         )
     # Get keyword arguments and set defaults if not set
-    labeled_mask = detect_color_card(rgb_img=rgb_img, min_size=kwargs.get("min_size", 1000),
+    labeled_mask = detect_color_card(rgb_img=rgb_img, card_type=card_type,
+                                     min_size=kwargs.get("min_size", 1000),
                                      radius=kwargs.get("radius", 20),
                                      adaptive_method=kwargs.get("adaptive_method", 1),
                                      block_size=kwargs.get("block_size", 51))
-    _, card_matrix = get_color_matrix(rgb_img=rgb_img, mask=labeled_mask)
-    std_matrix = std_color_matrix(pos=3)
+    _, card_matrix = get_color_matrix(rgb_img=rgb_img, mask=labeled_mask, card_type=card_type)
+    std_matrix = std_color_matrix(pos=3, card_type=card_type)
+
     return affine_color_correction(rgb_img=rgb_img, source_matrix=card_matrix,
                                    target_matrix=std_matrix)
