f0dbd6eb155fe2cb562684219b6613977acf03ca
NMiklave <50243212+NMiklave@users.noreply.github.com>
2025-07-13 17:29:53 -0400

    Add astro_color_matrix function to __init__

--- plantcv/plantcv/transform/__init__.py
+++ plantcv/plantcv/transform/__init__.py
@@ -9,6 +9,7 @@ from plantcv.plantcv.transform.color_correction import create_color_card_mask
 from plantcv.plantcv.transform.color_correction import quick_color_check
 from plantcv.plantcv.transform.find_color_card import find_color_card
 from plantcv.plantcv.transform.color_correction import std_color_matrix
+from plantcv.plantcv.transform.color_correction import astro_color_matrix
 from plantcv.plantcv.transform.color_correction import affine_color_correction
 from plantcv.plantcv.transform.detect_color_card import detect_color_card
 from plantcv.plantcv.transform.rescale import rescale
@@ -24,6 +25,7 @@ from plantcv.plantcv.transform.detect_color_card import mask_color_card
 
 __all__ = ["get_color_matrix", "get_matrix_m", "calc_transformation_matrix", "apply_transformation_matrix",
            "save_matrix", "load_matrix", "correct_color", "create_color_card_mask", "quick_color_check",
-           "find_color_card", "std_color_matrix", "affine_color_correction", "rescale", "nonuniform_illumination", "resize",
-           "resize_factor", "warp", "rotate", "warp", "warp_align", "gamma_correct", "detect_color_card", "checkerboard_calib",
-           "calibrate_camera", "merge_images", "auto_correct_color", "mask_color_card"]
+           "find_color_card", "std_color_matrix", "astro_color_matrix", "affine_color_correction", "rescale",
+           "nonuniform_illumination", "resize", "resize_factor", "warp", "rotate", "warp", "warp_align",
+           "gamma_correct", "detect_color_card", "checkerboard_calib", "calibrate_camera", "merge_images",
+           "auto_correct_color", "mask_color_card"]
