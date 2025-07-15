5541e9bc1035856d6255d397c6633f9b8810fd36
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2025-07-15 08:53:28 -0500

    optional rectangular filtering

--- plantcv/plantcv/transform/auto_correct_color.py
+++ plantcv/plantcv/transform/auto_correct_color.py
@@ -14,12 +14,16 @@ def auto_correct_color(rgb_img, label=None, **kwargs):
     label : str, optional
         modifies the variable name of observations recorded (default = pcv.params.sample_label).
     **kwargs
-        Other keyword arguments passed to cv2.adaptiveThreshold and cv2.circle.
+        Other keyword arguments passed to cv2.adaptiveThreshold, cv2.circle and _rect_filter.
         Valid keyword arguments:
         adaptive_method: 0 (mean) or 1 (Gaussian) (default = 1)
         block_size: int (default = 51)
         radius: int (default = 20)
         min_size: int (default = 1000)
+        x: int (default = 0)
+        y: int (default = 0)
+        h: int (default = np.shape(rgb_img)[0])
+        w: int (default = np.shape(rgb_img)[1])
     Returns
     -------
     numpy.ndarray
@@ -32,12 +36,31 @@ def auto_correct_color(rgb_img, label=None, **kwargs):
         "The 'label' parameter is no longer utilized, since color chip size is now metadata. "
         "It will be removed in PlantCV v5.0."
         )
-    # Get keyword arguments and set defaults if not set
-    labeled_mask = detect_color_card(rgb_img=rgb_img, min_size=kwargs.get("min_size", 1000),
-                                     radius=kwargs.get("radius", 20),
-                                     adaptive_method=kwargs.get("adaptive_method", 1),
-                                     block_size=kwargs.get("block_size", 51))
-    _, card_matrix = get_color_matrix(rgb_img=rgb_img, mask=labeled_mask)
+    # if x, y, h, w in kwargs then get color matrix from subset of image
+    if any(pt in ["x", "y", "h", "w"] for pt in kwargs.keys()):
+        labeled_mask = _rect_filter(rgb_img,
+                                    x=kwargs.get("x", 0),
+                                    y=kwargs.get("y", 0),
+                                    h=kwargs.get("h", np.shape(rgb_img)[0]),
+                                    w=kwargs.get("w", np.shape(rgb_img)[1]),
+                                    detect_color_card,
+                                    **kwargs
+                                    )
+        # calls _rect_filter twice but I am not seeing a clear way around that without
+        # changing detect_color_card to return the color matrix instead of a mask.
+        _, card_matrix = get_color_matrix(rgb_img=_rect_filter(rgb_img,
+                                                               x=kwargs.get("x", 0),
+                                                               y=kwargs.get("y", 0),
+                                                               h=kwargs.get("h", np.shape(rgb_img)[0]),
+                                                               w=kwargs.get("w", np.shape(rgb_img)[1])),
+                                          mask=labeled_mask)
+    else: # else, use entire image (the default)
+        # Get keyword arguments and set defaults if not set
+        labeled_mask = detect_color_card(rgb_img=rgb_img, min_size=kwargs.get("min_size", 1000),
+                                         radius=kwargs.get("radius", 20),
+                                         adaptive_method=kwargs.get("adaptive_method", 1),
+                                         block_size=kwargs.get("block_size", 51))
+        _, card_matrix = get_color_matrix(rgb_img=rgb_img, mask=labeled_mask)
     std_matrix = std_color_matrix(pos=3)
     return affine_color_correction(rgb_img=rgb_img, source_matrix=card_matrix,
                                    target_matrix=std_matrix)
