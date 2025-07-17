edfe56ee1de0c4b6283052c22f838afa1af893a6
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2025-07-17 14:59:34 -0500

    use _rect_filter in fill

--- plantcv/plantcv/fill.py
+++ plantcv/plantcv/fill.py
@@ -5,17 +5,18 @@ import os
 from plantcv.plantcv import fatal_error
 from plantcv.plantcv import params
 from plantcv.plantcv._debug import _debug
+from plantcv.plantcv._helpers import _rect_filter
 from skimage.morphology import remove_small_objects
 
 
-def fill(bin_img, size):
+def fill(bin_img, size, **kwargs):
     """
     Identifies objects and fills objects that are less than size.
 
     Inputs:
     bin_img      = Binary image data
     size         = minimum object area size in pixels (integer)
-
+    **kwargs     = other keyword arguments, namely x/y/h/w for rectangle subsetting
 
     Returns:
     filtered_img = image with objects filled
@@ -31,9 +32,15 @@ def fill(bin_img, size):
     # Cast binary image to boolean
     bool_img = bin_img.astype(bool)
 
-    # Find and fill contours
-    bool_img = remove_small_objects(bool_img, size)
-
+    # Find and fill contours, possibly within bounding rectangle
+    bool_img = _rect_filter(bool_img,
+                            xstart=kwargs.get("x", 0),
+                            ystart=kwargs.get("y", 0),
+                            height=kwargs.get("h", np.shape(bool_img)[0]),
+                            width=kwargs.get("w", np.shape(bool_img)[1]),
+                            function=remove_small_objects,
+                            replace=True,
+                            **{"min_size" : size})
     # Cast boolean image to binary and make a copy of the binary image for returning
     filtered_img = np.copy(bool_img.astype(np.uint8) * 255)
 
