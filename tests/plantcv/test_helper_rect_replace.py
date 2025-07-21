15066484c36f6c59a234df711d655d41ec7b23b4
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2025-07-21 11:27:38 -0500

    deepsource

--- tests/plantcv/test_helper_rect_replace.py
+++ tests/plantcv/test_helper_rect_replace.py
@@ -7,7 +7,7 @@ def test_rect_replace(test_data):
     """Test for PlantCV."""
     # Load rgb image
     rgb_img = cv2.imread(test_data.small_gray_img)
-    roi = pcv.roi.rectangle(rgb_img, x = 10, y = 20, h = 20, w = 20)
+    roi = rectangle(rgb_img, x = 10, y = 20, h = 20, w = 20)
     sub_img = _rect_filter(img=rgb_img, roi = roi)
     new_img = _rect_replace(rgb_img, sub_img, roi = roi)
     assert np.shape(new_img) == np.shape(rgb_img)
