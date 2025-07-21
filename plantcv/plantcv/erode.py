fafe8dbe11a0d0705578e984f010dff7afeb068b
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2025-07-21 14:13:26 -0500

    roi arg for erode

--- plantcv/plantcv/erode.py
+++ plantcv/plantcv/erode.py
@@ -1,13 +1,14 @@
 # Erosion filter
 
-import cv2
 import numpy as np
 import os
 from plantcv.plantcv._debug import _debug
+from plantcv.plantcv._helpers import _rect_filter, _rect_replace
 from plantcv.plantcv import params
+from cv2 import erode
 
 
-def erode(gray_img, ksize, i):
+def erode(gray_img, ksize, i, roi=None):
     """
     Perform morphological 'erosion' filtering. Keeps pixel in center of the kernel if conditions set in kernel are
        true, otherwise removes pixel.
@@ -16,6 +17,7 @@ def erode(gray_img, ksize, i):
     gray_img = Grayscale (usually binary) image data
     ksize   = Kernel size (int). A ksize x ksize kernel will be built. Must be greater than 1 to have an effect.
     i        = interations, i.e. number of consecutive filtering passes
+    roi      = Rectangular ROI to erode within
 
     Returns:
     er_img = eroded image
@@ -23,6 +25,7 @@ def erode(gray_img, ksize, i):
     :param gray_img: numpy.ndarray
     :param ksize: int
     :param i: int
+    "param roi: Objects
     :return er_img: numpy.ndarray
     """
     if ksize <= 1:
@@ -30,8 +33,12 @@ def erode(gray_img, ksize, i):
 
     kernel1 = int(ksize)
     kernel2 = np.ones((kernel1, kernel1), np.uint8)
-    er_img = cv2.erode(src=gray_img, kernel=kernel2, iterations=i)
-
+    sub_er_img = _rect_filter(img = gray_img,
+                           roi=roi,
+                           function=erode,
+                           **{"kernel":kernel2, "iterations":i})
+    er_img = _rect_replace(gray_img, sub_er_img, roi)
+    
     _debug(er_img,
            filename=os.path.join(params.debug_outdir,
                                  str(params.device) + '_er_image' + str(ksize) + '_itr_' + str(i) + '.png'),
