b4078bf8485955084bc5725e982eaf8a3e8964af
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2025-07-22 17:28:04 -0500

    use helper for image subtraction

--- plantcv/plantcv/image_subtract.py
+++ plantcv/plantcv/image_subtract.py
@@ -1,9 +1,9 @@
 # Image subtraction
 
 from plantcv.plantcv._debug import _debug
+from plantcv.plantcv._helpers import _image_subtract
 from plantcv.plantcv import params
 from plantcv.plantcv import fatal_error
-import numpy as np
 import os
 
 
@@ -24,13 +24,7 @@ def image_subtract(gray_img1, gray_img2):
     :param gray_img2: numpy.ndarray
     :return new_img: numpy.ndarray
     """
-    # check inputs for gray-scale
-    if len(np.shape(gray_img1)) != 2 or len(np.shape(gray_img2)) != 2:
-        fatal_error("Input image is not gray-scale")
-
-    new_img = gray_img1.astype(np.float64) - gray_img2.astype(np.float64)  # subtract values
-    new_img[np.where(new_img < 0)] = 0  # force negative array values to zero
-    new_img = new_img.astype(np.uint8)  # typecast image to 8-bit image
+    new_img = _image_subtract(gray_img1, gray_img2)
 
     _debug(visual=new_img,
            filename=os.path.join(params.debug_outdir, str(params.device) + "_subtraction.png"),
