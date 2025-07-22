d609de2b1547040de7cbdd0ceeb8f5e8e830de1e
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2025-07-22 07:38:26 -0500

    adding roi to floodfill

--- plantcv/plantcv/floodfill.py
+++ plantcv/plantcv/floodfill.py
@@ -3,19 +3,21 @@
 import numpy as np
 import os
 from plantcv.plantcv._debug import _debug
+from plantcv.plantcv._helpers import _rect_filter, _rect_replace
 from plantcv.plantcv import fatal_error
 from plantcv.plantcv import params
 from skimage.segmentation import flood_fill
 
 
-def floodfill(bin_img, points, value=0):
+def floodfill(bin_img, points, value=0, roi=None):
     """
     Flood fills holes in a binary mask
 
     Inputs:
     bin_img = Binary image data
-    points = list of seed points to start flood fill e.g. ([(x,y)])
-    value  = value from 0-255 (default value=0)
+    points  = list of seed points to start flood fill e.g. ([(x,y)])
+    value   = value from 0-255 (default value=0)
+    roi     = optional rectangular ROI (note points are relative to this ROI)
 
     Returns:
     filled_img = image with objects filled
@@ -23,6 +25,7 @@ def floodfill(bin_img, points, value=0):
     :param bin_img: numpy.ndarray
     :param points: list
     :param value: float
+    :param roi: Objects class
     :return filled_img: numpy.ndarray
     """
     # Make sure the image is binary
@@ -36,7 +39,9 @@ def floodfill(bin_img, points, value=0):
     points1 = list(map(lambda sub: (sub[1], sub[0]), points))
 
     for x in points1:
-        bool_img = flood_fill(bool_img, x, value)
+        sub_bool_img = _rect_filter(bool_img, roi=roi, function=flood_fill,
+                                    **{"seed_point":x, "new_value":value})
+        bool_img = _rect_replace(bool_img, sub_bool_img, roi)
 
     # Cast boolean image to binary and make a copy of the binary image for returning
     filled_img = np.copy(bool_img.astype(np.uint8) * 255)
