09efd2302754e320d42ddb4b075214d79ef2c205
HaleySchuhl <haleyschuhl@gmail.com>
2025-08-28 11:59:46 -0500

    try another order of colors

--- plantcv/plantcv/analyze/size.py
+++ plantcv/plantcv/analyze/size.py
@@ -123,22 +123,23 @@ def _analyze_size(img, mask, label):
         longest_path = euclidean(tuple(caliper_transpose[caliper_length - 1]), tuple(caliper_transpose[0]))
 
         # Debugging measurements onto Diagnostic image
-        
+        # Viridis color palette in BGR: (84, 1, 68) = violet; (139, 82, 59) = purple
+        # (140, 145, 33) - blue; (37, 231, 255) = yellow; (132, 168, 34) = teal
         # Draw object outline (previously only draw perimeter in v4.9 and earlier)
         cv2.drawContours(plt_img, cnt, -1, (84, 1, 68), params.line_thickness)
         # Draw perimeter outline
-        cv2.drawContours(plt_img, cnt, -1, (139, 82, 59), params.line_thickness)
+        cv2.drawContours(plt_img, obj, -1, (139, 82, 59), params.line_thickness)
         # Draw convex hull
         cv2.drawContours(plt_img, [hull], -1, (140, 145, 33), params.line_thickness)
         # Draw width annotation
-        cv2.line(plt_img, (x, y), (x + width, y), (255, 231, 37), params.line_thickness)
+        cv2.line(plt_img, (x, y), (x + width, y), (37, 231, 255), params.line_thickness)
         # Draw height annotation
-        cv2.line(plt_img, (int(cmx), y), (int(cmx), y + height), (255, 231, 37), params.line_thickness)
+        cv2.line(plt_img, (int(cmx), y), (int(cmx), y + height), (37, 231, 255), params.line_thickness)
         # Draw centroid annotation
-        cv2.circle(plt_img, (int(cmx), int(cmy)), 10, (255, 231, 37), params.line_thickness)
+        cv2.circle(plt_img, (int(cmx), int(cmy)), 10, (37, 231, 255), params.line_thickness)
         # Draw longest path annotation
         cv2.line(plt_img, (tuple(caliper_transpose[caliper_length - 1])), (tuple(caliper_transpose[0])),
-                 (98, 201, 94), params.line_thickness)
+                 (132, 168, 34), params.line_thickness)
         if params.verbose:
             # Label the object with object label
             cv2.putText(img=plt_img, text=label, org=(int(cmx), int(cmy)), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
