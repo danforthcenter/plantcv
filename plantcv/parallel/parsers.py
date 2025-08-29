e4961afac790e6e7deaad9c8629e0caf52b9b9a0
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2025-08-29 12:17:59 -0500

    Making _read_filenames accept tuples of imgformats

--- plantcv/parallel/parsers.py
+++ plantcv/parallel/parsers.py
@@ -397,13 +397,15 @@ def _read_filenames(config):
     # Get a list of all files
     if config.include_all_subdirs is False:
         # If subdirectories are excluded, use glob to get a list of all image files
-        fns = list(glob.glob(pathname=os.path.join(config.input_dir, f'*{config.imgformat}')))
+        # NOTE this does multiple loops I think.
+        # I don't see something as simple as R's `dir(pattern = paste0(many_extensions, collapse = "|"))`?
+        fns = [f for ext in config.img_format for f in glob.glob(os.path.join(config.input_dir, "*[.]" + ext))]
     else:
         # If subdirectories are included, recursively walk through the path
         fns = []
         for root, _, files in os.walk(config.input_dir):
             for file in files:
-                if file.endswith(config.imgformat):
+                if file.lower().endswith(config.imgformat):
                     # Keep the files that end with the image extension
                     fns.append(os.path.join(root, file))
     # Create a dataset
