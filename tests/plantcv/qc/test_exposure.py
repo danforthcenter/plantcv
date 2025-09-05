80799b77655f033b59c2913052d1f802f2ead2c2
HaleySchuhl <haleyschuhl@gmail.com>
2025-09-05 08:11:52 -0500

    try 'from file import function' syntax

--- tests/plantcv/qc/test_exposure.py
+++ tests/plantcv/qc/test_exposure.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 from plantcv.plantcv import outputs
-import plantcv.plantcv.qc.exposure as exposure
+from plantcv.plantcv.qc.exposure import exposure
 
 
 def test_plantcv_quality_control(test_data):
