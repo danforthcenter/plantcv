08bb0aed44d11995bbe6d243b0d30fa095aa4156
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2025-09-09 16:05:05 -0500

    simplifying bound_vertical

--- plantcv/plantcv/analyze/bound_vertical.py
+++ plantcv/plantcv/analyze/bound_vertical.py
@@ -1,9 +1,9 @@
 """Analyze the horizontal distribution of the plant relative to a vertical reference line."""
 import os
-import cv2
 import numpy as np
 from plantcv.plantcv._debug import _debug
-from plantcv.plantcv._helpers import _iterate_analysis, _cv2_findcontours, _object_composition, _grayscale_to_rgb, _scale_size
+from plantcv.plantcv._helpers import _iterate_analysis, _grayscale_to_rgb, _scale_size
+from plantcv.plantcv.analyze.bound_horizontal import _get_boundary_values
 from plantcv.plantcv import params
 from plantcv.plantcv import outputs
 
@@ -71,116 +71,18 @@ def _analyze_bound_vertical(img, mask, line_position, label):
 
     ori_img = np.copy(img)
     # Skip empty masks
-    if np.count_nonzero(mask) != 0:
+    if np.count_nonzero(mask) != 0 and line_position >= 0:
         # Draw line horizontal line through bottom of image, that is adjusted to user input height
         ori_img = _grayscale_to_rgb(img)
-        iy, ix, _ = np.shape(ori_img)
-        size = (iy, ix)
-        size1 = (iy, ix, 3)
-        background = np.zeros(size, dtype=np.uint8)
-        wback = (np.zeros(size1, dtype=np.uint8)) + 255
-        x_coor = 0 + int(line_position)
-        y_coor = int(iy)
-        rec_point1 = (0, 0)
-        rec_point2 = (x_coor, y_coor - 2)
-        cv2.rectangle(background, rec_point1, rec_point2, (255), -1)
-        right_contour, _ = _cv2_findcontours(bin_img=background)
-
-        # Find contours
-        cnt, cnt_str = _cv2_findcontours(bin_img=mask)
-
-        # Consolidate contours
-        obj = _object_composition(contours=cnt, hierarchy=cnt_str)
-
-        x, _, width, _ = cv2.boundingRect(obj)
-
-        if x_coor - x <= 0:
-            width_left_bound = 0
-            width_right_bound = width
-        elif x_coor - x > 0:
-            width_1 = x_coor - x
-            if width - width_1 <= 0:
-                width_left_bound = width
-                width_right_bound = 0
-            else:
-                width_left_bound = x_coor - x
-                width_right_bound = width - width_left_bound
-
-        right = []
-        left = []
-        mask_nonzerox, mask_nonzeroy = np.nonzero(mask)
-        obj_points = np.vstack((mask_nonzeroy, mask_nonzerox))
-        obj_points1 = np.transpose(obj_points)
-
-        for c in obj_points1:
-            xy = tuple(int(ci) for ci in c)
-            pptest = cv2.pointPolygonTest(right_contour[0], xy, measureDist=False)
-            if pptest == 1:
-                left.append(xy)
-                cv2.circle(ori_img, xy, 1, (155, 0, 255))
-                cv2.circle(wback, xy, 1, (155, 0, 255))
-            else:
-                right.append(xy)
-                cv2.circle(ori_img, xy, 1, (0, 255, 0))
-                cv2.circle(wback, xy, 1, (0, 255, 0))
-        right_bound_area = len(right)
-        left_bound_area = len(left)
-        percent_bound_area_right = ((float(right_bound_area)) / (float(left_bound_area + right_bound_area))) * 100
-        percent_bound_area_left = ((float(left_bound_area)) / (float(right_bound_area + left_bound_area))) * 100
-
-        analysis_images = []
-
-        if left_bound_area or right_bound_area:
-            point3 = (x_coor+2, 0)
-            point4 = (x_coor+2, y_coor)
-            cv2.line(ori_img, point3, point4, (255, 0, 255), params.line_thickness)
-            cv2.line(wback, point3, point4, (255, 0, 255), params.line_thickness)
-            m = cv2.moments(mask, binaryImage=True)
-            _, cmy = (m['m10'] / m['m00'], m['m01'] / m['m00'])
-            if x_coor - x <= 0:
-                cv2.line(ori_img, (x, int(cmy)), (x + width, int(cmy)), (0, 255, 0), params.line_thickness)
-                cv2.line(wback, (x, int(cmy)), (x + width, int(cmy)), (0, 255, 0), params.line_thickness)
-            elif x_coor - x > 0:
-                width_1 = x_coor - x
-                if width - width_1 <= 0:
-                    cv2.line(ori_img, (x, int(cmy)), (x + width, int(cmy)), (255, 0, 0), params.line_thickness)
-                    cv2.line(wback, (x, int(cmy)), (x + width, int(cmy)), (255, 0, 0), params.line_thickness)
-                else:
-                    cv2.line(ori_img, (x_coor + 2, int(cmy)), (x_coor + width_left_bound, int(cmy)), (255, 0, 0),
-                             params.line_thickness)
-                    cv2.line(ori_img, (x_coor + 2, int(cmy)), (x_coor - width_right_bound, int(cmy)), (0, 255, 0),
-                             params.line_thickness)
-                    cv2.line(wback, (x_coor + 2, int(cmy)), (x_coor + width_left_bound, int(cmy)), (255, 0, 0),
-                             params.line_thickness)
-                    cv2.line(wback, (x_coor + 2, int(cmy)), (x_coor - width_right_bound, int(cmy)), (0, 255, 0),
-                             params.line_thickness)
-            # Output images with boundary line
-            analysis_images.append(wback)
-            analysis_images.append(ori_img)
-
-        point3 = (x_coor+2, 0)
-        point4 = (x_coor+2, y_coor)
-        cv2.line(ori_img, point3, point4, (255, 0, 255), params.line_thickness)
-        cv2.line(wback, point3, point4, (255, 0, 255), params.line_thickness)
-        m = cv2.moments(mask, binaryImage=True)
-        _, cmy = (m['m10'] / m['m00'], m['m01'] / m['m00'])
-        if x_coor - x <= 0:
-            cv2.line(ori_img, (x, int(cmy)), (x + width, int(cmy)), (0, 255, 0), params.line_thickness)
-            cv2.line(wback, (x, int(cmy)), (x + width, int(cmy)), (0, 255, 0), params.line_thickness)
-        elif x_coor - x > 0:
-            width_1 = x_coor - x
-            if width - width_1 <= 0:
-                cv2.line(ori_img, (x, int(cmy)), (x + width, int(cmy)), (255, 0, 0), params.line_thickness)
-                cv2.line(wback, (x, int(cmy)), (x + width, int(cmy)), (255, 0, 0), params.line_thickness)
-            else:
-                cv2.line(ori_img, (x_coor + 2, int(cmy)), (x_coor + width_left_bound, int(cmy)), (255, 0, 0),
-                         params.line_thickness)
-                cv2.line(ori_img, (x_coor + 2, int(cmy)), (x_coor - width_right_bound, int(cmy)), (0, 255, 0),
-                         params.line_thickness)
-                cv2.line(wback, (x_coor + 2, int(cmy)), (x_coor + width_left_bound, int(cmy)), (255, 0, 0),
-                         params.line_thickness)
-                cv2.line(wback, (x_coor + 2, int(cmy)), (x_coor - width_right_bound, int(cmy)), (0, 255, 0),
-                         params.line_thickness)
+        left_mask = np.copy(mask).astype(bool)
+        right_mask = np.copy(mask).astype(bool)
+        left_mask[:, line_position:np.shape(left_mask)[1] + 1] = np.zeros(
+            np.shape(left_mask[:, line_position:np.shape(left_mask)[1] + 1]))
+        right_mask[:, 0:line_position - 1] = np.zeros(
+            np.shape(right_mask[:, 0:line_position - 1]))
+        tot_area = np.sum(mask.astype(bool))
+        left_bound_area, width_left_bound, percent_bound_area_left = _get_boundary_values(left_mask, tot_area, 1)
+        right_bound_area, width_right_bound, percent_bound_area_right = _get_boundary_values(right_mask, tot_area, 1)
 
     outputs.add_observation(sample=label, variable='vertical_reference_position', trait='vertical reference position',
                             method='plantcv.plantcv.analyze.bound_vertical', scale='none', datatype=int,
@@ -204,8 +106,5 @@ def _analyze_bound_vertical(img, mask, line_position, label):
     outputs.add_observation(sample=label, variable='percent_area_right_reference',
                             trait='percent area right of reference', method='plantcv.plantcv.analyze.bound_vertical',
                             scale='none', datatype=float, value=percent_bound_area_right, label='none')
-
-    # Store images
-    outputs.images.append(analysis_images)
 
     return ori_img
