491cc69fea366aa16b6c061467e62b6006618640
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2025-09-11 15:27:13 -0500

    indentation error

--- plantcv/plantcv/homology/acute.py
+++ plantcv/plantcv/homology/acute.py
@@ -235,37 +235,37 @@ def _process_islands(obj, isle, chain, mask):
                 ptvals.append(sum(vals) / len(vals))
             else:
                 ptvals.append('NaN')
-            if len(island) >= 3:               # If landmark is multiple points (distance scan for position)
-                if params.verbose:
-                    print('route C')
-                ss = obj[island[0]]            # Store isle "x" start site
-                ts = obj[island[-1]]           # Store isle "x" termination site
-                dist_1 = 0
-                for d in island:   # Scan from ss to ts within isle "x"
-                    site = obj[[d]]
-                    ss_d = np.sqrt(np.square(ss[0][0] - site[0][0][0]) + np.square(ss[0][1] - site[0][0][1]))
-                    ts_d = np.sqrt(np.square(ts[0][0] - site[0][0][0]) + np.square(ts[0][1] - site[0][0][1]))
-                    # Current mean distance of 'd' to 'ss' & 'ts'
-                    dist_2 = np.mean([np.abs(ss_d), np.abs(ts_d)])
-                    max_dist.append([d, dist_2, chain[d]])
-                    if dist_2 > dist_1:                          # Current mean distance better fit that previous best?
-                        pt = d
-                        dist_1 = dist_2                          # Current mean becomes new best mean
-                # print pt
-                if params.verbose:
-                    print(f"Landmark site: {pt}, Start site: {island[0]}, Term. site: {island[-1]}")
-
-                maxpts.append(pt)           # Empty 'pts' prior to next mean distance scan
-                ss_pts.append(island[0])
-                ts_pts.append(island[-1])
-
+        if len(island) >= 3:               # If landmark is multiple points (distance scan for position)
             if params.verbose:
-                print(f'Landmark point indices: {maxpts}')
-                print(f'Starting site indices: {ss_pts}')
-                print(f'Termination site indices: {ts_pts}')
-        homolog_pts = obj[maxpts]
-        start_pts = obj[ss_pts]
-        stop_pts = obj[ts_pts]
+                print('route C')
+            ss = obj[island[0]]            # Store isle "x" start site
+            ts = obj[island[-1]]           # Store isle "x" termination site
+            dist_1 = 0
+            for d in island:   # Scan from ss to ts within isle "x"
+                site = obj[[d]]
+                ss_d = np.sqrt(np.square(ss[0][0] - site[0][0][0]) + np.square(ss[0][1] - site[0][0][1]))
+                ts_d = np.sqrt(np.square(ts[0][0] - site[0][0][0]) + np.square(ts[0][1] - site[0][0][1]))
+                # Current mean distance of 'd' to 'ss' & 'ts'
+                dist_2 = np.mean([np.abs(ss_d), np.abs(ts_d)])
+                max_dist.append([d, dist_2, chain[d]])
+                if dist_2 > dist_1:                          # Current mean distance better fit that previous best?
+                    pt = d
+                    dist_1 = dist_2                          # Current mean becomes new best mean
+            # print pt
+            if params.verbose:
+                print(f"Landmark site: {pt}, Start site: {island[0]}, Term. site: {island[-1]}")
+
+            maxpts.append(pt)           # Empty 'pts' prior to next mean distance scan
+            ss_pts.append(island[0])
+            ts_pts.append(island[-1])
+
+        if params.verbose:
+            print(f'Landmark point indices: {maxpts}')
+            print(f'Starting site indices: {ss_pts}')
+            print(f'Termination site indices: {ts_pts}')
+    homolog_pts = obj[maxpts]
+    start_pts = obj[ss_pts]
+    stop_pts = obj[ts_pts]
 
     return homolog_pts, start_pts, stop_pts
     
