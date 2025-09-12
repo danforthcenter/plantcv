25720a8e0da55ffcec39e0cffcbb29dbeb174ee7
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2025-09-12 15:44:50 -0500

    removing hacky string method

--- plantcv/parallel/inspect_dataset.py
+++ plantcv/parallel/inspect_dataset.py
@@ -6,30 +6,11 @@ def inspect_dataset(config):
     """Inspect a dataset before running plantcv in parallel over the directory
     Parameters
     ----------
-    config   = plantcv.parallel.WorkflowConfig object or str, if string then this should be the input directory path.
+    config   = plantcv.parallel.WorkflowConfig object
 
     Returns = pandas.core.frame.DataFrame, dataframe of image metadata.
     -------
     """
-    # if config is a path then make a config out of it
-    # NOTE i can't just grab WorkflowConfig from parallel because I can't
-    # export this while with parallel while that workflowconfig class is defined
-    # in that __init__.py file.
-    if isinstance(config, str):
-        input_dir = config
-        # if there is no config file then make a cheap copy
-        config = type('dummyconfig', (), {'input_dir': input_dir,
-                                          'filename_metadata': ["filepath"],
-                                          'include_all_subdirs':True,
-                                          'metadata_terms': {'timestamp'},
-                                          'metadata_filters':{},
-                                          'timestampformat':"%Y-%m-%dT%H:%M:%S.%fZ",
-                                          'start_date': None,
-                                          'end_date': None,
-                                          'imgformat':"png",
-                                          'delimiter':'&&&',
-                                          'groupby':'filepath'
-                                          })
     # run the metadata parser to find images and return dataframes
     meta, removed = metadata_parser(config)
     # make dataframe out of groupby object
