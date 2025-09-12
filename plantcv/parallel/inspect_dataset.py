0fec41962c2d657ff35b60779c6d6a7aec33835c
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2025-09-12 14:31:48 -0500

    make inspection function to call metadata_parsers and format output

---
+++ plantcv/parallel/inspect_dataset.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from plantcv.parallel import WorkflowConfig
+
+
+def inspect_dataset(config):
+    """Inspect a dataset before running plantcv in parallel over the directory
+    Parameters
+    ----------
+    config   = plantcv.parallel.WorkflowConfig object or str, if string then this should be the input directory path.
+
+    Returns = pandas.core.frame.DataFrame, dataframe of image metadata.
+    -------
+    """
+    # if config is a path then make a config out of it
+    if isinstance(config, str):
+        input_dir = config
+        config = WorkflowConfig()
+        config.input_dir = input_dir
+    # run the metadata parser to find images and return dataframes
+    meta, removed = metadata_parser(config)
+    # make dataframe out of groupby object
+    meta = meta.apply(lambda x: x, include_groups=False)
+    # flag kept images
+    meta["status"] = "Kept"
+    # combine both dataframes
+    df = pd.concat([meta, removed])
+    return df
+
+
+def _summarize_dataset(df):
+    """
+    """
+    return(df)
