370a769addf8ee4717f6571d01b78d75c3c9bdc1
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2025-09-12 15:44:01 -0500

    working string input
    This lets the idea of just giving this thing a file path work, but the implementation is very hacky

--- plantcv/parallel/inspect_dataset.py
+++ plantcv/parallel/inspect_dataset.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from plantcv.parallel.workflow_inputs import WorkflowConfig
+from plantcv.parallel.parsers import metadata_parser
 
 
 def inspect_dataset(config):
@@ -12,14 +12,32 @@ def inspect_dataset(config):
     -------
     """
     # if config is a path then make a config out of it
+    # NOTE i can't just grab WorkflowConfig from parallel because I can't
+    # export this while with parallel while that workflowconfig class is defined
+    # in that __init__.py file.
     if isinstance(config, str):
         input_dir = config
-        config = WorkflowConfig()
-        config.input_dir = input_dir
+        # if there is no config file then make a cheap copy
+        config = type('dummyconfig', (), {'input_dir': input_dir,
+                                          'filename_metadata': ["filepath"],
+                                          'include_all_subdirs':True,
+                                          'metadata_terms': {'timestamp'},
+                                          'metadata_filters':{},
+                                          'timestampformat':"%Y-%m-%dT%H:%M:%S.%fZ",
+                                          'start_date': None,
+                                          'end_date': None,
+                                          'imgformat':"png",
+                                          'delimiter':'&&&',
+                                          'groupby':'filepath'
+                                          })
     # run the metadata parser to find images and return dataframes
     meta, removed = metadata_parser(config)
     # make dataframe out of groupby object
+    meta_filepaths = []
+    for i, _ in meta["filepath"]:
+        meta_filepaths.append(i[0])
     meta = meta.apply(lambda x: x, include_groups=False)
+    meta["filepath"] = meta_filepaths
     # flag kept images
     meta["status"] = "Kept"
     # combine both dataframes
