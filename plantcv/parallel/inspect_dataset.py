80657ab1e37f04a8bd5e2ed49a8df1668796a2b1
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2025-09-15 15:05:54 -0500

    allowing for string input and making results summary

--- plantcv/parallel/inspect_dataset.py
+++ plantcv/parallel/inspect_dataset.py
@@ -1,32 +1,98 @@
 import pandas as pd
-from plantcv.parallel.parsers import metadata_parser
+import os
+from plantcv.parallel.parsers import metadata_parser, _read_dataset
 
 
 def inspect_dataset(config):
     """Inspect a dataset before running plantcv in parallel over the directory
     Parameters
     ----------
-    config   = plantcv.parallel.WorkflowConfig object
+    config   = plantcv.parallel.WorkflowConfig object or str, if str then very minimal processing is done
 
-    Returns = pandas.core.frame.DataFrame, dataframe of image metadata.
+    Returns
     -------
+    summary_df     = pandas.core.frame.DataFrame,
+        dataframe of number of unique values of metadata filters and statuses.
+    meta     = pandas.core.frame.DataFrame, dataframe of image metadata.
     """
-    # run the metadata parser to find images and return dataframes
-    meta, removed = metadata_parser(config)
-    # make dataframe out of groupby object
-    meta_filepaths = []
-    for i, _ in meta["filepath"]:
-        meta_filepaths.append(i[0])
-    meta = meta.apply(lambda x: x, include_groups=False)
-    meta["filepath"] = meta_filepaths
+    if isinstance(config, str):
+        input_dir = config
+        config = type('inspectionconfig', (), {'input_dir':input_dir,
+                                               'imgformat':'png',
+                                               'include_all_subdirs':True,
+                                               'delimiter':'_',
+                                               'filename_metadata': ["filepath"],
+                                               'metadata_filters':{},
+                                               'metadata_terms':{'filepath'}
+                                               })
+        dataset = _read_dataset(config)
+        meta = _naive_dataset2dataframe(dataset, config)
+        removed = pd.DataFrame()
+    else:
+        # run the metadata parser to find images and return dataframes
+        meta, removed = metadata_parser(config)
+        # make dataframe out of groupby object
+        meta_filepaths = []
+        for i, _ in meta["filepath"]:
+            meta_filepaths.append(i[0])
+        meta = meta.apply(lambda x: x, include_groups=False)
+        meta["filepath"] = meta_filepaths
     # flag kept images
     meta["status"] = "Kept"
     # combine both dataframes
-    df = pd.concat([meta, removed])
+    if not removed.empty:
+        meta = pd.concat(df.dropna(axis=1, how='all') for df in [meta, removed])
+    # pull extensions
+    meta['extension'] = meta['filepath'].apply(
+        lambda x: os.path.splitext(os.path.basename(x))[1]
+    )
+    # count unique values by all filtered metadata and steps where images were dropped
+    summary_df = meta.groupby(
+        ['status', *config.metadata_filters],
+        dropna = False
+    ).agg(_agg_unique_values)
+    return summary_df, meta
+
+
+def _naive_dataset2dataframe(dataset, config):
+    """Convert a dataset to a dataframe with little additional information.
+    Parameters
+    ----------
+    dataset = dict, metadata dictionary as returned by plantcv.parallel.parsers._read_dataset
+
+    Return
+    ------
+    meta    = pandas.DataFrame, metadata dataframe
+    """
+    metadata = {
+        "filepath": []
+    }
+    for image in dataset["images"]:
+        metadata["filepath"].append(os.path.join(config.input_dir, image))
+    df = pd.DataFrame(data=metadata)
     return df
 
 
-def _summarize_dataset(df):
+def _agg_unique_values(series):
+    """Aggregate columns to a number of unique values and possibly their values
+    Parameters
+    ----------
+    series     = pandas.core.series.Series, a metadata column of a pandas dataframe
+
+    Return
+    ------
+    label      = str, a string describing the number of unique values and their
+                 observed levels if there are <4.
     """
-    """
-    return(df)
+    n_unique = series.nunique()
+    context = ""
+    if n_unique:
+        context = " (...)"
+    series = series.astype("string")
+    # if there are only a few options then print what they are
+    if n_unique and n_unique < 4:
+        uniques = series.dropna().unique()
+        unique_vals = ", ".join(uniques)
+        context = " (" + unique_vals + ")"
+    label = str(n_unique) + context
+    return label
