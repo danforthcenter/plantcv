62098f77dbc9f22922629e7f29d2281a244ed2a7
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2025-09-16 08:11:05 -0500

    better tests

--- tests/parallel/test_inspect_config.py
+++ tests/parallel/test_inspect_config.py
@@ -9,10 +9,12 @@ def test_inspect_dataset(parallel_test_data):
     config = WorkflowConfig()
     config.input_dir = parallel_test_data.flat_imgdir
     config.imgformat = "jpg"
+    config.filename_metadata = ["imgtype", "camera", "angle", "zoom", "gain", "exposure", "lifter", "id"]
+    config.metadata_filters = {"imgtype" : "VIS"}
     # inspect dataset
     sdf, df = inspect_dataset(config)
 
-    assert sdf.shape == (1, 18) and df.shape == (2, 19)
+    assert sdf.shape == (2, 17) and df.shape == (2, 19)
 
 
 def test_inspect_dataset_string(parallel_test_data):
@@ -20,6 +22,6 @@ def test_inspect_dataset_string(parallel_test_data):
     Testing inspection for config files
     """
     # initialize config
-    sdf, df = inspect_dataset(parallel_test_data.flat_imgdir)
+    sdf, df = inspect_dataset(parallel_test_data.phenodata_imgdir)
     # nothing found because default is png imgformat
-    assert sdf.shape == (0, 2) and df.shape == (0, 3)
+    assert sdf.shape == (1, 2) and df.shape == (12, 3)
