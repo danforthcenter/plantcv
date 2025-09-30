349a2085126aab16bbc28632403d67229037f790
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2025-09-30 11:29:12 -0500

    workflow_inputs as a class

--- plantcv/parallel/workflow_inputs.py
+++ plantcv/parallel/workflow_inputs.py
@@ -25,49 +25,61 @@ class WorkflowInputs:
         self.__dict__.update(_name_images(names=names, img_list=images))
 
 
-def workflow_inputs(*other_args) -> argparse.Namespace:
-    """Parse PlantCV workflow command-line options.
+class workflow_inputs:
+    """Class for parsed PlantCV workflow command-line options"""
 
-    Keyword arguments:
-    other_args = additional, user-defined workflow inputs.
+    def __init__(self, *other_args):
+        """Configure input variables"""
+        parser = argparse.ArgumentParser(description="PlantCV user workflow.")
+        parser.add_argument("images", help="Input image files.", type=str, nargs="+", metavar="IMAGES")
+        parser.add_argument("--names", help="A unique name mapped to each input image.", required=True)
+        parser.add_argument("--result", help="Output workflow results file.", required=True, dest="_result")
+        parser.add_argument("--checkpoint", help="Path to checkpointing csv file.", required=True)
+        parser.add_argument("--outdir", help="Output directory for image files.", default=".")
+        parser.add_argument("--writeimg", help="Save output images.", default=False, action="store_true")
+        parser.add_argument("--debug", help="Turn on debug, prints/plot intermediate images.",
+                            choices=[None, "print", "plot"], default=None)
+        # Parse additional user-defined workflow inputs
+        for arg in other_args:
+            parser.add_argument(f"--{arg}", help="Additional, user-defined workflow input.", required=False)
+        args = parser.parse_args()
+        for key, value in args.__dict__.items():
+            setattr(self, key, value)
+        # name images
+        images = self._name_images()
+        self.__dict__.update(images)
+        # start checkpointing if possible
+        self.touch_checkpoint(value="attempted")
 
-    Outputs:
-    args = an argparse ArgumentParser object with command-line keyword attributes.
+    def _name_images(self):
+        """Pair image names with image file paths"""
+        # initialize new dictionary for image names and paths
+        images = {}
+        # Enumerate the list of names split from the input comma-delimited string
+        for i, name in enumerate(self.names.split(",")):
+            # Pair each name with the corresponding image file path
+            images[name.lower()] = self.images[i]
+        return images
 
-    :param other_args: list
-    :return args: argparse.Namespace
-    """
-    parser = argparse.ArgumentParser(description="PlantCV user workflow.")
-    parser.add_argument("images", help="Input image files.", type=str, nargs="+", metavar="IMAGES")
-    parser.add_argument("--names", help="A unique name mapped to each input image.", required=True)
-    parser.add_argument("--result", help="Output workflow results file.", required=True)
-    parser.add_argument("--outdir", help="Output directory for image files.", default=".")
-    parser.add_argument("--writeimg", help="Save output images.", default=False, action="store_true")
-    parser.add_argument("--debug", help="Turn on debug, prints/plot intermediate images.",
-                        choices=[None, "print", "plot"], default=None)
-    # Parse additional user-defined workflow inputs
-    for arg in other_args:
-        parser.add_argument(f"--{arg}", help="Additional, user-defined workflow input.", required=False)
-    args = parser.parse_args()
+    def touch_checkpoint(self, value):
+        """Edit the checkpoint file"""
+        # I'd initially imagined working with the config, but maybe that isn't how this can work?
+        # just need the path to the metadata file and a way to index the rows so I can write `value`
+        #if os.path.exists(self.checkpoint):
+            # find row, can do that by reading the outfile or by using the image name?
+            # read csv (might be problematic if several read at once to edit?)
+            # edit csv (could I do this outside of python more easily?)
+            # write csv
+        print("~Something~ to touch " + self.checkpoint + " and add '" + value + "' to 'checkpointing' column")
 
-    images = _name_images(names=args.names, img_list=args.images)
-    args.__dict__.update(images)
-    return args
+    @property
+    def result(self):
+        # getter function should only be called for saving results which can trigger end_checkpoint()
+        # this would still need to know if there is information in the params.outputs, not if self._results is populated
+        self.touch_checkpoint(value="completed")
+        # once that is done, return hidden version of results
+        return self._result
 
-
-def _name_images(names: str, img_list: list) -> dict:
-    """Pair image names with image file paths.
-
-    Keyword arguments:
-    names = a comma-delimited string of unique names.
-    img_list = a list of image file paths.
-
-    Outputs:
-    images = a dictionary of name-image file path pairings.
-    """
-    images = {}
-    # Enumerate the list of names split from the input comma-delimited string
-    for i, name in enumerate(names.split(",")):
-        # Pair each name with the corresponding image file path
-        images[name.lower()] = img_list[i]
-    return images
+    @result.setter
+    def result(self, new):
+        self._result = new
