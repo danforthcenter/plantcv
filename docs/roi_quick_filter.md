c10bfa99433d633de5d5da06f736be04eff922db
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2025-10-16 15:45:33 -0500

    adding methods to roi.quick_filter

--- docs/roi_quick_filter.md
+++ docs/roi_quick_filter.md
@@ -1,16 +1,17 @@
 ## Filter a Mask using a Region of Interest
 
 Filter objects (connected regions of non-zero pixels) within a region of interest. This function is similar to
-[plantcv.roi.filter](roi_filter.md) but is faster, though it only supports the partial overlap method
-(`roi_type='partial'` in `plantcv.roi.filter`).
+[plantcv.roi.filter](roi_filter.md) but is faster, though it does not support all methods in `plantcv.roi.filter`.
 
-**plantcv.roi.quick_filter**(*mask, roi*)
+**plantcv.roi.quick_filter**(*mask, roi, roi_type*)
 
 **returns** filtered_mask
 
 - **Parameters:**
     - mask = binary image data to be filtered
     - roi = region of interest, an instance of the Objects class, output from one of the pcv.roi subpackage functions
+	- roi_type = 'partial' (for partially inside, default), 'cutto' (cut objects to the inside of the ROI),
+	'within' (keep only objects fully inside ROI)
 
 - **Context:**
     - Used to filter objects within a region of interest and decide which ones to keep.
--- tests/plantcv/roi/test_quick_filter.py
+++ tests/plantcv/roi/test_quick_filter.py
@@ -1,27 +1,13 @@
 """quick_filter tests module."""
 import cv2
+import pytest
 import numpy as np
 from plantcv.plantcv import Objects
 from plantcv.plantcv.roi.quick_filter import quick_filter, _quick_cutto
 
 
-def test_quick_filter(test_data):
-    """Test for PlantCV."""
-    # Read in test data
-    img = cv2.imread(test_data.small_rgb_img)
-    mask = np.zeros(np.shape(img)[:2], dtype=np.uint8)
-    cnt, cnt_str = test_data.load_contours(test_data.small_contours_file)
-    cv2.drawContours(mask, cnt, -1, (255), -1, lineType=8, hierarchy=cnt_str)
-    roi = [np.array([[[150, 150]], [[150, 174]], [[249, 174]], [[249, 150]]], dtype=np.int32)]
-    roi_str = np.array([[[-1, -1, -1, -1]]], dtype=np.int32)
-    roi_obj = Objects(contours=[roi], hierarchy=[roi_str])
-    filtered_mask = quick_filter(mask=mask, roi=roi_obj)
-    area = cv2.countNonZero(filtered_mask)
-    # Assert that the contours were filtered as expected
-    assert area == 221
-
-
-def test_quick_cutto(test_data):
+@pytest.mark.parametrize("mode,exp", [["partial", 221], ["cutto", 7], ["within", 0]])
+def test_quick_filter(test_data, mode, exp):
     """Test for PlantCV."""
     # Read in test data
     img = cv2.imread(test_data.small_rgb_img)
@@ -31,7 +17,7 @@ def test_quick_cutto(test_data):
     roi = [np.array([[[200, 200]], [[200, 190]], [[249, 190]], [[249, 200]]], dtype=np.int32)]
     roi_str = np.array([[[-1, -1, -1, -1]]], dtype=np.int32)
     roi_obj = Objects(contours=[roi], hierarchy=[roi_str])
-    filtered_mask, _, _ = _quick_cutto(mask=mask, roi=roi_obj)
+    filtered_mask = quick_filter(mask=mask, roi=roi_obj, roi_type=mode)
     area = cv2.countNonZero(filtered_mask)
     # Assert that the contours were filtered as expected
-    assert area == 7
+    assert area == exp
