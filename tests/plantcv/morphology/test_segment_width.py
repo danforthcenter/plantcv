07e136d81b1236cca88b452475b83ef20524e24e
HaleySchuhl <haleyschuhl@gmail.com>
2025-10-17 11:37:20 -0500

    Update test_segment_width.py

--- tests/plantcv/morphology/test_segment_width.py
+++ tests/plantcv/morphology/test_segment_width.py
@@ -4,13 +4,13 @@ from plantcv.plantcv import outputs
 from plantcv.plantcv.morphology import segment_width
 
 
-def test_segment_width(test_data):
+def test_segment_width(morphology_test_data):
     """Test for PlantCV."""
     # Clear previous outputs
     outputs.clear()
     # Read in test data
-    mask = cv2.imread(test_data.labeled_mask, -1)
-    skeleton = cv2.imread(test_data.small_bin_fill, -1)
-    _ = segment_width(img=skeleton, labeled_mask=mask)
-    assert int(sum(outputs.observations['default']['segment_width']['value'])) == 12
+    mask = cv2.imread(morphology_test_data.bin_img, -1)
+    skeleton = cv2.imread(morphology_test_data.skel_img, -1)
+    _ = segment_width(skel_img=skeleton, labeled_mask=mask)
+    assert int(sum(outputs.observations['default']['segment_width']['value'])) == 6
 
