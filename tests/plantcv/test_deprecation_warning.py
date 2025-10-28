b592c6ca33e8967c839dc4e028c11185a817715b
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2025-10-28 10:08:22 -0500

    cyclic error

--- tests/plantcv/test_deprecation_warning.py
+++ tests/plantcv/test_deprecation_warning.py
@@ -1,9 +1,11 @@
-from plantcv.plantcv import params, deprecation_warning
+from plantcv.plantcv.Classes import Params
+from plantcv.plantcv.deprecation_warning import deprecation_warning
 
 
 def test_deprecation_warning():
     """Test for PlantCV."""
     # Verify that the fatal_error function raises a RuntimeError
+    params = Params()
     params.verbose = 2
     deprecation_warning("This is just a test, and it's just a warning")
     assert True
