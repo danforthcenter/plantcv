239726d82e2855b18ee3c8eafbae8808943114b6
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2025-11-11 13:20:51 -0600

    testing process_results and json2csv in pcv

--- tests/plantcv/test_process_results.py
+++ tests/plantcv/test_process_results.py
@@ -1,51 +1,51 @@
 import pytest
 import os
-from plantcv.parallel import process_results
+from plantcv.plantcv.process_results import process_results
 
 
-def test_process_results(parallel_test_data, tmpdir):
+def test_process_results(test_data, tmpdir):
     """Test for PlantCV."""
     # Create a test tmp directory and results file
     tmp_dir = tmpdir.mkdir("sub")
     result_file = tmp_dir.join("appended_results.json")
     config = type("smallconfig", (),
-                  {"tmp_dir": parallel_test_data.parallel_results_dir,
+                  {"tmp_dir": test_data.parallel_results_dir,
                    "checkpoint": False,
                    "results": result_file})
     # Run twice to create appended results
-    process_results(config)
-    process_results(config)
+    process_results(config, outformat="json")
+    process_results(config, outformat="json")
     # Assert that the output JSON file matches the expected output JSON file
-    results = parallel_test_data.load_json(json_file=result_file)
-    expected = parallel_test_data.appended_results()
+    results = test_data.load_json(json_file=result_file)
+    expected = test_data.appended_results()
     assert results == expected
 
 
-def test_process_results_new_output(parallel_test_data, tmpdir):
+def test_process_results_new_output(test_data, tmpdir):
     """Test for PlantCV."""
     # Create a test tmp directory and results file
     result_file = tmpdir.mkdir("sub").join("new_result.json")
     config = type("smallconfig", (),
-                  {"tmp_dir": parallel_test_data.parallel_results_dir,
+                  {"tmp_dir": test_data.parallel_results_dir,
                    "checkpoint": False,
                    "results": result_file})
-    process_results(config)
+    process_results(config, outformat="json")
 
     # Assert output matches expected values
-    results = parallel_test_data.load_json(json_file=result_file)
-    expected = parallel_test_data.new_results()
+    results = test_data.load_json(json_file=result_file)
+    expected = test_data.new_results()
     assert results == expected
 
 
-def test_process_results_valid_json(parallel_test_data):
+def test_process_results_valid_json(test_data):
     """Test for PlantCV."""
     config = type("smallconfig", (),
-                  {"tmp_dir": parallel_test_data.parallel_results_dir,
+                  {"tmp_dir": test_data.parallel_results_dir,
                    "checkpoint": "false",
-                   "results": parallel_test_data.valid_json_file})
+                   "results": test_data.valid_json_file})
     # Test when the file is a valid json file but doesn't contain expected keys
     with pytest.raises(RuntimeError):
-        process_results(config)
+        process_results(config, outformat="json")
 
 
 def test_process_results_invalid_json(tmpdir):
@@ -58,4 +58,4 @@ def test_process_results_invalid_json(tmpdir):
                    "checkpoint": "false",
                    "results": result_file})
     with pytest.raises(RuntimeError):
-        process_results(config)
+        process_results(config, outformat="json")
