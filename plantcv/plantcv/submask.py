495ef9ea60f66a91fd72145dea24b125ce6615d0
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2026-01-13 14:33:05 -0600

    checking for overlapping circles

--- plantcv/plantcv/submask.py
+++ plantcv/plantcv/submask.py
@@ -6,6 +6,64 @@ import random
 import cv2
 
 
+def sub_mask(img, mask, num_masks=1, radius=5):
+    """
+    Make circular sub-masks inside the mask of an object
+
+    Parameters
+    ----------
+    img       = numpy.ndarray, input image
+    mask      = numpy.ndarray, binary mask of object
+    num_masks = int, number of circular sub-masks to make
+    radius    = int, radius of circular mask to make. Defaults to 5.
+
+    Returns
+    -------
+    labeled_mask = numpy.ndarray, labelled mask of circular masks each within complete mask.
+    """
+    # Create an empty mask
+    labeled_mask = np.zeros_like(mask)
+    tries = 0
+    sample_num = 0
+    while len(np.unique(labeled_mask)) - 1 < num_masks:
+        tries += 1
+        spot = _make_random_circle(img=img, mask=mask, radius=radius)
+        spot_mask = roi2mask(img=img, roi=spot)
+        within = _is_mask_within(full_mask=mask, submask=spot_mask)
+        if within and _check_overlapping_masks(labeled_mask, spot_mask):
+            sample_num += 1
+            # Label spots with unique integers
+            labeled_mask[np.where(spot_mask > 0)] = sample_num
+        if tries > num_masks * 100:  # Prevent infinite loop
+            # warn if stuck, break loop
+            warn("Too many iterations. Placed " + str(sample_num) +
+                 " circular masks instead of " + str(num_masks))
+            break
+    return labeled_mask
+
+
+def _check_overlapping_masks(labeled_mask, new_mask):
+    """
+    Check for any overlapping masks
+
+    Parameters
+    ----------
+    labeled_mask = numpy.ndarray, the labeled_mask that the new_mask may be added to
+    new_mask     = numpy.ndarray, the new proposed region to mask
+    
+    Returns
+    -------
+    Boolean, True if new_mask does not overlap the labeled mask
+    """
+    # set all labeled mask values to 1 for bin_mask
+    bin_mask = np.copy(labeled_mask)
+    bin_mask[np.where(bin_mask > 0)] = 1
+    new_bin = np.copy(new_mask)
+    new_bin[np.where(new_bin > 0)] = 1
+    # check max of sum of binary mask and new_mask
+    return np.max(np.add(bin_mask, new_bin)) == 1
+
+
 def _make_random_circle(img, mask, radius=5):
     """
     Make a circular ROI at a random point in a mask
@@ -30,7 +88,7 @@ def _make_random_circle(img, mask, radius=5):
     return spot
 
 
-def _is_mask_within(full_mask, sub_mask):
+def _is_mask_within(full_mask, submask):
     """
     Check if a mask is wholly within another mask
 
@@ -43,42 +101,6 @@ def _is_mask_within(full_mask, sub_mask):
     -------
     within = Boolean, comparison of full_mask against full_mask and sub_mask.
     """
-    combined = cv2.bitwise_and(sub_mask, full_mask)
-    within = np.array_equal(combined, sub_mask)
+    combined = cv2.bitwise_and(submask, full_mask)
+    within = np.array_equal(combined, submask)
     return within
-
-
-def sub_mask(img, mask, num_masks=1, radius=5):
-    """
-    Make circular sub-masks inside the mask of an object
-
-    Parameters
-    ----------
-    img       = numpy.ndarray, input image
-    mask      = numpy.ndarray, binary mask of object
-    num_masks = int, number of circular sub-masks to make
-    radius    = int, radius of circular mask to make. Defaults to 5.
-
-    Returns
-    -------
-    labeled_mask = numpy.ndarray, labelled mask of circular masks each within complete mask.
-    """
-    # Create an empty mask
-    labeled_mask = np.zeros_like(mask)
-    tries = 0
-    sample_num = 0
-    while len(np.unique(labeled_mask)) - 1 < num_masks:
-        tries += 1
-        spot = _make_random_circle(img=img, mask=mask, radius=radius)
-        spot_mask = roi2mask(img=img, roi=spot)
-        within = _is_mask_within(full_mask=mask, sub_mask=spot_mask)
-        if within:
-            sample_num += 1
-            # Label spots with unique integers
-            labeled_mask[np.where(spot_mask > 0)] = sample_num
-        if tries > num_masks * 100:  # Prevent infinite loop
-            # warn if stuck, break loop
-            warn("Too many iterations. Placed " + str(sample_num) +
-                 " circular masks instead of " + str(num_masks))
-            break
-    return labeled_mask
--- tests/plantcv/test_sub_mask.py
+++ tests/plantcv/test_sub_mask.py
@@ -16,3 +16,11 @@ def test_sub_mask_too_large(test_data):
     mask = cv2.imread(test_data.small_bin_img, -1)
     spots = sub_mask(img, mask, 2, 20)
     assert len(np.unique(spots)) == 1
+
+
+def test_sub_mask_too_many(test_data):
+    """Test for PlantCV."""
+    img = cv2.imread(test_data.small_rgb_img, -1)
+    mask = cv2.imread(test_data.small_bin_img, -1)
+    spots = sub_mask(img, mask, 5, 3)
+    assert len(np.unique(spots)) == 2
