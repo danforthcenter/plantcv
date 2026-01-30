7a4bc2058dab0793bcccb0f70980885dc0bf6222
KeelyBrown <156026125+k034b363@users.noreply.github.com>
2026-01-30 14:20:50 -0600

    trailing whitespace

--- plantcv/plantcv/transform/detect_color_card.py
+++ plantcv/plantcv/transform/detect_color_card.py
@@ -708,7 +708,7 @@ def detect_color_card(rgb_img, color_chip_size=None, roi=None, **kwargs):
 
     Returns
     -------
-    color matrix 
+    color matrix
         Matrix containing the average red value, average green value, and average blue value for each color chip
     """
     if type(color_chip_size) is str and color_chip_size.upper() == 'ASTRO':
