2ed1d5f2a5391481f3c82dee859502614ce11ab7
Katie Murphy <58490237+kmurphy61@users.noreply.github.com>
2026-02-26 12:54:49 -0600

    Update test with proper path

--- tests/plantcv/photosynthesis/test_read_cropreporter.py
+++ tests/plantcv/photosynthesis/test_read_cropreporter.py
@@ -124,9 +124,9 @@ def test_read_cropreporter_pmt_only(photosynthesis_test_data, tmpdir):
     # Create a test tmp directory
     cache_dir = tmpdir.mkdir("sub")
     # Create dataset with only PMT
-    shutil.copyfile(photosynthesis_test_data.cropreporter_aph, os.path.join(cache_dir,
+    shutil.copyfile(photosynthesis_test_data.cropreporter_pmt, os.path.join(cache_dir,
                                                                             "HDR_E0001P0007N0001_GCU24100090_20260226.INF"))
-    pmt_dat = photosynthesis_test_data.cropreporter_aph.replace("HDR", "PMT")
+    pmt_dat = photosynthesis_test_data.cropreporter_pmt.replace("HDR", "PMT")
     pmt_dat = pmt_dat.replace("INF", "DAT")
     shutil.copyfile(pmt_dat, os.path.join(cache_dir, "PMT_E0001P0007N0001_GCU24100090_20260226.DAT"))
     fluor_filename = os.path.join(cache_dir, "HDR_E0001P0007N0001_GCU24100090_20260226.INF")
