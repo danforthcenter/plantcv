12ab67387d4effa25b83051f52e5b1bc071078f8
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2026-05-12 14:34:12 -0500

    move quick_color_check to qc submodule

--- plantcv/plantcv/qc/__init__.py
+++ plantcv/plantcv/qc/__init__.py
@@ -1,3 +1,5 @@
 from plantcv.plantcv.qc.exposure import exposure
+from plantcv.plantcv.qc.quick_color_check import quick_color_check
 
-__all__ = ["exposure"]
+
+__all__ = ["exposure", "quick_color_check"]
