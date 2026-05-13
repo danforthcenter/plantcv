eead4bcf60b18dbe66ce457298ea4c176ce5da0c
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2026-05-13 14:17:01 -0500

    fstring formatting

--- plantcv/plantcv/qc/plot_delta_e.py
+++ plantcv/plantcv/qc/plot_delta_e.py
@@ -99,7 +99,7 @@ def plot_deltaE(deltaE_matrix):
     )
     swatch_chip_nos = (std_mat[:, 0] / 10).astype(int)
     swatch_hex = [
-        f"#{:02X}{:02X}{:02X}".format(int(r * 255), int(g * 255), int(b * 255))
+        f"{int(r * 255):02X}{int(g * 255):02X}{int(b * 255):02X}"
         for r, g, b in std_mat[:, 1:4]
     ]
     swatch_df = pd.DataFrame({
