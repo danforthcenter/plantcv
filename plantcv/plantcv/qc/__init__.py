ebfe12c7a1f715b4980340d9d210904575a7a935
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2026-05-13 13:24:40 -0500

    add plot_deltaE function

--- plantcv/plantcv/qc/__init__.py
+++ plantcv/plantcv/qc/__init__.py
@@ -2,6 +2,8 @@ from plantcv.plantcv.qc.exposure import exposure
 from plantcv.plantcv.qc.quick_color_check import quick_color_check
 from plantcv.plantcv.qc.color_correction_scatter import color_correction_plot
 from plantcv.plantcv.qc.color_chip_comparison import color_chip_comparison
+from plantcv.plantcv.qc.plot_delta_e import plot_deltaE
 
 
-__all__ = ["exposure", "quick_color_check", "color_correction_plot", "color_chip_comparison"]
+__all__ = ["exposure", "quick_color_check", "color_correction_plot", "color_chip_comparison",
+           "plot_delta_e"]
