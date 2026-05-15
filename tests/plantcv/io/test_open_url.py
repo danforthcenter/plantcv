0544f4b43703a725014bc532e23b45515c7cebff
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2026-05-15 11:11:16 -0500

    monkeypatch open url to avoid various problems

--- tests/plantcv/io/test_open_url.py
+++ tests/plantcv/io/test_open_url.py
@@ -1,17 +1,22 @@
 """Tests for the open_url function."""
+import os
+import numpy as np
+import imageio.v3 as iio
 import pytest
 from plantcv.plantcv.io import open_url
 
 
-def test_open_url():
+def test_open_url(monkeypatch):
     """PlantCV Test"""
     url = ("https://datasci.danforthcenter.org/test.jpg")
+    monkeypatch.setattr(iio, "imread", lambda *args, **kwargs: np.zeros((2464, 3280, 3)).astype(np.float32))
     rgb_img = open_url(url=url)
     assert rgb_img.shape == (2464, 3280, 3)
 
 
-def test_open_url_unsupported():
+def test_open_url_unsupported(monkeypatch):
     """PlantCV Test"""
     url = "https://datasci.danforthcenter.org/test.gif"
+    monkeypatch.setattr(iio, "imread", lambda *args, **kwargs: np.zeros((2464)).astype(np.float32))
     with pytest.raises(RuntimeError):
         _ = open_url(url=url)
