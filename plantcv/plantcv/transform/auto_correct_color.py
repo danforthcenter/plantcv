7e2342fd7af2ae8e7e167cfe54779918aa8de96d
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2026-05-18 16:17:18 -0500

    whitespace

--- plantcv/plantcv/transform/auto_correct_color.py
+++ plantcv/plantcv/transform/auto_correct_color.py
@@ -8,6 +8,7 @@ from plantcv.plantcv.transform.color_correction import (
 from plantcv.plantcv.transform.get_color_matrix import get_matrix_m
 from plantcv.plantcv.transform.standard_matrices import std_color_matrix, astro_color_matrix
 
+
 def auto_correct_color(rgb_img, color_chip_size=None, roi=None, **kwargs):
     """Automatically detect a color card.
     Parameters
