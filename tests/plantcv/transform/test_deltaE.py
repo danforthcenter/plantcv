b84580c6888edbb779f134faf885ee61bab66aab
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2026-05-19 12:17:34 -0500

    output all delta E values as a list

--- tests/plantcv/transform/test_deltaE.py
+++ tests/plantcv/transform/test_deltaE.py
@@ -11,9 +11,9 @@ def test_deltaE_macbeth(transform_test_data):
     """Test for PlantCV."""
     outputs.clear()
     rgb_img = cv2.imread(transform_test_data.colorcard_img)
-    de_matrix = deltaE(rgb_img=rgb_img, color_chip_size="classic")
+    de_matrix = deltaE(rgb_img=rgb_img, obs="testname", color_chip_size="classic")
     assert np.shape(de_matrix) == (6, 4)
-    assert outputs.metadata["max_deltaE_calibrated"]["value"] == np.float64(40.10999900620317)
+    assert np.max(outputs.metadata["deltaE_testname"]["value"]) == np.float64(40.10999900620317)
 
 
 def test_deltaE_astro(transform_test_data):
@@ -22,7 +22,7 @@ def test_deltaE_astro(transform_test_data):
     rgb_img = cv2.imread(transform_test_data.astrocard_img)
     de_matrix = deltaE(rgb_img=rgb_img, color_chip_size="astro")
     assert np.shape(de_matrix) == (3, 5)
-    assert outputs.metadata["max_deltaE_calibrated"]["value"] == np.float64(78.00042935949308)
+    assert np.max(outputs.metadata["deltaE_calibrated"]["value"]) == np.float64(78.00042935949308)
 
 
 @pytest.mark.parametrize("debug", ["print", "plot", None])
