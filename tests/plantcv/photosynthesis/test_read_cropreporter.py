1b6ea08a3acf3e4a16a8366ec8e8bb6c1c46de25
Noah Fahlgren <noahfahlgren@gmail.com>
2026-05-27 13:19:00 -0500

    Update tests with PSD and PSL classes

--- tests/plantcv/photosynthesis/test_read_cropreporter.py
+++ tests/plantcv/photosynthesis/test_read_cropreporter.py
@@ -9,14 +9,14 @@ from plantcv.plantcv.photosynthesis import read_cropreporter
 def test_read_cropreporter(photosynthesis_test_data, tmpdir):
     """Test for PlantCV."""
     ps = read_cropreporter(filename=photosynthesis_test_data.cropreporter)
-    assert isinstance(ps, PSII_data) and ps.ojip_dark.shape == (966, 1296, 21, 1)
+    assert isinstance(ps, PSII_data) and ps.psd.load().shape == (966, 1296, 21, 1)
 
     # Check with different naming convention of Phenovation
     ps = read_cropreporter(filename=photosynthesis_test_data.cropreporter_v653)
-    assert isinstance(ps, PSII_data) and ps.ojip_dark.shape == (1500, 2048, 7, 1)
+    assert isinstance(ps, PSII_data) and ps.psd.load().shape == (1500, 2048, 7, 1)
     # check labels
     true_labels = ['Fdark', 'F0', 'PSD2', 'PSD3', 'Fm', 'PSD5', 'PSD6']
-    assert all(a == b for a, b in zip(ps.ojip_dark.coords['frame_label'].to_dict()['data'], true_labels))
+    assert all(a == b for a, b in zip(ps.psd.load().coords['frame_label'].to_dict()['data'], true_labels))
 
     # Create dataset with only 3 frames
     cache_dir = os.path.join(tmpdir, "sub")
@@ -40,9 +40,9 @@ def test_read_cropreporter(photosynthesis_test_data, tmpdir):
     ps = read_cropreporter(filename=inffilename)
     # check labels
     true_dark_labels = ['Fdark', 'F0', 'Fm', 'PSD3', 'PSD4', 'PSD5', 'PSD6']
-    assert all(a == b for a, b in zip(ps.ojip_dark.coords['frame_label'].to_dict()['data'], true_dark_labels))
+    assert all(a == b for a, b in zip(ps.psd.load().coords['frame_label'].to_dict()['data'], true_dark_labels))
     true_light_labels = ['Flight', 'Fp', 'Fmp', 'PSL3', 'PSL4', 'PSL5', 'PSL6']
-    assert all(a == b for a, b in zip(ps.ojip_light.coords['frame_label'].to_dict()['data'], true_light_labels))
+    assert all(a == b for a, b in zip(ps.psl.load().coords['frame_label'].to_dict()['data'], true_light_labels))
     true_pam_dark_labels = ["Fdark", "F0", "Fm", "Fdarksat"]
     assert all(a == b for a, b in zip(ps.pam_dark.coords['frame_label'].to_dict()['data'], true_pam_dark_labels))
     true_pam_light_labels = ["Flight", "Fp", "Fmp", "Flightsat"]
