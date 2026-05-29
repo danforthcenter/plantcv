da2b8583675fcea428fd186c706e04e3f8fb47b4
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2026-05-29 15:00:05 -0500

    attempt

--- tests/plantcv/analyze/test_spectral_reflectance.py
+++ tests/plantcv/analyze/test_spectral_reflectance.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 from plantcv.plantcv.analyze import spectral_reflectance
 from plantcv.plantcv import outputs
 
@@ -10,3 +11,15 @@ def test_spectral_reflectance(test_data):
     mask = cv2.imread(test_data.hsi_mask_file, -1)
     _ = spectral_reflectance(hsi=test_data.load_hsi(test_data.hsi_file), labeled_mask=mask)
     assert len(outputs.observations['default_1']['wavelength_means']['value']) == 978
+
+
+
+def test_spectral_reflectance_key_conversion(test_data):
+    """Test for PlantCV."""
+    # Clear previous outputs
+    outputs.clear()
+    mask = cv2.imread(test_data.hsi_mask_file, -1)
+    hsi = test_data.load_hsi(test_data.hsi_file)
+    hsi.wavelength_dict = {np.array(k, dtype=np.int64).tolist(): v for k, v in hsi.wavelength_dict.items()}
+    _ = spectral_reflectance(hsi=hsi, labeled_mask=mask)
+    assert len(outputs.observations['default_1']['wavelength_means']['value']) == 622
