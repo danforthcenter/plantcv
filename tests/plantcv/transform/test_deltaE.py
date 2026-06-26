3cd6f007c452eb6f74ddaa7b947eb01743a4d991
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2026-06-26 09:52:37 -0500

    reset function_args params if it gets modified in tests

--- tests/plantcv/transform/test_deltaE.py
+++ tests/plantcv/transform/test_deltaE.py
@@ -13,6 +13,7 @@ def test_deltaE_macbeth(transform_test_data):
     params.function_args = {}
     rgb_img = cv2.imread(transform_test_data.colorcard_img)
     de_matrix = deltaE(rgb_img=rgb_img, obs="testname", color_chip_size="classic")
+    params.function_args = {}
     assert np.shape(de_matrix) == (6, 4)
     assert np.max(outputs.metadata["deltaE_testname"]["value"]) == [pytest.approx(np.float64(15.279), 0.001)]
 
@@ -22,6 +23,7 @@ def test_deltaE_astro(transform_test_data):
     outputs.clear()
     rgb_img = cv2.imread(transform_test_data.astrocard_img)
     de_matrix = deltaE(rgb_img=rgb_img, color_chip_size="astro")
+    params.function_args = {}
     assert np.shape(de_matrix) == (3, 5)
     assert np.max(outputs.metadata["deltaE_calibrated"]["value"]) == [pytest.approx(np.float64(35.899), 0.001)]
 
@@ -32,6 +34,7 @@ def test_deltaE_bad_param(transform_test_data, monkeypatch):
     rgb_img = cv2.imread(transform_test_data.astrocard_img)
     with pytest.raises(RuntimeError):
         _ =  deltaE(rgb_img=rgb_img, color_chip_size="astro")
+    params.function_args = {}
 
 
 @pytest.mark.parametrize("debug", ["print", "plot", None])
@@ -45,4 +48,5 @@ def test_deltaE_plotting(debug, transform_test_data, tmpdir):
     de_matrix = deltaE(rgb_img=rgb_img, color_chip_size="classic")
     params.debug_outdir = debug_outdir
     params.debug = debug
+    params.function_args = {}
     assert np.shape(de_matrix) == (6, 4)
--- tests/plantcv/transform/test_detect_color_card.py
+++ tests/plantcv/transform/test_detect_color_card.py
@@ -2,6 +2,7 @@
 import cv2
 import pytest
 import numpy as np
+from plantcv.plantcv._globals import params
 from plantcv.plantcv.transform.detect_color_card import detect_color_card
 
 
@@ -10,6 +11,7 @@ def test_detect_color_card(transform_test_data):
     # Load rgb image
     rgb_img = cv2.imread(transform_test_data.colorcard_img)
     color_matrix = detect_color_card(rgb_img=rgb_img, color_chip_size="classic")
+    params.function_args = {}
     assert np.shape(color_matrix) == (24, 4)
 
 
@@ -18,6 +20,7 @@ def test_detect_color_card_set_size(transform_test_data):
     # Load rgb image
     rgb_img = cv2.imread(transform_test_data.colorcard_img)
     color_matrix = detect_color_card(rgb_img=rgb_img, color_chip_size=(40, 40))
+    params.function_args = {}
     assert np.shape(color_matrix) == (24, 4)
 
 
@@ -27,6 +30,7 @@ def test_draw_nonuniform_color_chips(transform_test_data):
     rgb_img = cv2.imread(transform_test_data.colorcard_img)
     radii = [3*(i+1) for i in range(24)]
     color_matrix = detect_color_card(rgb_img=rgb_img, color_chip_size=(40, 40), radius=radii)
+    params.function_args = {}
     assert np.shape(color_matrix) == (24, 4)
 
 
@@ -37,6 +41,7 @@ def test_draw_nonuniform_color_chips_len_mismatch(transform_test_data):
     radii = [3*(i+1) for i in range(5)]
     with pytest.raises(RuntimeError):
         _ = detect_color_card(rgb_img=rgb_img, color_chip_size=(40, 40), radius=radii)
+    params.function_args = {}
 
 
 def test_detect_color_card_none_found(transform_test_data):
@@ -45,6 +50,7 @@ def test_detect_color_card_none_found(transform_test_data):
     rgb_img = cv2.imread(transform_test_data.target_img)
     with pytest.raises(RuntimeError):
         _ = detect_color_card(rgb_img=rgb_img)
+    params.function_args = {}
 
 
 def test_detect_color_card_incorrect_block_size(transform_test_data):
@@ -53,6 +59,7 @@ def test_detect_color_card_incorrect_block_size(transform_test_data):
     rgb_img = cv2.imread(transform_test_data.colorcard_img)
     with pytest.raises(RuntimeError):
         _ = detect_color_card(rgb_img=rgb_img, block_size=2)
+    params.function_args = {}
 
 
 def test_detect_color_card_partial_card(transform_test_data):
@@ -61,6 +68,7 @@ def test_detect_color_card_partial_card(transform_test_data):
     rgb_img = cv2.imread(transform_test_data.partial_card_rgb_img)
     with pytest.raises(RuntimeError):
         _ = detect_color_card(rgb_img=rgb_img, aspect_ratio=2, solidity=0.5)
+    params.function_args = {}
 
 
 def test_detect_color_card_incorrect_card_size(transform_test_data):
@@ -69,6 +77,7 @@ def test_detect_color_card_incorrect_card_size(transform_test_data):
     rgb_img = cv2.imread(transform_test_data.colorcard_img)
     with pytest.raises(RuntimeError):
         _ = detect_color_card(rgb_img=rgb_img, color_chip_size=100)
+    params.function_args = {}
 
 
 def test_detect_color_card_incorrect_card_type(transform_test_data):
@@ -77,6 +86,7 @@ def test_detect_color_card_incorrect_card_type(transform_test_data):
     rgb_img = cv2.imread(transform_test_data.colorcard_img)
     with pytest.raises(RuntimeError):
         _ = detect_color_card(rgb_img=rgb_img, color_chip_size="pantone")
+    params.function_args = {}
 
 
 def test_detect_color_card_masked_aruco_tags(transform_test_data, capsys):
@@ -84,6 +94,7 @@ def test_detect_color_card_masked_aruco_tags(transform_test_data, capsys):
     # Load rgb image
     rgb_img = cv2.imread(transform_test_data.astrocard_img)
     _ = detect_color_card(rgb_img=rgb_img)
+    params.function_args = {}
     captured = capsys.readouterr()
     assert captured.err == "Warning: Image contains an ArUco tag, should you be using color_chip_size='astro'?\n"
 
@@ -92,6 +103,7 @@ def test_detect_astro_card(transform_test_data):
     """Test for PlantCV."""
     rgb_img = cv2.imread(transform_test_data.astrocard_img)
     color_matrix = detect_color_card(rgb_img=rgb_img, color_chip_size="astro")
+    params.function_args = {}
     assert np.shape(color_matrix) == (15, 4)
 
 
@@ -99,6 +111,7 @@ def test_detect_astro_card_missing_aruco_tags(transform_test_data):
     """Test for PlantCV."""
     rgb_img = cv2.imread(transform_test_data.astrocard_missing_tags)
     color_matrix = detect_color_card(rgb_img=rgb_img, color_chip_size="astro")
+    params.function_args = {}
     assert np.shape(color_matrix) == (15, 4)
 
 
@@ -107,6 +120,7 @@ def test_detect_astro_card_duplicate_aruco_tags(transform_test_data):
     rgb_img = cv2.imread(transform_test_data.duplicate_aruco_tags)
     with pytest.raises(RuntimeError):
         _ = detect_color_card(rgb_img=rgb_img, color_chip_size="astro")
+    params.function_args = {}
 
 
 def test_detect_astro_card_no_aruco_tags(transform_test_data):
@@ -114,6 +128,7 @@ def test_detect_astro_card_no_aruco_tags(transform_test_data):
     rgb_img = cv2.imread(transform_test_data.colorcard_img)
     with pytest.raises(RuntimeError):
         _ = detect_color_card(rgb_img=rgb_img, color_chip_size="astro")
+    params.function_args = {}
 
 
 def test_detect_astro_card_wrong_aruco_tags(transform_test_data):
@@ -121,3 +136,4 @@ def test_detect_astro_card_wrong_aruco_tags(transform_test_data):
     rgb_img = cv2.imread(transform_test_data.wrong_aruco_tags)
     with pytest.raises(RuntimeError):
         _ = detect_color_card(rgb_img=rgb_img, color_chip_size="astro")
+    params.function_args = {}
