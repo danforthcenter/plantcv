1472823e28b0f3747a4e58edcd352421f82d854e
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2026-06-29 15:27:16 -0500

    labeled mask debug

--- plantcv/plantcv/roi/roi2mask.py
+++ plantcv/plantcv/roi/roi2mask.py
@@ -3,9 +3,9 @@
 import os
 import cv2
 import numpy as np
+from skimage.color import label2rgb
 from plantcv.plantcv._globals import params
 from plantcv.plantcv._debug import _debug
-from plantcv.plantcv.create_labels import create_labels
 
 
 def roi2mask(img, roi):
@@ -32,7 +32,14 @@ def roi2mask(img, roi):
         # Pixel intensity of (i+1) such that the first object has value 1
         cv2.drawContours(labeled_mask, obj.contours[0], -1, (i+1), -1)
 
+    colorful = label2rgb(labeled_mask)
+    colorful2 = (255*colorful).astype(np.uint8)
+
     if num_labels == 1:
-        labeled_mask = (labeled_mask * 255).astype(np.uint8)
+        labeled_mask = (labeled_mask*255).astype(np.uint8)
+
+    _debug(visual=colorful2,
+           filename=os.path.join(params.debug_outdir,
+                                 f"{params.device}_roi2mask.png"))
 
     return labeled_mask
