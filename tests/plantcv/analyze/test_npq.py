288f9768ada32d8507a4d1a12d45bfb49b168c04
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2026-08-04 16:07:57 -0500

    remove waltz for now

--- tests/plantcv/analyze/test_npq.py
+++ tests/plantcv/analyze/test_npq.py
@@ -16,16 +16,6 @@ def test_npq_cropreporter(test_data):
     assert np.isclose(outputs.observations["prefix_1"]["npq_median_Fq/Fm"]["value"], 0.25)
 
 
-def test_npq_waltz(test_data):
-    """Test for PlantCV."""
-    # Clear results
-    outputs.clear()
-    psdata = test_data.psii_walz_new('ojip_both')
-    _ = analyze_npq(ps=psdata, labeled_mask=test_data.create_ps_mask(), auto_fm=True,
-                    measurement_labels=None, label="prefix", min_bin="auto", max_bin="auto")
-    assert np.isclose(outputs.observations["prefix_1"]["npq_median_t40"]["value"], float((200 / 185) - 1))
-
-
 @pytest.mark.parametrize("mlabels, tmask",
                          # test wrong mask shape
                          [[None, np.ones((2, 2))],
