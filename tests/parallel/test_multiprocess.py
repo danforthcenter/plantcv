d29d95c381f349f72356f8992272acfff60f3ef9
Noah Fahlgren <noahfahlgren@gmail.com>
2026-09-08 17:17:41 -0500

    Close dask clients whether tests pass or fail

--- tests/parallel/test_multiprocess.py
+++ tests/parallel/test_multiprocess.py
@@ -14,8 +14,11 @@ def test_create_dask_cluster_local(tmpdir):
     # Set the temp directory for dask
     dask.config.set(temporary_directory=tmp_dir)
     client = create_dask_cluster(cluster="LocalCluster", cluster_config={})
-    status = client.status
-    assert status == "running"
+    try:
+        status = client.status
+        assert status == "running"
+    finally:
+        client.close()
 
 
 def test_create_dask_cluster():
@@ -48,8 +51,11 @@ def test_multiprocess(parallel_test_data, tmpdir):
              '--writeimg', '--other', 'on', image_path]]
     # Create a dask LocalCluster client
     client = Client(n_workers=1)
-    multiprocess(jobs, client=client)
-    assert os.path.exists(result_file)
+    try:
+        multiprocess(jobs, client=client)
+        assert os.path.exists(result_file)
+    finally:
+        client.close()
 
 
 def test_process_images_multiproc():
